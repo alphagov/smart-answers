814710ffb643a291794cd42f1df988b4cee5a941
Frances Berriman <phaeness@phae.lan>
2011-11-01 19:16:03 +0000

    Removing unneeded var

--- app/assets/javascripts/smart_answers.js
+++ app/assets/javascripts/smart_answers.js
@@ -1,17 +1,11 @@
 $(document).ready(function() {
-	var hasPushState = false,
-		formSelector = ".steps .current form";
+	var formSelector = ".steps .current form";
 	
-	if(history && history.pushState){
-		hasPushState = true;
-	};
-
 	// if hashed, means it's a non-pushstated URL that we need to generate the content for
 	if(window.location.hash){		
 		var hash = window.location.hash,
 			hash = hash.split('#')[1];
-			reloadQuestions(hash);
-			
+			reloadQuestions(hash);	
 	};
 
 	// events
@@ -30,6 +24,15 @@ $(document).ready(function() {
 		return false;
 	});
 
+	window.onpopstate = function (event) {
+		var url = window.location;
+		if(event.state != null){
+			url = event.state.url;
+		}
+	  $.get(url, function(data) {
+      updateContent(data['html_fragment']);
+    }, 'json');
+	}
 
 	// helper functions
 	// replace all the questions currently in the page with whatever is returned for given url
@@ -50,7 +53,8 @@ $(document).ready(function() {
 	
 	// manage the URL
 	function updateURL(data, url){
-		if(hasPushState){
+		if(history && history.pushState){
+			// need the title from the json
 			history.pushState(data, "???", url);
 		}
 		else{
@@ -66,16 +70,7 @@ $(document).ready(function() {
 		//$(formSelector+' input[type=submit]').attr('disabled', 'disabled');
 	};
 	
-	window.onpopstate = function (event) {
-		var url = window.location;
-		if(event.state != null){
-			url = event.state.url;
-		}
-	  $.get(url, function(data) {
-      updateContent(data['html_fragment']);
-      
-    }, 'json');
-	}
+
 	
 
 });
