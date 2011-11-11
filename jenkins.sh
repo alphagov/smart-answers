9de8e04d56a6620848c6a97cbbfebb61e02fceef
David Heath <david@davidheath.org>
2011-11-11 13:10:53 +0000

    Javascript directory is called "javascripts" not "javascript"

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@ source '/usr/local/lib/rvm'
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment --without=development
 
 # DELETE STATIC SYMLINKS AND RECONNECT...
-for dir in images javascript templates stylesheets; do
+for dir in images javascripts templates stylesheets; do
   rm /var/lib/jenkins/jobs/Smart_Answers/workspace/public/$dir
   ln -s /var/lib/jenkins/jobs/Static/workspace/public/$dir /var/lib/jenkins/jobs/Smart_Answers/workspace/public/$dir
 done
