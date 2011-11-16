9b5204e49568454cdc25335195bc3cee92361d6d
Frances Berriman <phaeness@Phae.local>
2011-11-16 15:51:14 +0000

    Removed hash option for those without history.pushstate

--- app/assets/javascripts/smart_answers.js
+++ app/assets/javascripts/smart_answers.js
@@ -3,41 +3,48 @@ function browserSupportsHtml5HistoryApi() {
 };
 
 $(document).ready(function() {
-  var formSelector = ".current form";
-  initializeHistory();
-  toggleSubmit();
+	if(browserSupportsHtml5HistoryApi()){
+  		var formSelector = ".current form";
+  		initializeHistory();
+  		toggleSubmit();
+			
+			// events
+		  // get new questions on submit
+		  $(formSelector).live('submit', function(event) {
+		    $('input[type=submit]', this).attr('disabled', 'disabled');
+		    var form = $(this);
+		    reloadQuestions(form.attr('action'), form.serializeArray());
+		    event.preventDefault();
+		    return false;
+		  });
+		
+			// we want to start over with whatever gets provided if someone clicks to change the answer
+		  $(".undo a").live('click', function() {
+		    reloadQuestions($(this).attr("href"), "");
+		    return false;
+		  });
+		
+			// manage next/back by tracking popstate event
+		  window.onpopstate = function (event) {
+		    if(event.state != null) {
+		      updateContent(event.state['html_fragment']);
+		    }
+		    else if (urlFromHashtag()) {
+		      $.get(toJsonUrl(urlFromHashtag()), function(data) {
+		        updateContent(data['html_fragment']);
+		      });
+		    } else {
+		      return false;
+		    }
+		  }
+	}
   
-  // events
-  // get new questions on submit
-  $(formSelector).live('submit', function(event) {
-    $('input[type=submit]', this).attr('disabled', 'disabled');
-    var form = $(this);
-    reloadQuestions(form.attr('action'), form.serializeArray());
-    event.preventDefault();
-    return false;
-  });
   
   $('#current-error').focus();
 
-  // we want to start over with whatever gets provided if someone clicks to change the answer
-  $(".undo a").live('click', function() {
-    reloadQuestions($(this).attr("href"), "");
-    return false;
-  });
+  
 
-  // manage next/back by tracking popstate event
-  window.onpopstate = function (event) {
-    if(event.state != null) {
-      updateContent(event.state['html_fragment']);
-    }
-    else if (urlFromHashtag()) {
-      $.get(toJsonUrl(urlFromHashtag()), function(data) {
-        updateContent(data['html_fragment']);
-      });
-    } else {
-      return false;
-    }
-  }
+  
 
   // helper functions
   function urlFromHashtag() {
@@ -75,34 +82,14 @@ $(document).ready(function() {
   // update the content (i.e. plonk in the html fragment)
   function updateContent(fragment){
     $('.smart_answer section').html(fragment);
-
-		//toggleSubmit();
   };
   
-	function toggleSubmit(){
-		$('input[type=submit]', this).attr('disabled', 'disabled');
-	/*	if($($(formSelector)+ " select:selected", $(formSelector)+ " radio").is(':checked')){
-			$('input[type=submit]', this).removeAttr('disabled');
-		}
-		else{
-			$($(formSelector)+ " select", $(formSelector)+ " radio").live('change', 
-			function(){
-				$('input[type=submit]', this).removeAttr('disabled');
-			});
-		};*/
-	};
 	
 
-
   function initializeHistory(data) {
-    // if hashed, means it's a non-pushstated URL that we need to generate the content for
-    if (urlFromHashtag()) {
-      reloadQuestions(urlFromHashtag(), "");
-    } else {
-      if (! browserSupportsHtml5HistoryApi() && window.location.pathname.match(/\/.*\//) ) {
-        addToHistory({url: window.location.pathname});
-      }
-    }
+		if (! browserSupportsHtml5HistoryApi() && window.location.pathname.match(/\/.*\//) ) {
+			addToHistory({url: window.location.pathname});
+		}
     
     if (browserSupportsHtml5HistoryApi()) {
       data = {
