4a1ba17237fbc78582ce2654d5cac862e6f78186
Paul Battley <pbattley@gmail.com>
2011-11-24 16:03:39 +0000

    Add a text presenter for search indexing.
    Ignores text that needs interpolation.

---
+++ app/presenters/text_presenter.rb
@@ -0,0 +1,39 @@
+class TextPresenter
+  def initialize(flow)
+    @flow = flow
+    @i18n_prefix = "flow.#{@flow.name}"
+  end
+
+  NODE_PRESENTER_METHODS = [:title, :subtitle, :body, :hint]
+
+  def text
+    @flow.nodes.inject([translate("body")]) { |acc, node|
+      pres = NodePresenter.new(@i18n_prefix, node)
+      acc.concat(NODE_PRESENTER_METHODS.map { |method|
+        lookup_ignoring_interpolation_errors(pres, method)
+      })
+    }.compact.join(" ")
+  end
+
+  def title
+    translate("title")
+  end
+
+  def description
+    translate("meta.description")
+  end
+
+private
+  def lookup_ignoring_interpolation_errors(presenter, method)
+    presenter.__send__(method)
+  rescue I18n::MissingInterpolationArgument
+    nil
+  end
+
+  def translate(subkey)
+    key = [@i18n_prefix, subkey].join(".")
+    I18n.translate!(key)
+  rescue I18n::MissingTranslationData
+    nil
+  end
+end
