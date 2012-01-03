a2d6c60b42919879e02de26d79524b98aa8d92f0
James Stewart <jys@ketlai.co.uk>
2012-01-03 13:41:39 +0000

    Class doesn't support split

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,6 @@
 unless Rails.env.development? or Rails.env.test?
   Rails.application.config.middleware.use ExceptionNotifier,
-    :email_prefix => "[#{Rails.application.split('::').first}] ",
+    :email_prefix => "[#{Rails.application.to_s.split('::').first}] ",
     :sender_address => %{"Winston Smith-Churchill" <winston@alphagov.co.uk>},
     :exception_recipients => %w{govuk-dev@digital.cabinet-office.gov.uk}
 end
