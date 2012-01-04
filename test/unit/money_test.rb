f07e7b445fa8c1e7988350f24dff22c56ae67198
Jordan Hatch <jordan@jordanh.net>
2012-01-04 17:04:21 +0000

    Added new test for parsing money correctly

--- test/unit/money_test.rb
+++ test/unit/money_test.rb
@@ -9,13 +9,21 @@ module SmartAnswer
       assert Money.new("2") == two
       assert Money.new("3") > two
       assert Money.new("1") < two
-    end
+    end                           
 
     test "Can compare money with number" do
       m = Money.new("520")
       assert m == 520
       assert m > 1
       assert m < 1000
+    end                           
+    
+    test "Money with spaces or commas gets parsed correctly" do
+      one = Money.new("1,000")                        
+      assert one == 1000
+      
+      two = Money.new("2 000")
+      assert two == 2000
     end
   end
 end
