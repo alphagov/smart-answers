4f0bb85c6d06dca9453f5ec71d403c82b543c001
Jordan Hatch <jordan@jordanh.net>
2012-02-06 16:21:26 +0000

    Split Q3 into two questions and DRY code into class

--- lib/flows/married-couples-allowance.rb
+++ lib/flows/married-couples-allowance.rb
@@ -4,32 +4,43 @@ section_slug "family"
 subsection_slug "marriage-and-civil-partnership"
 satisfies_need 2012
 
+class MarriedCouplesAllowanceAnswer
+  def self.calculate_allowance(amount)
+    if amount > 24000
+      income = (amount - 24000)/2 - 2651
+      income = (7295-income) * 0.1
+      (income < 280) ? Money.new(280) : Money.new(income)
+    else
+      Money.new(729.50)
+    end
+  end
+end
+
 multiple_choice :were_you_or_your_partner_born_on_or_before_6_april_1935? do
   option :yes => :did_you_marry_or_civil_partner_before_5_december_2005?
   option :no => :sorry
 end
 
 multiple_choice :did_you_marry_or_civil_partner_before_5_december_2005? do
-  option :yes
-  option :no
+  option :yes => :whats_the_husbands_income?
+  option :no => :whats_the_highest_earners_income?
 
   calculate :result_strings do
-    (responses.last == :yes) ? PhraseList.new(:before_2005) : PhraseList.new(:after_2005)
+    (responses.last == "yes") ? PhraseList.new(:before_2005) : PhraseList.new(:after_2005)
   end
-
-  next_node :whats_your_income?
 end
 
-money_question :whats_your_income? do
-
+money_question :whats_the_husbands_income? do
   calculate :allowance do
-    if responses.last > 24000
-      income = (responses.last - 24000)/2 - 2651
-      income = (7295-income) * 0.1
-      (income < 280) ? Money.new(280) : Money.new(income)
-    else
-      Money.new(729.50)
-    end
+    MarriedCouplesAllowanceAnswer::calculate_allowance(responses.last)
+  end
+
+  next_node :done
+end
+
+money_question :whats_the_highest_earners_income? do
+  calculate :allowance do
+    MarriedCouplesAllowanceAnswer::calculate_allowance(responses.last)
   end
 
   next_node :done
