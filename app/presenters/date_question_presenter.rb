369ce21d875ddeaca39bc2f24c464ddd51f76fd3
Joshua Marshall <joshua@workwithgreat.com>
2012-03-07 17:35:58 +0000

    Modified smart answers to handle date of birth stuff better. Thanks Jordan.

--- app/presenters/date_question_presenter.rb
+++ app/presenters/date_question_presenter.rb
@@ -2,4 +2,12 @@ class DateQuestionPresenter < QuestionPresenter
   def response_label(value)
     I18n.localize(Date.parse(value), format: :long)
   end
+
+  def start_date
+    @node.range == false ? 1.year.ago : @node.range.begin
+  end
+
+  def end_date
+    @node.range == false ? 3.years.from_now : @node.range.end
+  end
 end
