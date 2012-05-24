4738f88307c90ec4f19632c139cb5bdaa0f7cad6
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-05-24 13:47:34 +0100

    More tests and fixing compound licence codes

--- test/integration/what_can_i_drive_by_age.rb
+++ test/integration/what_can_i_drive_by_age.rb
@@ -4,6 +4,8 @@ require_relative 'maternity_answer_logic'
 require_relative 'smart_answer_test_helper'
 
 class WhatCanIDriveByAgeTest < ActionDispatch::IntegrationTest
+  include SmartAnswerTestHelper
+
   def setup
     visit "/what-can-i-drive-by-age"
     click_on "Get started"
@@ -38,11 +40,25 @@ class WhatCanIDriveByAgeTest < ActionDispatch::IntegrationTest
     assert_contains_licence_codes %w(K P F)
   end
 
+  test "17 years old" do
+    choose_age "17"
+
+    expect_question "Are you in the armed forces?"
+  end
+
+  test "17 years old not in military" do
+    choose_age "17"
+
+    choose_and_next "I am not a member of the Armed Forces"
+
+    assert_contains_licence_codes ["B", "K", "F", "P", "B1", "A, A1"]
+  end
+
 private
 
   def choose_and_next(choice)
     choose choice
-    click_button "Next step"
+    click_next_step
   end
 
   def choose_age(age)
