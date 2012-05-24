135566d74982cc2f168267a9456c44f93dc6834d
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2012-05-24 13:32:46 +0100

    Basic licence code finder with helper methods

--- test/integration/what_can_i_drive_by_age.rb
+++ test/integration/what_can_i_drive_by_age.rb
@@ -10,10 +10,37 @@ class WhatCanIDriveByAgeTest < ActionDispatch::IntegrationTest
   end
 
   test "< 16 year olds can't drive" do
-    choose "Under 16"
-    click_button "Next step"
+    choose_age "Under 16"
 
     # FIXME: how can I get around this HTML encoding?
     assert_match /You can&\#8217;t drive anything/, page.body
+  end
+
+  test "16 year olds asked about DLA" do
+    choose_age "16"
+
+    assert_match /I get DLA/, page.body
+  end
+
+  test "16 year olds with DLA" do
+    choose_age "16"
+
+    choose "I get DLA"
+    click_button "Next step"
+
+    contains_licence_codes %w(B K F P B1)
+  end
+
+private
+
+  def choose_age(age)
+    choose age
+    click_button "Next step"
+  end
+
+  def contains_licence_codes(codes)
+    for code in codes do
+      assert_match "[#{code}]", page.body
+    end
   end
 end
