d0ff7407ea2d772c8f17345d324cebbdc769f305
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-05-30 03:14:59 -0700

    Add more Flow testing helper methods

--- test/integration/flows/flow_test_helper.rb
+++ test/integration/flows/flow_test_helper.rb
@@ -4,9 +4,25 @@ module FlowTestHelper
   def setup_for_testing_flow(flow_slug)
     @flow_registry ||= SmartAnswer::FlowRegistry.new(:show_drafts => true)
     @flow = @flow_registry.find(flow_slug)
+    @responses = []
   end
 
+  def add_response(resp)
+    @responses << resp.to_s
+  end
+
+  # Deprecated
   def node_for_responses(responses)
     @flow.process(responses).current_node
+  end
+
+  def current_state
+    state = @flow.process(@responses)
+    raise SmartAnswer::InvalidResponse.new(state.error) if state.error
+    state
+  end
+
+  def assert_current_node(node_name)
+    assert_equal node_name, current_state.current_node
   end
 end
