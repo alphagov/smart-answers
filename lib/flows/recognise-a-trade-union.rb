00c1c050c2f2ad1eaba5433c2197cff1b5a6ad70
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-06-11 06:35:03 -0700

    Add first draft of 'recognise a smart answer'

---
+++ lib/flows/recognise-a-trade-union.rb
@@ -0,0 +1,90 @@
+satisfies_need "B193"
+section_slug "work"
+subsection_slug ""
+status :draft
+
+multiple_choice :what_do_you_need_to_do? do
+  option :recognise_a_trade_union => :do_you_want_to_recognise_the_union_voluntarily?
+  option :derecoognise_a_trade_union => :has_it_been_3_years_since_gaining_recognition?
+end
+
+multiple_choice :do_you_want_to_recognise_the_union_voluntarily? do
+  option :yes => :you_agree_to_recognise_the_union
+  option :no => :how_many_employees_do_you_have?
+end
+
+multiple_choice :how_many_employees_do_you_have? do
+  option :"21_or_more" => :have_they_submitted_an_application?
+  option :fewer_than_21 => :the_union_cannot_apply_for_statutory_recognition
+end
+
+multiple_choice :have_they_submitted_an_application? do
+  option :yes => :no_action_required
+  option :no => :have_cac_accepted_the_application?
+end
+
+multiple_choice :have_cac_accepted_the_application? do
+  option :yes => :has_the_cac_ordered_a_ballot?
+  option :no => :you_do_not_have_to_recognise_the_union_can_reapply
+end
+
+multiple_choice :has_the_cac_ordered_a_ballot? do
+  option :declared_recognition => :you_agree_to_recognise_the_union
+  option :ordered_ballot => :did_the_majority_support_the_union_in_the_ballot?
+end
+
+multiple_choice :did_the_majority_support_the_union_in_the_ballot? do
+  option :yes => :you_agree_to_recognise_the_union
+  option :no => :you_do_not_have_to_recognise_the_union_cannot_reapply
+end
+
+multiple_choice :has_it_been_3_years_since_gaining_recognition? do
+  option :yes => :on_what_grounds_are_you_seeking_derecognition?
+  option :no => :you_cannot_seek_derecognition
+end
+
+multiple_choice :on_what_grounds_are_you_seeking_derecognition? do
+  option :lack_of_support_for_bargaining => :does_the_union_agree_with_derecognition_lack_of_bargaining_support?
+  option :falling_union_membership => :does_the_union_agree_with_derecognition_falling_union_membership?
+  option :reduced_workforce => :is_your_derecognition_valid?
+end
+
+multiple_choice :does_the_union_agree_with_derecognition_lack_of_bargaining_support? do
+  option :agree => :the_union_is_derecognised_and_bargaining_ends
+  option :does_not_agree => :will_the_cac_hold_a_ballot_lack_of_bargaining_support?
+end
+
+multiple_choice :will_the_cac_hold_a_ballot_lack_of_bargaining_support? do
+  option :hold_a_ballot => :what_is_the_cacs_decision_on_the_ballot?
+  option :do_not_hold_a_ballot => :you_must_continue_with_the_existing_bargaining_arrangements
+end
+
+multiple_choice :does_the_union_agree_with_derecognition_falling_union_membership? do
+  option :agree => :the_union_is_derecognised_and_bargaining_ends
+  option :does_not_agree => :will_the_cac_hold_a_ballot_falling_union_membership?
+end
+
+multiple_choice :will_the_cac_hold_a_ballot_falling_union_membership? do
+  option :hold_a_ballot => :what_is_the_cacs_decision_on_the_ballot?
+  option :do_not_hold_a_ballot => :you_must_continue_with_the_existing_bargaining_arrangements
+end
+
+multiple_choice :what_is_the_cacs_decision_on_the_ballot? do
+  option :end_collective_bargaining => :the_union_is_derecognised_and_bargaining_ends
+  option :continue_collective_bargaining => :you_must_continue_with_the_existing_bargaining_arrangements
+end
+
+multiple_choice :is_your_derecognition_valid? do
+  option :valid => :the_union_is_derecognised_and_bargaining_will_end
+  option :not_valid => :you_cannot_seek_derecognition
+end
+
+outcome :you_agree_to_recognise_the_union
+outcome :the_union_cannot_apply_for_statutory_recognition
+outcome :no_action_required
+outcome :you_do_not_have_to_recognise_the_union_can_reapply
+outcome :you_cannot_seek_derecognition
+outcome :the_union_is_derecognised_and_bargaining_ends
+outcome :the_union_is_derecognised_and_bargaining_will_end
+outcome :you_must_continue_with_the_existing_bargaining_arrangements
+outcome :you_do_not_have_to_recognise_the_union_cannot_reapply
---
+++ test/integration/flows/recognise_a_trade_union_test.rb
@@ -0,0 +1,254 @@
+require_relative '../../test_helper'
+require_relative 'flow_test_helper'
+
+class RecogniseATradeUnion < ActiveSupport::TestCase
+  include FlowTestHelper
+
+  setup do
+    setup_for_testing_flow 'recognise-a-trade-union'
+  end
+
+  should "ask what you need to do" do
+    assert_current_node :what_do_you_need_to_do?
+  end
+
+  context "recognise a trade union" do
+    setup do
+      add_response :recognise_a_trade_union
+    end
+
+    should "ask about voluntary recognition" do
+      assert_current_node :do_you_want_to_recognise_the_union_voluntarily?
+    end
+
+    should "agree to recognise the union" do
+      add_response :yes
+      assert_current_node :you_agree_to_recognise_the_union
+    end
+
+    context "do not agree" do
+      setup do
+        add_response :no
+      end
+
+      should "ask how many employees there are" do
+        assert_current_node :how_many_employees_do_you_have?
+      end
+
+      should "not be able to apply to statutory recognition" do
+        add_response :fewer_than_21
+        assert_current_node :the_union_cannot_apply_for_statutory_recognition
+      end
+
+      context "21 or more employees" do
+        setup do
+          add_response :"21_or_more"
+        end
+
+        should "ask if they have submitted an application" do
+          assert_current_node :have_they_submitted_an_application?
+        end
+
+        should "not require any action" do
+          add_response :yes
+          assert_current_node :no_action_required
+        end
+
+        context "application not submitted" do
+          setup do
+            add_response :no
+          end
+
+          should "ask if CAC have accepted the application" do
+            assert_current_node :have_cac_accepted_the_application?
+          end
+
+          should "not require you to recognise the union, although they can reapply" do
+            add_response :no
+            assert_current_node :you_do_not_have_to_recognise_the_union_can_reapply
+          end
+
+          context "CAC have accepted the application" do
+            setup do
+              add_response :yes
+            end
+
+            should "ask if CAC have ordered a ballot" do
+              assert_current_node :has_the_cac_ordered_a_ballot?
+            end
+
+            should "recognise the union if CAC have declared recognition" do
+              add_response :declared_recognition
+              assert_current_node :you_agree_to_recognise_the_union
+            end
+
+            context "ballot ordered" do
+              setup do
+                add_response :ordered_ballot
+              end
+
+              should "ask if the majority support the union" do
+                assert_current_node :did_the_majority_support_the_union_in_the_ballot?
+              end
+
+              should "recognise the union" do
+                add_response :yes
+                assert_current_node :you_agree_to_recognise_the_union
+              end
+
+              should "not recognise the union and they cannot reapply within 3 years" do
+                add_response :no
+                assert_current_node :you_do_not_have_to_recognise_the_union_cannot_reapply
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  context "derecognise a trade union" do
+    setup do
+      add_response :derecoognise_a_trade_union
+    end
+
+    should "ask if it has been 3 years since gaining recognition" do
+      assert_current_node :has_it_been_3_years_since_gaining_recognition?
+    end
+
+    should "not be able to seek derecognition" do
+      add_response :no
+      assert_current_node :you_cannot_seek_derecognition
+    end
+
+    context "is has been more than 3 years since recognition" do
+      setup do
+        add_response :yes
+      end
+
+      should "ask on what grounds you are seeking recognition" do
+        assert_current_node :on_what_grounds_are_you_seeking_derecognition?
+      end
+
+      context "lack of support for bargaining" do
+        setup do
+          add_response :lack_of_support_for_bargaining
+        end
+
+        should "ask if the union agrees" do
+          assert_current_node :does_the_union_agree_with_derecognition_lack_of_bargaining_support?
+        end
+
+        should "derecognise the union" do
+          add_response :agree
+          assert_current_node :the_union_is_derecognised_and_bargaining_ends
+        end
+
+        context "union does not agree" do
+          setup do
+            add_response :does_not_agree
+          end
+
+          should "ask if CAC will hold a ballot" do
+            assert_current_node :will_the_cac_hold_a_ballot_lack_of_bargaining_support?
+          end
+
+          should "continue with the existing arrangements" do
+            add_response :do_not_hold_a_ballot
+            assert_current_node :you_must_continue_with_the_existing_bargaining_arrangements
+          end
+
+          context "hold a ballot" do
+            setup do
+              add_response :hold_a_ballot
+            end
+
+            should "ask what the CAS's decision on the ballot is" do
+              assert_current_node :what_is_the_cacs_decision_on_the_ballot?
+            end
+
+            should "derecognise union" do
+              add_response :end_collective_bargaining
+              assert_current_node :the_union_is_derecognised_and_bargaining_ends
+            end
+
+            should "continue with existing bargaining arrangements" do
+              add_response :continue_collective_bargaining
+              assert_current_node :you_must_continue_with_the_existing_bargaining_arrangements
+            end
+          end
+        end
+      end
+
+      context "falling union membership" do
+        setup do
+          add_response :falling_union_membership
+        end
+
+        should "ask if the union agrees" do
+          assert_current_node :does_the_union_agree_with_derecognition_falling_union_membership?
+        end
+
+        should "derecognise the union" do
+          add_response :agree
+          assert_current_node :the_union_is_derecognised_and_bargaining_ends
+        end
+
+        context "union does not agree" do
+          setup do
+            add_response :does_not_agree
+          end
+
+          should "ask if CAC will hold a ballot" do
+            assert_current_node :will_the_cac_hold_a_ballot_falling_union_membership?
+          end
+
+          should "continue with existing arrangements" do
+            add_response :do_not_hold_a_ballot
+            assert_current_node :you_must_continue_with_the_existing_bargaining_arrangements
+          end
+
+          context "hold a ballot" do
+            setup do
+              add_response :hold_a_ballot
+            end
+
+            should "ask what the CAS's decision on the ballot is" do
+              assert_current_node :what_is_the_cacs_decision_on_the_ballot?
+            end
+
+            should "derecognise union" do
+              add_response :end_collective_bargaining
+              assert_current_node :the_union_is_derecognised_and_bargaining_ends
+            end
+
+            should "continue with existing bargaining arrangements" do
+              add_response :continue_collective_bargaining
+              assert_current_node :you_must_continue_with_the_existing_bargaining_arrangements
+            end
+          end
+        end
+      end
+
+      context "reduced workforce" do
+        setup do
+          add_response :reduced_workforce
+        end
+
+        should "ask if CAS has decided your notice is valid" do
+          assert_current_node :is_your_derecognition_valid?
+        end
+
+        should "not be able to seek derecognition" do
+          add_response :not_valid
+          assert_current_node :you_cannot_seek_derecognition
+        end
+
+        should "be derecognised" do
+          add_response :valid
+          assert_current_node :the_union_is_derecognised_and_bargaining_will_end
+        end
+      end
+    end
+  end
+end
