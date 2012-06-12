cbdcc83054f6ab1b7b025ade82c3c403e86b2b4f
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-06-12 02:34:17 -0700

    Switch 'claim NI refund' tests to use new test helpers

--- test/integration/flows/claim_a_national_insurance_refund_test.rb
+++ test/integration/flows/claim_a_national_insurance_refund_test.rb
@@ -10,59 +10,62 @@ class ClaimANationalInsuranceRefundTest < ActiveSupport::TestCase
   end
 
   should "ask what class of NI" do
-    assert_equal :which_class?, node_for_responses([])
+    assert_current_node :which_class?
   end
 
   should "be outcome_1 for Class 1" do
-    assert_equal :outcome_1, node_for_responses(['class_1'])
+    add_response :class_1
+    assert_current_node :outcome_1
   end
 
   context "Class 2" do
     setup do
-      @responses = ['class_2']
+      add_response :class_2
     end
 
     should "ask if you paid when you didn't need to" do
-      assert_equal :did_you_pay_when_you_didnt_need_to?, node_for_responses(@responses)
+      assert_current_node :did_you_pay_when_you_didnt_need_to?
     end
 
     should "be outcome_5 if yes" do
-      @responses << 'yes'
-      assert_equal :outcome_5, node_for_responses(@responses)
+      add_response :yes
+      assert_current_node :outcome_5
     end
 
     should "be outcome_1a if no" do
-      @responses << 'no'
-      assert_equal :outcome_1a, node_for_responses(@responses)
+      add_response :no
+      assert_current_node :outcome_1a
     end
   end
 
   should "be outcome_2 for Class 3" do
-    assert_equal :outcome_2, node_for_responses(['class_3'])
+    add_response :class_3
+    assert_current_node :outcome_2
   end
 
   context "Class 4" do
     setup do
-      @responses = ['class_4']
+      add_response :class_4
     end
 
     should "ask if you paid when you didn't need to" do
-      assert_equal :did_you_pay_when_you_didnt_need_to?, node_for_responses(@responses)
+      assert_current_node :did_you_pay_when_you_didnt_need_to?
     end
 
     should "be outcome_3 if yes" do
-      @responses << 'yes'
-      assert_equal :outcome_3, node_for_responses(@responses)
+      add_response :yes
+      assert_current_node :outcome_3
     end
 
     should "be outcome_4 if no" do
-      @responses << 'no'
-      assert_equal :outcome_4, node_for_responses(@responses)
+      add_response :no
+      assert_current_node :outcome_4
     end
   end
 
   should "work with symbols as well as strings" do
-    assert_equal :outcome_4, node_for_responses([:class_4, :no])
+    @responses = [:class_4, :no]
+    assert_current_node :outcome_4
   end
 end
 
