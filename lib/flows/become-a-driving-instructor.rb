bfe6934dbc640f933e6b3ea322c827b728186bbe
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-06-18 05:14:42 -0700

    Copy and logic changed for 'Become a driving instructor"

--- lib/flows/become-a-driving-instructor.rb
+++ lib/flows/become-a-driving-instructor.rb
@@ -1,8 +1,19 @@
 satisfies_need "B188"
 status :draft
 
-multiple_choice :have_you_had_licence_for_4_of_6_years? do
-  option :yes => :have_you_been_disqualified_in_last_4_years?
+multiple_choice :are_you_21_or_over? do
+  option :yes => :have_you_had_licence_for_3_years?
+  option :no => :not_old_enough
+
+  calculate :content_sections do
+    if responses.last == 'no'
+      PhraseList.new :ADI_required_legal_warning, :acronym_definitions
+    end
+  end
+end
+
+multiple_choice :have_you_had_licence_for_3_years? do
+  option :yes => :are_you_driving_instructor_in_ec_country?
   option :no => :havent_had_licence_for_long_enough
 
   calculate :content_sections do
@@ -12,77 +23,75 @@ multiple_choice :have_you_had_licence_for_4_of_6_years? do
   end
 end
 
-multiple_choice :have_you_been_disqualified_in_last_4_years? do
-  option :yes => :cant_because_disqualified
-  option :no => :do_you_have_a_criminal_record?
+multiple_choice :are_you_driving_instructor_in_ec_country? do
+  option :yes => :can_apply_to_transfer_registration
+  option :no => :have_you_been_disqualified_or_6_points?
 
   calculate :content_sections do
     if responses.last == 'yes'
-      PhraseList.new :unlikely_apply_anyway, :apply_steps, :criminal_record_check, :apply_to_dsa, :ADI_required_legal_warning, :acronym_definitions
+      PhraseList.new :acronym_definitions
     end
   end
 end
 
-multiple_choice :do_you_have_a_criminal_record? do
-  option :yes => :was_offence_violent_or_sex_related?
-  option :no => :do_you_have_a_disability?
+multiple_choice :have_you_been_disqualified_or_6_points? do
+  option :yes => :can_start_process_of_applying
+  option :no => :what_licence_type?
 
-  calculate :have_criminal_record do
+  calculate :content_sections do
+    if responses.last == 'yes'
+      PhraseList.new :apply_to_dsa_with_endorsments, :apply_with_caveats_what_next, :acronym_definitions
+    end
+  end
+end
+
+multiple_choice :what_licence_type? do
+  option :manual => :non_motoring_offences?
+  option :automatic => :because_of_disability?
+end
+
+multiple_choice :because_of_disability? do
+  option :yes => :non_motoring_offences?
+  option :no => :cant_because_limited_licence
+
+  calculate :disabled_driver do
     responses.last == 'yes'
   end
-end
-
-multiple_choice :was_offence_violent_or_sex_related? do
-  option :yes => :very_unlikely_because_of_criminal_record
-  option :no => :do_you_have_a_disability?
-
   calculate :content_sections do
-    if responses.last == 'yes'
-      PhraseList.new :unlikely_apply_anyway, :apply_steps, :criminal_record_check, :apply_to_dsa, :ADI_required_legal_warning, :acronym_definitions
+    if ! disabled_driver
+      PhraseList.new :ADI_required_legal_warning, :acronym_definitions
     end
   end
 end
 
-multiple_choice :do_you_have_a_disability? do
+multiple_choice :non_motoring_offences? do
   option :yes
   option :no
 
-  calculate :have_disability do
-    responses.last == 'yes'
-  end
-
-  next_node :are_you_driving_instructor_in_ec_country?
-end
-
-multiple_choice :are_you_driving_instructor_in_ec_country? do
-  option :yes => :can_apply_to_transfer_registration
-  option :no => :can_start_process_of_applying
-
   calculate :content_sections do
     sections = PhraseList.new
-    if responses.last == 'no'
-      if have_disability
-        sections << :apply_steps_with_emergency_control << :emergency_control
+    if responses.last == 'yes'
+      sections << :apply_to_dsa_with_criminal_record
+      sections << (disabled_driver ? :apply_with_caveats_and_emergency_control_what_next : :apply_with_caveats_what_next)
+    else
+      if disabled_driver
+        sections << :apply_steps_with_emergency_control
+        sections << :emergency_control
       else
         sections << :apply_steps
       end
       sections << :criminal_record_check
-      sections << :criminal_record_warning if have_criminal_record
       sections << :apply_to_dsa
-    else
-      sections << (have_disability ? :transfer_steps_with_emergency_control : :transfer_steps)
-      sections << :gb_counterpart_licence
-      sections << :emergency_control if have_disability
-      sections << :transfer_registration
-      sections << :criminal_record_warning if have_criminal_record
     end
     sections << :acronym_definitions
   end
+
+  next_node :can_start_process_of_applying
 end
 
 outcome :can_start_process_of_applying
 outcome :can_apply_to_transfer_registration
 
+outcome :not_old_enough
 outcome :havent_had_licence_for_long_enough
-outcome :cant_because_disqualified
-outcome :very_unlikely_because_of_criminal_record
+outcome :cant_because_limited_licence
--- test/integration/flows/become_a_driving_instructor_test.rb
+++ test/integration/flows/become_a_driving_instructor_test.rb
@@ -9,173 +9,131 @@ class BecomeADrivingInstructorTest < ActiveSupport::TestCase
     setup_for_testing_flow 'become-a-driving-instructor'
   end
 
-  should "ask if you've had a licence for 4 of the last 6 years" do
-    assert_current_node :have_you_had_licence_for_4_of_6_years?
+  should "ask if you're 21 or over" do
+    assert_current_node :are_you_21_or_over?
   end
 
-  should "not be possible if you've not have a licence for long enough" do # response 1
+  should "not be possible if under 21" do # response 1
     add_response :no
-    assert_current_node :havent_had_licence_for_long_enough
+    assert_current_node :not_old_enough
     assert_phrase_list :content_sections, [:ADI_required_legal_warning, :acronym_definitions]
   end
 
-  context "having had a licence for long enough" do
+  context "when 21 or over" do
     setup do
       add_response :yes
     end
 
-    should "ask if you've been disqualified from driving in last 4 years" do
-      assert_current_node :have_you_been_disqualified_in_last_4_years?
+    should "ask if you've had a licence for 3 years or more" do
+      assert_current_node :have_you_had_licence_for_3_years?
     end
 
-    should "not be allowed if you've been disqualified" do # response 2
-      add_response :yes
-      assert_current_node :cant_because_disqualified
-      assert_phrase_list :content_sections, [:unlikely_apply_anyway, :apply_steps, :criminal_record_check, 
-        :apply_to_dsa, :ADI_required_legal_warning, :acronym_definitions]
+    should "not be possible if you haven't had licence for 3 years" do # response 2
+      add_response :no
+      assert_current_node :havent_had_licence_for_long_enough
+      assert_phrase_list :content_sections, [:ADI_required_legal_warning, :acronym_definitions]
     end
 
-    context "without disqualifications" do
+    context "having had a licence for long enough" do
       setup do
-        add_response :no
+        add_response :yes
       end
 
-      should "ask if you have a criminal record" do
-        assert_current_node :do_you_have_a_criminal_record?
+      should "ask if you're an EC ADI" do
+        assert_current_node :are_you_driving_instructor_in_ec_country?
       end
 
-      context "without a criminal record" do
+      should "be able to transfer registration if EC ADI" do # response 3
+        add_response :yes
+        assert_current_node :can_apply_to_transfer_registration
+        assert_phrase_list :content_sections, [:acronym_definitions]
+      end
+
+      context "not EC ADI" do
         setup do
           add_response :no
         end
 
-        should "ask if you have a disability" do
-          assert_current_node :do_you_have_a_disability?
+        should "ask if you've been disqualified etc." do
+          assert_current_node :have_you_been_disqualified_or_6_points?
         end
 
-        context "without a disability" do
-          setup do
-            add_response :no
-          end
-
-          should "ask if you're already registered in another EC country" do
-            assert_current_node :are_you_driving_instructor_in_ec_country?
-          end
-
-          should "be able to apply if not an EC instructor" do # response 4
-            add_response :no
-            assert_current_node :can_start_process_of_applying
-            assert_phrase_list :content_sections, [:apply_steps, :criminal_record_check, :apply_to_dsa, :acronym_definitions]
-          end
-
-          should "be able to apply to transfer if an EC instructor" do # response 5
-            add_response :yes
-            assert_current_node :can_apply_to_transfer_registration
-            assert_phrase_list :content_sections, [:transfer_steps, :gb_counterpart_licence, :transfer_registration, :acronym_definitions]
-          end
-        end # without a disability
-
-        context "with a disability" do
-          setup do
-            add_response :yes
-          end
-
-          should "ask if you're already registered in another EC country" do
-            assert_current_node :are_you_driving_instructor_in_ec_country?
-          end
-
-          should "be able to apply if not an EC instructor" do # response 6
-            add_response :no
-            assert_current_node :can_start_process_of_applying
-            assert_phrase_list :content_sections,
-              [:apply_steps_with_emergency_control, :emergency_control, :criminal_record_check, :apply_to_dsa, :acronym_definitions]
-          end
-
-          should "be able to apply to transfer if an EC instructor" do # response 7
-            add_response :yes
-            assert_current_node :can_apply_to_transfer_registration
-            assert_phrase_list :content_sections,
-              [:transfer_steps_with_emergency_control, :gb_counterpart_licence, :emergency_control, :transfer_registration, :acronym_definitions]
-          end
-        end # with a disability
-      end # without a criminal record
-
-      context "with a criminal record" do
-        setup do
+        should "be possible with caveats if have disqualifications etc." do # response 4
           add_response :yes
+          assert_current_node :can_start_process_of_applying
+          assert_phrase_list :content_sections, [:apply_to_dsa_with_endorsments, :apply_with_caveats_what_next, :acronym_definitions]
         end
 
-        should "ask if the offence is voilent or sex related" do
-          assert_current_node :was_offence_violent_or_sex_related?
-        end
-
-        should "not be allowed if offence was violent or sex related" do # response 3
-          add_response :yes
-          assert_current_node :very_unlikely_because_of_criminal_record
-          assert_phrase_list :content_sections, [:unlikely_apply_anyway, :apply_steps, :criminal_record_check,
-            :apply_to_dsa, :ADI_required_legal_warning, :acronym_definitions]
-        end
-
-        context "non-voilent offence" do
+        context "with no disqualifications etc." do
           setup do
             add_response :no
           end
 
-          should "ask if you have a disability" do
-            assert_current_node :do_you_have_a_disability?
+          should "ask what type of licence you have" do
+            assert_current_node :what_licence_type?
           end
 
-          context "without a disability" do
+          context "with a manual licence" do
             setup do
-              add_response :no
+              add_response :manual
             end
 
-            should "ask if you're already registered in another EC country" do
-              assert_current_node :are_you_driving_instructor_in_ec_country?
+            should "ask if you have any non-motoring convictions" do
+              assert_current_node :non_motoring_offences?
             end
 
-            should "be able to apply if not an EC instructor" do # response 8
+            should "be possible to apply with no convictions" do # response 6
               add_response :no
               assert_current_node :can_start_process_of_applying
-              assert_phrase_list :content_sections,
-                [:apply_steps, :criminal_record_check, :criminal_record_warning, :apply_to_dsa, :acronym_definitions]
+              assert_phrase_list :content_sections, [:apply_steps, :criminal_record_check, :apply_to_dsa, :acronym_definitions]
             end
 
-            should "be able to apply to transfer if an EC instructor" do # response 9
+            should "be possible to apply with caveats with convictions" do # response 5
               add_response :yes
-              assert_current_node :can_apply_to_transfer_registration
-              assert_phrase_list :content_sections,
-                [:transfer_steps, :gb_counterpart_licence, :transfer_registration, :criminal_record_warning, :acronym_definitions]
-            end
-          end # without a disability
-
-          context "with a disability" do
-            setup do
-              add_response :yes
-            end
-
-            should "ask if you're already registered in another EC country" do
-              assert_current_node :are_you_driving_instructor_in_ec_country?
-            end
-
-            should "be able to apply if not an EC instructor" do # response 10
-              add_response :no
               assert_current_node :can_start_process_of_applying
-              assert_phrase_list :content_sections,
-                [:apply_steps_with_emergency_control, :emergency_control, :criminal_record_check, :criminal_record_warning,
-                  :apply_to_dsa, :acronym_definitions]
+              assert_phrase_list :content_sections, [:apply_to_dsa_with_criminal_record, :apply_with_caveats_what_next, :acronym_definitions]
+            end
+          end # manual licence
+
+          context "with an automatic licence" do
+            setup do
+              add_response :automatic
             end
 
-            should "be able to apply to transfer if an EC instructor" do # response 11
-              add_response :yes
-              assert_current_node :can_apply_to_transfer_registration
-              assert_phrase_list :content_sections,
-                [:transfer_steps_with_emergency_control, :gb_counterpart_licence, :emergency_control, 
-                  :transfer_registration, :criminal_record_warning, :acronym_definitions]
+            should "ask if limited to automitic because of a disability" do
+              assert_current_node :because_of_disability?
             end
-          end # with a disability
-        end # non-voilent offence
-      end # with a criminal record
-    end # without disqualifications
-  end # have licence for long enough
+
+            should "not be possible with limited licence" do # response 7
+              add_response :no
+              assert_current_node :cant_because_limited_licence
+              assert_phrase_list :content_sections, [:ADI_required_legal_warning, :acronym_definitions]
+            end
+
+            context "limited die to disability" do
+              setup do
+                add_response :yes
+              end
+
+              should "ask if you have any non-motoring convictions" do
+                assert_current_node :non_motoring_offences?
+              end
+
+              should "be possible to apply with no convictions" do # response 9
+                add_response :no
+                assert_current_node :can_start_process_of_applying
+                assert_phrase_list :content_sections, [:apply_steps_with_emergency_control, :emergency_control, :criminal_record_check, :apply_to_dsa, :acronym_definitions]
+              end
+
+              should "be possible to apply with caveats with convictions" do # response 8
+                add_response :yes
+                assert_current_node :can_start_process_of_applying
+                assert_phrase_list :content_sections, [:apply_to_dsa_with_criminal_record, :apply_with_caveats_and_emergency_control_what_next, :acronym_definitions]
+              end
+            end # limited fue to disability
+          end # automatic licence
+        end # without disqualifications
+      end # not EC ADI
+    end # have licence for long enough
+  end # over 21
 end
