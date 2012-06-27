ab0640e3b1d7d8d390527c7c0736203609dd5251
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-06-27 09:22:29 -0700

    Add smart answer for estimating self assessment penalties
    Slug: estimate-self-assessment-penalties
    Need: B692
    
    This smart answer consists of a relatively simple flow and a relatively
    complex set of calculations with multiple output variables. Significant
    dates are configured at the top of the flow file.
    
    Squashed commit of the following:
    
    commit 8bcf560de097a23ff33a4b81046bb0ecf1b660af
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Wed Jun 27 09:17:27 2012 -0700
    
        Add dates and rename calculator_dates.
    
        CALCULATE_DATES was renamed to avoid constant reassignment warnings when running tests.
        Add dates hash to calculator mock creation expectation.
    
    commit b389a5d552c2216fb10246dafd4a34cf940ac6e0
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Wed Jun 27 16:07:01 2012 +0100
    
        Implement penalty results and use Money object.
    
        Implement copy for results with penalties and use the SmartAnswer::Money
        class for money values (rather than manual formatting).
    
    commit 4190bde3e280efef9765ca21534fb4a6ae3bdd81
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Wed Jun 27 07:08:54 2012 -0700
    
        Refactor self assessment penalties calculator.
    
        Remove redundant methods and factor out static dates.
    
    commit 15b123de1f1ad2ee4377e0fea035ef795bf835dc
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Tue Jun 26 08:33:35 2012 -0700
    
        Add self assessment penalties calculations
    
        First pass, tests pass but needs refactoring.
    
    commit 09f26183f05e93bef65c3508b213a54d5d171bad
    Merge: 7c493f8 00d2821
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Mon Jun 25 16:22:08 2012 +0100
    
        Merge branch 'sa-self-assessment-penalties' of github.com:alphagov/smart-answers into sa-self-assessment-penalties
    
    commit 7c493f8eacc963f824a167f9f162cf09b608dfcf
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Fri Jun 22 08:53:57 2012 -0700
    
        Add copy and result parameters for self assessment penalties.
    
    commit e6c02d050172d56924be494d32dbf8eb2a550983
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Fri Jun 22 01:15:03 2012 -0700
    
        Add self assessment penalties smart answer.
    
    commit 00d2821f20ed0e7e3adb3db0f81e801c54080124
    Merge: df47505 2a85340
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Mon Jun 25 16:10:05 2012 +0100
    
        Merge branch 'master' into sa-self-assessment-penalties
    
    commit df47505705f9c94aa0e02500451f80864ddf3776
    Merge: 0ddddc0 79ba892
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Fri Jun 22 08:54:30 2012 -0700
    
        Merge branch 'master' into sa-self-assessment-penalties
    
    commit 0ddddc01a99f9aa9504fd7625d931b0c7070f283
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Fri Jun 22 08:53:57 2012 -0700
    
        Add copy and result parameters for self assessment penalties.
    
    commit 37ca2fde2cd01846fb4f0ee9ab46bf4c2c501e2c
    Author: Rob Young <rob.young@digital.cabinet-office.gov.uk>
    Date:   Fri Jun 22 01:15:03 2012 -0700
    
        Add self assessment penalties smart answer.

---
+++ lib/flows/estimate-self-assessment-penalties.rb
@@ -0,0 +1,89 @@
+status :draft
+
+satisfies_need "B692"
+
+calculator_dates = {
+  :online_filing_deadline => Date.new(2012, 1, 31),
+  :offline_filing_deadline => Date.new(2011, 10, 31),
+  :payment_deadline => Date.new(2012, 1, 31),
+  :penalty1date => Date.new(2012, 3, 2),
+  :penalty2date => Date.new(2012, 8, 2),
+  :penalty3date => Date.new(2013, 2, 2)
+}
+
+multiple_choice :how_submitted? do
+  option :online => :when_submitted?
+  option :paper => :when_submitted?
+
+  save_input_as :submission_method
+end
+
+date_question :when_submitted? do
+  save_input_as :filing_date
+
+  next_node :when_paid?
+end
+
+date_question :when_paid? do
+  save_input_as :payment_date
+
+  next_node do |response|
+    calculator = Calculators::SelfAssessmentPenalties.new(
+      :submission_method => submission_method,
+      :filing_date => filing_date,
+      :payment_date => response,
+      :dates => calculator_dates
+    )
+    if calculator.paid_on_time?
+      :filed_and_paid_on_time
+    else
+      :how_much_tax?
+    end
+  end
+end
+
+money_question :how_much_tax? do
+  save_input_as :estimated_bill
+
+  calculate :calculator do
+    Calculators::SelfAssessmentPenalties.new(
+      :submission_method => submission_method,
+      :filing_date => filing_date,
+      :payment_date => payment_date,
+      :estimated_bill => responses.last,
+      :dates => calculator_dates
+    )
+  end
+
+  calculate :late_filing_penalty do
+    calculator.late_filing_penalty
+  end
+
+  calculate :total_owed do
+    calculator.total_owed
+  end
+
+  calculate :interest do
+    calculator.interest
+  end
+
+  calculate :late_payment_penalty do
+    calculator.late_payment_penalty
+  end
+
+  calculate :result_parts do
+    phrase_list = PhraseList.new(calculator.late_filing_penalty == 0 ? :result_part1_no_penalty : :result_part1_penalty)
+    if calculator.late_payment_penalty == 0
+      phrase_list << :result_part2_no_penalty
+    else
+      phrase_list << :result_part2_penalty
+    end
+    phrase_list
+  end
+
+  next_node :late
+end
+
+
+outcome :late
+outcome :filed_and_paid_on_time
---
+++ test/unit/calculators/self_assessment_penalties_test.rb
@@ -0,0 +1,123 @@
+require_relative "../../test_helper"
+
+TEST_CALCULATOR_DATES = {
+  :online_filing_deadline => Date.new(2012, 1, 31),
+  :offline_filing_deadline => Date.new(2011, 10, 31),
+  :payment_deadline => Date.new(2012, 1, 31),
+  :penalty1date => Date.new(2012, 3, 2),
+  :penalty2date => Date.new(2012, 8, 2),
+  :penalty3date => Date.new(2013, 2, 2)
+}
+
+module SmartAnswer::Calculators
+  class SelfAssessmentPenaltiesTest < ActiveSupport::TestCase
+    def setup
+      @calculator = SelfAssessmentPenalties.new(
+        submission_method: "online", filing_date: "2012-01-10",
+        payment_date: "2012-03-10", estimated_bill: SmartAnswer::Money.new(1200.5),
+        dates: TEST_CALCULATOR_DATES
+      )
+    end
+
+    context "online submission" do
+      context "filed and paid on time" do
+        setup do
+          @calculator.filing_date = "2012-01-10"
+          @calculator.payment_date = "2012-01-10"
+        end
+
+        should "confirm payment was made on time" do
+          assert @calculator.paid_on_time?
+        end
+      end
+
+      context "filed or paid late" do
+        should "confirm payment was made late" do
+          refute @calculator.paid_on_time?
+        end
+
+        should "calculate late filing penalty" do
+          # band one
+          assert_equal 0, @calculator.late_filing_penalty
+          # band two
+          @calculator.filing_date = "2012-02-02"
+          assert_equal 100, @calculator.late_filing_penalty
+          # band three
+          @calculator.filing_date = "2012-06-06"
+          assert_equal 470, @calculator.late_filing_penalty
+          # band four
+          @calculator.filing_date = "2012-09-06"
+          assert_equal 1300, @calculator.late_filing_penalty
+          # band four (5% estimated bill larger than £300)
+          @calculator.estimated_bill = SmartAnswer::Money.new(11000)
+          assert_equal 1550, @calculator.late_filing_penalty
+          # band five
+          @calculator.filing_date = "2013-02-02"
+          assert_equal 1600, @calculator.late_filing_penalty
+          # band five (5% estimated bill larger than £600)
+          @calculator.estimated_bill = SmartAnswer::Money.new(13000)
+          assert_equal 1650, @calculator.late_filing_penalty
+        end
+
+        should "calculate interest payment" do
+          @calculator.payment_date = "2012-01-01"
+          assert_equal 0, @calculator.interest
+          @calculator.payment_date = "2012-03-03"
+          assert_equal 3.16, @calculator.interest
+          # should calculate PenaltyInterest1
+          @calculator.payment_date = "2012-04-02"
+          @calculator.estimated_bill = SmartAnswer::Money.new(20000)
+          assert_equal 102, @calculator.interest
+          # should calculate PenaltyInterest2
+          @calculator.payment_date = "2012-08-20"
+          assert_equal 343.64, @calculator.interest
+          # should calculate PenaltyInterest3
+          @calculator.payment_date = "2013-02-20"
+          assert_equal 675.37, @calculator.interest
+        end
+
+        should "calculate late payment penalty" do
+          assert_equal 60.03, @calculator.late_payment_penalty
+          @calculator.payment_date = "2012-08-03"
+          assert_equal 120.05, @calculator.late_payment_penalty
+          @calculator.payment_date = "2013-02-02"
+          assert_equal 180.08, @calculator.late_payment_penalty
+        end
+
+        should "calculate total owed" do
+          assert_equal 1264.38, @calculator.total_owed
+          @calculator.payment_date = "2012-08-03"
+          assert_equal 1339.42, @calculator.total_owed
+          @calculator.payment_date = "2013-02-02"
+          assert_equal 1419.17, @calculator.total_owed
+        end
+      end
+    end
+
+    context "offline submission" do
+      setup do
+        @calculator.submission_method = "paper"
+      end
+      context "filed and paid on time" do
+        setup do
+          @calculator.filing_date = "2011-10-30"
+          @calculator.payment_date = "2012-01-10"
+        end
+
+        should "confirm payment was made on time" do
+          assert @calculator.paid_on_time?
+        end
+      end
+
+      context "filed or paid late" do
+        setup do
+          @calculator.filing_date = "2012-01-10"
+          @calculator.payment_date = "2012-01-10"
+        end
+        should "confirm payment was made late" do
+          refute @calculator.paid_on_time?
+        end
+      end
+    end
+  end
+end
