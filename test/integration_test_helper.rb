1b27d430273c1719932298eb6a17bfa590a715bf
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-07-09 12:52:56 +0100

    assert_current_url test helper

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -14,6 +14,15 @@ class ActionDispatch::IntegrationTest
   def assert_page_has_content(text)
     assert page.has_content?(text), %(expected there to be content #{text} in #{page.text.inspect})
   end
+
+  def assert_current_url(path_with_query, options = {})
+    expected = URI.parse(path_with_query)
+    current = URI.parse(current_url)
+    assert_equal expected.path, current.path
+    unless options[:ignore_query]
+      assert_equal Rack::Utils.parse_query(expected.query), Rack::Utils.parse_query(current.query)
+    end
+  end
 end
 
 class JavascriptIntegrationTest < ActionDispatch::IntegrationTest
