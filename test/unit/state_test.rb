d081ff77bc3e3eb3ca7fa4edd9cb6dfbdfa6ec2a
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-07-16 14:12:04 +0100

    Fix bug with input validation.

---
+++ test/unit/state_test.rb
@@ -0,0 +1,23 @@
+# coding:utf-8
+
+require_relative '../test_helper'
+
+module SmartAnswer
+  class StateTest < ActiveSupport::TestCase
+
+    context "transitioning to a new state" do
+
+      should "not modify the existing state" do
+        old_state = State.new(:state2)
+        old_state.path << :state1
+        old_state.responses << 'yes'
+        old_state.freeze
+
+        new_state = old_state.transition_to(:state3, 'fooey')
+
+        assert_equal ['yes'], old_state.responses
+        assert_equal [:state1], old_state.path
+      end
+    end
+  end
+end
