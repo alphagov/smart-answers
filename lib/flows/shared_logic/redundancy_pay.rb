55003795d625a5583fe007e7fd730bd34b990731
Aleks Maricic <aleks.maricic@digital.cabinet-office.gov.uk>
2012-07-18 15:26:49 +0100

    Updated text after 2i review. Added validation for numeric input and error message text

--- lib/flows/shared_logic/redundancy_pay.rb
+++ lib/flows/shared_logic/redundancy_pay.rb
@@ -2,20 +2,22 @@ calculator = Calculators::RedundancyCalculator.new
 
 multiple_choice :age_of_employee? do
   option "over-41"
-  option "22-41"
+  option "22-40"
   option "under-22"
 
   save_input_as :employee_age
   next_node :years_employed?
 end
 
+# This needs validation - any string not representing a numeric value will be converted to 0.0 e.g. 'whatever'.to_f => 0.0 
+# Using Float(response) instead will fail with an ArgumentError that will be handled by the flow controller
 value_question :years_employed? do
   save_input_as :years_employed
   calculate :years_employed do
-    responses.last.to_f.floor
+    Float(responses.last).floor
   end
   next_node do |response|
-    if response.to_f.floor < 2
+    if Float(response).floor < 2
       :done_no_statutory
     else
       :weekly_pay_before_tax?
--- lib/smart_answer/calculators/redundancy_calculator.rb
+++ lib/smart_answer/calculators/redundancy_calculator.rb
@@ -6,7 +6,7 @@ module SmartAnswer::Calculators
     def pay(age, years, weekly_pay)
       ratio_lookup = {
         "over-41" => 1.5,
-        "22-41" => 1.0,
+        "22-40" => 1.0,
         "under-22" => 0.5
       }
       [430.00, weekly_pay.to_f].min * (years.to_f.floor * ratio_lookup[age])
