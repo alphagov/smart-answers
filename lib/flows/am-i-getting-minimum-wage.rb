7c12feb932245edf8a68361e716400b37dd9a4fd
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-07-20 17:39:27 +0100

    Initial amends for questions 1-5

--- lib/flows/am-i-getting-minimum-wage.rb
+++ lib/flows/am-i-getting-minimum-wage.rb
@@ -3,78 +3,116 @@ section_slug "money-and-tax"
 subsection_slug "tax"
 satisfies_need "2013"
 
+maximum_number_of_days_in_month = 31
+
 calculator = Calculators::MinimumWageCalculator.new
 
-multiple_choice :how_do_you_get_paid? do
-  option :per_hour
-  option :per_piece
-
-  save_input_as :payment_method
-
-  next_node :how_old_are_you?
+# Q1
+multiple_choice :what_would_you_like_to_check? do
+  option "current_payment"
+  option "past_payment"
+  save_input_as :current_or_past_payments
+  next_node :are_you_an_apprentice?
 end
 
-multiple_choice :how_old_are_you? do
-  option :"21_or_over"
-  option :"18_to_20"
-  option :"under_18"
-  option :under_19
-  option :"19_or_over"
+# Q2
+multiple_choice :are_you_an_apprentice? do
+  option "no" => :how_old_are_you?
+  option "apprentice_under_19" => :how_often_do_you_get_paid?
+  option "apprentice_over_19" => :how_often_do_you_get_paid?
+end
 
-  save_input_as :age
+# Q3
+value_question :how_old_are_you? do
+  next_node :how_do_you_get_paid?
+end
 
-  calculate :per_hour_minimum_wage do
-    "%0.2f" % calculator.per_hour_minimum_wage(responses.last)
-  end
-
-  next_node do
-    if payment_method.to_sym == :per_hour
-      :how_many_hours_per_week_worked?
-    else
-      :how_many_pieces_do_you_produce_per_week?
+# Q4
+value_question :how_often_do_you_get_paid? do
+  calculate :pay_frequency do
+    if responses.last.to_i > maximum_number_of_days_in_month
+      raise SmartAnswer::InvalidResponse
     end
+    responses.last.to_i
   end
+  next_node :how_many_hours_do_you_work_during_the_pay_period?
 end
 
-value_question :how_many_hours_per_week_worked? do
-  save_input_as :hours_per_week
-
-  calculate :per_week_minimum_wage do
-    "%0.2f" % calculator.per_week_minimum_wage(age, responses.last)
-  end
-
-  next_node :per_hour_minimum_wage
+# Q5
+value_question :how_many_hours_do_you_work_during_the_pay_period? do
 end
 
-value_question :how_many_pieces_do_you_produce_per_week? do
-  save_input_as :pieces_per_week
 
-  next_node :how_much_do_you_get_paid_per_piece?
-end
+#multiple_choice :how_do_you_get_paid? do
+#  option :per_hour
+#  option :per_piece
 
-value_question :how_much_do_you_get_paid_per_piece? do
-  save_input_as :pay_per_piece
+#  save_input_as :payment_method
 
-  next_node :how_many_hours_do_you_work_per_week?
-end
+#  next_node :how_old_are_you?
+#end
 
-value_question :how_many_hours_do_you_work_per_week? do
-  save_input_as :hours_per_week
+#multiple_choice :how_old_are_you? do
+#  option :"21_or_over"
+#  option :"18_to_20"
+#  option :"under_18"
+#  option :under_19
+#  option :"19_or_over"
 
-  calculate :hourly_wage do
-    "%0.2f" % calculator.per_piece_hourly_wage(pay_per_piece, pieces_per_week, responses.last)
-  end
+#  save_input_as :age
 
-  calculate :above_below do
-    if calculator.is_below_minimum_wage?(age, pay_per_piece, pieces_per_week, responses.last)
-      "below"
-    else
-      "above"
-    end
-  end
+#  calculate :per_hour_minimum_wage do
+#    "%0.2f" % calculator.per_hour_minimum_wage(responses.last)
+#  end
 
-  next_node :per_piece_minimum_wage
-end
+#  next_node do
+#    if payment_method.to_sym == :per_hour
+#      :how_many_hours_per_week_worked?
+#    else
+#      :how_many_pieces_do_you_produce_per_week?
+#    end
+#  end
+#end
+
+#value_question :how_many_hours_per_week_worked? do
+#  save_input_as :hours_per_week
+
+#  calculate :per_week_minimum_wage do
+#    "%0.2f" % calculator.per_week_minimum_wage(age, responses.last)
+#  end
+
+#  next_node :per_hour_minimum_wage
+#end
+
+#value_question :how_many_pieces_do_you_produce_per_week? do
+#  save_input_as :pieces_per_week
+
+#  next_node :how_much_do_you_get_paid_per_piece?
+#end
+
+#value_question :how_much_do_you_get_paid_per_piece? do
+#  save_input_as :pay_per_piece
+
+#  next_node :how_many_hours_do_you_work_per_week?
+#end
+
+#value_question :how_many_hours_do_you_work_per_week? do
+#  save_input_as :hours_per_week
+
+#  calculate :hourly_wage do
+#    "%0.2f" % calculator.per_piece_hourly_wage(pay_per_piece, pieces_per_week, responses.last)
+#  end
+
+#  calculate :above_below do
+#    if calculator.is_below_minimum_wage?(age, pay_per_piece, pieces_per_week, responses.last)
+#      "below"
+#    else
+#      "above"
+#    end
+#  end
+
+#  next_node :per_piece_minimum_wage
+#end
 
 outcome :per_hour_minimum_wage
 outcome :per_piece_minimum_wage
--- test/integration/flows/am_i_getting_minimum_wage_test.rb
+++ test/integration/flows/am_i_getting_minimum_wage_test.rb
@@ -7,84 +7,116 @@ class AmIGettingMinimumWageTest < ActiveSupport::TestCase
   setup do
     setup_for_testing_flow "am-i-getting-minimum-wage"
   end
-
-  should "ask how you get paid" do
-    assert_current_node :how_do_you_get_paid?
+  
+  should "ask 'what would you like to check?'" do
+    assert_current_node :what_would_you_like_to_check?
   end
-
-  context "paid per hour" do
+  
+  context "when checking current pay" do
     setup do
-      add_response :per_hour
+      add_response :current_payment
     end
-
-    should "ask how old you are" do
-      assert_current_node :how_old_are_you?
+    
+    should "ask 'are you an apprentice?'" do
+      assert_current_node :are_you_an_apprentice?
     end
-
-    context "age provided" do
-      setup do
-        add_response "21_or_over"
-      end
-
-      should "ask how many hours you work per week" do
-        assert_current_node :how_many_hours_per_week_worked?
-      end
-
-      should "show minimum wage if hours per week are provided" do
-        add_response "40"
-        assert_current_node :per_hour_minimum_wage
-        assert_state_variable :per_hour_minimum_wage, "6.08"
-        assert_state_variable :hours_per_week, "40"
-        assert_state_variable :per_week_minimum_wage, "243.20"
+    
+    context "answered 'no' to 'are you an apprentice?'" do
+      should "ask 'how old are you?'" do
+        add_response :no
+        assert_current_node :how_old_are_you?
       end
     end
+    
+    context "answered 'apprentice under 19' to 'are you an apprentice?'" do
+      should "ask 'how often do you get paid?'" do
+        add_response :apprentice_under_19
+        assert_current_node :how_often_do_you_get_paid?
+      end
+    end
+    
+    context "answered 'apprentice over 19' to 'are you an apprentice?'" do
+      should "ask 'how often do you get paid?'" do
+        add_response :apprentice_over_19
+        assert_current_node :how_often_do_you_get_paid?
+      end
+    end
+    
   end
 
-  context "paid per piece" do
-    setup do
-      add_response :per_piece
-    end
+#  context "paid per hour" do
+#    setup do
+#      add_response :per_hour
+#    end
 
-    should "ask how old you are" do
-      assert_current_node :how_old_are_you?
-    end
+#    should "ask how old you are" do
+#      assert_current_node :how_old_are_you?
+#    end
 
-    context "age provided" do
-      setup do
-        add_response "21_or_over"
-      end
+#    context "age provided" do
+#      setup do
+#        add_response "21_or_over"
+#      end
 
-      should "ask how many pieces you produce per week" do
-        assert_current_node :how_many_pieces_do_you_produce_per_week?
-      end
+#      should "ask how many hours you work per week" do
+#        assert_current_node :how_many_hours_per_week_worked?
+#      end
 
-      context "number of pieces provided" do
-        setup do
-          add_response "10"
-        end
+#      should "show minimum wage if hours per week are provided" do
+#        add_response "40"
+#        assert_current_node :per_hour_minimum_wage
+#        assert_state_variable :per_hour_minimum_wage, "6.08"
+#        assert_state_variable :hours_per_week, "40"
+#        assert_state_variable :per_week_minimum_wage, "243.20"
+#      end
+#    end
+#  end
 
-        should "ask how much you get paid per piece" do
-          assert_current_node :how_much_do_you_get_paid_per_piece?
-        end
+#  context "paid per piece" do
+#    setup do
+#      add_response :per_piece
+#    end
 
-        context "pay per piece provided" do
-          setup do
-            add_response "30"
-          end
+#    should "ask how old you are" do
+#      assert_current_node :how_old_are_you?
+#    end
 
-          should "ask how many hours you work per week" do
-            assert_current_node :how_many_hours_do_you_work_per_week?
-          end
+#    context "age provided" do
+#      setup do
+#        add_response "21_or_over"
+#      end
 
-          should "show minimum wage if pay per piece is provided" do
-            add_response "40"
-            assert_current_node :per_piece_minimum_wage
-            assert_state_variable :hourly_wage, "7.50"
-            assert_state_variable :per_hour_minimum_wage, "6.08"
-            assert_state_variable :above_below, "above"
-          end
-        end
-      end
-    end
-  end
+#      should "ask how many pieces you produce per week" do
+#        assert_current_node :how_many_pieces_do_you_produce_per_week?
+#      end
+
+#      context "number of pieces provided" do
+#        setup do
+#          add_response "10"
+#        end
+
+#        should "ask how much you get paid per piece" do
+#          assert_current_node :how_much_do_you_get_paid_per_piece?
+#        end
+
+#        context "pay per piece provided" do
+#          setup do
+#            add_response "30"
+#          end
+
+#          should "ask how many hours you work per week" do
+#            assert_current_node :how_many_hours_do_you_work_per_week?
+#          end
+
+#          should "show minimum wage if pay per piece is provided" do
+#            add_response "40"
+#            assert_current_node :per_piece_minimum_wage
+#            assert_state_variable :hourly_wage, "7.50"
+#            assert_state_variable :per_hour_minimum_wage, "6.08"
+#            assert_state_variable :above_below, "above"
+#          end
+#        end
+#      end
+#    end
+#  end
 end
