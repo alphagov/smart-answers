bc27f2daf7de6670b3368a431fe7c7391a06989e
steventux <steve.laing@gmail.com>
2012-07-23 17:30:33 +0100

    Reworked the minimum wage calculator to suit new logic and historical wages and ranges

--- lib/smart_answer/calculators/minimum_wage_calculator.rb
+++ lib/smart_answer/calculators/minimum_wage_calculator.rb
@@ -3,32 +3,48 @@ module SmartAnswer::Calculators
   
     ACCOMMODATION_CHARGE_THRESHOLD = 4.73
     
-    def per_hour_minimum_wage(age)
-      case age
-        when "21_or_over"
-          6.08
-        when "18_to_20"
-          4.98
-        when "under_18"
-          3.68
-        when "under_19", "19_or_over"
-          2.6
-        else
-          raise "Invalid age [#{age}]"
+    HISTORICAL_MINIMUM_WAGES = {
+      "2012" => [3.68, 4.98, 6.08],
+      "2011" => [3.68, 4.98, 6.08],
+      "2010" => [3.64, 4.92, 5.93],
+      "2009" => [3.57, 4.83, 5.80],
+      "2008" => [3.53, 4.77, 5.73],
+      "2007" => [3.40, 4.60, 5.52],
+      "2006" => [3.30, 4.45, 5.35],
+      "2005" => [3.00, 4.25, 5.05]
+    }
+    
+    def per_hour_minimum_wage(age, year = Date.today.year)
+      wages = HISTORICAL_MINIMUM_WAGES[year.to_s]
+      if age < 18
+        wages[0]
+      # Before 2010 the mid-age range was 18 to 21, after 2010 it was 18 to 20
+      elsif age >= 18 and ((year < 2010 and age < 22) or (age < 21))
+        wages[1]
+      else
+        wages[2]
       end
+    end
+    
+    def apprentice_rate
+      2.6
     end
 
     def per_week_minimum_wage(age, hours_per_week)
       (hours_per_week.to_f * per_hour_minimum_wage(age)).round(2)
     end
 
-    def per_piece_hourly_wage(pay_per_piece, pieces_per_week, hours_per_week)
-      (pay_per_piece.to_f * pieces_per_week.to_f / hours_per_week.to_f).round(2)
+#    def per_piece_hourly_wage(pay_per_piece, pieces_per_week, hours_per_week)
+#      (pay_per_piece.to_f * pieces_per_week.to_f / hours_per_week.to_f).round(2)
+#    end
+
+    def is_below_minimum_wage?(age, total_hourly_rate)
+      total_hourly_rate.to_f < per_hour_minimum_wage(age)
     end
 
-    def is_below_minimum_wage?(age, pay_per_piece, pieces_per_week, hours_per_week)
-      per_piece_hourly_wage(pay_per_piece, pieces_per_week, hours_per_week).to_f < per_hour_minimum_wage(age)
-    end
+#    def is_below_minimum_wage?(age, pay_per_piece, pieces_per_week, hours_per_week)
+#      per_piece_hourly_wage(pay_per_piece, pieces_per_week, hours_per_week).to_f < per_hour_minimum_wage(age)
+#    end
     
     def accommodation_adjustment(charge, number_of_nights)
       charge = charge.to_f
--- test/unit/calculators/minimum_wage_calculator_test.rb
+++ test/unit/calculators/minimum_wage_calculator_test.rb
@@ -5,63 +5,21 @@ module SmartAnswer::Calculators
     def setup
       @calculator = MinimumWageCalculator.new
     end
-
-    context "paid per hour" do
-      should "calculate minimum wage for those aged 21 or over" do
-        assert_equal @calculator.per_week_minimum_wage("21_or_over", "120"), 729.6
-        assert_equal @calculator.per_week_minimum_wage("21_or_over", "20"), 121.6
+    
+    context "per hour minimum wage" do
+      should "give the minimum wage for this year for a given age" do
+        assert_equal 3.68, @calculator.per_hour_minimum_wage(17)
+        assert_equal 4.98, @calculator.per_hour_minimum_wage(18)
+        assert_equal 6.08, @calculator.per_hour_minimum_wage(21)
       end
-
-      should "calculate minimum wage for those aged 18 to 20" do
-        assert_equal @calculator.per_week_minimum_wage("18_to_20", "120"), 597.6
-        assert_equal @calculator.per_week_minimum_wage("18_to_20", "20"), 99.6
+      should "give the historical minimum wage for a given age and year" do
+        assert_equal 3.64, @calculator.per_hour_minimum_wage(17, 2010)
+        assert_equal 4.92, @calculator.per_hour_minimum_wage(18, 2010)
+        assert_equal 5.93, @calculator.per_hour_minimum_wage(21, 2010)
       end
-
-      should "calculate minimum wage for those aged under 18" do
-        assert_equal @calculator.per_week_minimum_wage("under_18", "120"), 441.6
-        assert_equal @calculator.per_week_minimum_wage("under_18", "20"), 73.6
-      end
-
-      should "calculate minimum wage for apprentices aged under 19" do
-        assert_equal @calculator.per_week_minimum_wage("under_19", "120"), 312
-        assert_equal @calculator.per_week_minimum_wage("under_19", "20"), 52
-      end
-
-      should "calculate minimum wage for apprentices aged 19 or over" do
-        assert_equal @calculator.per_week_minimum_wage("19_or_over", "120"), 312
-        assert_equal @calculator.per_week_minimum_wage("19_or_over", "20"), 52
-      end
-    end
-
-    context "paid per piece" do
-      should "calculate per hour pay" do
-        assert_equal @calculator.per_piece_hourly_wage("5", "25", "120"), 1.04
-        assert_equal @calculator.per_piece_hourly_wage("5", "25", "20"), 6.25
-      end
-
-      should "determine whether wage is below minimum wage for those aged 21 or over" do
-        assert @calculator.is_below_minimum_wage?("21_or_over", "5", "25", "120")
-        refute @calculator.is_below_minimum_wage?("21_or_over", "5", "25", "20")
-      end
-
-      should "determine whether wage is below minimum wage for those aged 18 to 20" do
-        assert @calculator.is_below_minimum_wage?("18_to_20", "5", "25", "120")
-        refute @calculator.is_below_minimum_wage?("18_to_20", "5", "25", "20")
-      end
-
-      should "determine whether wage is below minimum wage for those aged under 18" do
-        assert @calculator.is_below_minimum_wage?("under_18", "5", "25", "120")
-        refute @calculator.is_below_minimum_wage?("under_18", "5", "25", "20")
-      end
-
-      should "determine whether wage is below minimum wage for apprentices aged under 19" do
-        assert @calculator.is_below_minimum_wage?("under_19", "5", "25", "120")
-        refute @calculator.is_below_minimum_wage?("under_19", "5", "25", "20")
-      end
-
-      should "determine whether wage is below minimum wage for apprentices aged 19 and over" do
-        assert @calculator.is_below_minimum_wage?("19_or_over", "5", "25", "120")
-        refute @calculator.is_below_minimum_wage?("19_or_over", "5", "25", "20")
+      should "account for the 18-22 age range before 2010" do
+        assert_equal 4.83, @calculator.per_hour_minimum_wage(21, 2009)
+        assert_equal 5.8, @calculator.per_hour_minimum_wage(22, 2009)
       end
     end
     
@@ -80,7 +38,7 @@ module SmartAnswer::Calculators
         number_of_nights = 5
         free_adjustment = (@th * number_of_nights).round(2) 
         charged_adjustment = @calculator.accommodation_adjustment(charge, number_of_nights)
-        assert_equal free_adjustment - (charge * number_of_nights).round(2), charged_adjustment
+        assert_equal (free_adjustment - (charge * number_of_nights)).round(2), charged_adjustment
         assert 0 > charged_adjustment # this should always be less than zero
       end
     end
