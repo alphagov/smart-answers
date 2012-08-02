eeea54dde45f6138ab7ffd04dd2c45def3adc304
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-08-02 09:47:09 +0100

    Class stubs for childcare tax credit smart answer

---
+++ lib/flows/childcare-costs-for-tax-credits.rb
@@ -0,0 +1,15 @@
+satisfies_need "9999"
+status :draft
+section_slug "money-and-tax"
+subsection_slug "tax"
+
+# Q1
+multiple_choice :first_time_claim? do
+end
+
+
+# Q2
+
+# Q3
+
+# Q4
---
+++ test/integration/flows/childcare_costs_for_tax_credits_test.rb
@@ -0,0 +1,17 @@
+# encoding: UTF-8
+require_relative '../../test_helper'
+require_relative 'flow_test_helper'
+
+class ChildcareCostsForTaxCreditsTest < ActiveSupport::TestCase
+  include FlowTestHelper
+  
+  context "this" do
+    setup do
+    end
+    
+    should "be true" do
+      assert true
+    end
+  end
+  
+end
