a1bffd070bf34c7416096aada5f25a61904d0cef
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-08-02 12:13:42 +0100

    Flow tests up to Question 5, merged with calculator work

--- lib/flows/childcare-costs-for-tax-credits.rb
+++ lib/flows/childcare-costs-for-tax-credits.rb
@@ -3,13 +3,135 @@ status :draft
 section_slug "money-and-tax"
 subsection_slug "tax"
 
+# Questions
+#
+
 # Q1
 multiple_choice :first_time_claim? do
+  option :yes => :frequency_of_childcare?
+  option :no => :have_the_costs_changed?
 end
 
-
 # Q2
+multiple_choice :frequency_of_childcare? do
+  option :regularly_less_than_a_year => :how_often_do_you_pay?
+  option :regularly_more_than_a_year => :do_you_pay_the_same_every_time?
+  option :intermittently => :call_the_helpline # A19
+end
 
 # Q3
+multiple_choice :have_the_costs_changed? do
+  option :yes => :how_often_and_what_do_you_pay_your_providers? # Q7
+  option :no => :no_change_to_credits # A20
+end
 
 # Q4
+multiple_choice :how_often_do_you_pay? do
+  option :same_amount_weekly => :round_up_total # A1
+  option :varying_amount_weekly => :costs_for_year_in_weeks? # C1
+  option :same_monthly => :how_much_do_you_pay_each_month? # C3
+  option :varying_amount_monthly => :costs_for_year_in_months? # C4
+  option :other => :costs_for_year_in_months? # C2 
+end
+
+# Q5
+multiple_choice :do_you_pay_the_same_every_time? do
+  option :yes => :how_often_do_you_pay_your_providers? # Q6
+  option :no => :varying_annual_cost? # C9
+end
+
+# Q6
+multiple_choice :how_often_do_you_pay_your_providers? do
+  option :weekly => :round_up_total # A6
+  option :fornightly => :how_much_do_you_pay_each_fortnight? # C5
+  option :every_four_weeks => :how_much_do_you_pay_every_four_weeks? # C6
+  option :monthly => :how_much_do_you_pay_each_month? # C7
+  option :termly => :contact_the_tax_credit_office # A10
+  option :yearly => :how_much_do_you_pay_anually? # C8
+  option :other => :contact_the_tax_credit_office # A12
+end
+
+# Q7
+multiple_choice :how_often_and_what_do_you_pay_your_providers? do
+#  option :same_amount_weekly => # C10
+#  option :varying_amount_weekly => # C11
+#  option :same_monthly => # C13
+#  option :varying_amount_monthly => # C14
+#  option :other => # C12
+end
+
+# Calculation Questions
+#
+
+# C1
+value_question :costs_for_year_in_weeks? do
+  
+end
+
+# C2, C4
+value_question :costs_for_year_in_months? do
+  
+end
+
+# C3, C7
+value_question :how_much_do_you_pay_each_month? do
+  
+end
+
+# C5
+value_question :how_much_do_you_pay_each_fortnight? do
+  
+end
+
+# C6
+value_question :how_much_do_you_pay_every_four_weeks? do
+  
+end
+
+# C8
+value_question :how_much_do_you_pay_anually? do
+  
+end
+
+# C9
+value_question :varying_annual_cost? do
+  
+end
+
+# C10A
+value_question :old_weekly_costs? do
+  
+end
+
+# C10B
+value_question :new_weekly_costs? do
+  
+end
+
+# C11A, C12A
+value_question :old_annual_costs? do
+  
+end
+
+# C11B, C12B
+value_question :new_annual_costs? do
+  
+end
+
+# C13A
+value_question :old_average_weekly_costs? do
+  
+end
+
+# C13B
+value_question :new_average_weekly_costs? do
+  
+end
+
+outcome :round_up_total # A1, A6
+outcome :weekly_costs # A2, A3, A4, A5
+outcome :weekly_costs_for_claim_form # A7, A8, A9, A11, A13
+outcome :contact_the_tax_credit_office # A10, A12
+outcome :costs_have_increased # A14, A15, A16, A17, A18
+outcome :call_the_helpline # A19
+outcome :no_change_to_credits # A20
