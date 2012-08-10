e31ba2e5b7bec30be2a78a9fa748ae3db86f4209
John Griffin <johnog@gmail.com>
2012-08-10 10:40:41 +0000

    Add initial question and answer

--- lib/flows/auto-enrolled-into-workplace-pension.rb
+++ lib/flows/auto-enrolled-into-workplace-pension.rb
@@ -4,4 +4,11 @@ subsection_slug "pension"
 satisfies_need "99999"
 
 multiple_choice :work_in_uk? do
+  option :yes => :self_employed?
+  option :no => :not_enrolled
 end
+
+multiple_choice :self_employed? do
+end
+
+outcome :not_enrolled
--- test/integration/flows/auto_enrolled_into_workplace_pension_test.rb
+++ test/integration/flows/auto_enrolled_into_workplace_pension_test.rb
@@ -12,4 +12,21 @@ class AutoEnrolledIntoWorkplacePensionTest < ActiveSupport::TestCase
   should "ask if you work in the UK" do
     assert_current_node :work_in_uk?
   end
+
+  context "does not work in the UK" do
+    should "not be enrolled in pension" do
+      add_response :no
+      assert_current_node :not_enrolled
+    end
+  end
+
+  context "works in the UK" do
+    setup do
+      add_response :yes
+    end
+
+    should "ask if self employed" do
+      assert_current_node :self_employed?
+    end
+  end
 end
