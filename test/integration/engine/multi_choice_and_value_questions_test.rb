b6920bd6afcfcddfd4a99a9649592bd95b520dd1
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-08-13 13:10:46 +0100

    Fix trailing slash in form URL for first question.
    This was causing problems in production where Varnish would strip the
    trailing /, and return a 301 redirect.  That doesn't work for a POST...

--- test/integration/engine/multi_choice_and_value_questions_test.rb
+++ test/integration/engine/multi_choice_and_value_questions_test.rb
@@ -32,6 +32,11 @@ class MultiChoiceAndValudQuestionsTest < EngineIntegrationTest
 
       assert_current_url "/bridge-of-death/y"
 
+      # This is asserting that the form URL doesn't get created with a trailing /
+      # If this happens, the cache servers strip off the / and redirect.  This breaks things.
+      form = page.find(:xpath, "id('content')//form")
+      assert_equal "/bridge-of-death/y", form[:action]
+
       assert page.has_xpath?("//meta[@name = 'robots'][@content = 'noindex']")
 
       within '.current-question' do
