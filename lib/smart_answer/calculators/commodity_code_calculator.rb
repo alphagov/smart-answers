e286aef1f670a77d2dcf8cdc20f9cc0dec960bd6
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-08-29 14:13:23 +0000

    WIP matrix lookup pseudo code

--- lib/smart_answer/calculators/commodity_code_calculator.rb
+++ lib/smart_answer/calculators/commodity_code_calculator.rb
@@ -1,11 +1,14 @@
 module SmartAnswer::Calculators
   class CommodityCodeCalculator
     
-    attr_reader :matrix_data
+    attr_reader :matrix_data, :commodity_code_matrix
     
     def initialize(weights)
       load_maxtrix_data
-#      ["starch_glucose_weight", "sucrsoe_weight", "milk_fat_weight", "milk_protein_weight"].each do |n|
+      populate_commodity_code_matrix
+      
+      puts commodity_code_matrix.first
+#      ["starch_glucose_weight", "sucrose_weight", "milk_fat_weight", "milk_protein_weight"].each do |n|
 #        send("@#{n}=", weights[n.to_sym].to_i)
 #      end
       @starch_glucose_weight = weights[:starch_glucose_weight].to_i
@@ -14,9 +17,15 @@ module SmartAnswer::Calculators
       @milk_protein_weight = weights[:milk_protein_weight].to_i
     end
     
-#    def commodity_code
-#      
-#    end
+    def commodity_code
+      glucose_sucrose_index = starch_glucose_to_sucrose[@starch_glucose_weight][@sucrose_weight]
+      milk_fat_milk_protein_index = milk_fat_to_milk_protein[@milk_fat_weight][@milk_protein_weight]
+      @commodity_code_matrix[glucose_sucrose_index][milk_fat_milk_protein_index].to_s     
+    end
+    
+    def populate_commodity_code_matrix
+      @commodity_code_matrix = @matrix_data[:commodity_code_matrix].lines.map { |l| l.split }
+    end
     
     def starch_glucose_to_sucrose
       @matrix_data[:starch_glucose_to_sucrose]
--- test/unit/calculators/commodity_code_calculator_test.rb
+++ test/unit/calculators/commodity_code_calculator_test.rb
@@ -11,6 +11,14 @@ module SmartAnswer::Calculators
         assert_equal Hash, @calculator.matrix_data[:starch_glucose_to_sucrose].class
         assert_equal 2, @calculator.matrix_data[:starch_glucose_to_sucrose][0][30]
       end
+      
+      context "populate_commodity_code_matrix method" do
+        should "build a 2D array of commodity code suffixes" do
+#          puts @matrix_data[:commodity_code_matrix].lines
+#          assert_equal "030", @calculator.commodity_code_matrix[1][10]
+#          # assert_equal "000", @calculator.commodity_code_matrix[0][20]
+        end
+      end
     end
   end
 end
