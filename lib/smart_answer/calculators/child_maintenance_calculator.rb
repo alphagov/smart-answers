e1c4cdd8a5191f10ce4785850b562d5bdc64b48e
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-08-30 11:33:04 +0000

    Reduced and basic rate calculations added

--- lib/smart_answer/calculators/child_maintenance_calculator.rb
+++ lib/smart_answer/calculators/child_maintenance_calculator.rb
@@ -25,8 +25,15 @@ module SmartAnswer::Calculators
       [0.19,  0.27,  0.375],
       [0.175, 0.25,  0.325]
     ]
+    BASIC_RATE_MULTIPLIERS = [
+      [0.12,  0.16,  0.19],
+      [0.12,  0.16,  0.19],
+      [0.131, 0.198, 0.249],
+      [0.124, 0.189, 0.237]
+    ]
     
     SHARED_CARE_REDUCTIONS = [0, 0.14, 0.28, 0.42, 0.5]
+    RELEVANT_OTHER_CHILD_REDUCTIONS = [0, 0.12, 0.16, 0.19]
     
     attr_reader :calculation_scheme
     attr_accessor :net_income, :number_of_other_children, :number_of_shared_care_nights
@@ -41,18 +48,39 @@ module SmartAnswer::Calculators
     end
     
     def calculate_reduced_rate_payment
-      reduced_rate = ((@net_income_of_payee - 100) * reduced_rate_mulitplier) + 7
+      reduced_rate = ((@net_income - 100) * reduced_rate_mulitplier) + 7
       (reduced_rate - (reduced_rate * shared_care_multiplier)).round(2)
     end
     
+    def calculate_basic_rate_payment
+      basic_rate = @net_income - (@net_income * relevant_other_child_multiplier)
+      basic_rate = (basic_rate * basic_rate_multiplier)
+      (basic_rate - (basic_rate * shared_care_multiplier)).round(2)
+    end
+    
     def reduced_rate_mulitplier
-      number_of_qualifying_children = @number_of_children > 3 ? 3 : @number_of_children
-      number_of_other_children = @number_of_other_children > 3 ? 3 : @number_of_other_children
-      REDUCED_RATE_MULTIPLIERS[number_of_other_children][number_of_qualifying_children - 1]
+      REDUCED_RATE_MULTIPLIERS[number_of_other_children_index][number_of_qualifying_children_index]
+    end
+    
+    def basic_rate_multiplier
+      BASIC_RATE_MULTIPLIERS[number_of_other_children_index][number_of_qualifying_children_index]
     end
     
     def shared_care_multiplier
       SHARED_CARE_REDUCTIONS[@number_of_shared_care_nights]
     end
+    
+    def relevant_other_child_multiplier
+      RELEVANT_OTHER_CHILD_REDUCTIONS[number_of_other_children_index]
+    end
+
+    def number_of_other_children_index
+      @number_of_other_children > 3 ? 3 : @number_of_other_children
+    end
+    
+    def number_of_qualifying_children_index
+      (@number_of_children > 3 ? 3 : @number_of_children) - 1
+    end
+
   end
 end
--- test/unit/calculators/child_maintenance_calculator_test.rb
+++ test/unit/calculators/child_maintenance_calculator_test.rb
@@ -17,24 +17,47 @@ module SmartAnswer::Calculators
         assert_equal :flat, @calculator.rate_type
       end
     end
-    context "new scheme calculations" do
-      setup do
-        @calculator = ChildMaintenanceCalculator.new(5)
-        @calculator.net_income = 173
-      end 
-      context "reduced_rate_mulitplier method" do
-        should "give the multiplier to use when calculating the new reduced rate" do
-          @calculator.number_of_other_children = 2
-          assert_equal 0.375, @calculator.reduced_rate_mulitplier
-          @calculator.number_of_other_children = 1
-          assert_equal 0.37, @calculator.reduced_rate_mulitplier
-        end
+#    context "new scheme calculations" do
+#      setup do
+#        @calculator = ChildMaintenanceCalculator.new(5)
+#        @calculator.net_income = 173.00
+#      end 
+#      context "reduced_rate_mulitplier method" do
+#        should "give the multiplier to use when calculating the new reduced rate" do
+#          @calculator.number_of_other_children = 2
+#          assert_equal 0.375, @calculator.reduced_rate_mulitplier
+#          @calculator.number_of_other_children = 1
+#          assert_equal 0.37, @calculator.reduced_rate_mulitplier
+#        end
+#      end
+#      context "shared_care_multiplier method" do
+#        should "give the multiplier to use when adjusting the new reduced rate" do
+#          @calculator.number_of_shared_care_nights = 2
+#          assert_equal 0.28, @calculator.shared_care_multiplier
+#        end
+#      end
+#    end
+    context "calculate_reduced_rate_payment method" do
+      should "give the reduced rate payment total" do
+        @calculator = ChildMaintenanceCalculator.new(2)
+        @calculator.net_income = 173.00
+        @calculator.number_of_other_children = 1
+        @calculator.number_of_shared_care_nights = 1
+        assert_equal 0.29, @calculator.reduced_rate_mulitplier
+        assert_equal 0.14, @calculator.shared_care_multiplier
+        assert_equal 24.23, @calculator.calculate_reduced_rate_payment
       end
-      context "shared_care_multiplier method" do
-        should "give the multiplier to use when adjusting the new reduced rate" do
-          @calculator.number_of_shared_care_nights = 2
-          assert_equal 0.28, @calculator.shared_care_multiplier
-        end
+    end
+    context "calculate_basic_rate_payment method" do
+      should "give the reduced rate payment total" do
+        @calculator = ChildMaintenanceCalculator.new(1)
+        @calculator.net_income = 307.00
+        @calculator.number_of_other_children = 2
+        @calculator.number_of_shared_care_nights = 2
+        assert_equal 0.16, @calculator.relevant_other_child_multiplier
+        assert_equal 0.131, @calculator.basic_rate_multiplier
+        assert_equal 0.28, @calculator.shared_care_multiplier
+        assert_equal 24.32, @calculator.calculate_basic_rate_payment
       end
     end
   end
