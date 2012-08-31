c73b9c0ebbe300271ee6ff54b53261b78d9c75fc
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-08-31 11:02:16 +0000

    Flow logic added

--- lib/flows/additional-commodity-code.rb
+++ lib/flows/additional-commodity-code.rb
@@ -33,6 +33,9 @@ multiple_choice :how_much_sucrose_1? do
   option 30
   option 50
   option 75
+
+  save_input_as :sucrose_weight  
+  next_node :how_much_milk_fat?
 end
 
 # Q2c
@@ -41,6 +44,9 @@ multiple_choice :how_much_sucrose_2? do
   option 5
   option 30
   option 50
+
+  save_input_as :sucrose_weight  
+  next_node :how_much_milk_fat?
 end
 
 # Q2d
@@ -48,11 +54,137 @@ multiple_choice :how_much_sucrose_3? do
   option 0
   option 5
   option 30
+
+  save_input_as :sucrose_weight  
+  next_node :how_much_milk_fat?
 end
 
 # Q2e
 multiple_choice :how_much_sucrose_4? do
   option 0
   option 5
+
+  save_input_as :sucrose_weight  
+  next_node :how_much_milk_fat?
 end
 
+# Q3
+multiple_choice :how_much_milk_fat? do
+  option 0
+  option 1.5
+  option 3
+  option 6
+  option 9
+  option 12
+  option 18
+  option 26
+  option 40
+  option 55
+  option 70
+  option 85
+  
+  calculate :calculator do
+    Calculators::CommodityCodeCalculator.new(
+      starch_glucose_weight: starch_glucose_weight,
+      sucrose_weight: sucrose_weight,
+      milk_fat_weight: responses.last,
+      milk_protein_weight: 0)
+  end
+  
+  calculate :commodity_code do
+    calculator.commodity_code
+  end
+  
+  save_input_as :milk_fat_weight
+  
+  next_node do |response|
+    case response.to_i
+      when 0
+      when 1.5
+        :how_much_milk_protein_ab?
+      when 3
+        :how_much_milk_protein_c?
+      when 6
+        :how_much_milk_protein_d?
+      when 9
+      when 12
+        :how_much_milk_protein_ef?
+      when 18
+      when 26
+        :how_much_milk_protein_gh?
+      else
+        :commodity_code_result
+      end
+  end
+end
+
+# Q3ab
+multiple_choice :how_much_milk_protein_ab? do
+  option 0
+  option 2.5
+  option 6
+  option 18
+  option 30
+  option 60
+  
+  calculate :commodity_code do
+    calculator.milk_protein_weight = responses.last.to_i
+    calculator.commodity_code
+  end
+  
+  next_node :commodity_code_result
+end
+
+# Q3c
+multiple_choice :how_much_milk_protein_c? do
+  option 0
+  option 2.5
+  option 12
+
+  calculate :commodity_code do
+    calculator.milk_protein_weight = responses.last.to_i
+    calculator.commodity_code
+  end
+
+  next_node :commodity_code_result
+end
+
+# Q3d
+multiple_choice :how_much_milk_protein_d? do
+  option 0
+  option 4
+  option 15
+  
+  calculate :commodity_code do
+    calculator.milk_protein_weight = responses.last.to_i
+    calculator.commodity_code
+  end
+  next_node :commodity_code_result
+end
+
+# Q3ef
+multiple_choice :how_much_milk_protein_ef? do
+  option 0
+  option 6
+  option 18
+  
+  calculate :commodity_code do
+    calculator.milk_protein_weight = responses.last.to_i
+    calculator.commodity_code
+  end
+  next_node :commodity_code_result
+end
+
+# Q3gh
+multiple_choice :how_much_milk_protein_gh? do
+  option 0
+  option 6
+  
+  calculate :commodity_code do
+    calculator.milk_protein_weight = responses.last.to_i
+    calculator.commodity_code
+  end
+  next_node :commodity_code_result
+end
+
+outcome :commodity_code_result
--- test/integration/flows/additional_commodity_code_test.rb
+++ test/integration/flows/additional_commodity_code_test.rb
@@ -7,4 +7,93 @@ class AdditionalCommodityCodeTest < ActiveSupport::TestCase
   setup do
     setup_for_testing_flow "additional-commodity-code"
   end
+  ## Q1
+  should "ask how much starch glucose the product contains" do
+    assert_current_node :how_much_starch_glucose?
+  end
+  
+  context "answer 25" do
+    ## Q2c
+    should "ask how much sucrose the product contains" do
+      add_response 25
+      assert_state_variable "starch_glucose_weight", "25"
+      assert_current_node :how_much_sucrose_2?
+    end
+  end
+  context "answer 50" do
+    ## Q2d
+    should "ask how much sucrose the product contains" do
+      add_response 50
+      assert_state_variable "starch_glucose_weight", "50"
+      assert_current_node :how_much_sucrose_3?
+    end
+  end
+  context "answer 75" do
+    ## Q2e
+    should "ask how much sucrose the product contains" do
+      add_response 75
+      assert_state_variable "starch_glucose_weight", "75"
+      assert_current_node :how_much_sucrose_4?
+    end
+  end
+  context "answer 5" do
+    setup do
+      add_response 5
+    end
+    ## Q2ab
+    should "ask how much sucrose the product contains" do
+      assert_state_variable "starch_glucose_weight", "5"
+      assert_current_node :how_much_sucrose_1?
+    end
+    context "answer 30" do
+      setup do
+        add_response 30
+      end
+      ## Q3
+      should "save the input" do
+        assert_state_variable "sucrose_weight", "30"
+      end
+      should "ask how much milk fat the product contains" do
+        assert_current_node :how_much_milk_fat?
+      end
+      context "answer 40, 55, 70 or 85" do
+        should "create a calculator and lookup the commodity code" do
+          add_response 55
+          assert_state_variable "commodity_code", "747"
+        end
+        should "give the commodity code result" do
+          add_response 40
+          assert_current_node :commodity_code_result
+        end
+        should "give the commodity code result" do
+          add_response 55
+          assert_current_node :commodity_code_result
+        end
+        should "give the commodity code result" do
+          add_response 70
+          assert_current_node :commodity_code_result
+        end
+        should "give the commodity code result" do
+          add_response 85
+          assert_current_node :commodity_code_result
+        end
+      end
+      context "answer 3" do
+        setup do
+          add_response 3
+        end
+        ## Q3c
+        should "ask how much milk protein the product contains" do
+          assert_current_node :how_much_milk_protein_c?
+        end
+        context "answer 12" do
+          should "give the commodity code result" do
+            add_response 12
+            assert_current_node :commodity_code_result
+            assert_state_variable "commodity_code", "267"
+          end
+        end
+      end
+    end
+  end
 end
