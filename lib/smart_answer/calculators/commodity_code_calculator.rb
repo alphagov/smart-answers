dad1d8b7856034dff0d219a596b7f01739e6b519
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-08-31 11:01:53 +0000

    Basic lookup of commodity codes

--- lib/smart_answer/calculators/commodity_code_calculator.rb
+++ lib/smart_answer/calculators/commodity_code_calculator.rb
@@ -2,15 +2,16 @@ module SmartAnswer::Calculators
   class CommodityCodeCalculator
     
     attr_reader :matrix_data, :commodity_code_matrix
+    attr_accessor :milk_protein_weight
     
     def initialize(weights)
       load_maxtrix_data
       populate_commodity_code_matrix
       
-      puts commodity_code_matrix.first
 #      ["starch_glucose_weight", "sucrose_weight", "milk_fat_weight", "milk_protein_weight"].each do |n|
 #        send("@#{n}=", weights[n.to_sym].to_i)
 #      end
+
       @starch_glucose_weight = weights[:starch_glucose_weight].to_i
       @sucrose_weight = weights[:sucrose_weight].to_i
       @milk_fat_weight = weights[:milk_fat_weight].to_i
@@ -18,13 +19,22 @@ module SmartAnswer::Calculators
     end
     
     def commodity_code
-      glucose_sucrose_index = starch_glucose_to_sucrose[@starch_glucose_weight][@sucrose_weight]
-      milk_fat_milk_protein_index = milk_fat_to_milk_protein[@milk_fat_weight][@milk_protein_weight]
-      @commodity_code_matrix[glucose_sucrose_index][milk_fat_milk_protein_index].to_s     
+      # puts "@commodity_code_matrix[#{milk_fat_milk_protein_index}][#{glucose_sucrose_index}] : #{@commodity_code_matrix[milk_fat_milk_protein_index][glucose_sucrose_index]}"
+      @commodity_code_matrix[milk_fat_milk_protein_index][glucose_sucrose_index]  
+    end
+    
+    def glucose_sucrose_index
+      starch_glucose_to_sucrose[@starch_glucose_weight][@sucrose_weight]
+    end
+    
+    def milk_fat_milk_protein_index
+      milk_fat_to_milk_protein[@milk_fat_weight][@milk_protein_weight]
     end
     
     def populate_commodity_code_matrix
-      @commodity_code_matrix = @matrix_data[:commodity_code_matrix].lines.map { |l| l.split }
+      # TODO: Find an elegant way of doing this...
+      @commodity_code_matrix = []
+      @matrix_data[:commodity_code_matrix].each_line { |l| @commodity_code_matrix << l.split }
     end
     
     def starch_glucose_to_sucrose
