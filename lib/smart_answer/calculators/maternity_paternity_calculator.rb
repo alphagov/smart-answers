a519549bbf150a4e087b5abc17d4a15a652c2b8f
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-09-03 15:03:56 +0000

    Instance variables for calculating outcomes added to calculator

--- lib/smart_answer/calculators/maternity_paternity_calculator.rb
+++ lib/smart_answer/calculators/maternity_paternity_calculator.rb
@@ -1,15 +1,21 @@
 module SmartAnswer::Calculators
   class MaternityPaternityCalculator
   
-    attr_reader :expected_week, :qualifying_week, :employment_start
-  
+    attr_reader :expected_week, :qualifying_week, :employment_start, :notice_of_leave_deadline, :max_leave_start_date
+    attr_accessor :employment_contract, :leave_start_date
+    
     def initialize(due_date)
       @due_date = due_date
       expected_start = due_date - due_date.wday
       @expected_week = expected_start .. expected_start + 6.days
-      qualifying_start = 15.weeks.ago(expected_start)
+      @notice_of_leave_deadline = qualifying_start = 15.weeks.ago(expected_start)
       @qualifying_week = qualifying_start .. qualifying_start + 6.days
       @employment_start = 26.weeks.ago(expected_start)
+      @max_leave_start_date = 11.weeks.ago(due_date)
+    end
+    
+    def leave_end_date
+      52.weeks.since(@leave_start_date)
     end
   end
 end
