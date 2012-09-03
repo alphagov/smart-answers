1181a382e83cfe4faa4841c7ea0f1b9b3dfebd01
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-09-03 15:59:00 +0000

    Proof of pregnancy instance var added

--- lib/smart_answer/calculators/maternity_paternity_calculator.rb
+++ lib/smart_answer/calculators/maternity_paternity_calculator.rb
@@ -1,8 +1,11 @@
 module SmartAnswer::Calculators
   class MaternityPaternityCalculator
   
-    attr_reader :expected_week, :qualifying_week, :employment_start, :notice_of_leave_deadline, :max_leave_start_date
-    attr_accessor :employment_contract, :leave_start_date
+    attr_reader :expected_week, :qualifying_week, :employment_start, :notice_of_leave_deadline, 
+      :max_leave_start_date, :proof_of_pregnancy_date
+    attr_accessor :employment_contract, :leave_start_date, :average_weekly_earnings
+    
+    LOWER_EARNING_LIMITS = { 2011 => 102, 2012 => 107 }
     
     def initialize(due_date)
       @due_date = due_date
@@ -12,10 +15,35 @@ module SmartAnswer::Calculators
       @qualifying_week = qualifying_start .. qualifying_start + 6.days
       @employment_start = 26.weeks.ago(expected_start)
       @max_leave_start_date = 11.weeks.ago(due_date)
+      @proof_of_pregnancy_date = 13.weeks.ago(due_date)
     end
     
     def leave_end_date
       52.weeks.since(@leave_start_date)
+    end
+    
+    def pay_start_date
+      @leave_start_date
+    end
+    
+    def pay_end_date
+      39.weeks.since(pay_start_date)
+    end
+    
+    def statutory_pay_rate_a
+      
+    end
+    
+    def statutory_maternity_pay_a
+      ((@average_weekly_earnings.to_f * 0.9) * 6).round(2)
+    end
+    
+    def statutory_maternity_pay_b
+      
+    end
+    
+    def lower_earning_limit(year=Date.today.year)
+      LOWER_EARNING_LIMITS[year]
     end
   end
 end
--- test/unit/calculators/maternity_paternity_calculator_test.rb
+++ test/unit/calculators/maternity_paternity_calculator_test.rb
@@ -5,9 +5,9 @@ module SmartAnswer::Calculators
     
     context MaternityPaternityCalculator do
       setup do
-        @four_months_from_now = 4.months.since(Date.today)
-        @start_of_week_in_four_months = @four_months_from_now - @four_months_from_now.wday
-        @calculator = MaternityPaternityCalculator.new(@four_months_from_now)
+        @due_date = 4.months.since(Date.today)
+        @start_of_week_in_four_months = @due_date - @due_date.wday
+        @calculator = MaternityPaternityCalculator.new(@due_date)
       end
       
       should "calculate expected birth week" do
@@ -27,16 +27,29 @@ module SmartAnswer::Calculators
       end
       
       should "calculate the earliest leave start date" do
-        assert_equal 11.weeks.ago(@four_months_from_now), @calculator.max_leave_start_date
+        assert_equal 11.weeks.ago(@due_date), @calculator.max_leave_start_date
+      end
+      
+      should "calculate the proof of pregnancy deadline" do
+        assert_equal 13.weeks.ago(@due_date), @calculator.proof_of_pregnancy_date
       end
       
       context "with a requested leave date in one month's time" do
         setup do
-          @calculator.leave_start_date = 1.month.since(Date.today)
+          @leave_start_date = 1.month.since(Date.today)
+          @calculator.leave_start_date = @leave_start_date
         end
         
         should "make the leave end date 52 weeks from the leave start date" do
-          assert_equal 52.weeks.since(@calculator.leave_start_date), @calculator.leave_end_date
+          assert_equal 52.weeks.since(@leave_start_date), @calculator.leave_end_date
+        end
+        
+        should "make the pay start date the same date" do
+          assert_equal @leave_start_date, @calculator.pay_start_date
+        end
+        
+        should "make the pay end date 39 weeks from the start date" do
+          assert_equal 39.weeks.since(@leave_start_date), @calculator.pay_end_date
         end
       end
       
