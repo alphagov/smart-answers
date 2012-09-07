9d1567f2df5176e924eee28f10e9a6dd08d699e8
Robin Mayfield <robin.mayfield@digital.cabinet-office.gov.uk>
2012-09-07 17:10:59 +0200

    request-for-flexible-working copy and logic changes

--- lib/flows/request-for-flexible-working.rb
+++ lib/flows/request-for-flexible-working.rb
@@ -2,47 +2,91 @@ status :draft
 section_slug "work"
 satisfies_need "9999"
 
-checkbox_question :tick_boxes_that_apply? do
-  option :employee
-  option :continuous_employment
-  option :not_applied_for_flexible_working
-  option :agency_worker
-  option :armed_forces
-
-  next_node do |response|
-    options = response.split(',')
-    if options.sort == %w(continuous_employment employee not_applied_for_flexible_working)
-      :caring_for_child?
-    else
-      :no_right_to_apply
-    end
+## Q0
+multiple_choice :member_of_armed_services? do
+  calculate :no_right_to_apply_reason do
+    PhraseList.new :armed_services_reason
   end
+  option :yes => :no_right_to_apply
+  option :no => :are_you_employee?
 end
 
+## Q0.1
+multiple_choice :are_you_employee? do
+  calculate :no_right_to_apply_reason do
+    PhraseList.new :employee_reason
+  end
+  option :yes => :applied_for_flexible_working?
+  option :no => :no_right_to_apply
+end
+
+## Q0.2
+multiple_choice :applied_for_flexible_working? do
+  calculate :no_right_to_apply_reason do
+    PhraseList.new :statutory_request_reason
+  end
+  option :yes => :no_right_to_apply
+  option :no => :caring_for_child?
+end
+
+
+
+## Q1
+# checkbox_question :employment_status? do
+#   option :employee
+#   option :continuous_employment
+#   option :not_applied_for_flexible_working
+#   option :agency_worker
+#   option :armed_forces
+
+#   next_node do |response|
+#     options = response.split(',')
+#     if options.sort == %w(continuous_employment employee not_applied_for_flexible_working)
+#       :caring_for_child?
+#     else
+#       :no_right_to_apply
+#     end
+#   end
+# end
+
+## Q2
 multiple_choice :caring_for_child? do
+  calculate :no_right_to_apply_reason do
+    PhraseList.new :child_care_reason
+  end
   option :caring_for_child => :relationship_with_child?
   option :caring_for_adult => :relationship_with_adult?
   option :neither => :no_right_to_apply
 end
 
-checkbox_question :relationship_with_child? do
-  option :relationship
-  option :responsible_for_upbringing
-
-  next_node do |response|
-    options = response.split(',')
-    if options.sort == %w(relationship responsible_for_upbringing)
-      :right_to_apply
-    else
-      :no_right_to_apply
-    end
+## Q3
+multiple_choice :relationship_with_child? do
+  calculate :no_right_to_apply_reason do
+    PhraseList.new :wrong_relationship_reason
   end
+  option :yes => :responsible_for_upbringing? 
+  option :no => :no_right_to_apply
 end
 
+## Q4
 multiple_choice :relationship_with_adult? do
+  calculate :no_right_to_apply_reason do
+    PhraseList.new :wrong_relationship_reason
+  end
   option :one_of_these => :right_to_apply
   option :not_one_of_these => :no_right_to_apply
 end
 
+## Q5 
+multiple_choice :responsible_for_upbringing? do
+  calculate :no_right_to_apply_reason do
+    PhraseList.new :upbringing_reason
+  end
+  option :yes => :right_to_apply
+  option :no => :no_right_to_apply
+end
+
+## A1
 outcome :no_right_to_apply
+## A2
 outcome :right_to_apply
