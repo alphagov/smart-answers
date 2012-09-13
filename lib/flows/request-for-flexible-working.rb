09ef28e7116ec6b48c4c7955e3e56e5e81f8513c
Robin Mayfield <robin.mayfield@digital.cabinet-office.gov.uk>
2012-09-13 11:23:12 +0200

    logic and comments now match updated documetation; old code removed

--- lib/flows/request-for-flexible-working.rb
+++ lib/flows/request-for-flexible-working.rb
@@ -2,7 +2,7 @@ status :draft
 section_slug "work"
 satisfies_need "9999"
 
-## Q0
+## Q1
 multiple_choice :member_of_armed_services? do
   calculate :no_right_to_apply_reason do
     PhraseList.new :armed_services_reason
@@ -11,7 +11,7 @@ multiple_choice :member_of_armed_services? do
   option :no => :are_you_employee?
 end
 
-## Q0.1
+## Q2
 multiple_choice :are_you_employee? do
   calculate :no_right_to_apply_reason do
     PhraseList.new :employee_reason
@@ -20,7 +20,7 @@ multiple_choice :are_you_employee? do
   option :no => :no_right_to_apply
 end
 
-## Q0.2
+## Q3
 multiple_choice :applied_for_flexible_working? do
   calculate :no_right_to_apply_reason do
     PhraseList.new :statutory_request_reason
@@ -29,38 +29,17 @@ multiple_choice :applied_for_flexible_working? do
   option :no => :caring_for_child?
 end
 
-
-
-## Q1
-# checkbox_question :employment_status? do
-#   option :employee
-#   option :continuous_employment
-#   option :not_applied_for_flexible_working
-#   option :agency_worker
-#   option :armed_forces
-
-#   next_node do |response|
-#     options = response.split(',')
-#     if options.sort == %w(continuous_employment employee not_applied_for_flexible_working)
-#       :caring_for_child?
-#     else
-#       :no_right_to_apply
-#     end
-#   end
-# end
-
-## Q2
+## Q4
 multiple_choice :caring_for_child? do
   calculate :no_right_to_apply_reason do
     PhraseList.new :child_care_reason
   end
   option :caring_for_child => :relationship_with_child?
   option :caring_for_adult => :relationship_with_adult_group?
-  # option :caring_for_adult => :relationship_with_adult?
   option :neither => :no_right_to_apply
 end
 
-## Q3
+## Q5
 multiple_choice :relationship_with_child? do
   calculate :no_right_to_apply_reason do
     PhraseList.new :wrong_relationship_reason
@@ -69,40 +48,7 @@ multiple_choice :relationship_with_child? do
   option :no => :no_right_to_apply
 end
 
-## Q4
-# multiple_choice :relationship_with_adult? do
-#   calculate :no_right_to_apply_reason do
-#     PhraseList.new :wrong_relationship_reason
-#   end
-#   option :one_of_these => :right_to_apply
-#   option :not_one_of_these => :no_right_to_apply
-# end
-
-## Q4.1
-multiple_choice :relationship_with_adult_group? do
-  calculate :no_right_to_apply_reason do
-    PhraseList.new :wrong_relationship_reason
-  end
-  option :family_member  => :right_to_apply #:relationship_with_adult_family?
-  option :partner => :right_to_apply
-  option :guardian => :right_to_apply
-  option :other_relationship => :right_to_apply
-  option :none => :no_right_to_apply
-end
-
-## Q4.2
-multiple_choice :relationship_with_adult_family? do
-  calculate :no_right_to_apply_reason do
-    PhraseList.new :wrong_relationship_reason
-  end
-  option :husband_wife  => :right_to_apply
-  option :mother_father => :right_to_apply
-  option :uncle_aunt => :right_to_apply
-  option :grandparent => :right_to_apply
-  option :none => :no_right_to_apply
-end
-
-## Q5 
+## Q6 
 multiple_choice :responsible_for_upbringing? do
   calculate :no_right_to_apply_reason do
     PhraseList.new :upbringing_reason
@@ -110,6 +56,19 @@ multiple_choice :responsible_for_upbringing? do
   option :yes => :right_to_apply
   option :no => :no_right_to_apply
 end
+
+## Q7
+multiple_choice :relationship_with_adult_group? do
+  calculate :no_right_to_apply_reason do
+    PhraseList.new :wrong_relationship_reason
+  end
+  option :family_member  => :right_to_apply
+  option :partner => :right_to_apply
+  option :guardian => :right_to_apply
+  option :other_relationship => :right_to_apply
+  option :none => :no_right_to_apply
+end
+
 
 ## A1
 outcome :no_right_to_apply
