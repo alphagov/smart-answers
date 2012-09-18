015663dacaeaba7dcbe60778ff9fdc3a08d94039
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-09-18 11:09:32 +0000

    Fix slimmer config.
    Rails.logger isn't set when production.rb is run.  Moving it all into an
    intializer fixes this.

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'htmlentities', '~> 4'
 if ENV['SLIMMER_DEV']
   gem 'slimmer', :path => '../slimmer'
 else
-  gem 'slimmer', '3.3.1'
+  gem 'slimmer', '3.3.2'
 end
 
 if ENV['GOVSPEAK_DEV']
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,9 +60,6 @@ SmartAnswers::Application.configure do
 
   config.action_controller.asset_host = Plek.current.find('cdn')
 
-  config.slimmer.asset_host = Plek.current.find('assets')
-  config.slimmer.use_cache = true
-
   config.action_mailer.default_url_options = { :host => Plek.current.find('smartanswers') }
   config.action_mailer.delivery_method = :ses
   config.lograge.enabled = true
