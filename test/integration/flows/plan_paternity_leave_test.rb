dd873f8747f31b584c28e775aab7b9a94641dddf
Robin Mayfield <robin.mayfield@digital.cabinet-office.gov.uk>
2012-10-02 12:49:30 +0000

    plan maternity and paternity updated flows, calc and tests

--- test/integration/flows/plan_paternity_leave_test.rb
+++ test/integration/flows/plan_paternity_leave_test.rb
@@ -14,31 +14,83 @@ class PlanPaternityLeaveTest < ActiveSupport::TestCase
   		assert_current_node :baby_due_date?
   	end
 
-  	should "error on over 9 months" do
+  	should "no error on over 9 months" do
   		add_response 1.years.since
-  		assert_current_node_is_error
+  		assert_current_node :leave_duration?
+    end
+
+    should "no error on due_date before today" do
+      add_response 3.months.ago
+      assert_current_node :leave_duration?
   	end
 
-  	should "error on due_date before today" do
-  		add_response 3.months.ago
-  		assert_current_node_is_error
-  	end
-
-  	context "set 3 months to baby_due_date" do
+    context "set 3 months to baby_due_date" do
   		setup do
-		  	add_response 3.months.since
+		  	@due_date = 3.months.since
+        add_response @due_date
 		  end
   		
-  		should "be on leave_start?" do
-	  		assert_current_node :leave_start?
-	  	end
+      should "be on leave_duration?" do
+        assert_current_node :leave_duration?
+      end
 
-	  	context "set 2 weeks to leave_start?" do
-	  		setup {add_response :weeks_2}
-	  		should "go to outcome" do
-	  			assert_current_node :paternity_leave_details
-	  		end
-  		end
+
+      context "set one week leave" do
+        setup do
+          add_response :one_week
+        end
+        should "succeed on 7 weeks" do
+          add_response 7.weeks.since(@due_date)
+          assert_current_node :paternity_leave_details 
+        end
+
+        should "fail on 7 weeks and 1 day" do
+          add_response 1.day.since(7.weeks.since(@due_date))
+          assert_current_node_is_error
+        end
+
+        should "fail on 8 weeks after due_date" do
+          add_response 8.weeks.since(@due_date)
+          assert_current_node_is_error
+        end
+      end
+
+      context "set two weeks leave" do
+        setup do
+          add_response :two_weeks
+        end
+
+        should "be on leave_start?" do
+          assert_current_node :leave_start?
+        end
+        
+        should "fail on before due_date" do
+          add_response 2.days.ago(@due_date)
+          assert_current_node_is_error
+        end
+
+        should "fail on 7 weeks" do
+          add_response 7.weeks.since(@due_date)
+          assert_current_node_is_error
+        end
+
+        should "fail on 6 weeks and 1 day" do
+          add_response 1.day.since(6.weeks.since(@due_date))
+          assert_current_node_is_error
+        end
+
+        should "succeed on 6 weeks" do
+          add_response 6.weeks.since(@due_date)
+          assert_current_node :paternity_leave_details
+        end
+
+  	  	context "set 2 weeks to leave_start?" do
+  	  		setup {add_response 2.weeks.since(@due_date)}
+  	  		should "go to outcome" do
+  	  			assert_current_node :paternity_leave_details
+  	  		end
+    		end
+      end
 	  end
   end
 end
