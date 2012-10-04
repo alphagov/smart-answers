aa9164736c306208787289cb463369083291858c
Robin Mayfield <robin.mayfield@digital.cabinet-office.gov.uk>
2012-10-04 15:21:49 +0000

    Updates to auto_enrolled_into_workplace_pension, including:
     - removed self-employment question
     - added number of org employees question
     - added calculator and YAML data for pension auto_enroll start date by employee number
     - text changes
     - tests and more tests

--- lib/flows/auto-enrolled-into-workplace-pension.rb
+++ lib/flows/auto-enrolled-into-workplace-pension.rb
@@ -2,21 +2,36 @@ status :draft
 satisfies_need "2483"
 
 multiple_choice :work_in_uk? do
-  option :yes => :self_employed?
+  option :yes => :workplace_pension?
   option :no => :not_enrolled
-end
-
-multiple_choice :self_employed? do
-  option :yes => :not_enrolled_self_employed
-  option :no => :workplace_pension?
 end
 
 multiple_choice :workplace_pension? do
   option :yes => :continue_to_pay
-  option :no => :how_old?
+  option :no => :how_many_people?
+end
+
+value_question :how_many_people? do
+  calculate :num_employees do
+    num = responses.last.to_i
+    raise SmartAnswer::InvalidResponse if num < 1 or num > 9999999
+    num
+  end
+
+  next_node do |response| 
+    if response.to_i < 30
+      :not_enrolled_automatically
+    else 
+      :how_old?
+    end
+  end
 end
 
 multiple_choice :how_old? do
+  precalculate :enrollment_date do
+    Calculators::WorkplacePensionCalculator.enrollment_date(num_employees)
+  end
+
   option :between_16_21 => :annual_earnings?
   option :between_22_sp => :annual_earnings2?
   option :state_pension_age => :annual_earnings?
@@ -41,11 +56,12 @@ multiple_choice :one_of_the_following? do
   option :overseas_company => :enrolled_overseas
   option :contract => :enrolled_contract
   option :office_holder => :not_enrolled_office
+  option :carer => :not_enrolled_carer
   option :none => :enrolled
 end
 
+outcome :not_enrolled_automatically
 outcome :not_enrolled
-outcome :not_enrolled_self_employed
 outcome :continue_to_pay
 outcome :not_enrolled_with_options
 outcome :not_enrolled_opt_in
@@ -56,4 +72,5 @@ outcome :enrolled_several
 outcome :enrolled_overseas
 outcome :enrolled_contract
 outcome :not_enrolled_office
+outcome :not_enrolled_carer
 outcome :enrolled
