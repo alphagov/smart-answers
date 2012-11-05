4c37264e088c8368b5605bd2354a947508699c27
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-11-05 13:56:06 +0000

    Add realistic error scenario

--- test/fixtures/flows/custom-errors-sample.rb
+++ test/fixtures/flows/custom-errors-sample.rb
@@ -2,7 +2,7 @@ status :draft
 
 value_question :how_many_things_do_you_own? do
   next_node do |response|
-    raise SmartAnswer::InvalidResponse, :custom_error unless response.is_a?(Integer)
+    raise SmartAnswer::InvalidResponse, :custom_error unless response.to_i > 0
     :done
   end
 end
