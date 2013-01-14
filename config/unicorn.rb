2b4b93722eeb12bc44fed60482086c7db56de6fb
Nick Stenning <nick@whiteink.com>
2013-01-14 19:38:09 +0000

    Bump number of unicorn workers

---
+++ config/unicorn.rb
@@ -0,0 +1,6 @@
+def load_file_if_exists(config, file)
+  config.instance_eval(File.read(file)) if File.exist?(file)
+end
+load_file_if_exists(self, "/etc/govuk/unicorn.rb")
+working_directory File.dirname(File.dirname(__FILE__))
+worker_processes 6
