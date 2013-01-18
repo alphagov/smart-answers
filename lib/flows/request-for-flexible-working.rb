d49bec1a32af2d853ee66fa055ad661780d0617d
Kushal Pisavadia <kushi.p@gmail.com>
2013-01-18 14:36:38 +0000

    Use explicitly separate nodes for employee/employer
    The intention here is that we'll need separate copy. Rather than
    hacking this with PhraseList objects it's far easier to read by
    doubling the number of nodes in this smart answer.
    
    In this case we're making 3 nodes look like 6.
    
    I checked this over with Steve in a discussion we had.

--- lib/flows/request-for-flexible-working.rb
+++ lib/flows/request-for-flexible-working.rb
@@ -4,12 +4,12 @@ satisfies_need "357"
 
 ## Q1
 multiple_choice :are_you_an_employee_or_employer? do
-  option :employee => :which_one_of_these_describes_you?
+  option :employee => :employee_which_one_of_these_describes_you?
   option :employer => :which_one_of_these_describes_you?
 end
 
 ## Q2
-checkbox_question :which_one_of_these_describes_you? do
+checkbox_question :employee_which_one_of_these_describes_you? do
   option :under_17
   option :care_for_adult
   option :less_than_26_weeks
@@ -22,33 +22,33 @@ checkbox_question :which_one_of_these_describes_you? do
     describes_you = response.split(",")
 
     if describes_you == ["none_of_these"] || describes_you.include?("none_of_these")
-      :no_right_to_apply
+      :employee_no_right_to_apply
     elsif describes_you == ["under_17"]
-      :responsible_for_childs_upbringing?
+      :employee_responsible_for_childs_upbringing?
     elsif describes_you == ["care_for_adult"]
-      :do_any_of_these_describe_the_adult_youre_caring_for?
+      :employee_do_any_of_these_describe_the_adult_youre_caring_for?
     elsif describes_you.to_set.superset?(["under_17", "care_for_adult"].to_set)
-      :no_right_to_apply
+      :employee_no_right_to_apply
     elsif describes_you.to_set.superset?(["less_than_26_weeks","agency_worker","member_of_armed_forces","request_in_last_12_months"].to_set) ||
         !(describes_you & ["less_than_26_weeks","agency_worker","member_of_armed_forces","request_in_last_12_months"]).empty?
-      :no_right_to_apply
+      :employee_no_right_to_apply
     end
   end
 end
 
 ## Q3
-multiple_choice :responsible_for_childs_upbringing? do
-  option :yes => :right_to_apply
-  option :no => :no_right_to_apply
+multiple_choice :employee_responsible_for_childs_upbringing? do
+  option :yes => :employee_right_to_apply
+  option :no => :employee_no_right_to_apply
 end
 
 ## Q4
-multiple_choice :do_any_of_these_describe_the_adult_youre_caring_for? do
-  option :yes => :right_to_apply
-  option :no => :no_right_to_apply
+multiple_choice :employee_do_any_of_these_describe_the_adult_youre_caring_for? do
+  option :yes => :employee_right_to_apply
+  option :no => :employee_no_right_to_apply
 end
 
 ## A1
-outcome :no_right_to_apply
+outcome :employee_no_right_to_apply
 ## A2
-outcome :right_to_apply
+outcome :employee_right_to_apply
--- test/integration/flows/request_for_flexible_working_test.rb
+++ test/integration/flows/request_for_flexible_working_test.rb
@@ -19,13 +19,13 @@ class RequestForFlexibleWorkingTest < ActiveSupport::TestCase
     end
 
     should "ask which one describes you" do
-      assert_current_node :which_one_of_these_describes_you?
+      assert_current_node :employee_which_one_of_these_describes_you?
     end
 
     context "selecting 'under_17' and 'care_for_adult'" do
       should "show result 1 if 'under_17' and 'care_for_adult' selected" do
         add_response "under_17,care_for_adult"
-        assert_current_node :no_right_to_apply
+        assert_current_node :employee_no_right_to_apply
       end
 
       should "show result 1 if 'under_17', 'care_for_aduly', and 'less_than_26_weeks' selected" do
@@ -34,35 +34,35 @@ class RequestForFlexibleWorkingTest < ActiveSupport::TestCase
         # as well as any permutation of the other options (minus
         # 'none_of_these') that we show the :no_right_to_apply outcome.
         add_response "under_17,care_for_adult,less_than_26_weeks"
-        assert_current_node :no_right_to_apply
+        assert_current_node :employee_no_right_to_apply
       end
     end
 
     context "selecting permutations of 'less_than_26_weeks', 'agency_worker', 'member_of_armed_forces', and 'request_in_last_12_months'" do
       should "show result 1 if 'less_than_26_weeks', 'agency_worker', 'member_of_armed_forces', and 'request_in_last_12_months' checked" do
         add_response "less_than_26_weeks,agency_worker,member_of_armed_forces,request_in_last_12_months"
-        assert_current_node :no_right_to_apply
+        assert_current_node :employee_no_right_to_apply
       end
 
       should "show result 1 if 'less_than_26_weeks' selected" do
         add_response "less_than_26_weeks"
-        assert_current_node :no_right_to_apply
+        assert_current_node :employee_no_right_to_apply
       end
 
       should "show result 1 if 'less_than_26_weeks' and 'agency_worker' selected" do
         add_response "less_than_26_weeks,agency_worker"
-        assert_current_node :no_right_to_apply
+        assert_current_node :employee_no_right_to_apply
       end
 
       should "show result 1 if 'member_of_armed_forces' and 'request_in_last_12_months' selected" do
         add_response "member_of_armed_forces,request_in_last_12_months"
-        assert_current_node :no_right_to_apply
+        assert_current_node :employee_no_right_to_apply
       end
     end
 
     should "show result 1 if 'none_of_these' selected" do
       add_response "none_of_these"
-      assert_current_node :no_right_to_apply
+      assert_current_node :employee_no_right_to_apply
     end
 
     context "selecting 'under_17'" do
@@ -71,17 +71,17 @@ class RequestForFlexibleWorkingTest < ActiveSupport::TestCase
       end
 
       should "ask if they are responible for the child" do
-        assert_current_node :responsible_for_childs_upbringing?
+        assert_current_node :employee_responsible_for_childs_upbringing?
       end
 
       should "show result 1 if 'no' selected" do
         add_response "no"
-        assert_current_node :no_right_to_apply
+        assert_current_node :employee_no_right_to_apply
       end
 
       should "show result 2 if 'yes' selected" do
         add_response "yes"
-        assert_current_node :right_to_apply
+        assert_current_node :employee_right_to_apply
       end
     end
 
@@ -91,17 +91,17 @@ class RequestForFlexibleWorkingTest < ActiveSupport::TestCase
       end
 
       should "ask about the adult being cared for" do
-        assert_current_node :do_any_of_these_describe_the_adult_youre_caring_for?
+        assert_current_node :employee_do_any_of_these_describe_the_adult_youre_caring_for?
       end
 
       should "show result 2 if 'yes' selected" do
         add_response "yes"
-        assert_current_node :right_to_apply
+        assert_current_node :employee_right_to_apply
       end
 
       should "show result 1 if 'no' selected" do
         add_response "no"
-        assert_current_node :no_right_to_apply
+        assert_current_node :employee_no_right_to_apply
       end
     end
   end
