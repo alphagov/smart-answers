0c8775738379238613d2489c22ea2f2e726fdd7e
Kushal Pisavadia <kushi.p@gmail.com>
2013-01-18 17:02:29 +0000

    Add 'employer' branches of smart answer decision tree
    These provide the logic for the other half of the questions aimed at
    the employer. They'll need to be checked over by a content designer
    before they're deployed.

--- lib/flows/request-for-flexible-working.rb
+++ lib/flows/request-for-flexible-working.rb
@@ -5,7 +5,7 @@ satisfies_need "357"
 ## Q1
 multiple_choice :are_you_an_employee_or_employer? do
   option :employee => :employee_which_one_of_these_describes_you?
-  option :employer => :which_one_of_these_describes_you?
+  option :employer => :employer_which_one_of_these_describes_your_employee?
 end
 
 ## Q2
@@ -36,10 +36,39 @@ checkbox_question :employee_which_one_of_these_describes_you? do
   end
 end
 
+checkbox_question :employer_which_one_of_these_describes_your_employee? do
+  option :under_18
+  option :care_for_adult
+  option :less_than_26_weeks
+  option :agency_worker
+  option :member_of_armed_forces
+  option :request_in_last_12_months
+
+  next_node do |response|
+    describes_you = response.split(",")
+
+    if describes_you == ["under_18"]
+      :employer_responsible_for_childs_upbringing?
+    elsif describes_you == ["care_for_adult"]
+      :employer_do_any_of_these_describe_the_adult_being_cared_for?
+    elsif describes_you.to_set.superset?(["under_18", "care_for_adult"].to_set)
+      :employer_no_right_to_apply
+    elsif describes_you.to_set.superset?(["less_than_26_weeks","agency_worker","member_of_armed_forces","request_in_last_12_months"].to_set) ||
+        !(describes_you & ["less_than_26_weeks","agency_worker","member_of_armed_forces","request_in_last_12_months"]).empty?
+      :employer_no_right_to_apply
+    end
+  end
+end
+
 ## Q3
 multiple_choice :employee_responsible_for_childs_upbringing? do
   option :yes => :employee_right_to_apply
   option :no => :employee_no_right_to_apply
+end
+
+multiple_choice :employer_responsible_for_childs_upbringing? do
+  option :yes => :employer_right_to_apply
+  option :no => :employer_no_right_to_apply
 end
 
 ## Q4
@@ -48,7 +77,15 @@ multiple_choice :employee_do_any_of_these_describe_the_adult_youre_caring_for? d
   option :no => :employee_no_right_to_apply
 end
 
+multiple_choice :employer_do_any_of_these_describe_the_adult_being_cared_for? do
+  option :yes => :employer_right_to_apply
+  option :no => :employer_no_right_to_apply
+end
+
 ## A1
 outcome :employee_no_right_to_apply
+outcome :employer_no_right_to_apply
+
 ## A2
 outcome :employee_right_to_apply
+outcome :employer_right_to_apply
--- test/integration/flows/request_for_flexible_working_test.rb
+++ test/integration/flows/request_for_flexible_working_test.rb
@@ -110,5 +110,83 @@ class RequestForFlexibleWorkingTest < ActiveSupport::TestCase
     setup do
       add_response :employer
     end
+
+    should "ask user to describe themselves" do
+      assert_current_node :employer_which_one_of_these_describes_your_employee?
+    end
+
+    context "selecting 'under_18' and 'care_for_adult'" do
+      should "show result 1 if 'under_18' and 'care_for_adult' selected" do
+        add_response "under_18,care_for_adult"
+        assert_current_node :employer_no_right_to_apply
+      end
+
+      should "show result 1 if 'under_18', 'care_for_aduly', and 'less_than_26_weeks' selected" do
+        add_response "under_18,care_for_adult,less_than_26_weeks"
+        assert_current_node :employer_no_right_to_apply
+      end
+    end
+
+    context "selecting permutations of 'less_than_26_weeks', 'agency_worker', 'member_of_armed_forces', and 'request_in_last_12_months'" do
+      should "show result 1 if 'less_than_26_weeks', 'agency_worker', 'member_of_armed_forces', and 'request_in_last_12_months' checked" do
+        add_response "less_than_26_weeks,agency_worker,member_of_armed_forces,request_in_last_12_months"
+        assert_current_node :employer_no_right_to_apply
+      end
+
+      should "show result 1 if 'less_than_26_weeks' selected" do
+        add_response "less_than_26_weeks"
+        assert_current_node :employer_no_right_to_apply
+      end
+
+      should "show result 1 if 'less_than_26_weeks' and 'agency_worker' selected" do
+        add_response "less_than_26_weeks,agency_worker"
+        assert_current_node :employer_no_right_to_apply
+      end
+
+      should "show result 1 if 'member_of_armed_forces' and 'request_in_last_12_months' selected" do
+        add_response "member_of_armed_forces,request_in_last_12_months"
+        assert_current_node :employer_no_right_to_apply
+      end
+    end
+
+    context "selecting 'under_18'" do
+      setup do
+        add_response "under_18"
+      end
+
+      should "ask if they are responible for the child" do
+        assert_current_node :employer_responsible_for_childs_upbringing?
+      end
+
+      should "show result 1 if 'no' selected" do
+        add_response "no"
+        assert_current_node :employer_no_right_to_apply
+      end
+
+      should "show result 2 if 'yes' selected" do
+        add_response "yes"
+        assert_current_node :employer_right_to_apply
+      end
+    end
+
+    context "selecting 'care_for_adult'" do
+      setup do
+        add_response "care_for_adult"
+      end
+
+      should "ask about the adult being cared for" do
+        assert_current_node :employer_do_any_of_these_describe_the_adult_being_cared_for?
+      end
+
+      should "show result 2 if 'yes' selected" do
+        add_response "yes"
+        assert_current_node :employer_right_to_apply
+      end
+
+      should "show result 1 if 'no' selected" do
+        add_response "no"
+        assert_current_node :employer_no_right_to_apply
+      end
+    end
   end
 end
