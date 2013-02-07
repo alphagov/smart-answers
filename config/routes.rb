02d1547b52ecb9441aa84a4cddff4472962119f8
Alex Tomlins <alex@tomlins.org.uk>
2013-02-07 20:07:39 +0000

    Remove redundant warmup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 SmartAnswers::Application.routes.draw do
-  match '/warmup', controller: 'GdsWarmupController::Warmup', action: :index
-
   match '/:id(/:started(/*responses)).:format',
     :to => 'smart_answers#show',
     :as => :formatted_smart_answer,
