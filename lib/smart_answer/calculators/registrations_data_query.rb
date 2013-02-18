a1a3249a7289cecb81581fa9b5b3247b87bb2ad8
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-02-18 15:57:07 +0000

    Add high commission and consulate data

--- lib/smart_answer/calculators/registrations_data_query.rb
+++ lib/smart_answer/calculators/registrations_data_query.rb
@@ -1,6 +1,20 @@
 module SmartAnswer::Calculators
   class RegistrationsDataQuery
 
+    COMMONWEALTH_COUNTRIES = %w(anguilla ascension-island australia
+      bermuda british-indian-ocean-territory british-virgin-islands cayman-islands
+      canada falkland-islands gibraltar ireland montserrat new-zealand pitcairn
+      south-africa south-georgia-and-south-sandwich-islands turks-and-caicos-islands)
+
+    COUNTRIES_WITH_HIGH_COMMISSIONS = %w(
+      antigua-and-barbuda bangladesh barbados belize botswana brunei cameroon cyprus
+      dominica,-commonwealth-of fiji gambia ghana grenada guyana india jamaica kenya
+      malawi malaysia maldives malta mauritius mozambique namibia nigeria pakistan
+      papua-new-guinea seychelles sierra-leone singapore solomon-islands sri-lanka
+      tanzania trinidad-and-tobago uganda
+    )
+    COUNTRIES_WITH_CONSULATES = %w(china colombia israel russian-federation turkey)
+
     attr_reader :data
 
     def initialize
@@ -8,11 +22,19 @@ module SmartAnswer::Calculators
     end
 
     def commonwealth_country?(country_slug)
-      data['commonwealth_countries'].include?(country_slug) 
+      COMMONWEALTH_COUNTRIES.include?(country_slug) 
     end
 
     def clickbook(country_slug)
       data['death']['clickbook'][country_slug]
+    end
+
+    def has_high_commission?(country_slug)
+      COUNTRIES_WITH_HIGH_COMMISSIONS.include?(country_slug)
+    end
+
+    def has_consulate?(country_slug)
+      COUNTRIES_WITH_CONSULATES.include?(country_slug)
     end
 
     def self.registration_data
--- test/unit/calculators/registrations_data_query_test.rb
+++ test/unit/calculators/registrations_data_query_test.rb
@@ -10,7 +10,6 @@ module SmartAnswer::Calculators
       context "registration_data method" do
         should "read registrations data" do
           assert_equal Hash, @query.data.class
-          assert_equal Array, @query.data['commonwealth_countries'].class
         end
       end
       context "commonwealth_country? method" do
@@ -28,6 +27,18 @@ module SmartAnswer::Calculators
           assert_equal Hash, clickbook.class
           assert_equal "Beijing", clickbook.keys.first
           assert_equal "https://www.clickbook.net/dev/bc.nsf/sub/BritConChongqing", clickbook["Chongqing"]
+        end
+      end
+      context "has_high_commission?" do
+        should "be true for countries with a high commission" do
+          assert @query.has_high_commission?('trinidad-and-tobago')
+          refute @query.has_high_commission?('australia')
+        end
+      end
+      context "has_consulate?" do
+        should "be true for countries with a consulate" do
+          assert @query.has_consulate?('russian-federation')
+          refute @query.has_consulate?('uganda')
         end
       end
     end
