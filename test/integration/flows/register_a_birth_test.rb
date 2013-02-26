55b2ce525aa56ec486651fd690840dd9e5f112d1
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-02-26 16:46:25 +0000

    Add question 4 next node logic

--- test/integration/flows/register_a_birth_test.rb
+++ test/integration/flows/register_a_birth_test.rb
@@ -31,15 +31,45 @@ class RegisterABirthTest < ActiveSupport::TestCase
       setup do
         add_response 'mother'
       end
+      should "store the answer as british_national_parent" do
+        assert_state_variable :british_national_parent, 'mother'
+      end
       should "ask which country the child was born in" do
         assert_current_node :country_of_birth?
       end
+      context "answer Turkey" do
+        setup do
+          add_response 'turkey'
+        end
+        should "ask if you are married or civil partnered" do
+          assert_current_node :married_couple_or_civil_partnership?
+        end
+        context "answer no" do
+          setup do
+            add_response 'no'
+          end
+          should "ask where you are now" do
+            assert_current_node :where_are_you_now?
+          end
+        end # not married/cp
+      end # Turkey
       context "answer with a commonwealth country" do
         should "give the commonwealth result" do
           add_response 'australia'
           assert_current_node :commonwealth_result
         end
       end # commonweath result
+    end # mother
+    context "answer father" do
+      setup do
+        add_response 'father'
+      end
+      should "store the answer as british_national_parent" do
+        assert_state_variable :british_national_parent, 'father'
+      end
+      should "ask where the child was born" do
+        assert_current_node :country_of_birth?
+      end
       context "answer Spain" do
         setup do
           add_response 'spain'
@@ -47,16 +77,30 @@ class RegisterABirthTest < ActiveSupport::TestCase
         should "ask if you are married or civil partnered" do
           assert_current_node :married_couple_or_civil_partnership?
         end
+        context "answer no" do
+          setup do
+            add_response 'no'
+          end
+          should "ask when the child was born" do
+            assert_current_node :childs_date_of_birth?
+          end
+        end # not married/cp
+        context "answer yes" do
+          setup do
+            add_response 'yes'
+          end
+          should "ask where you are now" do
+            assert_current_node :where_are_you_now?
+          end
+        end
       end # Spain
-    end # mother
-    context "answer father" do
-      setup do
-        add_response 'father'
-      end
     end # father
     context "answer mother and father" do
       setup do
         add_response 'mother_and_father'
+      end
+      should "store the answer as british_national_parent" do
+        assert_state_variable :british_national_parent, 'mother_and_father'
       end
     end # mother and father
     context "answer neither" do
