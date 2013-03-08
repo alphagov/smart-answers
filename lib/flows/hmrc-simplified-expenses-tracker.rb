a099dc730c52ffd327e76c9dcbfa1cf59f75a740
Jack Franklin <jack@jackfranklin.net>
2013-03-08 09:19:02 +0000

    Write logic for 1st 2 questions for expenses calc

--- lib/flows/hmrc-simplified-expenses-tracker.rb
+++ lib/flows/hmrc-simplified-expenses-tracker.rb
@@ -1,1 +1,74 @@
 status :draft
+
+#Q1 - new or existing business
+multiple_choice :new_or_existing_business? do
+  option :new
+  option :existing
+
+  save_input_as :new_or_existing_business
+
+  calculate :is_new_business do
+    new_or_existing_business == "new"
+  end
+
+  calculate :is_existing_business do
+    !is_new_business
+  end
+
+  next_node :type_of_expense?
+end
+
+#Q2 - type of expense
+checkbox_question :type_of_expense? do
+  option :car_or_van
+  option :motorcycle
+  option :using_home_for_business
+  option :live_on_business_premises
+  option :none_of_these
+
+  calculate :list_of_expenses do
+    responses.last.split(",")
+  end
+
+  next_node do |response|
+    next_question = nil
+    responses = response.split(",")
+    if (responses & ["car_or_van", "motorcycle"]).size > 0
+      is_existing_business ? :how_much_write_off_tax? : :is_vehicle_green?
+    else
+      if responses.include?("using_home_for_business")
+        :current_claim_amount?
+      elsif responses.include?("live_on_business_premises")
+        :deduct_from_premises?
+      else
+        # at this point they must have only picked none_of_these
+        :you_cant_use_result
+      end
+    end
+  end
+end
+
+#Q3 - write off tax
+value_question :how_much_write_off_tax? do
+
+end
+
+#Q4 - is vehicle green?
+multiple_choice :is_vehicle_green? do
+  option :yes
+  option :no
+end
+
+#Q9 - how much do you claim?
+value_question :current_claim_amount? do
+
+end
+
+#Q11 = how much do you deduct from premises for private use?
+value_question :deduct_from_premises? do
+
+end
+
+outcome :you_cant_use_result
+
+
--- test/integration/flows/hmrc_simplified_expenses_tracker_test.rb
+++ test/integration/flows/hmrc_simplified_expenses_tracker_test.rb
@@ -5,6 +5,119 @@ class HelpIfYouAreArrestedAbroad < ActiveSupport::TestCase
   include FlowTestHelper
 
   setup do
-    setup_for_testing_flow "help-if-you-are-arrested-abroad"
+    setup_for_testing_flow "hmrc-simplified-expenses-tracker"
   end
+
+  should "ask new or existing business question" do
+    assert_current_node :new_or_existing_business?
+  end
+
+  context "answering 'new' to Q1" do
+    setup do
+      add_response :new
+    end
+
+    should "store Q1 answer in variable" do
+      assert_state_variable :new_or_existing_business, "new"
+      assert_state_variable :is_new_business, true
+      assert_state_variable :is_existing_business, false
+    end
+
+    context "selecting just car_van as expense type" do
+      setup do
+        add_response :car_or_van
+      end
+
+      should "calculate list of expenses array" do
+        assert_state_variable :list_of_expenses, ["car_or_van"]
+      end
+
+      should "take user to Q4" do
+        assert_current_node :is_vehicle_green?
+      end
+
+    end # car_van only on Q2
+
+    context "selecting car_or_van and motorcycle as expense type" do
+      setup do
+        add_response "car_or_van,motorcycle"
+      end
+
+      should "take the user to Q4" do
+        assert_current_node :is_vehicle_green?
+      end
+    end # car_van or motorcycle on Q2
+
+    context "selecting just motorcycle as expense type" do
+      setup do
+        add_response "motorcycle"
+      end
+
+      should "take the user to Q4" do
+        assert_current_node :is_vehicle_green?
+      end
+    end # just motorcycle on Q2
+
+  end # answering 'new' on Q1
+
+  context "answering 'existing' to Q1" do
+    setup do
+      add_response :existing
+    end
+
+    should "store Q1 answer in variable" do
+      assert_state_variable :new_or_existing_business, "existing"
+      assert_state_variable :is_existing_business, true
+      assert_state_variable :is_new_business, false
+    end
+
+    context "selecting just car_van as expense type" do
+      setup do
+        add_response :car_or_van
+      end
+
+      should "calculate list of expenses array" do
+        assert_state_variable :list_of_expenses, ["car_or_van"]
+      end
+
+      should "take user to Q3" do
+        assert_current_node :how_much_write_off_tax?
+      end
+
+    end # car_van only on Q2
+
+    context "selecting car_or_van and motorcycle as expense type" do
+      setup do
+        add_response "car_or_van,motorcycle"
+      end
+
+      should "take the user to Q3" do
+        assert_current_node :how_much_write_off_tax?
+      end
+    end # car_van or motorcycle on Q2
+
+    context "selecting just motorcycle as expense type" do
+      setup do
+        add_response "motorcycle"
+      end
+
+      should "take the user to Q3" do
+        assert_current_node :how_much_write_off_tax?
+      end
+    end # just motorcycle on Q2
+
+    context "selecting other options for Q2 should calculate list_of_expenses correctly" do
+      context "selecting all options other than the last" do
+        setup do
+          add_response "car_or_van,motorcycle,using_home_for_business,live_on_business_premises"
+        end
+        should "store responses in an array" do
+          assert_state_variable :list_of_expenses, ["car_or_van", "live_on_business_premises", "motorcycle", "using_home_for_business"]
+        end
+      end
+    end
+
+  end # answering 'existing' to Q1
+
+
 end
