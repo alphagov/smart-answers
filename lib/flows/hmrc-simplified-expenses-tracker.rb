e160f5dd88a9262a71519d8ab2d3000f3588af37
Jack Franklin <jack@jackfranklin.net>
2013-03-08 16:14:02 +0000

    Continue implementing logic for expenses calc

--- lib/flows/hmrc-simplified-expenses-tracker.rb
+++ lib/flows/hmrc-simplified-expenses-tracker.rb
@@ -37,7 +37,7 @@ checkbox_question :type_of_expense? do
       is_existing_business ? :how_much_write_off_tax? : :is_vehicle_green?
     else
       if responses.include?("using_home_for_business")
-        :current_claim_amount?
+        :current_claim_amount_home?
       elsif responses.include?("live_on_business_premises")
         :deduct_from_premises?
       else
@@ -51,7 +51,10 @@ end
 #Q3 - write off tax
 value_question :how_much_write_off_tax? do
   calculate :vehicle_tax_amount do
-    responses.last
+    responses.last.gsub(",","").to_f
+  end
+  next_node do
+    list_of_expenses.include?("car_or_van") ? :drive_business_miles_car_van? : :drive_business_miles_motorcycle?
   end
 end
 
@@ -90,24 +93,109 @@ value_question :price_of_vehicle? do
 
 
   next_node :vehicle_private_use_time?
-
 end
 
 #Q6 - vehicle private use time
 value_question :vehicle_private_use_time? do
+  # deduct percentage amount from [green_cost] or [dirty_cost] and store as [green_write_off] or [dirty_write_off]
+  # go to Q7 if [new_business] + [car_van]
+  # go to Q8 if [new_business] +[motorcycle]
+  # go to Q7 + Q8 if [new_business] + [car_van] + [motorcycle]
+  #
+  calculate :private_use_percent do
+    responses.last.gsub("%", "").to_f
+  end
+  calculate :green_vehicle_write_off do
+    if vehicle_is_green
+      green_vehicle_price * ( private_use_percent / 100 )
+    else
+      nil
+    end
+  end
 
+  calculate :dirty_vehicle_write_off do
+    unless vehicle_is_green
+      dirty_vehicle_price * ( private_use_percent / 100 )
+    end
+  end
+
+  next_node do
+    list_of_expenses.include?("car_or_van") ?
+      :drive_business_miles_car_van? : :drive_business_miles_motorcycle?
+  end
+end
+
+#Q7 - miles to drive for business car_or_van
+value_question :drive_business_miles_car_van? do
+  # Calculation:
+  # [user input 1-10,000] x 0.45
+  # [user input > 10,001]  x 0.24
+  calculate :simple_vehicle_costs do
+    answer = responses.last.gsub(",", "").to_f
+    if answer <= 10000
+      answer * 0.45
+    else
+      answer_over_amount = (answer - 10000) * 0.24
+      4500.0 + answer_over_amount
+    end
+  end
+  next_node do
+    if list_of_expenses.include?("motorcycle")
+      :drive_business_miles_motorcycle?
+    else
+      if list_of_expenses.include?("using_home_for_business")
+        :current_claim_amount_home?
+      else
+        :you_can_use_result
+      end
+    end
+  end
+end
+
+#Q8 - miles to drive for business motorcycle
+value_question :drive_business_miles_motorcycle? do
+  calculate :simple_motorcycle_costs do
+    responses.last.gsub(",","").to_f * 0.25
+  end
+  next_node do
+    if list_of_expenses.include?("using_home_for_business")
+      :current_claim_amount_home?
+    elsif list_of_expenses.include?("live_on_business_premises")
+      :deduct_from_premises?
+    else
+      :you_can_use_result
+    end
+  end
 end
 
 #Q9 - how much do you claim?
-value_question :current_claim_amount? do
+value_question :current_claim_amount_home? do
 
 end
+
+#Q10 - hours for home work
 
 #Q11 = how much do you deduct from premises for private use?
 value_question :deduct_from_premises? do
+  calculate :business_premises_cost do
+    responses.last.gsub(",", "").to_f
+  end
+
+  next_node :people_live_on_premises?
+end
+
+#Q12 - people who live on business premises?
+value_question :people_live_on_premises? do
+  calculate :live_on_premises do
+    responses.last.to_i
+  end
+
+  next_node :you_can_use_result
 
 end
 
+
 outcome :you_cant_use_result
+outcome :you_can_use_result
 
 
--- test/integration/flows/hmrc_simplified_expenses_tracker_test.rb
+++ test/integration/flows/hmrc_simplified_expenses_tracker_test.rb
@@ -59,7 +59,7 @@ class HelpIfYouAreArrestedAbroad < ActiveSupport::TestCase
       end # just motorcycle on Q2
     end # answering Q2
 
-    context "A/B on Q2 and new business on Q1" do
+    context "A on Q2 and new business on Q1" do
       setup do
         add_response "car_or_van" # answering Q2
       end
@@ -102,6 +102,20 @@ class HelpIfYouAreArrestedAbroad < ActiveSupport::TestCase
                 assert_state_variable :is_over_limit, true
               end
             end # answering > 250k
+
+            context "answering Q6 - % of private use" do
+              setup do
+                add_response "100000" # answer Q5
+              end
+
+              should "correctly calculate the write off amount" do
+                add_response "15"
+                assert_state_variable :private_use_percent, 15.0
+                # write off = 15% of Q5 answer
+                assert_state_variable :green_vehicle_write_off, 15000.0
+              end
+            end # Q6 % private use of vehicle
+
           end # Q5 vehicle price
         end # Q4 answering yes
 
@@ -133,18 +147,69 @@ class HelpIfYouAreArrestedAbroad < ActiveSupport::TestCase
               setup do
                 add_response "255,000"
               end
+
               should "calculate state variables correctly" do
                 assert_state_variable :is_over_limit, true
                 assert_state_variable :green_vehicle_price, nil
                 assert_state_variable :dirty_vehicle_price, 45900.0
               end
             end
-          end
+
+            context "answering Q6 with dirty vehicle" do
+              setup do
+                add_response "100000" # answering Q5
+              end
+
+              should "calculate variables correctly" do
+                add_response "15"
+                assert_state_variable :private_use_percent, 15.0
+                # write off = 15% of 18% of Q5 answer
+                assert_state_variable :dirty_vehicle_write_off, 2700.0
+              end
+
+              should "take the user to Q7" do
+                add_response "15" # answering Q6
+                assert_state_variable :private_use_percent, 15.0
+                assert_current_node :drive_business_miles_car_van?
+              end
+
+              context "answering Q7" do
+                setup do
+                  add_response "15" #answer Q6
+                end
+                should "calculate the amount correctly for value < 10k" do
+                  add_response "9000"
+                  assert_state_variable :simple_vehicle_costs, 4050.0
+                end
+
+                should "calculate the amount correctly for value > 10k" do
+                  add_response "10400"
+                  assert_state_variable :simple_vehicle_costs, 4596.0
+                end
+              end #Q7
+            end # answering Q6
+          end # answering Q5
+        end # Q4 answering no (vehicle isn't green)
+
+      end #answering Q4 "dirty"
+
+    end #answering Q2 "car_or_van"
+    context "answering Q2 with 'motorcycle,using_home_for_business'" do
+      context "answering Q8" do
+        setup do
+          add_response "car_or_van,motorcycle,using_home_for_business" #Q2
+          add_response :yes #Q4
+          add_response "100000" #Q5
+          add_response "15" #Q6
+          add_response "1000" #Q7
         end
+        should "calculate the motorcycle cost correctly" do
+          add_response "1000" #Q8
+          assert_state_variable :simple_motorcycle_costs, 250
+        end
+      end
 
-      end #answering Q4
-
-    end #answering Q2
+    end
   end # answering 'new' on Q1
 
   context "answering 'existing' to Q1" do
@@ -214,7 +279,7 @@ class HelpIfYouAreArrestedAbroad < ActiveSupport::TestCase
       context "answering just C for Q2 takes you to Q9" do
         should "take you through to Q9" do
           add_response "using_home_for_business"
-          assert_current_node :current_claim_amount?
+          assert_current_node :current_claim_amount_home?
         end
       end # answering using_home_for_business on Q2
 
@@ -223,9 +288,112 @@ class HelpIfYouAreArrestedAbroad < ActiveSupport::TestCase
           add_response "live_on_business_premises"
           assert_current_node :deduct_from_premises?
         end
+
+        context "Answering Q11" do
+          setup do
+            add_response "live_on_business_premises"
+          end
+
+          should "calculate the cost and take user to Q12" do
+            add_response "500"
+            assert_state_variable :business_premises_cost, 500.0
+            assert_current_node :people_live_on_premises?
+          end
+
+          context "Answering Q12" do
+            setup do
+              add_response "500"
+            end
+
+            should "calculate number and take user to result" do
+              add_response "5"
+              assert_state_variable :live_on_premises, 5
+              assert_current_node :you_can_use_result
+            end
+          end # answering Q12
+
+        end # answering Q11
       end #answering live_on_business_premises for Q2
+
+      context "answering Q3" do
+        should "take user to Q7 if they picked car_or_van in Q2" do
+          add_response "car_or_van" #Q2
+          add_response "100"
+          assert_state_variable :vehicle_tax_amount, 100
+          assert_current_node :drive_business_miles_car_van?
+        end
+
+        should "take user to Q8 if user picked motorcycle but not car_or_van" do
+          add_response "motorcycle" #Q2
+          add_response "100"
+          assert_state_variable :vehicle_tax_amount, 100
+          assert_current_node :drive_business_miles_motorcycle?
+        end
+      end #answering Q3
     end # answering Q2
   end # answering 'existing' to Q1
 
+
+  # testing Q7 next_node logic
+  context "Progressing from Question 7" do
+    setup do
+      add_response :new #Q1
+    end
+
+    should "take the user to Q8 after answering Q7 if Q2 answer includes motorcycle" do
+      add_response "car_or_van,motorcycle" #Q2
+      add_response :yes #Q4
+      add_response "100000" #Q5
+      add_response "15" #Q6
+      add_response "10000"
+      assert_current_node :drive_business_miles_motorcycle?
+    end
+
+    should "skip Q7 if the user doesn't own a car or van" do
+      add_response "motorcycle" #Q2
+      add_response :yes #Q4
+      add_response "100000" #Q5
+      add_response "15" #Q6
+      assert_current_node :drive_business_miles_motorcycle?
+    end
+
+    should "skip Q8 if the user doesn't own a motorcycle" do
+      add_response "car_or_van" #Q2
+      add_response :yes #Q4
+      add_response "100000" #Q5
+      add_response "15" #Q6
+      add_response "1000" #Q7
+      assert_current_node :you_can_use_result
+    end
+
+    should "go from Q7 to Q9 if the user picked A and C on Q2" do
+      add_response "car_or_van,using_home_for_business" #Q2
+      add_response :yes #Q4
+      add_response "100000" #Q5
+      add_response "15" #Q6
+      add_response "1000" #Q7
+      assert_current_node :current_claim_amount_home?
+    end
+
+    should "go from Q8 to Q9 if the user picked A, B and C on Q2" do
+      add_response "car_or_van,motorcycle,using_home_for_business" #Q2
+      add_response :yes #Q4
+      add_response "100000" #Q5
+      add_response "15" #Q6
+      add_response "1000" #Q7
+      add_response "1000" #Q8
+      assert_current_node :current_claim_amount_home?
+    end
+
+    should "go from Q8 to Q11 if the user picked motorcycle and live_on_business_premises" do
+      add_response "car_or_van,motorcycle,live_on_business_premises" #Q2
+      add_response :yes #Q4
+      add_response "100000" #Q5
+      add_response "15" #Q6
+      add_response "1000" #Q7
+      add_response "1000" #Q8
+      assert_current_node :deduct_from_premises?
+    end
+  end # progressing from Q7
 
 end
