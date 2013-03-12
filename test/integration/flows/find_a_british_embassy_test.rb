7a3b9e925d62ad4ab5dc6c3ed5ca50ca9bfb87ee
Aaron <aaron.keogh@digital.cabinet-office.gov.uk>
2013-03-12 14:57:49 +0000

    Updates and bug fixes
    fixing bug preventing multiple embassy details showing
    adding countries to list to be prefixed by 'the'
    adding location_name for multiple embassy results

--- test/integration/flows/find_a_british_embassy_test.rb
+++ test/integration/flows/find_a_british_embassy_test.rb
@@ -19,11 +19,89 @@ class FindABritishEmbassyTest < ActiveSupport::TestCase
     end
     should "go to outcome" do
       assert_current_node :embassy_outcome
-      assert_state_variable :embassy_address, 'British Embassy'"\n"'15th Street, Roundabout Wazir Akbar Khan'"\n"'PO Box 334'"\n"'Kabul'"\n"'Afghanistan,Kabul'
-      assert_state_variable :embassy_website, 'Website: http://ukinafghanistan.fco.gov.uk/en/'
-      assert_state_variable :embassy_phone, 'Telephone: +93 (0) 700 102 000'
-      assert_state_variable :embassy_email, 'Email: britishembassy.kabul@fco.gov.uk'
-      assert_state_variable :embassy_office_hours, 'Office hours:'"\n"'0830-1630 (Sunday to Thursday)'
+      assert_state_variable :embassy_details, "\n\n\n$A\n  British Embassy\n15th Street, Roundabout Wazir Akbar Khan\nPO Box 334\nKabul\nAfghanistan,Kabul\n$A\n\n$C\n  britishembassy.kabul@fco.gov.uk\n\n  +93 (0) 700 102 000\n\n  0830-1630 (Sunday to Thursday)\n$C\n"
     end
   end
+  context "details in brazil" do
+    setup do
+      add_response 'brazil'
+    end
+    should "go to outcome" do
+      assert_current_node :embassy_outcome
+      assert_state_variable :embassy_details, "Brasilia
+
+
+$A
+  British Embassy 
+Setor de Embaixadas Sul 
+Quadra 801, Lote 8 
+CEP 70408-900 
+Brasilia - DF, Brazil
+$A
+
+$C
+  
+
+  (55) (61) 3329-2300
+
+  GMT:
+Mon-Thurs: 1130-1945 (lunch break: 1530-1630)
+Fri: 1130 - 1930 (lunch break: 1530-1630)
+
+Local Time:
+Mon-Thurs: 0830 - 1645 (lunch break: 1230-1330) 
+Friday: 0830 - 1630 (lunch break: 1230-1330)
+$C
+Rio de Janeiro
+
+
+$A
+  British Consulate-General
+Praia do Flamengo 284/2 andar
+22210-030
+Rio de Janeiro RJ
+$A
+
+$C
+  
+
+  (55) (21) 2555 9600 
+(55) (21) 2555 9640
+
+  GMT:
+Mon-Thurs: 1130-1945
+Fri: 1130-1530
+Local Time:
+Mon-Thurs: 0830-1645
+Fri: 0830-1630
+
+Opening hours (open to the public): Monday to Friday: 08:30 to 12:30
+$C
+Sao Paulo
+
+
+$A
+  British Consulate-General
+Rua Ferreira de Araujo, 741 - 2 Andar
+Pinheiros
+05428-002
+Sao Paulo-SP
+$A
+
+$C
+  
+
+  (55) (11) 3094 2700
+
+  GMT:
+Mon-Fri: 1030-1900
+
+Local Time:
+Mon-Fri: 0830-1700
+Opening hours (open to the public): Monday to Friday: 08:30 to 12:30
+$C
+"
+    end
+  end
+
 end
