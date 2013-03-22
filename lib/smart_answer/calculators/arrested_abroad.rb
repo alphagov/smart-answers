c7e4642cf3085b266f3a6918e78add322aa511d1
Jack Franklin <jack@jackfranklin.net>
2013-03-22 15:38:03 +0000

    Add link for suriname

--- lib/smart_answer/calculators/arrested_abroad.rb
+++ lib/smart_answer/calculators/arrested_abroad.rb
@@ -5,7 +5,7 @@ module SmartAnswer::Calculators
 
     def initialize
       @data = self.class.prisoner_packs
-    end
+      end
 
     def generate_url_for_download(country, field, text)
       url = @data.select { |c| c["slug"] == country }.first[field]
@@ -26,7 +26,7 @@ module SmartAnswer::Calculators
     end
 
     def countries_no_pack
-      %w{ afghanistan algeria american-samoa andorra angola anguilla aruba ascension-island bahamas benin bermuda bhutan bonaire-st-eustatius-saba bosnia-and-herzegovina british-antarctic-territory british-indian-ocean-territory british-virgin-islands burkina-faso burundi cape-verde cayman-islands central-african-republic chad comoros congo congo-(democratic-republic) cote-d_ivoire-(ivory-coast) curacao djibouti equatorial-guinea eritrea falkland-islands fiji french-guiana french-polynesia gabon gambia gibraltar guadeloupe guinea guinea-bissau haiti honduras iraq kazakhstan kenya kiribati kyrgyzstan kosovo laos lesotho liberia liechtenstein macao madagascar maldives mali malta marshall-islands martinique mauritania mayotte micronesia monaco mongolia montenegro nauru new-caledonia nicaragua niger nigeria north-korea oman palau papua-new-guinea pitcairn reunion samoa san-marino sao-tome-and-principe senegal somalia south-georgia-and-south-sandwich-islands south-sudan st-helena st-maarten st-pierre-and-miquelon sudan suriname swaziland syria togo tonga tristan-da-cunha turkmenistan turks-and-caicos-islands tuvalu vanuatu wallis-and-futuna western-sahara }
+      %w{ afghanistan algeria american-samoa andorra angola anguilla aruba ascension-island bahamas benin bermuda bhutan bonaire-st-eustatius-saba bosnia-and-herzegovina british-antarctic-territory british-indian-ocean-territory british-virgin-islands burkina-faso burundi cape-verde cayman-islands central-african-republic chad comoros congo congo-(democratic-republic) cote-d_ivoire-(ivory-coast) curacao djibouti equatorial-guinea eritrea falkland-islands fiji french-guiana french-polynesia gabon gambia gibraltar guadeloupe guinea guinea-bissau haiti honduras iraq kazakhstan kenya kiribati kyrgyzstan kosovo laos lesotho liberia liechtenstein macao madagascar maldives mali malta marshall-islands martinique mauritania mayotte micronesia monaco mongolia montenegro nauru new-caledonia nicaragua niger nigeria north-korea oman palau papua-new-guinea pitcairn reunion samoa san-marino sao-tome-and-principe senegal somalia south-georgia-and-south-sandwich-islands south-sudan st-helena st-maarten st-pierre-and-miquelon sudan swaziland syria togo tonga tristan-da-cunha turkmenistan turks-and-caicos-islands tuvalu vanuatu wallis-and-futuna western-sahara }
     end
 
     def countries_with_regions
