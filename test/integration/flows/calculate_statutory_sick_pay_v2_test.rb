341b2715154c6780aa944631e137efe93c030a90
Jack Franklin <jack@jackfranklin.net>
2013-04-18 14:27:05 +0100

    Rename test class to V2

--- test/integration/flows/calculate_statutory_sick_pay_v2_test.rb
+++ test/integration/flows/calculate_statutory_sick_pay_v2_test.rb
@@ -2,7 +2,7 @@
 require_relative '../../test_helper'
 require_relative 'flow_test_helper'
 
-class CalculateStatutorySickPayTest < ActiveSupport::TestCase
+class CalculateStatutorySickPayV2Test < ActiveSupport::TestCase
   include FlowTestHelper
   setup do
     setup_for_testing_flow 'calculate-statutory-sick-pay-v2'
