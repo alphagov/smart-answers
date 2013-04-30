85baf763719936d4b806c56e220d0728cb817f40
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-04-30 09:40:23 +0000

    Add tax credits flow

--- lib/flows/benefits-abroad.rb
+++ lib/flows/benefits-abroad.rb
@@ -5,7 +5,7 @@ eea_countries = %w(austria belgium bulgaria cyprus czech-republic denmark estoni
 
 social_security_countries = %w(croatia bosnia-and-herzegovina croatia guernsey jersey kosovo macedonia montenegro new-zealand serbia)
 
-maternity_ss_countries = social_security_countries + %w(barbados israel jersey turkey)
+
 
 # Q1
 multiple_choice :going_or_already_abroad? do
@@ -20,13 +20,13 @@ multiple_choice :which_benefit? do
   option :pension => :pension_outcome # A2
   option :wfp => :which_country_are_you_moving_to_wfp? # Q4
   option :maternity_benefits => :which_country_are_you_moving_to_maternity? # Q6
-  #option :child_benefits => # Q10
+  option :child_benefits => :which_country_are_you_moving_to_cb? # Q10
   #option :iidb => # Q24
-  #option :ssp => # Q12
+  option :ssp => :which_country_are_you_moving_to_ssp? # Q31
   #option :esa => # Q22
-  #option :disability_benefits => # Q26
+  #option :disability_benefits => # Q26 # Leave for now.
   #option :bereavement_benefits => # Q32
-  #option :tax_credits => # Q15
+  option :tax_credits => :eligible_for_tax_credits? # Q16
   save_input_as :which_benefit
 end
 # Q3
@@ -77,19 +77,103 @@ multiple_choice :eligible_for_maternity_pay? do
   option :yes => :smp_outcome # A9
   option :no => :smp_not_entitled #A8
 end
-# Q9
+# Q9, Q10
 multiple_choice :employer_paying_ni? do
   option :yes
   option :no
   next_node do |response|
+    maternity_ss_countries = social_security_countries + %w(barbados israel jersey turkey)
     if response == 'yes'
-      :eligible_for_maternity_pay?
+      :eligible_for_maternity_pay? # Q8
     elsif maternity_ss_countries.include?(maternity_country)
       :maternity_allowance # A10
     else
       :maternity_not_entitled # A11
     end
   end
+end
+# Q11
+country_select :which_country_are_you_moving_to_cb?, :use_legacy_data => true do
+  next_node do |response|
+    if eea_countries.include?(response)
+      :do_either_of_the_following_apply? # Q12
+    elsif %w(bosnia-and-herzegovina croatia kosovo macedonia montenegro serbia).include?(response)
+      :cb_fy_social_security_outcome # A12
+    elsif %w(barbados canada guernsey israel jersey mauritius new-zealand).include?(response)
+      :cb_social_security_outcome # A13
+    elsif %w(jamaica turkey united-states).include?(response)
+      :cb_jtu_not_entitled # A14
+    else
+      :cb_not_entitled # A16
+    end
+  end
+end
+# Q12
+multiple_choice :do_either_of_the_following_apply? do
+  option :yes => :cb_outcome # A15
+  option :no => :cb_not_entitled # A16
+end
+# Q13
+country_select :which_country_are_you_moving_to_ssp?, :use_legacy_data => true do
+  next_node do |response|
+    if eea_countries.include?(response)
+      :working_for_a_uk_employer_ssp? # Q14
+    else
+      :employer_paying_ni_ssp? # Q15
+    end
+  end
+end
+# Q14
+multiple_choice :working_for_a_uk_employer_ssp? do
+  option :yes => :ssp_outcome # A17
+  option :no => :ssp_not_entitled # A18
+end
+# Q15
+multiple_choice :employer_paying_ni_ssp? do
+  option :yes => :ssp_outcome # A17
+  option :no => :ssp_not_entitled # A18
+end
+# Q16
+multiple_choice :eligible_for_tax_credits? do
+  option :yes => :are_you_one_of_the_following? # Q17
+  option :no => :tax_credits_unlikely # A19
+end
+# Q17
+multiple_choice :are_you_one_of_the_following? do
+  option :crown_servant => :tax_credits_outcome # A20
+  option :cross_border_worker => :tax_credits_exceptions # A21
+  option :none_of_the_above => :how_long_are_you_abroad_for? # Q21
+end
+# Q18
+multiple_choice :do_you_have_children? do
+  option :yes => :where_are_you_moving_to_tax_credits? # Q19
+  option :no => :tax_credits_outcome # A20
+end
+# Q19
+country_select :where_are_you_moving_to_tax_credits?, :use_legacy_data => true do
+  next_node do |response|
+    if eea_countries.include?(response)
+      :currently_claiming? # Q20
+    else
+      :tax_credits_unlikely # A19
+    end
+  end
+end
+# Q20
+multiple_choice :currently_claiming? do
+  option :yes => :tax_credits_possible # A22
+  option :no => :tax_credits_unlikely # A19
+end
+# Q21
+multiple_choice :how_long_are_you_abroad_for? do
+  option :up_to_a_year => :why_are_you_going_abroad? # Q22
+  option :more_than_a_year => :do_you_have_children? # Q18
+end
+# Q22
+multiple_choice :why_are_you_going_abroad? do
+  option :holiday_or_business_trip => :tax_credits_continue_8_weeks # A23
+  option :medical_treatment => :tax_credits_continue_12_weeks # A24
+  option :death => :tax_credits_continue_12_weeks # A24
 end
 # A1
 outcome :not_paid_ni
@@ -111,5 +195,31 @@ outcome :smp_not_entitled
 outcome :smp_outcome
 # A10
 outcome :maternity_allowance
-# A 11
+# A11
 outcome :maternity_not_entitled
+# A12
+outcome :cb_fy_social_security_outcome
+# A13
+outcome :cb_social_security_outcome
+# A14
+outcome :cb_jtu_not_entitled
+# A15
+outcome :cb_outcome
+# A16
+outcome :cb_not_entitled
+# A17
+outcome :ssp_outcome
+# A18
+outcome :ssp_not_entitled
+# A19
+outcome :tax_credits_unlikely
+# A20
+outcome :tax_credits_outcome
+# A21
+outcome :tax_credits_exceptions
+# A22
+outcome :tax_credits_possible
+# A23
+outcome :tax_credits_continue_8_weeks
+# A24
+outcome :tax_credits_continue_12_weeks
--- test/integration/flows/benefits_abroad_test.rb
+++ test/integration/flows/benefits_abroad_test.rb
@@ -59,7 +59,7 @@ class BenefitsAbroadTest < ActiveSupport::TestCase
       setup do
         add_response 'wfp'
       end
-      # Q4
+      # Q6
       should "ask which country you are moving to" do
       end
       context "answer Austria (EEA country)" do
@@ -91,18 +91,289 @@ class BenefitsAbroadTest < ActiveSupport::TestCase
     end # Winter fuel payments
     context "answer maternity pay" do
       setup do
-        add_response 'maternity'
+        add_response 'maternity_benefits'
       end
       should "ask which country you are moving to" do
-        context "answer austria (EEA country)" do
+        assert_current_node :which_country_are_you_moving_to_maternity?
+      end
+      context "answer austria (EEA country)" do
+        setup do
+          add_response 'austria'
+        end
+        should "ask if you will be working for a UK employer" do
+          assert_current_node :working_for_a_uk_employer?
+        end
+        context "answer yes" do
           setup do
-            add_response 'austria'
+            add_response 'yes'
           end
-          should "ask if you will be working for a UK employer" do
-            assert_current_node :working_for_a_uk_employer?
+          should "ask if you are eligible for SMP" do
+            assert_current_node :eligible_for_maternity_pay?
+          end
+          context "answer yes" do
+            should "state SMP entitlement" do
+              add_response 'yes'
+              assert_current_node :smp_outcome
+            end
+          end
+          context "answer no" do
+            should "state you cant get SMP but may be able to get maternity allowance" do
+              add_response 'no'
+              assert_current_node :smp_not_entitled
+            end
+          end # Entitled to SMP
+        end # UK employer
+        context "answer no" do
+          should "state you're not entitled to SMP" do
+            add_response 'no'
+            assert_current_node :smp_not_entitled
           end
         end
-      end
+      end # EEA country
+      context "answer Croatia" do
+        setup do
+          add_response 'croatia'
+        end
+        should "ask if your employer is paying NI for you" do
+          assert_current_node :employer_paying_ni?
+        end
+        context "answer yes" do
+          should "ask of you are eligible for SMP" do
+            add_response 'yes'
+            assert_current_node :eligible_for_maternity_pay?
+          end
+        end
+        context "answer no" do
+          should "state you may be entitled to maternity allowance" do
+            add_response 'no'
+            assert_current_node :maternity_allowance
+          end
+        end
+      end # Social security country
+      context "answer Gambia, employer not paying NI" do
+        should "state you are not entitled to SMP or maternity allowance" do
+          add_response 'gambia'
+          add_response 'no'
+          assert_current_node :maternity_not_entitled
+        end
+      end # Gambia
     end # Maternity
+    context "answer Child benefit" do
+      setup do
+        add_response 'child_benefits'
+      end
+      should "ask which country you are moving to" do
+        assert_current_node :which_country_are_you_moving_to_cb?
+      end
+      context "answer Austria (EEA country)" do
+        setup do
+          add_response 'austria'
+        end
+        should "ask if your employer pays NI or you are already on benefits" do
+          assert_current_node :do_either_of_the_following_apply?
+        end
+        context "answer yes" do
+          should "state your entitlement" do
+            add_response 'yes'
+            assert_current_node :cb_outcome
+          end
+        end
+        context "answer no" do
+          should "state you are not entitled" do
+            add_response 'no'
+            assert_current_node :cb_not_entitled
+          end
+        end
+      end # EEA country
+      context "answer croatia" do
+        should "state entitlement for former Yugoslavian countries" do
+          add_response 'croatia'
+          assert_current_node :cb_fy_social_security_outcome
+        end
+      end # Former Yugoslavia
+      context "canada" do
+        should "state entitlement for countries with social security agreements" do
+          add_response 'canada'
+          assert_current_node :cb_social_security_outcome
+        end
+      end # Social security
+      context "answer Turkey" do
+        should "state exceptions for Jamaica, Turkey and USA" do
+          add_response 'turkey'
+          assert_current_node :cb_jtu_not_entitled
+        end
+      end # Jamaica Turkey USA exceptions
+      context "answer Australia" do
+        should "state you are not entitled" do
+          add_response 'australia'
+          assert_current_node :cb_not_entitled
+        end
+      end # 'other' country
+    end # Child benefit
+    context "answer SSP" do
+      setup do
+        add_response 'ssp'
+      end
+      should "ask which country you are moving to" do
+        assert_current_node :which_country_are_you_moving_to_ssp?
+      end
+      context "answer Austria (EEA country)" do
+        setup do
+          add_response 'austria'
+        end
+        should "ask if you are working for a UK employer" do
+          assert_current_node :working_for_a_uk_employer_ssp?
+        end
+        context "answer yes" do
+          should "state entitlement" do
+            add_response 'yes'
+            assert_current_node :ssp_outcome
+          end
+        end
+        context "answer no" do
+          should "state you are not entitled" do
+            add_response 'no'
+            assert_current_node :ssp_not_entitled
+          end
+        end
+      end # EEA country
+      context "answer Yemen" do
+        setup do
+          add_response 'yemen'
+        end
+        should "ask if your employer is paying NI" do
+          assert_current_node :employer_paying_ni_ssp?
+        end
+        context "answer yes" do
+          should "state entitlement" do
+            add_response 'yes'
+            assert_current_node :ssp_outcome
+          end
+        end
+        context "answer no" do
+          should "state you are not entitled" do
+            add_response 'no'
+            assert_current_node :ssp_not_entitled
+          end
+        end
+      end # 'Other' country
+    end # SSP
+    context "answer tax credits" do
+      setup do
+        add_response 'tax_credits'
+      end
+      # Q16
+      should "ask if you are eligible for tax credits" do
+        assert_current_node :eligible_for_tax_credits?
+      end
+      context "answer yes" do
+        setup do
+          add_response 'yes'
+        end
+        should "ask if you one of the following" do
+          assert_current_node :are_you_one_of_the_following?
+        end
+        context "answer crown servant" do
+          should "state tax credits eligibility" do
+            add_response 'crown_servant'
+            assert_current_node :tax_credits_outcome
+          end
+        end # Crown servant
+        context "answer cross border worker" do
+          should "state tax credits eligibility with exceptions" do
+            add_response 'cross_border_worker'
+            assert_current_node :tax_credits_exceptions
+          end
+        end # Cross border worker
+        context "answer none of the above" do
+          setup do
+            add_response 'none_of_the_above'
+          end
+          should "ask how long you are abroad for" do
+            assert_current_node :how_long_are_you_abroad_for?
+          end
+          context "answer less than a year" do
+            setup do
+              add_response 'up_to_a_year'
+            end
+            should "ask why you are going abroad" do
+              assert_current_node :why_are_you_going_abroad?
+            end
+            context "answer holiday or business" do
+              should "state tax credits can continue for 8 weeks" do
+                add_response 'holiday_or_business_trip'
+                assert_current_node :tax_credits_continue_8_weeks
+              end
+            end # going for holiday or business
+            context "answer for medical treatment" do
+              should "state that tax credits can continue for 12 weeks" do
+                add_response 'medical_treatment'
+                assert_current_node :tax_credits_continue_12_weeks
+              end
+            end # going for medical treatment
+            context "answer death" do
+              should "state that tax credits can continue for 12 weeks" do
+                add_response 'death'
+                assert_current_node :tax_credits_continue_12_weeks
+              end
+            end
+          end # going abroad for less than a year
+          context "answer more than a year" do
+            setup do
+              add_response 'more_than_a_year'
+            end
+            should "ask if you have children" do
+              assert_current_node :do_you_have_children?
+            end
+            context "answer yes" do
+              setup do
+                add_response 'yes'
+              end
+              should "ask where you are moving to" do
+                assert_current_node :where_are_you_moving_to_tax_credits?
+              end
+              context "answer Austria (EEA country)" do
+                setup do
+                  add_response 'austria'
+                end
+                should "ask whether you are currently claiming any of the following" do
+                  assert_current_node :currently_claiming?
+                end
+                context "answer yes" do
+                  should "state tax credit eligibility is possible" do
+                    add_response 'yes'
+                    assert_current_node :tax_credits_possible
+                  end
+                end
+                context "answer no" do
+                  should "state tax credits eligibility is unlikely" do
+                    add_response 'no'
+                    assert_current_node :tax_credits_unlikely
+                  end
+                end
+              end # EEA country
+              context "answer Australia" do
+                should "state that tax credit eligibility is unlikely" do
+                  add_response 'australia'
+                  assert_current_node :tax_credits_unlikely
+                end
+              end # 'Other' country
+            end # Yes have children
+            context "answer no" do
+              should "state tax credit eligibility" do
+                add_response 'no'
+                assert_current_node :tax_credits_outcome
+              end
+            end # No children
+          end # going for more than a year
+        end # None of the above
+      end # Currently eligible for tax credits in the UK.
+      context "answer no" do
+        should "state that tax credit eligibility is unlikely" do
+          add_response 'no'
+          assert_current_node :tax_credits_unlikely
+        end
+      end # Not currently eligible for tax credits in UK. 
+    end
   end
 end
