2e37466ae7299ab67474411462932b90293c2a96
Aaron Keogh <aaron.keogh@digital.cabinet-office.gov.uk>
2013-05-30 16:24:32 +0000

    logic and copy updates

--- lib/flows/simplified-expenses-checker.rb
+++ lib/flows/simplified-expenses-checker.rb
@@ -1,14 +1,15 @@
 status :draft
+satisfies_need ""
 
 #Q1 - new or existing business
-multiple_choice :new_or_existing_business? do
-  option :new
-  option :existing
+multiple_choice :claimed_expenses_for_current_business? do
+  option :no
+  option :yes
 
   save_input_as :new_or_existing_business
 
   calculate :is_new_business do
-    new_or_existing_business == "new"
+    new_or_existing_business == "no"
   end
 
   calculate :is_existing_business do
@@ -24,38 +25,79 @@ checkbox_question :type_of_expense? do
   option :motorcycle
   option :using_home_for_business
   option :live_on_business_premises
+  option :none_of_these
 
   calculate :list_of_expenses do
-    responses.last == "none" ? [] : responses.last.split(",")
+    responses.last == "none_of_these" ? [] : responses.last.split(",")
   end
 
   next_node do |response|
     next_question = nil
-    if response == "none"
+    if response == "none_of_these"
       :you_cant_use_result
     else
       responses = response.split(",")
       if (responses & ["car_or_van", "motorcycle"]).any?
-        is_existing_business ? :how_much_write_off_tax? : :is_vehicle_green?
+        :buying_new_vehicle?
       elsif responses.include?("using_home_for_business")
         :current_claim_amount_home?
-      else responses.include?("live_on_business_premises")
+      elsif responses.include?("live_on_business_premises")
         :deduct_from_premises?
       end
     end
   end
 end
 
-#Q3 - write off tax
-money_question :how_much_write_off_tax? do
-  save_input_as :vehicle_tax_amount
+#Q3 - buying new vehicle?
+multiple_choice :buying_new_vehicle? do
+  option :yes
+  option :no
 
-  next_node do
-    list_of_expenses.include?("car_or_van") ? :drive_business_miles_car_van? : :drive_business_miles_motorcycle?
+  next_node do |response|
+    if response == "yes"
+      :is_vehicle_green?
+    else
+      if is_existing_business
+        :capital_allowances?
+      else
+        :how_much_expect_to_claim?
+      end
+    end
   end
+
 end
 
-#Q4 - is vehicle green?
+#Q4 - capital allowances claimed?
+multiple_choice :capital_allowances? do
+  option :yes
+  option :no
+
+  calculate :capital_allowance_claimed do
+    responses.last == "yes" and (list_of_expenses & %w(using_home_for_business live_on_business_premises)).any?
+  end
+
+  next_node do |response|
+    if response == "yes"
+      if (list_of_expenses & %w(using_home_for_business live_on_business_premises)).any?
+        :how_much_expect_to_claim?
+      else
+        :capital_allowance_result
+      end
+    else
+      :how_much_expect_to_claim?
+    end
+  end
+
+end
+
+#Q5 - claim vehicle expenses
+money_question :how_much_expect_to_claim? do
+  save_input_as :vehicle_costs
+
+  next_node :drive_business_miles_car_van?
+end
+
+#Q6 - is vehicle green?
 multiple_choice :is_vehicle_green? do
   option :yes
   option :no
@@ -67,7 +109,7 @@ multiple_choice :is_vehicle_green? do
   next_node :price_of_vehicle?
 end
 
-#Q5 - price of vehicle
+#Q7 - price of vehicle
 money_question :price_of_vehicle? do
   # if green => take user input and store as [green_cost]
   # if dirty  => take 18% of user input and store as [dirty_cost]
@@ -86,22 +128,21 @@ money_question :price_of_vehicle? do
     vehicle_price > 250000.0
   end
 
-
-  next_node :vehicle_private_use_time?
+  next_node :vehicle_business_use_time?
 end
 
-#Q6 - vehicle private use time
-value_question :vehicle_private_use_time? do
+#Q8 - vehicle private use time
+value_question :vehicle_business_use_time? do
   # deduct percentage amount from [green_cost] or [dirty_cost] and store as [green_write_off] or [dirty_write_off]
-  calculate :private_use_percent do
+  calculate :business_use_percent do
     responses.last.gsub("%", "").to_f
   end
   calculate :green_vehicle_write_off do
-    vehicle_is_green ? Money.new(green_vehicle_price * ( private_use_percent / 100 )) : nil
+    vehicle_is_green ? Money.new(green_vehicle_price * ( business_use_percent / 100 )) : nil
   end
 
   calculate :dirty_vehicle_write_off do
-    vehicle_is_green ? nil : Money.new(dirty_vehicle_price * ( private_use_percent / 100 ))
+    vehicle_is_green ? nil : Money.new(dirty_vehicle_price * ( business_use_percent / 100 ))
   end
 
   next_node do
@@ -110,7 +151,7 @@ value_question :vehicle_private_use_time? do
   end
 end
 
-#Q7 - miles to drive for business car_or_van
+#Q9 - miles to drive for business car_or_van
 value_question :drive_business_miles_car_van? do
   # Calculation:
   # [user input 1-10,000] x 0.45
@@ -129,13 +170,15 @@ value_question :drive_business_miles_car_van? do
       :drive_business_miles_motorcycle?
     elsif list_of_expenses.include?("using_home_for_business")
       :current_claim_amount_home?
+    elsif list_of_expenses.include?("live_on_business_premises")
+      :deduct_from_premises?
     else
       :you_can_use_result
     end
   end
 end
 
-#Q8 - miles to drive for business motorcycle
+#Q10 - miles to drive for business motorcycle
 value_question :drive_business_miles_motorcycle? do
   calculate :simple_motorcycle_costs do
     Money.new(responses.last.gsub(",","").to_f * 0.24)
@@ -151,7 +194,7 @@ value_question :drive_business_miles_motorcycle? do
   end
 end
 
-#Q9 - how much do you claim?
+#Q11 - how much do you claim?
 money_question :current_claim_amount_home? do
   save_input_as :home_costs
 
@@ -159,7 +202,7 @@ money_question :current_claim_amount_home? do
 
 end
 
-#Q10 - hours for home work
+#Q12 - hours for home work
 value_question :hours_work_home? do
   calculate :hours_worked_home do
     responses.last.gsub(",","").to_f
@@ -180,14 +223,14 @@ value_question :hours_work_home? do
   end
 end
 
-#Q11 = how much do you deduct from premises for private use?
+#Q13 = how much do you deduct from premises for private use?
 money_question :deduct_from_premises? do
   save_input_as :business_premises_cost
 
   next_node :people_live_on_premises?
 end
 
-#Q12 - people who live on business premises?
+#Q14 - people who live on business premises?
 value_question :people_live_on_premises? do
   calculate :live_on_premises do
     responses.last.to_i
@@ -221,7 +264,7 @@ outcome :you_can_use_result do
   end
 
   precalculate :current_scheme_costs do
-    vehicle = vehicle_tax_amount.to_f || 0
+    vehicle = vehicle_costs.to_f || 0
     green = green_vehicle_write_off.to_f || 0
     dirty = dirty_vehicle_write_off.to_f || 0
     home = home_costs.to_f || 0
@@ -233,22 +276,22 @@ outcome :you_can_use_result do
     simple_costs > current_scheme_costs
   end
 
-  precalculate :result_title do
-    PhraseList.new( can_use_simple ? :simplified_title : :current_title )
-  end
-
   precalculate :simplified_bullets do
     bullets = PhraseList.new
-    bullets << :simple_vehicle_costs_bullet unless simple_vehicle_costs.to_f == 0.0
+    bullets << :simple_vehicle_costs_bullet unless capital_allowance_claimed or simple_vehicle_costs.to_f == 0.0
     bullets << :simple_motorcycle_costs_bullet unless simple_motorcycle_costs.to_f == 0.0
     bullets << :simple_home_costs_bullet unless simple_home_costs.to_f == 0.0
     bullets << :simple_business_costs_bullet unless simple_business_costs.to_f == 0.0
     bullets
   end
 
+  precalculate :capital_allowances_claimed_message do
+    capital_allowance_claimed ? PhraseList.new(:cap_allow_text) : PhraseList.new
+  end
+
   precalculate :current_scheme_bullets do
     bullets = PhraseList.new
-    bullets << :current_vehicle_cost_bullet unless vehicle_tax_amount.to_f == 0.0
+    bullets << :current_vehicle_cost_bullet unless vehicle_costs.to_f == 0.0
     bullets << :current_green_vehicle_write_off_bullet unless green_vehicle_write_off.to_f == 0.0
     bullets << :current_dirty_vehicle_write_off_bullet unless dirty_vehicle_write_off.to_f == 0.0
     bullets << :current_home_costs_bullet unless home_costs.to_f == 0.0
@@ -259,7 +302,7 @@ outcome :you_can_use_result do
   precalculate :over_van_limit_message do
     is_over_limit ? PhraseList.new(:over_van_limit) : PhraseList.new
   end
-
 end
+outcome :capital_allowance_result
 
 
