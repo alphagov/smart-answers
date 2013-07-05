af5a8052f51d16851d1d9786af10e229e5d88595
Kushal Pisavadia <kushi.p@gmail.com>
2013-07-05 12:18:18 +0100

    i18n setup/teardown is no longer required for these tests

--- test/unit/govspeak_presenter_test.rb
+++ test/unit/govspeak_presenter_test.rb
@@ -4,19 +4,6 @@ require 'ostruct'
 
 module SmartAnswer
   class GovspeakPresenterTest < ActiveSupport::TestCase
-    def setup
-      @old_load_path = I18n.config.load_path.dup
-      @example_translation_file =
-        File.expand_path('../../fixtures/node_presenter_test/example.yml', __FILE__)
-      I18n.config.load_path.unshift(@example_translation_file)
-      I18n.reload!
-    end
-
-    def teardown
-      I18n.config.load_path = @old_load_path
-      I18n.reload!
-    end
-
     test "parses markdown into HTML" do
       assert_equal "<h1 id=\"this-is-a-title\">This is a title</h1>\n",
                    GovspeakPresenter.new("# This is a title").html
