4a8f4adbc5612324c24a7d6ed48f9073e77ebade
Aaron Keogh <aaron.keogh@digital.cabinet-office.gov.uk>
2013-07-16 13:07:02 +0000

    create V2 files for Child Maintenance Calculator

---
+++ lib/flows/calculate-your-child-maintenance-v2.rb
@@ -0,0 +1,159 @@
+status :draft
+satisfies_need "2548"
+
+## Q1
+  
+multiple_choice :how_many_children_paid_for? do
+  option "1_child"
+  option "2_children"
+  option "3_children"
+  option "4_children"
+  
+  calculate :number_of_children do
+    ## to_i will look for the first integer in the string
+    responses.last.to_i
+  end
+
+  ## initial filtering: 4+ children -> 2012 scheme
+  ## everyone else -> 2003 scheme
+  calculate :maintenance_scheme do
+    responses.last == '4_children' ? :new : :old
+  end
+
+  next_node do |response|
+    maintenance_scheme = response.to_i > 3 ? :new : :old
+    "gets_benefits_#{maintenance_scheme.to_s}?".to_sym
+  end
+end
+
+## Q2
+multiple_choice :gets_benefits_old? do
+  save_input_as :benefits
+  option "yes"
+  option "no"
+
+  calculate :calculator do
+    Calculators::ChildMaintenanceCalculatorV2.new(number_of_children, maintenance_scheme, benefits)
+  end
+  
+  next_node do |response|
+    if response == 'yes'
+      :how_many_nights_children_stay_with_payee?
+    else
+      :net_income_of_payee?
+    end
+  end
+end
+
+## Q2a
+multiple_choice :gets_benefits_new? do
+  save_input_as :benefits
+  option "yes"
+  option "no"
+
+  calculate :calculator do
+    Calculators::ChildMaintenanceCalculatorV2.new(number_of_children, maintenance_scheme, benefits)
+  end
+  
+  next_node do |response|
+    if response == 'yes'
+      :how_many_nights_children_stay_with_payee?
+    else
+      :gross_income_of_payee?
+    end
+  end
+end
+
+
+
+## Q3
+money_question :net_income_of_payee? do
+
+  next_node do |response|
+    calculator.income = response
+    rate_type = calculator.rate_type
+    if [:nil, :flat].include?(rate_type)
+      "#{rate_type.to_s}_rate_result".to_sym
+    else
+      :how_many_other_children_in_payees_household?
+    end
+  end
+end
+
+## Q3a
+money_question :gross_income_of_payee? do
+
+
+  calculate :flat_rate_amount do
+    calculator.base_amount
+  end
+  next_node do |response|
+    calculator.income = response
+    rate_type = calculator.rate_type
+    if [:nil, :flat].include?(rate_type)
+      "#{rate_type.to_s}_rate_result".to_sym
+    else
+      :how_many_other_children_in_payees_household?
+    end
+  end
+end
+
+## Q4
+value_question :how_many_other_children_in_payees_household? do
+  calculate :calculator do
+    # if converting to int messes things up, raise invalid response
+    # this deals with nil input through the API
+    raise SmartAnswer::InvalidResponse if responses.last.to_i.to_s != responses.last
+    calculator.number_of_other_children = responses.last.to_i
+    calculator
+  end
+  next_node :how_many_nights_children_stay_with_payee?
+end
+
+## Q5
+multiple_choice :how_many_nights_children_stay_with_payee? do
+  option 0
+  option 1
+  option 2
+  option 3
+  option 4
+
+  calculate :child_maintenance_payment do
+    calculator.number_of_shared_care_nights = responses.last.to_i
+    sprintf("%.0f", calculator.calculate_maintenance_payment)
+  end
+  next_node do |response|
+    calculator.number_of_shared_care_nights = response.to_i
+    rate_type = calculator.rate_type
+    if [:nil, :flat].include?(rate_type)
+      "#{rate_type.to_s}_rate_result".to_sym
+    else
+      :reduced_and_basic_rates_result
+    end
+  end
+end
+
+outcome :nil_rate_result do
+  precalculate :nil_rate_reason do
+    if benefits == 'yes'
+      PhraseList.new(:nil_rate_reason_benefits)
+    else
+      PhraseList.new(:nil_rate_reason_income)
+    end
+  end
+end
+outcome :flat_rate_result do
+  precalculate :flat_rate_amount do
+    calculator.base_amount
+  end
+end
+outcome :reduced_and_basic_rates_result do
+  precalculate :rate_type_formatted do
+    rate_type = calculator.rate_type
+    if rate_type.to_s == 'basic_plus'
+      'basic plus'
+    else
+      rate_type.to_s
+    end
+  end
+end
---
+++ test/integration/flows/calculate_child_maintenance_v2_test.rb
@@ -0,0 +1,162 @@
+# encoding: UTF-8
+require_relative '../../test_helper'
+require_relative 'flow_test_helper'
+
+class CalculateChildMaintentanceTest < ActiveSupport::TestCase
+  include FlowTestHelper
+
+  setup do
+    setup_for_testing_flow 'calculate-your-child-maintenance-v2'
+  end
+  
+  ## Q1
+  should "ask how many children maintenance is paid for" do
+    assert_current_node :how_many_children_paid_for?
+  end
+  
+  context "answer 0" do
+    should "raise an error" do
+      add_response 0
+      assert_current_node_is_error
+    end
+  end
+  
+  ## Old scheme
+  context "answer 3" do
+    setup do
+      add_response '3_children'
+    end
+    should "ask if person paying child benefit receives benefits" do
+      assert_current_node :gets_benefits_old?
+    end
+
+    context "answer yes to benefits" do
+      setup { add_response 'yes' }
+
+      should "ask about shared care" do
+        assert_current_node :how_many_nights_children_stay_with_payee?
+      end
+
+      context "no shared care" do
+        setup { add_response '0' }
+
+        should "display flat rate result" do
+          assert_state_variable "flat_rate_amount", 5
+          assert_current_node :flat_rate_result
+        end
+      end 
+
+      context "shared care" do
+        setup { add_response '1' }
+
+        should "display nil rate result" do
+          assert_current_node :nil_rate_result
+        end
+      end
+    end # yes to benefits
+
+    context "no to benefits" do
+      setup { add_response 'no' }
+
+      ## Q3
+      should "ask what the weekly net income of the payee" do
+        assert_current_node :net_income_of_payee?
+      end
+      context "answer 4.99" do
+        should "give flat rate result" do
+          add_response 4.99
+          assert_current_node :nil_rate_result
+        end
+      end
+      context "answer 100" do
+        should "give flat rate result" do
+          add_response 100
+          assert_state_variable "flat_rate_amount", 5
+          assert_current_node :flat_rate_result
+        end
+      end
+      context "answer 101" do
+        setup do
+          add_response 101
+        end
+        ## Q4
+        should "ask how many other children there are in the payees household" do
+          assert_current_node :how_many_other_children_in_payees_household?
+        end
+
+        should "not raise error if given nil" do
+          # add_response converts to string, we explicitly want to test nil here
+          @responses << nil
+          assert_current_node :how_many_other_children_in_payees_household?, :error => true
+        end
+      
+        context "answer 3" do
+          setup do
+            add_response 3
+          end
+          ## Q5
+          should "ask how many nights a week the children stay with the payee" do
+            assert_current_node :how_many_nights_children_stay_with_payee?
+          end
+          context "answer more than 3 nights a week" do
+            setup do
+              add_response "4"
+            end
+          
+            should "give the reduced and basic rates result" do
+              #test just the flow here - calculation values should be in the unit tests
+              assert_current_node :reduced_and_basic_rates_result
+              assert_state_variable "rate_type_formatted", "reduced"
+            end
+          end
+        end
+      end
+    end
+  end # Old scheme
+  
+  # New scheme
+  context "answer 4 children (with 1 other parent)" do
+    setup do
+      add_response '4_children'
+    end
+
+    should "ask about benefits" do
+       assert_current_node :gets_benefits_new?
+    end
+
+    context "no to benefits" do
+      setup { add_response 'no' }
+
+      ## Q3a
+      should "ask what the weekly gross income of the payee" do
+        assert_current_node :gross_income_of_payee?
+      end
+    
+      context "answer 100" do
+        should "give flat rate result" do
+          add_response 100
+          assert_state_variable "flat_rate_amount", 5
+          assert_current_node :flat_rate_result
+        end
+
+        should "flow through to calculation result" do
+          add_response 250.00
+          add_response 1
+          add_response 1
+          assert_current_node :reduced_and_basic_rates_result
+          assert_state_variable "rate_type_formatted", "basic"
+        end
+      end
+
+      context "answer 4000" do
+        should "cap the income at 3000" do
+          add_response 4000.0
+          add_response 0
+          add_response 0
+          assert_current_node :reduced_and_basic_rates_result
+          assert_state_variable "child_maintenance_payment", "482"
+        end
+      end
+    end
+  end # new scheme
+end
