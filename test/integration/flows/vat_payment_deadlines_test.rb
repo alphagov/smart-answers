6af5c2dd9a6a0e3f8ecf197b8b1f54fa46f8e010
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-07-23 10:19:01 +0100

    Add error for non end-of-month date in vat deadlines

--- test/integration/flows/vat_payment_deadlines_test.rb
+++ test/integration/flows/vat_payment_deadlines_test.rb
@@ -15,7 +15,19 @@ class VatPaymentDeadlinesTest < ActiveSupport::TestCase
     assert_current_node :when_does_your_vat_accounting_period_end?
   end
 
-  context "given a date" do
+  context "invalid dates" do
+    should "show error with non end-of-month date" do
+      add_response '2013-05-30'
+      assert_current_node :when_does_your_vat_accounting_period_end?, :error => true
+    end
+
+    should "handle leap years correctly" do
+      add_response '2012-02-28'
+      assert_current_node :when_does_your_vat_accounting_period_end?, :error => true
+    end
+  end
+
+  context "given a date that's the end of a month" do
     setup do
       add_response '2013-04-30'
     end
