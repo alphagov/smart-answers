b95ed20342e3a1acc529db018a4b7cedc602b39b
Tim Abell <tim@timwise.co.uk>
2013-09-06 10:46:42 +0100

    Create v2 of lost-stolen-passport
    https://www.pivotaltracker.com/story/show/55011586

---
+++ lib/flows/report-a-lost-or-stolen-passport-v2.rb
@@ -0,0 +1,64 @@
+status :draft
+satisfies_need 388
+
+multiple_choice :has_your_passport_been_lost_or_stolen? do
+  option :lost
+  option :stolen
+
+  next_node :adult_or_child_passport?
+
+  save_input_as :lost_or_stolen
+end
+
+multiple_choice :adult_or_child_passport? do
+  option :adult
+  option :child
+
+  save_input_as :age
+
+  calculate :child_advice do
+    age == 'child' ? PhraseList.new(:child_forms) : PhraseList.new
+  end
+
+  next_node do
+    case lost_or_stolen
+      when 'lost' then :where_was_the_passport_lost?
+      when 'stolen' then :where_was_the_passport_stolen?
+    end
+  end
+end
+
+multiple_choice :where_was_the_passport_stolen? do
+  option :in_the_uk => :contact_the_police
+  option :abroad => :which_country?
+
+  save_input_as :location
+end
+
+multiple_choice :where_was_the_passport_lost? do
+  option :in_the_uk => :complete_LS01_form
+  option :abroad => :which_country?
+
+  save_input_as :location
+end
+
+country_select :which_country?, :use_legacy_data => true do
+  save_input_as :country
+
+  calculate :country_name do
+    country_list = LegacyCountry.all
+    country_list.select {|c| c.slug == country }.first.name
+  end
+
+  calculate :embassies do
+    embassies = YAML.load_file(Rails.root.join('lib','data','embassies.yml'))
+    raise SmartAnswer::InvalidResponse.new unless embassies[country]
+    embassies[country]
+  end
+
+  next_node :contact_the_embassy
+end
+
+outcome :contact_the_police
+outcome :contact_the_embassy
+outcome :complete_LS01_form
---
+++ test/integration/flows/report_a_lost_or_stolen_passport_v2_test.rb
@@ -0,0 +1,212 @@
+require_relative "../../test_helper"
+require_relative "flow_test_helper"
+
+class ReportALostOrStolenPassportV2Test < ActiveSupport::TestCase
+  include FlowTestHelper
+
+  setup do
+    setup_for_testing_flow "report-a-lost-or-stolen-passport-v2"
+  end
+
+  should "ask whether your passport has been lost or stolen" do
+    assert_current_node :has_your_passport_been_lost_or_stolen?
+  end
+
+  context "Lost passport" do
+    setup do
+      add_response :lost
+    end
+
+    should "ask whether the passport is for a child or an adult" do
+      assert_current_node :adult_or_child_passport?
+    end
+
+    context "for an Adult" do
+      setup do
+        add_response :adult
+      end
+
+      should "ask where your passport was lost" do
+        assert_current_node :where_was_the_passport_lost?
+      end
+
+      context "in the UK" do
+        setup do
+          add_response :in_the_uk
+        end
+
+        should "tell you to fill out the LS01 form" do
+          assert_current_node :complete_LS01_form
+          assert_phrase_list :child_advice, []
+          assert_state_variable :lost_or_stolen, 'lost'
+        end
+      end
+
+      context "abroad" do
+        setup do
+          add_response :abroad
+        end
+
+        should "ask which country you lost your passport in" do
+          assert_current_node :which_country?
+        end
+
+        context "in Azerbaijan" do
+          setup do
+            add_response "azerbaijan"
+          end
+
+          should "tell you to report it to the embassy" do
+            assert_current_node :contact_the_embassy
+            assert_phrase_list :child_advice, []
+            assert_state_variable :lost_or_stolen, 'lost'
+            assert_state_variable :country_name, 'Azerbaijan'
+            assert_equal "British Embassy\n45 Khagani Street\nBaku\nAZ1010", current_state.embassies[0]["address"]
+          end
+        end
+      end
+    end
+
+    context "for a Child" do
+      setup do
+        add_response :child
+      end
+
+      should "ask where your passport was lost" do
+        assert_current_node :where_was_the_passport_lost?
+      end
+
+      context "in UK" do
+        setup do
+          add_response :in_the_uk
+        end
+
+        should "tell you to fill out the LS01 form" do
+          assert_current_node :complete_LS01_form
+          assert_phrase_list :child_advice, [:child_forms]
+          assert_state_variable :lost_or_stolen, 'lost'
+        end
+      end
+
+      context "Abroad" do
+        setup do
+          add_response :abroad
+        end
+
+        should "ask which country you lost your passport in" do
+          assert_current_node :which_country?
+        end
+
+        context "in Azerbaijan" do
+          setup do
+            add_response "azerbaijan"
+          end
+
+          should "tell you to report it to the embassy" do
+            assert_current_node :contact_the_embassy
+            assert_phrase_list :child_advice, [:child_forms]
+            assert_state_variable :lost_or_stolen, 'lost'
+          end
+        end
+      end
+    end
+  end
+
+  context "Passport stolen" do
+    setup do
+      add_response :stolen
+    end
+
+    should "ask whether the passport is for a child or adult" do
+      assert_current_node :adult_or_child_passport?
+    end
+
+    context "for an Adult" do
+      setup do
+        add_response :adult
+      end
+
+      should "ask where your passport was stolen" do
+        assert_current_node :where_was_the_passport_stolen?
+      end
+
+      context "in the UK" do
+        setup do
+          add_response :in_the_uk
+        end
+
+        should "tell you to report it to the police" do
+          assert_current_node :contact_the_police
+          assert_phrase_list :child_advice, []
+          assert_state_variable :lost_or_stolen, 'stolen'
+        end
+      end
+
+      context "abroad" do
+        setup do
+          add_response :abroad
+        end
+
+        should "ask in which country your passport was stolen" do
+          assert_current_node :which_country?
+        end
+
+        context "in Azerbaijan" do
+          setup do
+            add_response "azerbaijan"
+          end
+
+          should "tell you to report it to the embassy" do
+            assert_current_node :contact_the_embassy
+            assert_phrase_list :child_advice, []
+            assert_state_variable :lost_or_stolen, 'stolen'
+          end
+        end
+      end
+    end
+
+    context "for a Child" do
+      setup do
+        add_response :child
+      end
+
+      should "ask where your passport was stolen" do
+        assert_current_node :where_was_the_passport_stolen?
+      end
+
+      context "in the UK" do
+        setup do
+          add_response :in_the_uk
+        end
+
+        should "tell you to report it to the police" do
+          assert_current_node :contact_the_police
+          assert_phrase_list :child_advice, [:child_forms]
+            assert_state_variable :lost_or_stolen, 'stolen'
+        end
+      end
+
+      context "abroad" do
+        setup do
+          add_response :abroad
+        end
+
+        should "ask in which country your passport was stolen" do
+          assert_current_node :which_country?
+        end
+
+        context "in Azerbaijan" do
+          setup do
+            add_response "azerbaijan"
+          end
+
+          should "tell you to report it to the embassy" do
+            assert_current_node :contact_the_embassy
+            assert_phrase_list :child_advice, [:child_forms]
+            assert_state_variable :lost_or_stolen, 'stolen'
+          end
+        end
+      end
+    end
+  end
+end
