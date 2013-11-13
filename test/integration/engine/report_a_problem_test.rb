888fdecb9735979270e63776482976f803fada64
Tim Abell <tim@timwise.co.uk>
2013-11-13 15:42:23 +0000

    Test expected report a problem behaviour
    Monkeypatch to stub report a problem html as under test slimmer doesn't
    put any of the real content in. Added just enough markup to demonstrate
    correct behaviour of the smart-answers code to avoid replicating slimmer
    tests.
    Two Failing tests left:
     * Shouldn't show on question pages
     * Url not being set in form on outcome

---
+++ test/integration/engine/report_a_problem_test.rb
@@ -0,0 +1,54 @@
+# encoding: UTF-8
+require_relative 'engine_test_helper'
+
+class ReportAProblemTest < EngineIntegrationTest
+  with_and_without_javascript do
+    context "smart-answer" do
+      setup do
+        visit "/bridge-of-death"
+      end
+
+      context "start page" do
+        should "show report-a-problem" do
+          assert page.has_selector?('.report-a-problem-toggle a'), "Report a problem link not visible"
+        end
+
+        should "have current url in report-a-problem" do
+          assert_match /\/bridge-of-death$/, page.find('#url', visible: false).value
+        end
+      end
+
+      context "question" do
+        setup do
+          click_on "Start now"
+          fill_in "Name:", :with => "Lancelot"
+          click_on "Next step"
+        end
+
+        should "not show report-a-problem" do
+          refute page.has_selector?('.report-a-problem-toggle a'), "Report a problem link shouldn't be visible"
+        end
+      end
+
+      context "outcome" do
+        setup do
+          click_on "Start now"
+          fill_in "Name:", :with => "Lancelot"
+          click_on "Next step"
+          choose "To seek the Holy Grail"
+          click_on "Next step"
+          choose "Blue"
+          click_on "Next step"
+        end
+
+        should "show report-a-problem" do
+          assert page.has_selector?('.report-a-problem-toggle a'), "Report a problem link not visible"
+        end
+
+        should "have current url in report-a-problem" do
+          assert_match /\/bridge-of-death\/y\/Lancelot\/to_seek_the_holy_grail\/blue$/, page.find('#url', visible: false).value
+        end
+      end
+    end
+  end
+end
