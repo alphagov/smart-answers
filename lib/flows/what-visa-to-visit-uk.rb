f5be593aab2b268b79b37ded5736795f475a4586
Aaron Keogh <aaron.keogh@digital.cabinet-office.gov.uk>
2013-11-15 11:56:17 +0000

    build out the flow, write the tests

--- lib/flows/what-visa-to-visit-uk.rb
+++ lib/flows/what-visa-to-visit-uk.rb
@@ -7,28 +7,110 @@ country_group_ukot = %w(anguilla bermuda british-virgin-islands falkland-islands
 
 country_group_non_visa_national = %w(andorra antigua-and-barbuda argentina australia bahamas barbados belize botswana brazil brunei canada chile costa-rica dominica timor-leste el-salvador grenada guatemala honduras  hong-kong israel japan kiribati south-korea macao malaysia maldives marshall-islands mauritius mexico micronesia monaco namibia nauru new-zealand nicaragua palau panama papua-new-guinea paraguay st-kitts-and-nevis st-lucia st-vincent-and-the-grenadines samoa san-marino seychelles singapore solomon-islands taiwan tonga trinidad-and-tobago tuvalu usa uruguay vanuatu)
 
-country_group_visa_national = %w(armenia azerbaijan bahrain benin bhutan bosnia-and-herzegovina burkina-faso cambodia cape-verde central-african-republic chad china comoros cuba djibouti dominican-republic egypt equatorial-guinea fiji gabon georgia guyana haiti india indonesia jordan kazakhstan korea kuwait kyrgyzstan laos libya madagascar mali mauritania morocco mozambique niger pakistan peru philippines qatar russia sao-tome-and-principe saudi-arabia suriname syria tajikistan thailand togo tunisia turkmenistan ukraine united-arab-emirates uzbekistan vietnam yemen zambia
-)
+country_group_visa_national = %w(armenia azerbaijan bahrain benin bhutan bosnia-and-herzegovina burkina-faso cambodia cape-verde central-african-republic chad china comoros cuba djibouti dominican-republic egypt equatorial-guinea fiji gabon georgia guyana haiti india indonesia jordan kazakhstan korea kuwait kyrgyzstan laos libya madagascar mali mauritania morocco mozambique niger pakistan peru philippines qatar russia sao-tome-and-principe saudi-arabia suriname syria tajikistan thailand togo tunisia turkmenistan ukraine united-arab-emirates uzbekistan vietnam yemen zambia)
 
 country_group_datv = %w(afghanistan albania algeria angola bangladesh belarus bolivia burma burundi cameroon china colombia congo democratic-republic-of-congo ecuador eritrea ethiopia gambia ghana guinea guinea-bissau india iran iraq cote-d-ivoire jamaica kenya kosovo lebanon lesotho liberia macedonia malawi moldova mongolia montenegro nepal nigeria oman the-occupied-palestinian-territories rwanda senegal serbia sierra-leone somalia south-africa south-sudan sri-lanka sudan swaziland tanzania turkey uganda venezuela vietnam zimbabwe)
 
 country_group_eea = %w(austria belgium bulgaria croatia cyprus czech republic denmark estonia finland france germany greece hungary iceland ireland italy latvia liechtenstein lithuania luxemburg malta netherlands norway poland portugal romania slovakia slovenia spain sweden switzerland)
 
 # Q1
-country_select :what_nationality_are_you?, :exclude_countries => exclude_countries do
-  save_input_as :country_group
+country_select :what_passport_do_you_have?, :exclude_countries => exclude_countries do
+  save_input_as :passport_country
 
-  calculate :location do
-    loc = WorldLocation.find(current_location)
-    raise InvalidResponse unless loc
-    loc
+
+  next_node do |response|
+    if country_group_eea.include?(response)
+  	 :outcome_no_visa_needed
+    else
+      :purpose_of_visit?
+    end
   end
+end
 
-  next_node :just_testing
+# Q2
+multiple_choice :purpose_of_visit? do
+  option :study
+  option :work
+  option :visit
+  option :transit
+  option :family
+
+  next_node do |response|
+    case response
+    when 'study'
+      if country_group_visa_national.include?(passport_country) or country_group_datv.include?(passport_country)
+        :outcome_study_y
+      else
+        :outcome_study_m
+      end
+    when 'work'
+      if country_group_visa_national.include?(passport_country) or country_group_datv.include?(passport_country)
+        :outcome_work_y
+      else
+        :outcome_work_m
+      end
+    when 'visit'
+      :type_of_visit?
+    when 'transit'
+      if country_group_datv.include?(passport_country)
+        :outcome_transit_y
+      elsif country_group_visa_national.include?(passport_country)
+        :outcome_transit_m
+      else
+        :outcome_no_visa_needed
+      end
+    when 'family'
+      if country_group_ukot.include?(passport_country)
+        :outcome_family_m
+      else
+        :outcome_family_y
+      end
+    end
+  end
+end
+
+# Q3
+multiple_choice :type_of_visit? do
+  option :tourism
+  option :business
+  option :marriage
+  option :other
+
+  next_node do |response|
+    case response
+    when 'tourism'
+      if country_group_non_visa_national.include?(passport_country)
+        :outcome_visit_business_n
+      else
+        :outcome_general_y
+      end
+    when 'business'
+      if country_group_non_visa_national.include?(passport_country)
+        :outcome_visit_business_n
+      else
+        :outcome_business_y
+      end
+    when 'marriage'
+      :outcome_marriage
+    when 'other'
+      :outcome_all_visit
+    end
+  end
 end
 
 
 
-
-
-outcome :just_testing
+outcome :outcome_no_visa_needed
+outcome :outcome_study_y
+outcome :outcome_study_m
+outcome :outcome_work_y
+outcome :outcome_work_m
+outcome :outcome_transit_y
+outcome :outcome_transit_m
+outcome :outcome_family_y
+outcome :outcome_family_m
+outcome :outcome_visit_business_n
+outcome :outcome_general_y
+outcome :outcome_business_y
+outcome :outcome_marriage
+outcome :outcome_all_visit
