d911fac88c8ab134434780583fbfe71bcaaf879b
Aaron Keogh <aaron.keogh@digital.cabinet-office.gov.uk>
2013-12-04 16:17:02 +0000

    create the v2 files

---
+++ test/integration/flows/energy_grants_calculator_v2_test.rb
@@ -0,0 +1,17 @@
+# encoding: UTF-8
+require_relative '../../test_helper'
+require_relative 'flow_test_helper'
+
+class EnergyGrantsCalculatorV2Test < ActiveSupport::TestCase
+  include FlowTestHelper
+
+  setup do
+    setup_for_testing_flow 'energy-grants-calculator'
+  end
+
+  context "Energy grants calculator" do
+
+  end
+
+
+end
