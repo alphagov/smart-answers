a6a4e0064e408d43725d19f59d1fb3470a31ceeb
Lee Longmore <lee@turkeyred.co.uk>
2013-12-18 17:05:28 +0000

    Tidy whitespace

--- lib/flows/student-finance-forms.rb
+++ lib/flows/student-finance-forms.rb
@@ -1,10 +1,9 @@
 status :published
 satisfies_need "2837"
 
-
 ## Q1
 multiple_choice :type_of_student? do
-  option :"uk-full-time" 
+  option :"uk-full-time"
   option :"uk-part-time"
   option :"eu-full-time"
   option :"eu-part-time"
@@ -31,7 +30,6 @@ multiple_choice :type_of_student? do
   end
 end
 
- 
 ## Q2a
 multiple_choice :form_needed_for_1? do
   option :"apply-loans-grants"
@@ -73,7 +71,7 @@ multiple_choice :form_needed_for_2? do
       when 'dsa-expenses'
         :outcome_dsa_expenses
       else
-        :what_year?     
+        :what_year?
     end
   end
 end
@@ -87,26 +85,26 @@ multiple_choice :what_year? do
   save_input_as :year_required
 
   next_node do |response|
-    if student_type == 'uk-full-time' or student_type == 'uk-part-time' 
-      if form_required == 'apply-loans-grants' 
+    if student_type == 'uk-full-time' or student_type == 'uk-part-time'
+      if form_required == 'apply-loans-grants'
         :continuing_student?
-      elsif form_required == 'proof-identity' 
+      elsif form_required == 'proof-identity'
         if response == 'year-1314'
           if student_type == 'uk-full-time'
             :outcome_proof_identity_1314
           else
             :outcome_proof_identity_1314_pt
           end
-        else 
+        else
           :outcome_proof_identity_1415
         end
-      elsif form_required == 'income-details' 
-        if response == 'year-1314' 
+      elsif form_required == 'income-details'
+        if response == 'year-1314'
           :outcome_parent_partner_1314
         else
           :outcome_parent_partner_1415
         end
-      elsif form_required == 'apply-dsa' 
+      elsif form_required == 'apply-dsa'
         if response == 'year-1314'
           if student_type == 'uk-full-time'
             :outcome_dsa_1314
@@ -116,8 +114,8 @@ multiple_choice :what_year? do
         else
           :outcome_dsa_1415
         end
-      elsif form_required == 'apply-ccg' 
-        if response == 'year-1314' 
+      elsif form_required == 'apply-ccg'
+        if response == 'year-1314'
           :outcome_ccg_1314
         else
           :outcome_ccg_1415
@@ -138,33 +136,33 @@ multiple_choice :continuing_student? do
   save_input_as :continuing_student_state
   next_node do |response|
 
-    if student_type == "uk-full-time" 
-      if form_required == "apply-loans-grants"  
-        if year_required == "year-1314" 
-          if response == "continuing-student" 
+    if student_type == "uk-full-time"
+      if form_required == "apply-loans-grants"
+        if year_required == "year-1314"
+          if response == "continuing-student"
             :outcome_uk_ft_1314_continuing
-          else 
+          else
             :outcome_uk_ft_1314_new
           end
-        elsif year_required == 'year-1415' 
-          if response == 'continuing-student' 
+        elsif year_required == 'year-1415'
+          if response == 'continuing-student'
             :outcome_uk_ft_1415_continuing
-          else 
+          else
             :outcome_uk_ft_1415_new
           end
         end
       end
-    
-    elsif student_type == 'uk-part-time' 
+
+    elsif student_type == 'uk-part-time'
       if form_required == 'apply-loans-grants'
         :pt_course_start?
       end
-        
+
     elsif student_type == 'eu-full-time'
-      if year_required == "year-1314" 
+      if year_required == "year-1314"
         if response == 'continuing-student'
           :outcome_eu_ft_1314_continuing
-        else 
+        else
           :outcome_eu_ft_1314_new
         end
       elsif year_required == "year-1415"
@@ -174,7 +172,7 @@ multiple_choice :continuing_student? do
           :outcome_eu_ft_1415_new
         end
       end
-    
+
     elsif student_type == 'eu-part-time'
       if year_required == 'year-1314'
         if response == 'continuing-student'
