23f550872bf8878b2442e6e9d6027a0c7afd25d0
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2014-06-04 15:55:16 +0000

    Check SASS compiles as part of Jenkins test runner
    * Running asset precompile will catch SASS syntactic/reference errors earlier.
    * Use set -e, to immediately fail if any command fails,  rather than than relying
    on a manual exit, as we're running more than one 'test' command, more robust.

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -x
+
+set -e
+
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment --without=development
 export GOVUK_APP_DOMAIN=dev.gov.uk
 export GOVUK_ASSET_HOST=http://static.dev.gov.uk
@@ -12,5 +15,5 @@ done
 export DISPLAY=:99
 bundle exec rake stats
 RAILS_ENV=test bundle exec rake test
-RESULT=$?
-exit $RESULT
+
+bundle exec rake assets:precompile
