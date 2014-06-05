ab7bb6a4bf88db43b2e4302f4b95c035d3448abb
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-06-05 14:55:35 +0100

    Add unit tests for graph presenter
    including regression test for scenario of missing outcome node
    title/body.

---
+++ test/unit/graph_presenter_test.rb
@@ -0,0 +1,43 @@
+require_relative '../test_helper'
+require_relative '../helpers/i18n_test_helper'
+
+module SmartAnswer
+  class GraphPresenterTest < ActiveSupport::TestCase
+    include I18nTestHelper
+
+    setup do
+      use_only_translation_file!(fixture_file('graph_presenter_test/graph.yml'))
+      @registry = FlowRegistry.new(load_path: fixture_file('graph_presenter_test'), show_drafts: true)
+      @flow = @registry.find('graph')
+      @presenter = GraphPresenter.new(@flow)
+    end
+
+    teardown do
+      reset_translation_files!
+    end
+
+    test "presents labels of simple graph" do
+      expected_labels = {
+        q1?: "MultipleChoice\n-\nWhat is the answer to q1?\n\n( ) yes\n( ) no",
+        done: "Outcome\n-\nDone"
+      }
+
+      assert_equal expected_labels, @presenter.labels
+    end
+
+    test "outcome node label falls back to node name if title missing" do
+      using_only_translation_file(fixture_file('graph_presenter_test/graph_missing_outcome_label.yml')) do
+        assert_equal "Outcome\n-\ndone", @presenter.labels[:done]
+      end
+    end
+
+    test "presents adjacency_list of simple graph" do
+      expected_adjacency_list = {
+        q1?: [[:done, "yes"], [:done, "no"]],
+        done: []
+      }
+
+      assert_equal expected_adjacency_list, @presenter.adjacency_list
+    end
+  end
+end
