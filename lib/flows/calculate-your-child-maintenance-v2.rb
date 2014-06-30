30695885b48fe2037a88315d447f8401cc7cad39
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2014-06-30 15:04:11 +0000

    Update calculate your child maintenance content
    - Dynamic titles for questions based on paying or receiving.
    - Adjustments to content for questions and outcomes.
    - Refactored next nodes in flow.

--- lib/flows/calculate-your-child-maintenance-v2.rb
+++ lib/flows/calculate-your-child-maintenance-v2.rb
@@ -31,6 +31,10 @@ multiple_choice :how_many_children_paid_for? do
   option "2_children"
   option "3_children"
 
+  precalculate :paying_or_receiving_text do
+    paying_or_receiving == "pay" ? "paying" : "receiving"
+  end
+
   precalculate :paying_or_receiving_hint do
     PhraseList.new(:"#{paying_or_receiving}_hint")
   end
@@ -49,35 +53,42 @@ multiple_choice :gets_benefits? do
   option "yes"
   option "no"
 
+  precalculate :benefits_title do
+    PhraseList.new(:"#{paying_or_receiving}_benefits")
+  end
+
   calculate :calculator do
     Calculators::ChildMaintenanceCalculatorV2.new(number_of_children, benefits, paying_or_receiving)
   end
 
-  next_node do |response|
-    if response == 'yes'
-      :how_many_nights_children_stay_with_payee?
-    else
-      :gross_income_of_payee?
-    end
-  end
+  next_node_if(:how_many_nights_children_stay_with_payee?, responded_with('yes'))
+  next_node :gross_income_of_payee?
 end
 
 ## Q3
 money_question :gross_income_of_payee? do
 
-  next_node do |response|
-    calculator.income = response
-    rate_type = calculator.rate_type
-    if [:nil, :flat].include?(rate_type)
-      "#{rate_type.to_s}_rate_result".to_sym
-    else
-      :how_many_other_children_in_payees_household?
-    end
+  precalculate :income_title do
+    PhraseList.new(:"#{paying_or_receiving}_income")
   end
+
+  next_node_calculation :rate_type do |response|
+    calculator.income = response
+    calculator.rate_type
+  end
+
+  next_node_if(:nil_rate_result) { rate_type == :nil }
+  next_node_if(:flat_rate_result) { rate_type == :flat }
+  next_node :how_many_other_children_in_payees_household?
 end
 
 ## Q4
 value_question :how_many_other_children_in_payees_household? do
+
+  precalculate :number_of_children_title do
+    PhraseList.new(:"#{paying_or_receiving}_number_of_children")
+  end
+
   calculate :calculator do
     # if converting to int messes things up, raise invalid response
     # this deals with nil input through the API
@@ -96,20 +107,23 @@ multiple_choice :how_many_nights_children_stay_with_payee? do
   option 3
   option 4
 
+  precalculate :how_many_nights_title do
+    PhraseList.new(:"#{paying_or_receiving}_how_many_nights")
+  end
+
   calculate :child_maintenance_payment do
     calculator.number_of_shared_care_nights = responses.last.to_i
     sprintf("%.0f", calculator.calculate_maintenance_payment)
   end
 
-  next_node do |response|
+  next_node_calculation :rate_type do |response|
     calculator.number_of_shared_care_nights = response.to_i
-    rate_type = calculator.rate_type
-    if [:nil, :flat].include?(rate_type)
-      "#{rate_type.to_s}_rate_result".to_sym
-    else
-      :reduced_and_basic_rates_result
-    end
+    calculator.rate_type
   end
+
+  next_node_if(:nil_rate_result) { rate_type == :nil }
+  next_node_if(:flat_rate_result) { rate_type == :flat }
+  next_node :reduced_and_basic_rates_result
 end
 
 outcome :nil_rate_result do
