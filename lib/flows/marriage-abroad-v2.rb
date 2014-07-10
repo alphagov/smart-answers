091fa36d2c5434f6cc40c7b2c52183fe6807804a
Jack Scotti <jack.scotti@digital.cabinet-office.gov.uk>
2014-07-10 15:52:48 +0000

    Same sex marriage and partnership code refactor

--- lib/flows/marriage-abroad-v2.rb
+++ lib/flows/marriage-abroad-v2.rb
@@ -1220,12 +1220,12 @@ outcome :outcome_ss_marriage do
       phrases << :contact_embassy_or_consulate << :embassies_data
     end
     unless %w(japan).include?(ceremony_country)
-      phrases << :"documents_needed_#{marriage_and_partnership_phrases}"
+      phrases << :documents_needed_ss_marriage_and_partnership
     end
-    phrases << :"what_to_do_#{marriage_and_partnership_phrases}" << :partner_naturalisation_in_uk <<
-                :"fees_table_#{ss_fees_table}" << :list_of_consular_fees << :pay_by_cash_or_credit_card_no_cheque
+    phrases << :"what_to_do_#{marriage_and_partnership_phrases}" << :will_display_in_14_days << :"no_objection_in_14_days_#{marriage_and_partnership_phrases}" << :"provide_two_witnesses_#{marriage_and_partnership_phrases}" << :ss_marriage_footnote << :partner_naturalisation_in_uk << :"fees_table_#{ss_fees_table}" << :list_of_consular_fees << :pay_by_cash_or_credit_card_no_cheque
     phrases
   end
 end
+
 
 outcome :outcome_ss_marriage_not_possible
