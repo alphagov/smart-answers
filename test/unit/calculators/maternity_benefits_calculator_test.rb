a727f242d9f8e560fe98cae395de44b7db6ff81f
Jack Scotti <jack.scotti@digital.cabinet-office.gov.uk>
2014-07-14 13:46:41 +0000

    Promote v2 files into v1

--- test/unit/calculators/maternity_benefits_calculator_test.rb
+++ test/unit/calculators/maternity_benefits_calculator_test.rb
@@ -92,10 +92,10 @@ module SmartAnswer::Calculators
       end
     end
 
-    context "eleven_weeks" do
+    context "sunday_beforeeleven_weeks" do
       should "work out earliest date maternity allowance payments can start" do
-        calculator = MaternityBenefitsCalculator.new(Date.parse("1st April 2013"))
-        assert_equal Date.parse("14 Jan 2013"), calculator.eleven_weeks
+        calculator = MaternityBenefitsCalculator.new(Date.parse("Thu, 19 June 2014"))
+        assert_equal Date.parse("30 March 2014"), calculator.sunday_before_eleven_weeks("Thu, 19 June 2014")
       end
     end
   end
