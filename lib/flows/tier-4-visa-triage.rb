756c362e63865f99c63c1c18bc69173717cad8e7
Jack Scotti <jack.scotti@digital.cabinet-office.gov.uk>
2014-07-17 12:58:20 +0000

    Update flow and content

--- lib/flows/tier-4-visa-triage.rb
+++ lib/flows/tier-4-visa-triage.rb
@@ -21,15 +21,27 @@ value_question :sponsor_id? do
   calculate :data do
     Calculators::StaticDataQuery.new("tier_4_triage_data").data
   end
+
   calculate :sponsor_name do
     name = data["post"].merge(data["online"])[responses.last]
     raise InvalidResponse, :error unless name
     name
   end
+
   calculate :application_link do
     phrases = PhraseList.new
     phrases << :post_link if data["post"].keys.include?(responses.last)
     phrases << :online_link if data["online"].keys.include?(responses.last)
+    phrases
+  end
+
+  calculate :extend_or_switch_visa do
+    phrases = PhraseList.new
+    phrases << :you_must_be_in_uk if %w(switch_general switch_child).include?(type_of_visa)
+    phrases << :general_extend if %w(extend_general).include?(type_of_visa)
+    phrases << :general_switch if %w(switch_general).include?(type_of_visa)
+    phrases << :child_extend if %w(extend_child).include?(type_of_visa)
+    phrases << :child_switch if %w(switch_child).include?(type_of_visa)
     phrases
   end
   next_node(:outcome)
