c2d7cb85d602fb3b535969b68447d06865dc44dc
Jack Scotti <jack.scotti@digital.cabinet-office.gov.uk>
2014-07-18 15:43:53 +0000

    Logic update for phraselists

--- test/integration/flows/tier_4_visa_triage_test.rb
+++ test/integration/flows/tier_4_visa_triage_test.rb
@@ -26,7 +26,7 @@ class Tier4VisaTriageTest < ActiveSupport::TestCase
       should "show outcome with post phraselist" do
         assert_state_variable :sponsor_name, "CAPITAL COLLEGE UK"
         assert_state_variable :sponsor_id, "01HXM2CP2"
-        assert_phrase_list :application_link, [:post_link]
+        assert_phrase_list :application_link, [:post_and_switch_link]
         assert_current_node :outcome
       end
     end
@@ -37,12 +37,12 @@ class Tier4VisaTriageTest < ActiveSupport::TestCase
       should "show outcome with online and switching general phraselists" do
         assert_state_variable :sponsor_name, "Alpha Omega College"
         assert_state_variable :sponsor_id, "1GC8FDP33"
-        assert_phrase_list :application_link, [:online_link]
+        assert_phrase_list :application_link, [:online_and_switch_link]
         assert_phrase_list :extend_or_switch_visa, [:you_must_be_in_uk, :general_switch]
         assert_current_node :outcome
       end
     end
-    context "existing sponsor id provided (online)" do
+    context "non existing sponsor id provided (online)" do
       setup do
         add_response "egrwijadvwbjdsva nk."
       end
@@ -59,12 +59,12 @@ class Tier4VisaTriageTest < ActiveSupport::TestCase
     should "show outcome with extending child phraselists" do
       assert_state_variable :sponsor_name, "WIMBLEDON HIGH SCHOOL"
       assert_state_variable :sponsor_id, "YYAX6VCR8"
-      assert_phrase_list :application_link, [:online_link]
+      assert_phrase_list :application_link, [:online_and_extend_link]
       assert_phrase_list :extend_or_switch_visa, [:child_extend]
       assert_current_node :outcome
     end
   end
-  context "Extending child visa" do
+  context "Switching child visa" do
     setup do
       add_response "switch_child"
       add_response "2W8TVPP77"
@@ -72,12 +72,12 @@ class Tier4VisaTriageTest < ActiveSupport::TestCase
     should "show outcome with extending child phraselists" do
       assert_state_variable :sponsor_name, "14 Stars (London) Ltd t/a EUROPEAN COLLEGE FOR HIGHER EDUCATION"
       assert_state_variable :sponsor_id, "2W8TVPP77"
-      assert_phrase_list :application_link, [:post_link]
+      assert_phrase_list :application_link, [:post_and_switch_link]
       assert_phrase_list :extend_or_switch_visa, [:you_must_be_in_uk, :child_switch]
       assert_current_node :outcome
     end
   end
-  context "Extending child visa" do
+  context "Extending general visa" do
     setup do
       add_response "extend_general"
       add_response "GFHRH18H5"
@@ -85,9 +85,10 @@ class Tier4VisaTriageTest < ActiveSupport::TestCase
     should "show outcome with extending child phraselists" do
       assert_state_variable :sponsor_name, "Kaplan International Colleges Cambridge"
       assert_state_variable :sponsor_id, "GFHRH18H5"
-      assert_phrase_list :application_link, [:post_link]
+      assert_phrase_list :application_link, [:post_and_extend_link]
       assert_phrase_list :extend_or_switch_visa, [:general_extend]
       assert_current_node :outcome
     end
   end
 end
+
