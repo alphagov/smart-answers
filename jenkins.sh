acbedd318e366db0eca64055b7a91b4bdf396c29
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2014-07-22 11:53:01 +0100

    Delete redundant asset/symlink removal on Jenkins
    The asset pipeline should take care of this for us.

--- jenkins.sh
+++ jenkins.sh
@@ -6,12 +6,6 @@ bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment --without
 export GOVUK_APP_DOMAIN=dev.gov.uk
 export GOVUK_ASSET_HOST=http://static.dev.gov.uk
 
-# DELETE STATIC SYMLINKS AND RECONNECT...
-for d in images javascripts templates stylesheets; do
-  rm public/$d
-  ln -s ../../Static/public/$d public/
-done
-
 export DISPLAY=:99
 bundle exec rake stats
 RAILS_ENV=test bundle exec rake test
