42802581930461f1da0ea4d2ed5b193af1a6a9c7
Camille Baldock <camille@camillebaldock.co.uk>
2014-08-07 10:32:19 +0100

    Student finance form auto-converted question, register as smartdown

--- lib/smartdown/registry.rb
+++ lib/smartdown/registry.rb
@@ -2,8 +2,9 @@ module Smartdown
   class Registry
 
     def self.check(name)
-      smartdown_questions = ["animal-example"]
+      smartdown_questions = ["animal-example", "student-finance-forms-sd"]
       smartdown_questions.include? name
     end
+
   end
 end
