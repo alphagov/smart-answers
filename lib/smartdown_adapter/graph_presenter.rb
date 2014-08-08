aeb0e9add8aaa0dd69418ee24919b2e994b41e59
Camille Baldock <camille@camillebaldock.co.uk>
2014-08-08 10:59:27 +0100

    Correct labels for predicate rules

--- lib/smartdown_adapter/graph_presenter.rb
+++ lib/smartdown_adapter/graph_presenter.rb
@@ -16,13 +16,7 @@ module SmartdownAdapter
           adjacency_list[node.name.to_s] = []
           node.next_nodes.each do |nextnode|
             nextnode.rules.each do |rule|
-              if rule.is_a? Smartdown::Model::NestedRule
-                rule.children.each do |nested_rule|
-                  adjacency_list[node.name.to_s] << [nested_rule.outcome.to_s, "TODO some rules"]
-                end
-              else
-                adjacency_list[node.name.to_s] << [rule.outcome.to_s, "TODO some rules"]
-              end
+              adjacency_list = add_rule_to_adjacency_list(adjacency_list, node, rule)
             end
           end
         end
@@ -41,12 +35,44 @@ module SmartdownAdapter
 
     def to_hash
       {
-          labels: labels,
-          adjacencyList: adjacency_list
+        labels: labels,
+        adjacencyList: adjacency_list
       }
     end
 
     private
+
+    def add_rule_to_adjacency_list(adjacency_list, node, rule, parent_rules = [])
+      if rule.is_a? Smartdown::Model::NestedRule
+        rule.children.each do |rule_child|
+          new_parent_rules = parent_rules + [rule]
+          adjacency_list = add_rule_to_adjacency_list(adjacency_list, node, rule_child, new_parent_rules)
+        end
+      else
+        adjacency_list[node.name.to_s] << [rule.outcome.to_s, rule_label(rule, parent_rules)]
+      end
+      adjacency_list
+    end
+
+    def rule_label(rule, parent_rules)
+      all_rules = parent_rules.push(rule)
+      predicate_labels = all_rules.map(&:predicate).map{ |predicate| predicate_label(predicate) }
+      predicate_labels.join(" AND ")
+    end
+
+    def predicate_label(predicate)
+      case predicate
+        when Smartdown::Model::Predicate::Equality
+          "#{predicate.varname} is #{predicate.expected_value}"
+        when Smartdown::Model::Predicate::Named
+          "#{predicate.name}"
+        when Smartdown::Model::Predicate::SetMembership
+          "#{predicate.varname} in [#{predicate.values.join(",")}]"
+        else
+          "Unknown predicate type #{predicate}"
+      end
+    end
+
     def graph_label_text(node)
       text = node.class.to_s.split("::").last + "\n-\n"
       case node
@@ -74,50 +100,5 @@ module SmartdownAdapter
       end * "\n"
     end
 
-    def allow_missing_interpolations(&block)
-      old = I18n.config.missing_interpolation_argument_handler
-      I18n.config.missing_interpolation_argument_handler = ->(key) { "((#{key}))" }
-      block.call
-    ensure
-      I18n.config.missing_interpolation_argument_handler = old
-    end
-
-    def i18n_prefix(node)
-      "flow.#{@flow.name}.#{node.name}"
-    end
-
-    def node_title(node)
-      allow_missing_interpolations do
-        I18n.translate!("#{i18n_prefix(node)}.title", {})
-      end
-    rescue I18n::MissingTranslationData
-      ""
-    end
-
-    def node_body(node)
-      allow_missing_interpolations do
-        I18n.translate!("#{i18n_prefix(node)}.body", {})
-      end
-    rescue I18n::MissingTranslationData
-      ""
-    end
-
-    def first_line_of_body(node)
-      node_body(node).split("\n\n").first || ""
-    end
-
-    def translate_option(node, option)
-      allow_missing_interpolations do
-        begin
-          I18n.translate!("flow.#{@flow.name}.options.#{option}")
-        rescue I18n::MissingTranslationData
-          I18n.translate("#{i18n_prefix(node)}.options.#{option}")
-        end
-      end
-    end
-
-    def presenter
-      @presenter ||= FlowRegistrationPresenter.new(@flow)
-    end
   end
 end
