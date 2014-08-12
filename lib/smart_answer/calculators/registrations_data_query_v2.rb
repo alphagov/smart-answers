4d914811122348b04604898febd97faa887ddf9d
Aaron Keogh <aaron.keogh@digital.cabinet-office.gov.uk>
2014-08-12 15:55:37 +0000

    adding oru phase 2 countries

--- lib/smart_answer/calculators/registrations_data_query_v2.rb
+++ lib/smart_answer/calculators/registrations_data_query_v2.rb
@@ -27,7 +27,7 @@ module SmartAnswer::Calculators
 
     CASH_AND_CARD_COUNTRIES = %w(estonia)
 
-    ORU_TRANSITIONED_COUNTRIES = %w(american-samoa andorra aruba belgium bonaire-st-eustatius-saba curacao france french-guiana french-polynesia germany greece guadeloupe italy liechtenstein luxembourg martinique mayotte monaco netherlands portugal reunion san-marino spain st-maarten st-pierre-and-miquelon switzerland united-arab-emirates usa wallis-and-futuna)
+    ORU_TRANSITIONED_COUNTRIES = %w(american-samoa andorra aruba belgium bonaire-st-eustatius-saba brunei burma cambodia curacao china denmark fiji finland france french-guiana french-polynesia germany greece guadeloupe hong-kong iceland indonesia italy japan kiribati liechtenstein luxembourg macao malaysia marshall-islands martinique mayotte micronesia monaco mongolia nauru netherlands north-korea norway papua-new-guinea portugal reunion samoa san-marino singapore solomon-islands south-korea spain st-maarten st-pierre-and-miquelon sweden switzerland taiwan thailand timor-leste tonga tuvalu united-arab-emirates usa vanuatu vietnam wallis-and-futuna)
 
     ORU_DOCUMENTS_VARIANT_COUNTRIES = %w(andorra belgium france italy monaco netherlands portugal spain united-arab-emirates usa)
 
