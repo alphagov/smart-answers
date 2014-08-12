13e27edb82168317bd23e960f0507a78301afcb6
Camille Baldock <camille@camillebaldock.co.uk>
2014-08-12 16:15:35 +0100

    Next steps

--- lib/smartdown_adapter/outcome.rb
+++ lib/smartdown_adapter/outcome.rb
@@ -6,7 +6,8 @@ module SmartdownAdapter
     end
 
     def next_steps
-      "not currently possible"
+      next_step_element = elements.find{|element| element.is_a? Smartdown::Model::Element::NextSteps}
+      GovspeakPresenter.new(next_step_element.content).html if next_step_element
     end
 
   end
