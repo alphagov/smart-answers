da026f3add767e852a959a6b75e24b82d4798cbe
David Singleton <david@dsingleton.co.uk>
2014-08-29 15:54:20 +0100

    Refactor Smartdown registry to treat transition like draft status
    Transition flows, by convention, have a '-transition' suffix to distinguish them from
    the original smart_answer version.
    
    When in transition we always want them to be available on preview, so
    treat them the same as draft status

--- lib/smartdown_adapter/registry.rb
+++ lib/smartdown_adapter/registry.rb
@@ -25,19 +25,13 @@ module SmartdownAdapter
     end
 
     def self.check(name, options = FLOW_REGISTRY_OPTIONS)
-      binding.pry
-      show_drafts = options.fetch(:show_drafts, false)
-      show_transitions = options.fetch(:show_transitions, false)
-      use_smartdown_question = false
-      if self.smartdown_questions.include? name
-        coversheet_path = Rails.root.join('lib', 'smartdown_flows', name, "#{name}.txt")
-        input = Smartdown::Api::DirectoryInput.new(coversheet_path)
-        smartdown_flow = Smartdown::Api::Flow.new(input)
-        use_smartdown_question = (smartdown_flow && smartdown_flow.draft? && show_drafts) ||
-        (smartdown_flow && smartdown_flow.transition? && show_transitions) ||
-        (smartdown_flow && smartdown_flow.published?)
-      end
-      use_smartdown_question
+      return unless self.smartdown_questions.include? name
+
+      flow = self.build_flow(name)
+
+      return true if flow.published?
+      return true if options[:show_transitions] && flow.transition?
+      return true if options[:show_drafts] && (flow.draft? || flow.transition?)
     end
 
     def self.flows
