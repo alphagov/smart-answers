bf66ba7a75ae1c54f57e0f25671f9ead945bd228
Brendan Butler <brendan.butler@digital.cabinet-office.gov.uk>
2014-09-05 13:51:06 +0000

    New class for formatting country names

---
+++ lib/smart_answer/calculators/country_name_formatter.rb
@@ -0,0 +1,28 @@
+module SmartAnswer::Calculators
+  class CountryNameFormatter
+
+    COUNTRIES_WITH_DEFINITIVE_ARTICLES = %w(bahamas british-virgin-islands cayman-islands czech-republic democratic-republic-of-congo dominican-republic falkland-islands gambia maldives marshall-islands netherlands philippines seychelles solomon-islands south-georgia-and-south-sandwich-islands turks-and-caicos-islands united-arab-emirates)
+
+    FRIENDLY_COUNTRY_NAME = {
+      "democratic-republic-of-congo" => "Democratic Republic of Congo",
+      "cote-d-ivoire" => "Cote d'Ivoire",
+      "pitcairn" => "Pitcairn Island",
+      "south-korea" => "South Korea",
+      "st-helena-ascension-and-tristan-da-cunha" => "St Helena, Ascension and Tristan da Cunha",
+      "usa" => "the USA"
+    }
+
+    def definitive_article(country, capitalized=false)
+      result = country_name(country)
+      if COUNTRIES_WITH_DEFINITIVE_ARTICLES.include?(country)
+        result = capitalized ? "The #{result}" : "the #{result}"
+      end
+      result
+    end
+
+    def country_name(country)
+      WorldLocation.all.find { |c| c.slug == country }.name
+    end
+
+  end
+end
