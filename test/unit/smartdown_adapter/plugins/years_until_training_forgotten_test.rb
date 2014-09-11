60eacee69a0cb085f83ca605df6b995f92d7f609
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2014-09-11 15:59:07 +0000

    Fix old-style Smartdown plugins
    Previously a function was a lambda on state, that was called with state.
    
    Since v0.4.0 a function is a defined concept, which can have arguments
    and is parsed as a lambda on state that is called with the arguments
    specified, eg
    
    %{function_name(answer_1)}
    
    This means we no longer need to fake out Smartdown internal state for
    tests in an entirely different app, which is an improvement.

--- test/unit/smartdown_adapter/plugins/years_until_training_forgotten_test.rb
+++ test/unit/smartdown_adapter/plugins/years_until_training_forgotten_test.rb
@@ -1,15 +1,9 @@
 require 'test_helper'
-require 'helpers/smartdown_adapter_helper'
 
 module SmartdownAdapter
   class YearsUntilTrainingForgottenTest < ActiveSupport::TestCase
-    include SmartdownAdapterHepler
-
     test "it should be 20 years after training date" do
-      state = mock_smartdown_state({
-        training_date: 5.years.ago.to_date.to_s
-      })
-      assert_equal 15, SmartdownAdapter::Plugins::YearsUntilTrainingForgotten.new.call(state)
+      assert_equal 15, SmartdownAdapter::Plugins::YearsUntilTrainingForgotten.new.call(5.years.ago.to_date.to_s)
     end
   end
 end
