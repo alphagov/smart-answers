42e4cbd8591b0bb7eaca0082d9272ccb75c3e288
Camille Baldock <camille@camillebaldock.co.uk>
2014-10-21 16:48:50 +0100

    Use Smartdown answer #humanize methods for previous answer display

--- lib/smartdown_adapter/previous_question_presenter.rb
+++ lib/smartdown_adapter/previous_question_presenter.rb
@@ -14,19 +14,7 @@ module SmartdownAdapter
     end
 
     def response_label(response_key)
-      case smartdown_previous_question.question
-        when Smartdown::Api::MultipleChoice
-          smartdown_previous_question.options.find{|option| option.value == response_key}.label
-        when Smartdown::Api::DateQuestion
-          #TODO: formatting date here until we return answer object as part of previous question object
-          #and we can use the smartdown humanize methods
-          Date.parse(response_key).strftime("%-d %B %Y")
-        when Smartdown::Api::SalaryQuestion
-          salary_array = response_key.split("-")
-          "#{salary_array[0]} per #{salary_array[1]}"
-        when Smartdown::Api::TextQuestion
-          response_key
-      end
+      smartdown_previous_question.answer.humanize
     end
 
   private
