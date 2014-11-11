4a313f114b864871deee7f00ae2aa359ef9326e5
Karl Entwistle <karl.entwistle@unboxedconsulting.com>
2014-11-11 13:41:03 +0000

    Tidy up example usage with a simple data button

--- lib/smartdown_plugins/animal-example-simple.rb
+++ lib/smartdown_plugins/animal-example-simple.rb
@@ -2,17 +2,9 @@ module SmartdownPlugins
   module AnimalExampleSimple
     extend DataPartial
 
-    def self.data_simple_button(_)
-      locals = {button_data: {url: 'hello', text: 'hello'}}
+    def self.data_simple_button(name)
+      locals = {button_data: {url: '#', text: name}}
       render 'button', locals: locals
-    end
-
-    def self.data_embassy(country_name)
-      location = ::WorldLocation.find('afghanistan')
-      organisation = location.fco_organisation
-      overseas_passports_embassies = organisation.offices_with_service 'Registrations of Marriage and Civil Partnerships'
-      locals = {overseas_passports_embassies: overseas_passports_embassies}
-      render 'overseas_passports_embassies', locals: locals
     end
   end
 end
