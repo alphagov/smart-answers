873f7b27b58eb9c22d0b1e29d2cf5ec6f3771f7e
Camille Baldock <camille@camillebaldock.co.uk>
2014-11-18 13:37:08 +0000

    Flow logic changes

--- lib/smartdown_plugins/pay-leave-for-parents/render_time.rb
+++ lib/smartdown_plugins/pay-leave-for-parents/render_time.rb
@@ -4,56 +4,40 @@ require 'smartdown/model/answer/date'
 module SmartdownPlugins
   module PayLeaveForParents
 
+    def self.continuity_start_date(date)
+      "TODO"
+    end
+
+    def self.continuity_end_date(date)
+      "TODO"
+    end
+
+    def self.lower_earnings_start_date(date)
+      "TODO"
+    end
+
+    def self.lower_earnings_end_date(date)
+      "TODO"
+    end
+
+    def self.earnings_employment_start_date(date)
+      "TODO"
+    end
+
+    def self.earnings_employment_end_date(date)
+      "TODO"
+    end
+
     def self.claim_date_maternity_allowance(date)
-      build_date_answer(date.value - 14.weeks)
+      "TODO"
     end
 
-    def self.earliest_start(date)
-      build_date_answer(date.value - 11.weeks)
-    end
-
-    def self.earnings_test_start_date(date)
-      build_date_answer(date.value - 66.weeks)
-    end
-
-    def self.end_of_14_week_maternity_allowance(date)
-      build_date_answer(date.value + 14.weeks)
-    end
-
-    def self.end_of_additional_leave(date)
-      build_date_answer(date.value + 52.weeks)
+    def self.earliest_start_mat_leave(date)
+      "TODO"
     end
 
     def self.end_of_additional_paternity_leave(date)
-      build_date_answer(date.value + 1.year)
-    end
-
-    def self.end_of_additional_paternity_pay_from_leave(date)
-      build_date_answer(date.value + 52.weeks)
-    end
-
-    def self.end_of_additional_paternity_pay_from_match_or_due_date(date)
-      build_date_answer(date.value + 28.weeks)
-    end
-
-    def self.end_of_adoption_pay(date)
-      build_date_answer(date.value + 39.weeks)
-    end
-
-    def self.end_of_maternity_allowance(date)
-      build_date_answer(date.value + 28.weeks)
-    end
-
-    def self.end_of_maternity_pay(date)
-      build_date_answer(date.value + 39.weeks)
-    end
-
-    def self.end_of_ordinary_leave(date)
-      build_date_answer(date.value + 26.weeks)
-    end
-
-    def self.end_of_paternity_leave(date)
-      build_date_answer(date.value + 2.weeks)
+      "TODO"
     end
 
     def self.end_of_shared_parental_leave(date)
@@ -61,31 +45,11 @@ module SmartdownPlugins
     end
 
     def self.latest_pat_leave(date)
-      build_date_answer(date.value + 56.days)
+      "TODO"
     end
 
-    def self.lower_earnings_start(date)
-      build_date_answer(date.value - 23.weeks)
-    end
-
-    def self.minimum_start_date(date)
-      build_date_answer(date.value - 41.weeks)
-    end
-
-    def self.minimum_end_date(date)
-      build_date_answer(date.value - 15.weeks)
-    end
-
-    def self.notice_adopt_leave(date)
-      build_date_answer(date.value + 7.days)
-    end
-
-    def self.notice_date_sap(date)
-      build_date_answer(date.value - 28.days)
-    end
-
-    def self.notice_date_smp(date)
-      build_date_answer(date.value - 28.days)
+    def self.maternity_leave_notice_date(date)
+      "TODO"
     end
 
     def self.paternity_leave_notice_date(date)
@@ -93,24 +57,20 @@ module SmartdownPlugins
       build_date_answer(preceding_monday - 15.weeks)
     end
 
-    def self.paternity_pay_notice_date(date_leave_2)
-      build_date_answer(date_leave_2.value - 28.days)
+    def self.start_of_additional_paternity_leave(date)
+      "TODO"
     end
 
-    def self.qualifying_week(date)
-      build_date_answer(date.value - 15.weeks)
-    end
-
-    def self.rate_of_paternity_pay(salary_2)
-      build_money_answer(nine_tenths_weekly_salary_capped_at_138_point_18(salary_2))
+    def self.start_of_maternity_allowance(date)
+      "TODO"
     end
 
     def self.rate_of_maternity_allowance(salary_1_66_weeks)
       build_money_answer(nine_tenths_weekly_salary_capped_at_138_point_18(salary_1_66_weeks))
     end
 
-    def self.rate_of_sap(salary_1)
-      build_money_answer(nine_tenths_weekly_salary_capped_at_138_point_18(salary_1))
+    def self.rate_of_paternity_pay(salary_2)
+      build_money_answer(nine_tenths_weekly_salary_capped_at_138_point_18(salary_2))
     end
 
     def self.rate_of_shpp(salary)
@@ -125,28 +85,12 @@ module SmartdownPlugins
       build_money_answer(nine_tenths_weekly_salary_capped_at_138_point_18(salary_1))
     end
 
-    def self.start_of_additional_leave(date_leave_1)
-      build_date_answer(date_leave_1.value + 26.weeks + 1.day)
-    end
-
-    def self.start_of_additional_paternity_leave(date)
-      build_date_answer(date.value + 20.weeks)
-    end
-
-    def self.start_of_maternity_allowance(date)
-      build_date_answer(date.value - 11.weeks)
-    end
-
     def self.total_aspp(salary_2)
       build_money_answer(rate_of_paternity_pay(salary_2) * 26)
     end
 
     def self.total_maternity_allowance(salary_1_66_weeks)
       build_money_answer(rate_of_maternity_allowance(salary_1_66_weeks) * 39)
-    end
-
-    def self.total_sap(salary_1)
-      build_money_answer(rate_of_sap(salary_1) * 39)
     end
 
     def self.total_smp(salary_1)
