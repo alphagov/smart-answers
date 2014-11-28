153ac3d7161e16d2c06e314a70b9d9eb474024ae
Karl Entwistle <karl.entwistle@unboxedconsulting.com>
2014-11-28 11:02:40 +0000

    what

--- jenkins.sh
+++ jenkins.sh
@@ -12,7 +12,6 @@ export GOVUK_APP_DOMAIN=dev.gov.uk
 export GOVUK_ASSET_HOST=http://static.dev.gov.uk
 
 export DISPLAY=:99
-bundle exec rake stats
 RAILS_ENV=test bundle exec rake test
 
 bundle exec rake assets:precompile
