6d02284a0fc5ce7e06c422fac58472ad1f563982
Sven Agnew <sven.agnew@digital.cabinet-office.gov.uk>
2015-02-16 12:04:02 +0000

    Test for outcome 1 at new exit in the flow
    This test now expects an outcome instead of continuing questions
    about parents for the case england-or-wales && partner && >250k
    && no-children.
    
    Sven

--- test/integration/smart_answer_flows/inherits_someone_dies_without_will_v2_test.rb
+++ test/integration/smart_answer_flows/inherits_someone_dies_without_will_v2_test.rb
@@ -39,32 +39,9 @@ class InheritsSomeoneDiesWithoutWillV2Test < ActiveSupport::TestCase
         context "no-children" do
           setup { add_response "no" }
 
-          context "no-parents" do
-            setup { add_response "no" }
-
-            context "no-siblings" do
-              setup { add_response "no" }
-              should "give outcome 1" do
-                assert_current_node :outcome_1 # T22
-                assert_phrase_list :next_step_links, [:wills_link, :inheritance_link]
-              end
-            end
-            context "siblings" do
-              setup { add_response "yes" }
-              should "give outcome 22" do
-                assert_current_node :outcome_22 # T22
-                assert_phrase_list :next_step_links, [:wills_link, :inheritance_link]
-              end
-            end
-          end
-
-          context "parents" do
-            setup { add_response "yes" }
-
-            should "give outcome 21" do
-              assert_current_node :outcome_21 # T23
-              assert_phrase_list :next_step_links, [:wills_link, :inheritance_link]
-            end
+          should "give outcome 1" do
+            assert_current_node :outcome_1
+            assert_phrase_list :next_step_links, [:wills_link, :inheritance_link]
           end
         end
       end
