02e0e4e995ae28eb8e82c5fdddd8d96980ed035c
Jacopo Scotti <jacopojackscotti@gmail.com>
2015-03-11 12:02:11 +0000

    Add temp secret_key_base
    Cookies are encrypted in Rails 4, I need to leave both secret_token and secret_key_base whilst upgrading.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 SmartAnswers::Application.config.secret_token = '8b5eb781d13b5968e41f62ac831d9c729f84983cb3bb29ab03f77c2b4948c9a679198f0d4111a825ad86607b82cd4e291c3bef35d86da04109f6d6799cc8e69e'
+
+SmartAnswers::Application.config.secret_key_base = 'xxx8b5eb781d13b5968e41f62ac831d9c729f84983cb3bb29ab03f77c2b4948c9a679198f0d4111a825ad86607b82cd4e291c3bef35d86da04109f6d6799cc8e69e'
