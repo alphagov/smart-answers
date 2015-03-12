aea403c6fb91bdba1b2663ec50459abe5fdbc71c
Ben Janecke and Sven Agnew <sven+BenJanecke+svenagnew@kangohammer.com>
2015-03-12 18:14:23 +0200

    Add test case for Part Three copy and logic changes

--- test/integration/smart_answer_flows/marriage_abroad_v2_test.rb
+++ test/integration/smart_answer_flows/marriage_abroad_v2_test.rb
@@ -2458,4 +2458,19 @@ class MarriageAbroadV2Test < ActiveSupport::TestCase
       assert_phrase_list :consular_cni_os_start, [:local_resident_os_consular_cni,:italy_os_consular_cni_ceremony_not_italy_or_spain,:consular_cni_all_what_you_need_to_do,:consular_cni_os_ceremony_not_spain_or_italy,:consular_cni_os_foreign_resident_ceremony_not_germany_italy,:check_with_embassy_consulate_or_notary_public,:embassies_data,:living_in_residence_country_3_days,:consular_cni_variant_local_resident_or_foreign_resident_notary_public,:consular_cni_os_not_uk_resident_ceremony_not_germany,:consular_cni_os_other_resident_ceremony_not_germany_or_spain,:consular_cni_os_download_documents_notary_public,:consular_cni_os_foreign_resident_ceremony_notary_public_greece]
     end
   end
+
+  context "ceremony in Uzbekistan, resident in the Uk, partner from anywhere, opposite sex" do
+    setup do
+      worldwide_api_has_organisations_for_location('uzbekistan', read_fixture_file('worldwide/uzbekistan_organisations.json'))
+      add_response 'uzbekistan'
+      add_response 'uk'
+      add_response 'uk_england'
+      add_response 'partner_other'
+      add_response 'opposite_sex'
+    end
+    should "not include the links to download documents" do
+      assert_current_node :outcome_os_consular_cni
+      assert_phrase_list :consular_cni_os_start, [:uk_resident_os_consular_cni,:italy_os_consular_cni_ceremony_not_italy_or_spain,:consular_cni_all_what_you_need_to_do,:consular_cni_os_ceremony_not_spain_or_italy,:cni_at_local_register_office_notary_public,:consular_cni_os_uk_resident_legalisation,:consular_cni_os_uk_resident_not_italy_or_portugal]
+    end
+  end
 end
