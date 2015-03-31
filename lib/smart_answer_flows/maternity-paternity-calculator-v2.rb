62418d51a506b09c0f0fcba89ebff396b6d067bb
Tadas Tamosauskas <tadas@pdfcv.com>
2015-03-31 13:46:55 +0100

    V2s for maternity-paternity calculator for employers

---
+++ lib/smart_answer_flows/maternity-paternity-calculator-v2.rb
@@ -0,0 +1,14 @@
+status :published
+satisfies_need "100990"
+
+## Q1
+multiple_choice :what_type_of_leave? do
+  save_input_as :leave_type
+  option maternity: :baby_due_date_maternity?
+  option paternity: :leave_or_pay_for_adoption?
+  option adoption: :taking_paternity_leave_for_adoption?
+end
+
+use_shared_logic ("adoption-calculator-v2")
+use_shared_logic ("paternity-calculator-v2")
+use_shared_logic ("maternity-calculator-v2")
