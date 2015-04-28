3437c3f7b19e185aa2632841e6fb98cded7005a7
Ben Janecke <benjanecke@gmail.com>
2015-04-28 11:55:23 +0100

    Create V3 of employee redundancy calculator

---
+++ lib/smart_answer/calculators/redundancy_calculator_v3.rb
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+include ActionView::Helpers::NumberHelper
+
+module SmartAnswer::Calculators
+  class RedundancyCalculatorV3
+    attr_reader :pay, :number_of_weeks_entitlement
+
+    def initialize(rate, age, years, weekly_pay)
+      @pay = @number_of_weeks_entitlement = 0
+      age = age.to_i
+      years = [20, years.to_i].min
+
+      (1..years.to_i).each do |i|
+        entitlement_ratio = ratio(age)
+        @pay += ([rate.to_f, weekly_pay.to_f].min * entitlement_ratio).round(10)
+        @number_of_weeks_entitlement += entitlement_ratio
+        age -= 1
+      end
+    end
+
+    def ratio(age)
+      return 0.5 if (0..22).include?(age)
+      return 1.0 if (23..41).include?(age)
+      return 1.5 if (42..1000).include?(age)
+    end
+
+    def format_money(amount)
+      self.class.format_money(amount)
+    end
+
+    def self.format_money(amount)
+      formatted_amount = number_to_currency(amount, precision: 2, locale: :gb, unit: "")
+      formatted_amount.sub(".00", "")
+    end
+
+    def self.redundancy_rates(date)
+      RatesQuery.new('redundancy_pay_v3').rates(date)
+    end
+  end
+end
