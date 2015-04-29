1c4fe68595bd928cb76a706e1aaefc0e81c8ed30
Ben Janecke <benjanecke@gmail.com>
2015-04-29 10:33:46 +0100

    Merge v3 of redundancy pay
    Uprated max amount for 2015/2016 from 13,920 to 14,250

--- test/unit/calculators/redundancy_calculator_test.rb
+++ test/unit/calculators/redundancy_calculator_test.rb
@@ -35,7 +35,9 @@ module SmartAnswer::Calculators
         assert_equal "13,500", RedundancyCalculator.redundancy_rates(Date.new(2013, 02, 01)).max
         assert_equal "13,500", RedundancyCalculator.redundancy_rates(Date.new(2014, 04, 05)).max
         assert_equal "13,920", RedundancyCalculator.redundancy_rates(Date.new(2014, 04, 06)).max
-        assert_equal "13,920", RedundancyCalculator.redundancy_rates(Date.new(Date.today.year, 12, 31)).max
+        assert_equal "13,920", RedundancyCalculator.redundancy_rates(Date.new(2015, 04, 05)).max
+        assert_equal "14,250", RedundancyCalculator.redundancy_rates(Date.new(2015, 04, 06)).max
+        assert_equal "14,250", RedundancyCalculator.redundancy_rates(Date.new(Date.today.year, 12, 31)).max
       end
 
       should "use the most recent rate for far future dates" do
