9dc0c1c91d878384a2af60b63646c74a17d388ca
James Mead <james@floehopper.org>
2015-04-30 15:28:03 +0100

    Use standard Rails testing Railtie
    The main motivation for doing this is to make the app more like a standard
    Rails app. However, it also has the benefit that we can remove the code which
    was defining the `test:all` rake task.
    
    Other side benefits are:
    
    * The ability to run a subset of tests by filename and/or test method name [1].
    * A bunch of pre-defined tasks for running subsets of tests
      e.g. `test:units` [2]. Annoyingly these rake tasks don't have descriptions
      and so they don't show up when you execute `rake -T`.
    
    [1]: http://guides.rubyonrails.org/v4.2.1/testing.html#running-tests
    [2]: http://guides.rubyonrails.org/v4.2.1/testing.html#rake-tasks-for-running-your-tests

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 Bundler.require(:default, Rails.env)
 
--- lib/tasks/testing.rake
+++ lib/tasks/testing.rake
@@ -14,5 +14,3 @@ Rake::TestTask.new("test:unit:calculators") do |t|
   t.libs << "test"
   t.test_files = Dir["test/unit/calculators/**/*_test.rb"]
 end
-
-task :default => :test
