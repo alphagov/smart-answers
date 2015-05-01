8090897177c6d16ca19ea17b1e966f6fcea84725
Tadas Tamosauskas <tadas@pdfcv.com>
2015-05-01 12:40:37 +0100

    Configure Errbit for preview env on heroku
    This configuration file is overwritten by the deploy script

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,7 +1,12 @@
 # This file is overwritten on deploy
-#
+
 Airbrake.configure do |config|
+  config.api_key = ENV["ERRBIT_API_KEY"]
+  config.host    = ENV["ERRBIT_HOST"]
+  config.environment_name = ENV["ERRBIT_ENV"]
+  config.secure  = ENV["ERRBIT_API_KEY"].present?
+
   # Adding "production" to the development environments causes Airbrake not
   # to attempt to send notifications.
-  config.development_environments << "production"
+  config.development_environments << "production" unless ENV["ERRBIT_API_KEY"].present?
 end
