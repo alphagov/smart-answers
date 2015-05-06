987abdf04f9027197125d19195788e7a81e8f893
James Mead <james@floehopper.org>
2015-05-06 13:59:27 +0100

    Use Date objects in appeal-a-benefits-decision
    I've made the conversion relatively simplistic to make the change as clear as
    possible. There are some obvious further simplifications that could be made,
    but I'm going to leave those for now.
    
    The outcomes for this smart answer do not display any dates, so we don't need
    to worry about how they might be rendered.

--- lib/smart_answer_flows/appeal-a-benefits-decision.rb
+++ lib/smart_answer_flows/appeal-a-benefits-decision.rb
@@ -17,13 +17,13 @@ multiple_choice :problem_with_tribunal_proceedure? do
 end
 
 # Q3
-date_question :date_of_decision_letter? do
+date_question :date_of_decision_letter?, parse: true do
   from { 5.years.ago }
   to { Date.today }
   save_input_as :decision_letter_date
 
   next_node do |response|
-    decision_date = Date.parse(response)
+    decision_date = response
     appeal_expiry_date = decision_appeal_limit_in_months.months.since(decision_date)
     if Date.today < appeal_expiry_date
       :had_written_explanation?
@@ -40,7 +40,7 @@ multiple_choice :had_written_explanation? do
   option :no
 
   calculate :appeal_expiry_date do
-    decision_date = Date.parse(decision_letter_date)
+    decision_date = decision_letter_date
     if (decision_date > 1.month.ago.to_date)
       1.month.since(decision_date)
     end
@@ -58,7 +58,7 @@ multiple_choice :had_written_explanation? do
     if response == 'written_explanation'
       :when_did_you_ask_for_it?
     else
-      a_month_has_passed = (Date.parse(decision_letter_date) < 1.month.ago.to_date)
+      a_month_has_passed = (decision_letter_date < 1.month.ago.to_date)
       if a_month_has_passed
         :special_circumstances?
       else
@@ -73,26 +73,26 @@ multiple_choice :had_written_explanation? do
 end
 
 # Q5
-date_question :when_did_you_ask_for_it? do
+date_question :when_did_you_ask_for_it?, parse: true do
   from { 5.years.ago }
   to { Date.today }
   calculate :written_explanation_request_date do |response|
-    Date.parse(response).strftime("%e %B %Y")
+    response
   end
   next_node :when_did_you_get_it?
 end
 
 # Q6
-date_question :when_did_you_get_it? do
+date_question :when_did_you_get_it?, parse: true do
 
   save_input_as :written_explanation_received_date
   from { 5.years.ago }
   to { Date.today }
 
   calculate :appeal_expiry_date do |response|
-    decision_date = Date.parse(decision_letter_date)
-    received_date = Date.parse(response)
-    request_date = Date.parse(written_explanation_request_date)
+    decision_date = decision_letter_date
+    received_date = response
+    request_date = written_explanation_request_date
     raise InvalidResponse if received_date < request_date
     received_within_a_month = received_date < 1.month.since(request_date)
 
@@ -115,10 +115,10 @@ date_question :when_did_you_get_it? do
   end
 
   next_node do |response|
-    received_date = Date.parse(response)
-    received_within_a_month = received_date < 1.month.since(Date.parse(written_explanation_request_date))
+    received_date = response
+    received_within_a_month = received_date < 1.month.since(written_explanation_request_date)
     a_fortnight_has_passed = Date.today > 1.fortnight.since(received_date)
-    decision_date = Date.parse(decision_letter_date)
+    decision_date = decision_letter_date
     a_month_and_a_fortnight_since_decision = Date.today > 1.fortnight.since(1.month.since(decision_date))
 
     if (!received_within_a_month and a_fortnight_has_passed) or
