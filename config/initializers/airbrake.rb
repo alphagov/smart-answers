a427fb08630e04175cec704bdaf46ee4a72570b1
Tadas Tamosauskas <tadas@pdfcv.com>
2015-05-06 10:10:53 +0100

    Update airbrake
    As per https://github.com/airbrake/airbrake/pull/268 it allows
    to remove the hack to avoid airbrake gem attempting to send error
    notices to errbit when not configured.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -5,8 +5,4 @@ Airbrake.configure do |config|
   config.host    = ENV["ERRBIT_HOST"]
   config.environment_name = ENV["ERRBIT_ENV"]
   config.secure  = ENV["ERRBIT_API_KEY"].present?
-
-  # Adding "production" to the development environments causes Airbrake not
-  # to attempt to send notifications.
-  config.development_environments << "production" unless ENV["ERRBIT_API_KEY"].present?
 end
