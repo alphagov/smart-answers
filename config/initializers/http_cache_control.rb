99d06d37fff655d1bd208467b4b26a6b4557639f
James Mead <james@floehopper.org>
2015-05-08 12:11:37 +0100

    Only set Cache-Control expiry for tests which need it set
    Fixes #1612
    
    It feels safer/less surprising to disable any caching mechanisms by default
    in the `test` environment and just enable them for the tests that need them.
    
    I've checked and the tests now pass with the seed mentioned in #1612 for
    both `phantomjs` v1.9.8 and v2.0.0.

--- config/initializers/http_cache_control.rb
+++ config/initializers/http_cache_control.rb
@@ -1,5 +1,5 @@
 SmartAnswers::Application.configure do
-  if Rails.env.development?
+  if Rails.env.development? || Rails.env.test?
     config.set_http_cache_control_expiry_time = false
   else
     config.set_http_cache_control_expiry_time = true
