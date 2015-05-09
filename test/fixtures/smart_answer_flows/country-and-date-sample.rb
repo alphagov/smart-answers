70f8803374e672aaa8aaa909cfadf4fc6949ccc8
James Mead <james@floehopper.org>
2015-05-09 21:30:23 +0100

    Use Date objects in country-and-date-sample flow

--- test/fixtures/smart_answer_flows/country-and-date-sample.rb
+++ test/fixtures/smart_answer_flows/country-and-date-sample.rb
@@ -5,13 +5,13 @@ country_select :which_country_do_you_live_in?, exclude_countries: %w(afghanistan
   next_node :what_date_did_you_move_there?
 end
 
-date_question :what_date_did_you_move_there? do
+date_question :what_date_did_you_move_there?, parse: true do
   from Date.parse('1900-01-01')
   to Date.today
 
   save_input_as :date_moved
   calculate :years_there do
-    ((Date.today - Date.parse(date_moved)) / 365.25).to_i
+    ((Date.today - date_moved) / 365.25).to_i
   end
 
   next_node :which_country_were_you_born_in?
