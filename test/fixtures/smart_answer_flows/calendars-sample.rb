4d8421bbfc0f12b762f4e48151529fdb1edf6b87
James Mead <james@floehopper.org>
2015-05-09 21:14:20 +0100

    Use Date objects in calendars-sample flow

--- test/fixtures/smart_answer_flows/calendars-sample.rb
+++ test/fixtures/smart_answer_flows/calendars-sample.rb
@@ -1,12 +1,12 @@
 status :draft
 
-date_question :when_are_you_starting? do
+date_question :when_are_you_starting?, parse: true do
   save_input_as :start_date
 
   next_node :when_are_you_stopping?
 end
 
-date_question :when_are_you_stopping? do
+date_question :when_are_you_stopping?, parse: true do
   save_input_as :stop_date
 
   next_node :do_you_want_a_calendar?
@@ -19,7 +19,7 @@ end
 
 outcome :date_ranges do
   calendar do |response|
-    date :start_stop, Date.parse(response.start_date)..Date.parse(response.stop_date)
+    date :start_stop, response.start_date..response.stop_date
     date :something_else, Date.parse("16 March 2013")
   end
 end
