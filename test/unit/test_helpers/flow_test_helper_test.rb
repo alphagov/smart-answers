2fd21d9662d0c2ec095aba83bf1fdfe0eff6c04b
Tadas Tamosauskas <tadas@pdfcv.com>
2015-05-13 10:51:35 +0100

    Bust current_state cache in FlowTestHelper correctly
    The state was cached regardless of the current state of @responses.
    As a side effect this forced tests to be nested within a context.
    This is no longer required.

---
+++ test/unit/test_helpers/flow_test_helper_test.rb
@@ -0,0 +1,34 @@
+require_relative '../../test_helper'
+require_relative '../../integration/smart_answer_flows/flow_test_helper'
+
+class FlowTestHelperTest < ActiveSupport::TestCase
+
+  class FlowTestHelperIncluder
+    include FlowTestHelper
+    attr_accessor :responses
+
+    def initialize(flow, responses)
+      @flow = flow
+      @responses = responses
+    end
+  end
+
+
+  test "caches current_state" do
+    flow = Object.new
+    flow.expects(:process).once.returns(:state)
+    includer = FlowTestHelperIncluder.new(flow, [:yes, :no])
+    includer.current_state
+    includer.current_state
+  end
+
+  test "busts current_state cache when responses change" do
+    flow = Object.new
+    flow.expects(:process).twice.returns(:state)
+    responses = [:yes, :no]
+    includer = FlowTestHelperIncluder.new(flow, responses)
+    includer.current_state
+    responses << :maybe
+    includer.current_state
+  end
+end
