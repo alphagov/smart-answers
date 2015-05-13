18d424900977b0d699cff3a4918a4cbefe15cedd
Ben Janecke <benjanecke@gmail.com>
2015-05-13 16:33:18 +0100

    Move LEL check to last question
    This had to be done because Q11 is being moved to happen before AWE gets calculated

--- test/integration/smart_answer_flows/calculate_statutory_sick_pay_test.rb
+++ test/integration/smart_answer_flows/calculate_statutory_sick_pay_test.rb
@@ -299,6 +299,7 @@ class CalculateStatutorySickPayTest < ActiveSupport::TestCase
       add_response '100' # Q7
       add_response '7' # Q7.1
       add_response 'no' # Q11
+      add_response '1,2,3,4,5' # Q13
     end
     should "take you to result A5 as awe < LEL (as of 2013-06-10)" do
       assert_state_variable :employee_average_weekly_earnings, 100
