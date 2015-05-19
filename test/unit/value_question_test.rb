1e140eda9b70ed6585eebe677df0ae97ce314b8f
James Mead <james@floehopper.org>
2015-05-19 11:42:21 +0100

    Improve unit test coverage for Question::Value

--- test/unit/value_question_test.rb
+++ test/unit/value_question_test.rb
@@ -5,11 +5,11 @@ require 'ostruct'
 
 module SmartAnswer
   class ValueQuestionTest < ActiveSupport::TestCase
-    def setup
+    setup do
       @initial_state = State.new(:example)
     end
 
-    test "Value is saved as a String by default" do
+    should "save value as a String by default" do
       q = Question::Value.new(:example) do
         save_input_as :myval
         next_node :done
@@ -19,27 +19,53 @@ module SmartAnswer
       assert_equal '123', new_state.myval
     end
 
-    test "Value is saved as an Integer using Integer() when specified by parse option" do
-      q = Question::Value.new(:example, parse: Integer) do
-        save_input_as :myval
-        next_node :done
+    context "when parse option is Integer" do
+      setup do
+        @q = Question::Value.new(:example, parse: Integer) do
+          save_input_as :myval
+          next_node :done
+        end
       end
 
-      new_state = q.transition(@initial_state, "123")
-      assert_equal 123, new_state.myval
-    end
-
-    test "Value is saved as an Integer using String#to_i when specified by parse option" do
-      q = Question::Value.new(:example, parse: :to_i) do
-        save_input_as :myval
-        next_node :done
+      should "save integer value as an Integer" do
+        new_state = @q.transition(@initial_state, "123")
+        assert_equal 123, new_state.myval
       end
 
-      new_state = q.transition(@initial_state, "")
-      assert_equal 0, new_state.myval
+      should "raise ArgumentError for non-integer value" do
+        assert_raises(ArgumentError) { @q.transition(@initial_state, "1.5") }
+      end
+
+      should "raise ArgumentError for blank value" do
+        assert_raises(ArgumentError) { @q.transition(@initial_state, "") }
+      end
     end
 
-    test "Value is saved as a String if parse option specifies unknown type" do
+    context "when parse option is :to_i" do
+      setup do
+        @q = Question::Value.new(:example, parse: :to_i) do
+          save_input_as :myval
+          next_node :done
+        end
+      end
+
+      should "save valid value as an Integer" do
+        new_state = @q.transition(@initial_state, "123")
+        assert_equal 123, new_state.myval
+      end
+
+      should "save non-integer value as an Integer" do
+        new_state = @q.transition(@initial_state, "1.23")
+        assert_equal 1, new_state.myval
+      end
+
+      should "save blank value as zero" do
+        new_state = @q.transition(@initial_state, "")
+        assert_equal 0, new_state.myval
+      end
+    end
+
+    should "save value as a String if parse option specifies unknown type" do
       q = Question::Value.new(:example, parse: Float) do
         save_input_as :myval
         next_node :done
