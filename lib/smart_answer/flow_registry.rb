8327b4a1a83d68b25f2440bbb3b4f196a1c72cb7
Ben Janecke <benjanecke@gmail.com>
2015-05-20 16:07:35 +0100

    Restore benefits-if-you-are-abroad

--- lib/smart_answer/flow_registry.rb
+++ lib/smart_answer/flow_registry.rb
@@ -5,6 +5,7 @@ SMART_ANSWER_FLOW_NAMES = %w(
   apply-tier-4-visa
   benefit-cap-calculator
   benefits-abroad
+  benefits-if-you-are-abroad
   calculate-agricultural-holiday-entitlement
   calculate-married-couples-allowance
   calculate-state-pension
