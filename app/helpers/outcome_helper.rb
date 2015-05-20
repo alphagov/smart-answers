47115bfcc2da0482ec0db76a6e349ae8f047c10a
Chris Roos <chris@seagul.co.uk>
2015-05-20 13:08:55 +0100

    Remove prettify_contact helper method
    This was only used in the legacy_embassies data partial which I removed in the
    previous commit.

--- app/helpers/outcome_helper.rb
+++ app/helpers/outcome_helper.rb
@@ -8,28 +8,4 @@ module OutcomeHelper
     filepath = "#{Rails.root}/app/views/smart_answers/#{template}.mustache"
     File.read(filepath).html_safe
   end
-
-  # Used by the legacy_emabssies data_partial
-  # This should be considered deprecated
-  def prettify_contact(contact)
-    contact = contact.dup
-
-    # format newlines correctly and remove any double line breaks or trailing line breaks
-    contact.gsub!("\n", "<br>")
-    contact.gsub!("<br>,", "<br>")
-    contact.gsub!(/,$/, "")
-    contact.gsub!(/(<br>\s?)+/, "<br>")
-    contact.gsub!(/<br>$/, "")
-    contact.gsub!(/\A<br>/, "")
-
-    # strip commas at end of lines
-    contact.gsub!(/,$/, "")
-    # force space after remaining commas
-    contact.gsub!(/,/, ", ")
-
-    # highlight the first line
-    contact.gsub!(/\A([^\<]+)/, "<strong>\\1</strong>")
-
-    contact.html_safe
-  end
 end
