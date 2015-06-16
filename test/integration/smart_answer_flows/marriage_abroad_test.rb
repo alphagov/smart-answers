eff29a444fb1c58ccbe38e97283fab940864b66c
Tadas Tamosauskas <tadas@pdfcv.com>
2015-06-16 11:28:57 +0100

    Move Belgium from CNI to Affirmation
    Belgium no longer issues CNIs. Apply affirmation rules with some
    exceptions. Affects all OS belgium outcomes.

--- test/integration/smart_answer_flows/marriage_abroad_test.rb
+++ test/integration/smart_answer_flows/marriage_abroad_test.rb
@@ -752,11 +752,9 @@ class MarriageAbroadTest < ActiveSupport::TestCase
       add_response 'partner_british'
       add_response 'opposite_sex'
     end
-    should "go to outcome_consular_cni_os_residing_in_third_country" do
-      assert_current_node :outcome_consular_cni_os_residing_in_third_country
-      assert_phrase_list :body, [:contact_local_authorities_in_country_marriage, :get_legal_and_travel_advice, :what_you_need_to_do, :you_may_be_asked_for_cni, :standard_ways_to_get_cni_in_third_country]
-      assert_state_variable :ceremony_country_residence_outcome_path, "/marriage-abroad/y/belgium/ceremony_country/partner_british/opposite_sex"
-      assert_state_variable :uk_residence_outcome_path, "/marriage-abroad/y/belgium/uk/uk_england/partner_british/opposite_sex" # uk_england part will get removed soon
+    should "go to outcome_os_affirmation" do
+      assert_current_node :outcome_os_affirmation
+      assert_phrase_list :affirmation_os_outcome, [:contact_local_authorities_in_country_marriage, :get_legal_and_travel_advice, :what_you_need_to_do_affirmation, :appointment_for_affidavit, "appointment_links.opposite_sex.belgium", :documents_guidance_belgium, :complete_affirmation_or_affidavit_forms, :download_and_fill_but_not_sign, :download_affidavit_and_affirmation_belgium, :legalisation_and_translation, :affirmation_os_translation_in_local_language_text, :docs_decree_and_death_certificate, :divorced_or_widowed_evidences, :change_of_name_evidence, :partner_probably_needs_affirmation, :fee_table_affirmation_55, :list_of_consular_fees, :pay_by_cash_or_credit_card_no_cheque]
     end
   end
 
