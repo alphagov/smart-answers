8514da72d22ccb2562e99a074518c3b656d4b405
James Mead <james@floehopper.org>
2015-06-19 16:25:59 +0100

    Convert redundancy-pay-related flows to classes.
    * `calculate-employee-redundancy-pay`
    * `calculate-your-redundancy-pay`
    
    In order to make test coverage data available for the flow.

--- lib/smart_answer_flows/calculate-your-redundancy-pay.rb
+++ lib/smart_answer_flows/calculate-your-redundancy-pay.rb
@@ -1,4 +1,12 @@
-status :published
-satisfies_need "100135"
+module SmartAnswer
+  class CalculateYourRedundancyPayFlow < Flow
+    def define
+      name 'calculate-your-redundancy-pay'
 
-use_shared_logic "redundancy_pay"
+      status :published
+      satisfies_need "100135"
+
+      use_shared_logic "redundancy_pay"
+    end
+  end
+end
