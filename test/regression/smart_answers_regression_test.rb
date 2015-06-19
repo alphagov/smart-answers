24ac19deb449699402b0da271c35dc56d60b59b9
James Mead <james@floehopper.org>
2015-06-19 12:07:52 +0100

    Avoid requiring test_helper in regression test
    This is in preparation for only running the setup once at the beginning of
    the regression tests.
    
    In this commit, I've copied over everything from `test/test_helper.rb`. I plan
    to remove the bits we don't need in subsequent commits.

--- test/regression/smart_answers_regression_test.rb
+++ test/regression/smart_answers_regression_test.rb
@@ -1,4 +1,39 @@
-require_relative "../test_helper"
+ENV["RAILS_ENV"] = "test"
+require File.expand_path('../../../config/environment', __FILE__)
+
+if ENV["TEST_COVERAGE"]
+  require 'simplecov'
+  require 'simplecov-rcov'
+
+  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+  SimpleCov.start 'rails'
+end
+
+FLOW_REGISTRY_OPTIONS[:preload_flows] = true
+
+require 'rails/test_help'
+
+require 'mocha/setup'
+
+require 'webmock/minitest'
+WebMock.disable_net_connect!(allow_localhost: true)
+
+class Minitest::Test
+  def teardown_with_customisations
+    teardown_without_customisations
+    Timecop.return
+    WorldLocation.reset_cache
+  end
+  alias_method_chain :teardown, :customisations
+end
+
+require 'gds_api/test_helpers/json_client_helper'
+require_relative '../support/fixture_methods'
+
+class ActiveSupport::TestCase
+  include FixtureMethods
+end
+
 require 'gds_api/test_helpers/content_api'
 require 'gds_api/test_helpers/worldwide'
 
