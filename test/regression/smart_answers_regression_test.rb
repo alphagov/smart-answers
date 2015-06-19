055a63bb479126256ccba671f274b5d2ffd5f9a7
James Mead <james@floehopper.org>
2015-06-19 13:48:12 +0100

    Remove test coverage option from regression test
    We don't need this, because we can get code coverage information by running
    `script/generate-responses-and-expected-results-for-smart-answer.rb`.

--- test/regression/smart_answers_regression_test.rb
+++ test/regression/smart_answers_regression_test.rb
@@ -1,14 +1,6 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../../config/environment', __FILE__)
 
-if ENV["TEST_COVERAGE"]
-  require 'simplecov'
-  require 'simplecov-rcov'
-
-  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
-  SimpleCov.start 'rails'
-end
-
 FLOW_REGISTRY_OPTIONS[:preload_flows] = true
 
 require 'rails/test_help'
