21a67ff48fff3f6f4a81b64c9908e84ad1320e64
James Mead <james@floehopper.org>
2015-06-23 20:55:03 +0100

    Convert esa_already_abroad_other_outcome outcome

--- lib/smart_answer_flows/uk-benefits-abroad.rb
+++ lib/smart_answer_flows/uk-benefits-abroad.rb
@@ -447,7 +447,7 @@ module SmartAnswer
       outcome :esa_already_abroad_under_a_year_other_outcome, use_outcome_templates: true # A26 already_abroad
       outcome :esa_already_abroad_eea_outcome, use_outcome_templates: true # A27 already_abroad
       outcome :esa_already_abroad_ss_outcome, use_outcome_templates: true # A28 already_abroad
-      outcome :esa_already_abroad_other_outcome # A29 already_abroad
+      outcome :esa_already_abroad_other_outcome, use_outcome_templates: true # A29 already_abroad
       outcome :iidb_maybe_outcome # A 30 already_abroad and A31 going_abroad
       outcome :iidb_already_abroad_eea_outcome # A31 already_abroad
       outcome :iidb_already_abroad_ss_outcome # A32 already_abroad
