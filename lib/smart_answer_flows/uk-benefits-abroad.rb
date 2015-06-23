4f10257308b48d5e1966c877929a8a2e3d43f568
James Mead <james@floehopper.org>
2015-06-23 18:00:27 +0100

    Convert bb_going_abroad_other_outcome outcome

--- lib/smart_answer_flows/uk-benefits-abroad.rb
+++ lib/smart_answer_flows/uk-benefits-abroad.rb
@@ -433,7 +433,7 @@ module SmartAnswer
       outcome :db_going_abroad_temporary_outcome, use_outcome_templates: true # A35 going_abroad
       outcome :db_going_abroad_other_outcome, use_outcome_templates: true # A36 going_abroad
       outcome :db_going_abroad_eea_outcome, use_outcome_templates: true # A37 going_abroad
-      outcome :bb_going_abroad_other_outcome # A38 going_abroad
+      outcome :bb_going_abroad_other_outcome, use_outcome_templates: true # A38 going_abroad
       outcome :bb_going_abroad_eea_outcome # A39 going_abroad
       outcome :bb_going_abroad_ss_outcome # A40 going_abroad
       outcome :is_more_than_a_year_outcome # A41 going_abroad
