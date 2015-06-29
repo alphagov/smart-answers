2356e57c29e244026f0143169c05c23465a8dd8c
Tadas Tamosauskas <tadas@pdfcv.com>
2015-06-29 10:46:20 +0100

    Point users in Kyrgyzstan to Kazakhstan
    Where the user needs to post notice in Kyrgyzstan, it can be done via
    the embassy in Kazakhstan. Update relevant references to the embassy:
    for the appointment link and service fees only, the rest of the process
    needs to be carried out via local authorities in Kyrgyzstan.

--- test/integration/smart_answer_flows/marriage_abroad_test.rb
+++ test/integration/smart_answer_flows/marriage_abroad_test.rb
@@ -2587,6 +2587,20 @@ class MarriageAbroadTest < ActiveSupport::TestCase
     end
   end
 
+  context "Kyrgyzstan" do
+    should "lead to the CNI outcome with a suggestion to post notice in Almaty, Kazakhstan" do
+      worldwide_api_has_no_organisations_for_location('kyrgyzstan')
+      add_response 'kyrgyzstan'
+      add_response 'ceremony_country'
+      add_response 'partner_british'
+      add_response 'opposite_sex'
+      assert_current_node :outcome_os_consular_cni
+
+      assert_phrase_list :consular_cni_os_start,  [:contact_local_authorities_in_country_marriage, :get_legal_advice, :what_you_need_to_do, :you_may_be_asked_for_cni, :consular_cni_os_giving_notice_in_ceremony_country, :living_in_ceremony_country_3_days, :kazakhstan_os_local_resident, "appointment_links.opposite_sex.kyrgyzstan", :required_supporting_documents_notary_public, :consular_cni_os_not_uk_resident_ceremony_not_germany, :consular_cni_os_other_resident_ceremony_not_germany_or_spain, :download_and_fill_notice_and_affidavit_but_not_sign, :consular_cni_os_foreign_resident_ceremony_notary_public]
+      assert_phrase_list :consular_cni_os_remainder, [:same_cni_process_and_fees_for_partner, :names_on_documents_must_match, :check_if_cni_needs_to_be_legalised, :no_need_to_stay_after_posting_notice, :consular_cni_os_fees_not_italy_not_uk, :list_of_consular_kazakhstan, :pay_by_cash_or_credit_card_no_cheque]
+    end
+  end
+
   context "when appointment links for opposite sex marriage exist" do
     # Kosovo is excluded, because it has a custom outcome
     (OS_COUNTRIES_WITH_APPOINTMENTS - ['kosovo']).each do |country|
