3920417c778f8de1f3bce09668114b45afe5d49b
James Mead <james@floehopper.org>
2015-07-02 19:50:07 +0100

    Convert result_2 outcome

--- lib/smart_answer_flows/pip-checker.rb
+++ lib/smart_answer_flows/pip-checker.rb
@@ -53,7 +53,7 @@ module SmartAnswer
       end
 
       outcome :result_1, use_outcome_templates: true
-      outcome :result_2
+      outcome :result_2, use_outcome_templates: true
       outcome :result_3
       outcome :result_4
       outcome :result_5
