107f2f289aa225601b15f6d3137515c27d0f1a70
Chris Roos <chris@seagul.co.uk>
2015-07-02 12:19:44 +0100

    Convert outcome_medical_n outcome

--- lib/smart_answer_flows/check-uk-visa.rb
+++ lib/smart_answer_flows/check-uk-visa.rb
@@ -148,7 +148,7 @@ module SmartAnswer
       outcome :outcome_school_n, use_outcome_templates: true
       outcome :outcome_school_y, use_outcome_templates: true
       outcome :outcome_medical_y, use_outcome_templates: true
-      outcome :outcome_medical_n
+      outcome :outcome_medical_n, use_outcome_templates: true
       outcome :outcome_visit_waiver do
         precalculate :if_exception do
           if %w(venezuela).include?(passport_country)
