26ac37e76452cd48ffc1b9623ea5872b291c18e7
Tadas Tamosauskas <tadas@pdfcv.com>
2015-07-09 15:08:03 +0100

    Change overseas-passports language for waiting times for some countries
    For all countries listed below, the ##How long it takes sentence should read:
    
    > Your application should take %{X} weeks from when it’s received by
      Her Majesty’s Passport Office in the UK.
    
    For all other countries, the ##How long it takes sentence should read:
    
    > Your application will take at least %{X} weeks from when it’s received by
      Her Majesty’s Passport Office in the UK.
    
    Reason for changes: these times were changed during last year's summer peak.
    They need to be changed to reflect realistic turnaround times. The wording is
    being changed to firstly bring it in line with the UK. And, also, to better meet
    customer expectations. Using 'at least' drives customer demand to other channels
    as it implies real doubt, whereas using 'it should' feels to customers that it's
    likely to hapen. UK experience suggests using 'at least' pushes customers to use
    counter services, and that 'it should' reduces that demand. Currently demand is
    high for the FCO for emergency travel documents and HMPO anticipate this change
    will have a similar impact and reduce the pressure on the FCO and reassure
    customers that their needs can be met.
    
    Script used to speed up the changes: https://gist.github.com/tadast/5dec3453709acf263665
    
    List of country slugs that changed:
    american-samoa
    andorra
    anguilla
    antigua-and-barbuda
    argentina
    armenia
    aruba
    australia
    austria
    bahamas
    bahrain
    barbados
    belgium
    belize
    bermuda
    bhutan
    bonaire-st-eustatius-saba
    bosnia-and-herzegovina
    botswana
    brazil
    british-virgin-islands
    brunei
    bulgaria
    canada
    cape-verde
    cayman-islands
    chile
    china
    colombia
    comoros
    costa-rica
    croatia
    cuba
    curacao
    cyprus
    czech-republic
    denmark
    dominica
    el-salvador
    estonia
    falkland-islands
    fiji
    finland
    france
    french-guiana
    french-polynesia
    germany
    gibraltar
    greece
    grenada
    guadeloupe
    guatemala
    honduras
    hong-kong
    hungary
    iceland
    indonesia
    ireland
    israel
    italy
    japan
    jordan
    kiribati
    kuwait
    latvia
    lesotho
    liechtenstein
    lithuania
    luxembourg
    macao
    macedonia
    malaysia
    maldives
    malta
    marshall-islands
    martinique
    mayotte
    mexico
    micronesia
    monaco
    montenegro
    montserrat
    namibia
    nauru
    netherlands
    new-caledonia
    new-zealand
    nicaragua
    norway
    oman
    palau
    panama
    papua-new-guinea
    peru
    pitcairn-island
    poland
    portugal
    qatar
    reunion
    romania
    samoa
    san-marino
    sao-tome-and-principe
    saudi-arabia
    serbia
    seychelles
    singapore
    slovakia
    slovenia
    solomon-islands
    south-africa
    south-korea
    spain
    st-kitts-and-nevis
    st-lucia
    st-maarten
    st-martin
    st-pierre-&-miquelon
    st-vincent-and-the-grenadines
    suriname
    swaziland
    sweden
    switzerland
    taiwan
    thailand
    tonga
    turkey
    turks-and-caicos-islands
    tuvalu
    united-arab-emirates
    uruguay
    usa
    vanuatu
    wallis-and-futuna

--- test/unit/calculators/passport_and_embassy_data_query_test.rb
+++ test/unit/calculators/passport_and_embassy_data_query_test.rb
@@ -14,10 +14,10 @@ module SmartAnswer::Calculators
           assert_equal 'ips_application_3', @query.find_passport_data('algeria')['type']
           assert_equal 'ips_documents_group_3', @query.find_passport_data('algeria')['group']
           assert_equal 'hmpo_1_application_form', @query.find_passport_data('algeria')['app_form']
-          assert_equal '6_weeks', @query.find_passport_data('afghanistan')['renewing_new']
-          assert_equal '6_months', @query.find_passport_data('afghanistan')['renewing_old']
-          assert_equal '6_months', @query.find_passport_data('afghanistan')['applying']
-          assert_equal '14_weeks', @query.find_passport_data('afghanistan')['replacing']
+          assert_equal '6 weeks', @query.find_passport_data('afghanistan')['renewing_new']
+          assert_equal '6 months', @query.find_passport_data('afghanistan')['renewing_old']
+          assert_equal '6 months', @query.find_passport_data('afghanistan')['applying']
+          assert_equal '14 weeks', @query.find_passport_data('afghanistan')['replacing']
         end
       end
 
