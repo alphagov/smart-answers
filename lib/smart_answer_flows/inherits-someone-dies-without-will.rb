ceaeb688d2078d45fcdf8c8a42e905db02f1dedf
James Mead <james@floehopper.org>
2015-07-09 16:00:00 +0100

    Convert outcome_4 outcome

--- lib/smart_answer_flows/inherits-someone-dies-without-will.rb
+++ lib/smart_answer_flows/inherits-someone-dies-without-will.rb
@@ -276,7 +276,7 @@ module SmartAnswer
       outcome :outcome_1, use_outcome_templates: true
       outcome :outcome_2, use_outcome_templates: true
       outcome :outcome_3, use_outcome_templates: true
-      outcome :outcome_4
+      outcome :outcome_4, use_outcome_templates: true
       outcome :outcome_5
       outcome :outcome_6
 
