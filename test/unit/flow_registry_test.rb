e4e8192bc14b635b0fb50207a6dc25e3da339a3e
James Mead <james@floehopper.org>
2015-07-13 11:48:23 +0100

    Make fixture flow filenames consistent
    All other flow filenames use hyphens rather than underscores. Although in the
    longer term I'd prefer to use underscores, because it's the Ruby standard, it's
    a step forward to at least make them all consistent.

--- test/unit/flow_registry_test.rb
+++ test/unit/flow_registry_test.rb
@@ -9,7 +9,7 @@ module SmartAnswer
     end
 
     test "Can load a flow from a file" do
-      flow = registry.find('flow_sample')
+      flow = registry.find('flow-sample')
       assert_equal 1, flow.questions.size
       assert_equal :hotter_or_colder?, flow.questions.first.name
       assert_equal %w{hotter colder}, flow.questions.first.options
@@ -24,19 +24,19 @@ module SmartAnswer
 
     test "should raise NotFound error for draft flow if show_drafts is not specified" do
       assert_raises FlowRegistry::NotFound do
-        registry(show_drafts: false).find("draft_flow_sample")
+        registry(show_drafts: false).find("draft-flow-sample")
       end
     end
 
     test "should find draft flow if show_drafts is specified" do
-      assert registry(show_drafts: true).find("draft_flow_sample")
+      assert registry(show_drafts: true).find("draft-flow-sample")
     end
 
     test "Should enumerate all flows" do
       flows = registry.flows
       assert_kind_of Enumerable, flows
       assert_kind_of Flow, flows.first
-      assert_equal "flow_sample", flows.first.name
+      assert_equal "flow-sample", flows.first.name
     end
 
     context "without preloaded flows" do
@@ -45,8 +45,8 @@ module SmartAnswer
       end
 
       should "build a new flow instance each time" do
-        first_call = registry.find('flow_sample')
-        second_call = registry.find('flow_sample')
+        first_call = registry.find('flow-sample')
+        second_call = registry.find('flow-sample')
 
         assert_equal first_call.name, second_call.name
         refute_equal first_call.object_id, second_call.object_id
@@ -62,7 +62,7 @@ module SmartAnswer
         Dir.expects(:[]).never
         File.expects(:read).never
 
-        assert @r.find('flow_sample').is_a?(Flow)
+        assert @r.find('flow-sample').is_a?(Flow)
       end
 
       should "not hit the filesystem when finding a non-existent flow" do
