b84cd58b569f1e3900d1fc2330108839caa4de13
James Mead <james@floehopper.org>
2015-07-13 11:48:23 +0100

    Convert flow-sample fixture flow to class
    Load flow directly - no need to use `SmartAnswer::FlowRegistry`

--- test/fixtures/flow_registraion_presenter_sample/flow-sample.rb
+++ test/fixtures/flow_registraion_presenter_sample/flow-sample.rb
@@ -1,9 +1,16 @@
-satisfies_need 4242
+module SmartAnswer
+  class FlowSampleFlow < Flow
+    def define
+      name 'flow-sample'
+      satisfies_need 4242
 
-multiple_choice :hotter_or_colder? do
-  option hotter: :hot
-  option colder: :cold
+      multiple_choice :hotter_or_colder? do
+        option hotter: :hot
+        option colder: :cold
+      end
+
+      outcome :hot
+      outcome :cold
+    end
+  end
 end
-
-outcome :hot
-outcome :cold
