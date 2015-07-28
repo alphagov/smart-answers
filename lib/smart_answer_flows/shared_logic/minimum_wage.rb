5ba3840bed112e82a7c6671c798fc4e4fbf997c9
Chris Roos <chris@seagul.co.uk>
2015-07-28 14:17:31 +0100

    Use validate method in how_often_do_you_get_paid?
    I've also introduced unit tests around this validation. We didn't seem to be
    testing this validation in the integration tests.

--- lib/smart_answer_flows/shared_logic/minimum_wage.rb
+++ lib/smart_answer_flows/shared_logic/minimum_wage.rb
@@ -88,12 +88,10 @@ end
 
 # Q4
 value_question :how_often_do_you_get_paid?, parse: :to_i do
-  calculate :pay_frequency do |response|
-    pay_frequency = response
-    if pay_frequency < 1 or pay_frequency > 31
-      raise SmartAnswer::InvalidResponse
-    end
-    pay_frequency
+  save_input_as :pay_frequency
+
+  validate do |response|
+    response >= 1 && response <= 31
   end
 
   next_node :how_many_hours_do_you_work?
