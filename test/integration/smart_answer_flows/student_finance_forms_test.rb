656be2ab1e03e26659e0927bdb6191deb9867454
Tadas Tamosauskas <tadas@pdfcv.com>
2015-08-13 16:53:54 +0100

    Translate scenarios to integration tests for student-finance-forms
    These integration tests are a naive direct conversion of smartdown scenarios
    /lib/smartdown_flows/student-finance-forms-old/scenarios/.
    
    Conversion was rather trivial, given a scenario:
    - question_1: answer_1
    - question_2: answer_2
    - outcome_1
    
    the integration test equivalent is:
    
    should "lead to outcome_1" do
      assert_current_node :question_1
      add_response :answer_1
      assert_current_node :question_2
      add_response :answer_2
      assert_current_node :outcome_1
    end
    
    One gotcha is that in scenarios, the value on the left might not directly
    correspond to the node name e.g. what_year_eu_students.txt node can have a
    question 'what_year', not 'what_year_eu_students'.
    
    There are 3 integration tests fewer than scenarios, because three of them where
    duplicated.
    
    This commit is a part of the student-finance-forms conversion from smartdown to
    ruby smartanswers.

--- test/integration/smart_answer_flows/student_finance_forms_test.rb
+++ test/integration/smart_answer_flows/student_finance_forms_test.rb
@@ -10,4 +10,369 @@ class StudentFinanceFormsTest < ActiveSupport::TestCase
     setup_for_testing_flow SmartAnswer::StudentFinanceFormsFlow
   end
 
+  should "start off by asking what type of student you are" do
+    assert_current_node :type_of_student?
+  end
+
+  context "for EU students" do
+    should "lead to outcome_eu_ft_1415_continuing" do
+      add_response 'eu-full-time'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :continuing_student?
+      add_response 'continuing-student'
+      assert_current_node :outcome_eu_ft_1415_continuing
+    end
+
+    should "lead to outcome_eu_ft_1415_new" do
+      add_response 'eu-full-time'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :continuing_student?
+      add_response 'new-student'
+      assert_current_node :outcome_eu_ft_1415_new
+    end
+
+    should "lead to outcome_eu_ft_1516_continuing" do
+      add_response 'eu-full-time'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :continuing_student?
+      add_response 'continuing-student'
+      assert_current_node :outcome_eu_ft_1516_continuing
+    end
+
+    should "lead to outcome_eu_ft_1516_new" do
+      add_response 'eu-full-time'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :continuing_student?
+      add_response 'new-student'
+      assert_current_node :outcome_eu_ft_1516_new
+    end
+
+    should "lead to outcome_eu_pt_1415_continuing" do
+      add_response 'eu-part-time'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :continuing_student?
+      add_response 'continuing-student'
+      assert_current_node :outcome_eu_pt_1415_continuing
+    end
+
+    should "lead to outcome_eu_pt_1415_new" do
+      add_response 'eu-part-time'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :continuing_student?
+      add_response 'new-student'
+      assert_current_node :outcome_eu_pt_1415_new
+    end
+
+    should "lead to outcome_eu_pt_1516_continuing" do
+      add_response 'eu-part-time'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :continuing_student?
+      add_response 'continuing-student'
+      assert_current_node :outcome_eu_pt_1516_continuing
+    end
+
+    should "lead to outcome_eu_pt_1516_new" do
+      add_response 'eu-part-time'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :continuing_student?
+      add_response 'new-student'
+      assert_current_node :outcome_eu_pt_1516_new
+    end
+  end
+
+  context "for UK students" do
+    should "lead to outcome_uk_ft_1415_continuing" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'apply-loans-grants'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :continuing_student?
+      add_response 'continuing-student'
+      assert_current_node :outcome_uk_ft_1415_continuing
+    end
+
+    should "lead to outcome_uk_ft_1415_new" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'apply-loans-grants'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :continuing_student?
+      add_response 'new-student'
+      assert_current_node :outcome_uk_ft_1415_new
+    end
+
+    should "lead to outcome_uk_ft_1516_continuing" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'apply-loans-grants'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :continuing_student?
+      add_response 'continuing-student'
+      assert_current_node :outcome_uk_ft_1516_continuing
+    end
+
+    should "lead to outcome_uk_ft_1516_new" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'apply-loans-grants'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :continuing_student?
+      add_response 'new-student'
+      assert_current_node :outcome_uk_ft_1516_new
+    end
+
+    should "lead to outcome_proof_identity_1415 for full time students" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'proof-identity'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :outcome_proof_identity_1415
+    end
+
+    should "lead to outcome_proof_identity_1516 for full time students" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'proof-identity'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :outcome_proof_identity_1516
+    end
+
+    should "lead to outcome_parent_partner_1415" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'income-details'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :outcome_parent_partner_1415
+    end
+
+    should "lead to outcome_parent_partner_1516" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'income-details'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :outcome_parent_partner_1516
+    end
+
+    should "lead to outcome_dsa_1415" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'apply-dsa'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :outcome_dsa_1415
+    end
+
+    should "lead to outcome_dsa_1516" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'apply-dsa'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :outcome_dsa_1516
+    end
+
+    should "lead to outcome_dsa_expenses for full time students" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'dsa-expenses'
+      assert_current_node :outcome_dsa_expenses
+    end
+
+    should "lead to outcome_ccg_1415" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'apply-ccg'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :outcome_ccg_1415
+    end
+
+    should "lead to outcome_ccg_1516" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'apply-ccg'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :outcome_ccg_1516
+    end
+
+    should "lead to outcome_travel" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'travel-grant'
+      assert_current_node :outcome_travel
+    end
+
+    should "lead to outcome_ccg_expenses" do
+      add_response 'uk-full-time'
+      assert_current_node :form_needed_for_1?
+      add_response 'ccg-expenses'
+      assert_current_node :outcome_ccg_expenses
+    end
+
+    should "lead to outcome_uk_pt_1415_grant for continuing students" do
+      add_response 'uk-part-time'
+      assert_current_node :form_needed_for_2?
+      add_response 'apply-loans-grants'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :continuing_student?
+      add_response 'continuing-student'
+      assert_current_node :pt_course_start?
+      add_response 'course-start-before-01092012'
+      assert_current_node :outcome_uk_pt_1415_grant
+    end
+
+    should "lead to outcome_uk_pt_1415_continuing" do
+      add_response 'uk-part-time'
+      assert_current_node :form_needed_for_2?
+      add_response 'apply-loans-grants'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :continuing_student?
+      add_response 'continuing-student'
+      assert_current_node :pt_course_start?
+      add_response 'course-start-after-01092012'
+      assert_current_node :outcome_uk_pt_1415_continuing
+    end
+
+    should "lead to outcome_uk_pt_1415_grant for new students" do
+      add_response 'uk-part-time'
+      assert_current_node :form_needed_for_2?
+      add_response 'apply-loans-grants'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :continuing_student?
+      add_response 'new-student'
+      assert_current_node :pt_course_start?
+      add_response 'course-start-before-01092012'
+      assert_current_node :outcome_uk_pt_1415_grant
+    end
+
+    should "lead to outcome_uk_pt_1415_new" do
+      add_response 'uk-part-time'
+      assert_current_node :form_needed_for_2?
+      add_response 'apply-loans-grants'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :continuing_student?
+      add_response 'new-student'
+      assert_current_node :pt_course_start?
+      add_response 'course-start-after-01092012'
+      assert_current_node :outcome_uk_pt_1415_new
+    end
+
+    should "lead to outcome_proof_identity_1415 for part time students" do
+      add_response 'uk-part-time'
+      assert_current_node :form_needed_for_2?
+      add_response 'proof-identity'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :outcome_proof_identity_1415
+    end
+
+    should "lead to outcome_dsa_1415_pt" do
+      add_response 'uk-part-time'
+      assert_current_node :form_needed_for_2?
+      add_response 'apply-dsa'
+      assert_current_node :what_year?
+      add_response 'year-1415'
+      assert_current_node :outcome_dsa_1415_pt
+    end
+
+    should "lead to outcome_uk_ptgc_1516_grant" do
+      add_response 'uk-part-time'
+      assert_current_node :form_needed_for_2?
+      add_response 'apply-loans-grants'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :continuing_student?
+      add_response 'continuing-student'
+      assert_current_node :pt_course_start?
+      add_response 'course-start-before-01092012'
+      # has marker: circumstances_changed
+      assert_current_node :outcome_uk_ptgc_1516_grant
+    end
+
+    should "lead to outcome_uk_pt_1516_continuing" do
+      add_response 'uk-part-time'
+      assert_current_node :form_needed_for_2?
+      add_response 'apply-loans-grants'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :continuing_student?
+      add_response 'continuing-student'
+      assert_current_node :pt_course_start?
+      add_response 'course-start-after-01092012'
+      assert_current_node :outcome_uk_pt_1516_continuing
+    end
+
+    should "lead to outcome_uk_ptgn_1516_grant" do
+      add_response 'uk-part-time'
+      assert_current_node :form_needed_for_2?
+      add_response 'apply-loans-grants'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :continuing_student?
+      add_response 'new-student'
+      assert_current_node :pt_course_start?
+      add_response 'course-start-before-01092012'
+      # has marker: circumstances_changed
+      assert_current_node :outcome_uk_ptgn_1516_grant
+    end
+
+    should "lead to outcome_uk_pt_1516_new" do
+      add_response 'uk-part-time'
+      assert_current_node :form_needed_for_2?
+      add_response 'apply-loans-grants'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :continuing_student?
+      add_response 'new-student'
+      assert_current_node :pt_course_start?
+      add_response 'course-start-after-01092012'
+      assert_current_node :outcome_uk_pt_1516_new
+    end
+
+    should "lead to outcome_proof_identity_1516 for part time students" do
+      add_response 'uk-part-time'
+      assert_current_node :form_needed_for_2?
+      add_response 'proof-identity'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :outcome_proof_identity_1516
+    end
+
+    should "lead to outcome_dsa_1516_pt" do
+      add_response 'uk-part-time'
+      assert_current_node :form_needed_for_2?
+      add_response 'apply-dsa'
+      assert_current_node :what_year?
+      add_response 'year-1516'
+      assert_current_node :outcome_dsa_1516_pt
+    end
+
+    should "lead to outcome_dsa_expenses for part time students" do
+      add_response 'uk-part-time'
+      assert_current_node :form_needed_for_2?
+      add_response 'dsa-expenses'
+      assert_current_node :outcome_dsa_expenses
+    end
+  end
 end
