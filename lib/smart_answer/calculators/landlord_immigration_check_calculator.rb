547ffc4707bd097ad1b48f791b702ae43639fc07
James Mead <james@floehopper.org>
2015-08-19 16:23:08 +0100

    Introduce LandlordImmigrationCheckCalculator
    This is pretty much a direct copy of
    `SmartdownPlugins::LandlordImmigrationCheck` and its associated unit test.
    
    The only difference is that we're expecting the smart answer flow to pass in
    the postcode as a `String` and not an object with a `value` attribute.
    
    This calculator will take over from the `SmartdownPlugins` version when we
    convert the smartdown flow `landlord-immigration-check` to a smart answer flow.
    
    The code in the `valid_postcode` method is a bit horrible, but it seems better
    to keep the code as similar as possible in this commit. I hope to improve it
    later.

---
+++ lib/smart_answer/calculators/landlord_immigration_check_calculator.rb
@@ -0,0 +1,27 @@
+module SmartAnswer::Calculators
+  class LandlordImmigrationCheckCalculator
+    # These MTD areas are currently different on mapit.mysociety.org
+    # to preview/production data. They are *-city-council in all cases
+    # on Mapit but a mixture of *-borough-council and *-city-council
+    # on the internal GOV.UK Mapit instance. The duplication here
+    # protects us against the effects of a data update.
+    VALID_BOROUGHS = %w(
+      birmingham-borough-council
+      birmingham-city-council
+      dudley-borough-council
+      dudley-city-council
+      sandwell-borough-council
+      sandwell-city-council
+      walsall-borough-council
+      walsall-city-council
+      wolverhampton-city-council
+    )
+
+    def self.valid_postcode(postcode)
+      response = $imminence.areas_for_postcode(postcode)
+      return false unless response and response.code == 200
+      areas = response.to_hash["results"]
+      ! areas.find { |a| VALID_BOROUGHS.include?(a["slug"]) }.nil?
+    end
+  end
+end
---
+++ test/unit/calculators/landlord_immigration_check_calculator_test.rb
@@ -0,0 +1,40 @@
+require_relative '../../test_helper'
+require 'gds_api/test_helpers/imminence'
+
+module SmartAnswer::Calculators
+  class LandlordImmigrationCheckCalculatorTest < ActiveSupport::TestCase
+    include GdsApi::TestHelpers::Imminence
+
+    setup do
+      imminence_has_areas_for_postcode("WC2B%206SE", [])
+      imminence_has_areas_for_postcode("B1%201EQ", [{ slug: "birmingham-city-council" }])
+      imminence_has_areas_for_postcode("B62%200BG", [{ slug: "dudley-city-council" }])
+      imminence_has_areas_for_postcode("B43%205AB", [{ slug: "sandwell-city-council" }])
+      imminence_has_areas_for_postcode("WV1%201ES", [{ slug: "wolverhampton-city-council" }])
+      imminence_has_areas_for_postcode("B43%207DG", [{ slug: "walsall-city-council" }, { slug: "london" }])
+    end
+
+    test "with an invalid postcode" do
+      stub_request(:get, %r{\A#{Plek.current.find('imminence')}/areas/E15\.json}).
+        to_return(body: { _response_info: { status: 404 }, total: 0, results: [] }.to_json)
+
+      response = $imminence.areas_for_postcode("E15")
+
+      assert_equal 404, response["_response_info"]["status"]
+      assert_equal 0, response["total"]
+      assert_empty response["results"]
+    end
+
+    test "with a valid postcode outside valid areas" do
+      refute LandlordImmigrationCheckCalculator.valid_postcode("WC2B 6SE")
+    end
+
+    test "with a valid postcode within the valid areas" do
+      assert LandlordImmigrationCheckCalculator.valid_postcode("B1 1EQ")
+      assert LandlordImmigrationCheckCalculator.valid_postcode("B62 0BG")
+      assert LandlordImmigrationCheckCalculator.valid_postcode("B43 5AB")
+      assert LandlordImmigrationCheckCalculator.valid_postcode("WV1 1ES")
+      assert LandlordImmigrationCheckCalculator.valid_postcode("B43 7DG")
+    end
+  end
+end
