3b7bdbaad01e9525254d4790661506252c3f2e49
Tadas Tamosauskas <tadas@pdfcv.com>
2015-09-16 14:27:40 +0100

    Refactor old student maintenance grants and loans calculations
    This gets rid of the comments and brings the implementation inline with the
    new calculations style. I decided to get rid of comments because now behaviour
    is covered by unit tests that explain logic in descriptions. One comment also
    had an incorrect value that was out of sync with code (£9.90 vs £9.59).

--- lib/smart_answer/calculators/student_finance_calculator.rb
+++ lib/smart_answer/calculators/student_finance_calculator.rb
@@ -101,39 +101,33 @@ module SmartAnswer
         end
 
         def maintenance_grant_amount
-          # 2015-16 rates are the same as 2014-15:
-          # max of £3,387 for income up to £25,000 then,
-          # £1 less than max for each whole £5.28 above £25000 up to £42,611
-          # min grant is £50 for income = £42,620
-          # no grant for  income above £42,620
-          if @household_income <= 25_000
-            Money.new('3387')
-          else
-            if @household_income > 42_620
-              Money.new('0')
-            else
-              Money.new(3387 - ((@household_income - 25_000) / 5.28).floor)
-            end
-          end
+          return Money.new('3387') if @household_income <= 25_000
+          return Money.new('0') if @household_income > 42_620
+          Money.new(3387 - grant_reduction_based_on_income)
         end
 
         def maintenance_loan_amount
           if @household_income <= 42_875
-            # reduce maintenance loan by £0.5 for each £1 of maintenance grant
-            Money.new(max_loan_amount - (maintenance_grant_amount.value / 2.0).floor)
+            Money.new(max_loan_amount - (maintenance_grant_amount.value * 0.5).floor)
           else
-            # reduce maintenance loan by £1 for each full £9.90 of income above £42875 until loan reaches 65% of max, when no further reduction applies
-            min_loan_amount = (0.65 * max_loan_amount.value).floor # to match the reference table
-            reduced_loan_amount = max_loan_amount - ((@household_income - 42_875) / 9.59).floor
-            if reduced_loan_amount > min_loan_amount
-              Money.new(reduced_loan_amount)
-            else
-              Money.new(min_loan_amount)
-            end
+            reduced_loan_amount = max_loan_amount - loan_reduction_based_on_income
+            Money.new([reduced_loan_amount, min_loan_amount].max)
           end
         end
 
       private
+
+        def loan_reduction_based_on_income
+          ((@household_income - 42_875) / 9.59).floor
+        end
+
+        def grant_reduction_based_on_income
+          ((@household_income - 25_000) / 5.28).floor
+        end
+
+        def min_loan_amount
+          (0.65 * max_loan_amount.value).floor
+        end
 
         def max_loan_amount
           Money.new(LOAN_MAXIMUMS[@course_start][@residence])
