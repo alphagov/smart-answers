fdf5e9ed2ab169539279e91cc267fa26541f67a5
tijmen <tijmen@gmail.com>
2015-09-22 15:36:44 +0100

    Silence logging in tests
    The registerer has a weird logging set up. It outputs to STDERR via de
    the API logger (which is a `Logger`) and STDOUT via `puts`.
    
    We don't have time now to normalise this so we'll have to silence the
    output in the tests.

--- test/unit/services/panopticon_registerer_test.rb
+++ test/unit/services/panopticon_registerer_test.rb
@@ -5,8 +5,21 @@ class PanopticonRegistererTest < ActiveSupport::TestCase
     request = stub_request(:put, %r[http://panopticon.dev.gov.uk/])
     registerables = [OpenStruct.new, OpenStruct.new]
 
-    PanopticonRegisterer.new(registerables).register
+    silence_logging do
+      PanopticonRegisterer.new(registerables).register
+    end
 
     assert_requested(request, times: 2)
+  end
+
+private
+
+  # The registerer is quite noisy.
+  def silence_logging
+    silence_stream $stdout do
+      silence_stream $stderr do
+        yield
+      end
+    end
   end
 end
