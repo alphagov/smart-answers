7bfb563b84ad6bbeeddacc9fca31a9f0f4d76104
tijmen <tijmen@gmail.com>
2015-09-23 10:52:51 +0100

    Use smartdown fixtures for testing
    Using the fixtures instead of the real thing makes the tests more
    resilient.

--- test/unit/smartdown_adapter/flow_registration_presenter_test.rb
+++ test/unit/smartdown_adapter/flow_registration_presenter_test.rb
@@ -4,11 +4,19 @@ module SmartdownAdapter
   class FlowRegistrationPresenterTest < ActiveSupport::TestCase
     context "content_id" do
       should "use the flow content_id" do
-        flow = SmartdownAdapter::Registry.instance.find('pay-leave-for-parents')
+        SmartdownAdapter::Registry.reset_instance
+        flow_registry_options = {
+          show_drafts: true,
+          preload_flows: true,
+          smartdown_load_path: Rails.root.join('test', 'fixtures', 'smartdown_flows')
+        }
+        flow = SmartdownAdapter::Registry.instance(flow_registry_options).find("animal-example-simple")
 
         presenter = SmartdownAdapter::FlowRegistrationPresenter.new(flow)
 
-        assert_equal "1f6b4ecc-ce2c-488a-b9c7-b78b3bba5598", presenter.content_id
+        assert_equal "5bb6964b-3147-4423-aab3-b87e1cb8b838", presenter.content_id
+
+        SmartdownAdapter::Registry.reset_instance
       end
     end
   end
