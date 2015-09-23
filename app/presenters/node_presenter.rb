517d6b422c7feb231d46b42a54cfab7a5b2600be
James Mead <james@floehopper.org>
2015-09-23 16:25:26 +0100

    Push i18n-interpolation methods down onto QuestionPresenter
    These method are only relevant to question nodes and thus it makes more sense
    to implement them on `QuestionPresenter` rather than `NodePresenter`.
    
    Note that I've also moved the inclusion of `ActionView::Helpers::NumberHelper`
    down into `QuestionPresenter` since its only used in that class or its
    subclasses.

--- app/presenters/node_presenter.rb
+++ app/presenters/node_presenter.rb
@@ -1,42 +1,8 @@
 class NodePresenter
-  include ActionView::Helpers::NumberHelper
-
   def initialize(i18n_prefix, node, state = nil)
     @i18n_prefix = i18n_prefix
     @node = node
     @state = state || SmartAnswer::State.new(nil)
-  end
-
-  def i18n_node_prefix
-    "#{@i18n_prefix}.#{@node.name}"
-  end
-
-  def state_for_interpolation(nested = false)
-    Hash[@state.to_hash.map { |k, v| [k, value_for_interpolation(v, nested)] }]
-  end
-
-  def value_for_interpolation(value, nested = false)
-    case value
-    when Date then I18n.localize(value, format: :long)
-    when ::SmartAnswer::Money then
-      number_to_currency(value, precision: ((value.to_f == value.to_f.round) ? 0 : 2))
-    when ::SmartAnswer::Salary then
-      number_to_currency(value.amount, precision: 0) + " per " + value.period
-    when ::SmartAnswer::PhraseList then
-      if nested == false
-        value.phrase_keys.map do |phrase_key|
-          begin
-            I18n.translate!("#{@i18n_prefix}.phrases.#{phrase_key}", state_for_interpolation(true))
-          rescue => e
-            Rails.logger.warn("[Missing phrase] The phrase being rendered is not present: #{e.key}\tResponses: #{@state.responses.join('/')}") if @node.outcome?
-            phrase_key
-          end
-        end.join("\n\n")
-      else
-        false
-      end
-    else value
-    end
   end
 
   def method_missing(method, *args)
--- app/presenters/question_presenter.rb
+++ app/presenters/question_presenter.rb
@@ -1,4 +1,10 @@
 class QuestionPresenter < NodePresenter
+  include ActionView::Helpers::NumberHelper
+
+  def i18n_node_prefix
+    "#{@i18n_prefix}.#{@node.name}"
+  end
+
   def translate_and_render(subkey, html: true)
     markup = translate!(subkey)
     return unless markup
@@ -9,6 +15,34 @@ class QuestionPresenter < NodePresenter
     I18n.translate!("#{i18n_node_prefix}.#{subkey}", state_for_interpolation)
   rescue I18n::MissingTranslationData
     nil
+  end
+
+  def state_for_interpolation(nested = false)
+    Hash[@state.to_hash.map { |k, v| [k, value_for_interpolation(v, nested)] }]
+  end
+
+  def value_for_interpolation(value, nested = false)
+    case value
+    when Date then I18n.localize(value, format: :long)
+    when ::SmartAnswer::Money then
+      number_to_currency(value, precision: ((value.to_f == value.to_f.round) ? 0 : 2))
+    when ::SmartAnswer::Salary then
+      number_to_currency(value.amount, precision: 0) + " per " + value.period
+    when ::SmartAnswer::PhraseList then
+      if nested == false
+        value.phrase_keys.map do |phrase_key|
+          begin
+            I18n.translate!("#{@i18n_prefix}.phrases.#{phrase_key}", state_for_interpolation(true))
+          rescue => e
+            Rails.logger.warn("[Missing phrase] The phrase being rendered is not present: #{e.key}\tResponses: #{@state.responses.join('/')}") if @node.outcome?
+            phrase_key
+          end
+        end.join("\n\n")
+      else
+        false
+      end
+    else value
+    end
   end
 
   def title
