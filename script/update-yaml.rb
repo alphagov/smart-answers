c7f1d7c241850cfb589483c7b8968702891b8a3f
James Mead <james@floehopper.org>
2015-10-15 10:22:46 +0100

    Revert "TEMP: Add update-yaml script"
    This reverts commit edcd63e8a1910c43e692d3df4bd225d520f578f0.
    
    Now that I've used this script it is surplus to requirements.

--- script/update-yaml.rb
+++
@@ -1,6 +0,0 @@
-Dir[Rails.root.join('test', 'data', '*.yml')].each do |filename|
-  data = YAML.load_file(filename)
-  File.open(filename, 'w') do |file|
-    file.write(YAML.dump(data))
-  end
-end
