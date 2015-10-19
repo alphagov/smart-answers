13fc6a2794e68dcb28f00aeaa2c1614320ad403d
James Mead <james@floehopper.org>
2015-10-19 17:55:59 +0100

    Move $worldwide_api global variable to Services module method

--- config/initializers/worldwide_api.rb
+++
@@ -1,9 +0,0 @@
-require 'gds_api/worldwide'
-
-# In development, point at the public version of the API
-# as we won't normally have whitehall running
-if Rails.env.development?
-  $worldwide_api = GdsApi::Worldwide.new("https://www.gov.uk")
-else
-  $worldwide_api = GdsApi::Worldwide.new(Plek.new.find('whitehall-admin'))
-end
--- lib/services.rb
+++ lib/services.rb
@@ -1,5 +1,6 @@
 require 'gds_api/publishing_api'
 require 'gds_api/imminence'
+require 'gds_api/worldwide'
 
 module Services
   def self.publishing_api
@@ -8,5 +9,15 @@ module Services
 
   def self.imminence_api
     @imminence_api ||= GdsApi::Imminence.new(Plek.new.find('imminence'))
+  end
+
+  def self.worldwide_api
+    # In development, point at the public version of the API
+    # as we won't normally have whitehall running
+    if Rails.env.development?
+      @worldwide_api ||= GdsApi::Worldwide.new("https://www.gov.uk")
+    else
+      @worldwide_api ||= GdsApi::Worldwide.new(Plek.new.find('whitehall-admin'))
+    end
   end
 end
