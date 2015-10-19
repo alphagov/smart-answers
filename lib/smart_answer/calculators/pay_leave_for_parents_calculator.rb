14f086380150a175d1ea862e32f29c5ba0c2c90e
Chris Roos <chris@seagul.co.uk>
2015-10-19 13:45:30 +0100

    Add precalculate blocks to mother_started_working_before_continuity_start_date
    So that we can interpolate the values into the question text.
    
    I've copied just the methods I need from pay_leave_for_parents/render_time.rb to
    the new calculator. I've chosen to make them instance methods as I'm not
    convinced they need to be class methods.
    
    I've copied the unit test for `continuity_start_date` from
    `SmartdownPlugins::PayLeaveForParentsTest`.

--- lib/smart_answer/calculators/pay_leave_for_parents_calculator.rb
+++ lib/smart_answer/calculators/pay_leave_for_parents_calculator.rb
@@ -1,4 +1,11 @@
 module SmartAnswer::Calculators
   class PayLeaveForParentsCalculator
+    def continuity_start_date(date)
+      saturday_before(date - 39.weeks)
+    end
+
+    def saturday_before(date)
+      (date - date.wday) - 1.day
+    end
   end
 end
--- test/unit/calculators/pay_leave_for_parents_calculator_test.rb
+++ test/unit/calculators/pay_leave_for_parents_calculator_test.rb
@@ -3,6 +3,12 @@ require_relative "../../test_helper"
 module SmartAnswer
   module Calculators
     class PayLeaveForParentsCalculatorTest < ActiveSupport::TestCase
+      test "continuity_start_date" do
+        due_date = Date.parse('2015-1-1')
+        expected = Date.parse('2014-3-29')
+        calculator = SmartAnswer::Calculators::PayLeaveForParentsCalculator.new
+        assert_equal expected, calculator.continuity_start_date(due_date)
+      end
     end
   end
 end
