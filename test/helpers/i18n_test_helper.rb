e7f056f07e6e48941426be9a96f36c448c261842
Chris Roos <chris@seagul.co.uk>
2015-11-02 16:53:13 +0000

    Extract I18nTestHelper#use_additional_translation_file
    In preparation for using it instead of `use_only_translation_file!`

--- test/helpers/i18n_test_helper.rb
+++ test/helpers/i18n_test_helper.rb
@@ -1,11 +1,15 @@
 module I18nTestHelper
   def using_additional_translation_file(filename, &block)
+    use_additional_translation_file(filename)
+    yield
+    reset_translation_files!
+  end
+
+  def use_additional_translation_file(filename)
     @i18n_load_path_stack ||= []
     @i18n_load_path_stack.push I18n.config.load_path.dup
     I18n.config.load_path.unshift(filename)
     I18n.reload!
-    yield
-    reset_translation_files!
   end
 
   def use_only_translation_file!(filename)
