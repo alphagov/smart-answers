6d69ef84a808e2dccee993be36040fef57c59f03
Chris Roos <chris@seagul.co.uk>
2015-11-02 16:52:02 +0000

    Rename I18nTestHelper#using_translation_file
    To make it clearer that we're _appending_ another file to the load path.

--- test/helpers/i18n_test_helper.rb
+++ test/helpers/i18n_test_helper.rb
@@ -1,5 +1,5 @@
 module I18nTestHelper
-  def using_translation_file(filename, &block)
+  def using_additional_translation_file(filename, &block)
     @i18n_load_path_stack ||= []
     @i18n_load_path_stack.push I18n.config.load_path.dup
     I18n.config.load_path.unshift(filename)
