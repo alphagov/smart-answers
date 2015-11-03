2fb6619eeb6936c7b660dd639e3ccd2add7b2899
Chris Roos <chris@seagul.co.uk>
2015-11-03 10:15:38 +0000

    Explicitly add test locales to the I18n.load_path
    Appending to the `I18n.load_path` in the integration_test_helper means that we
    affect the I18n.load_path for all tests when running `rake`. It feels better to
    explicitly append the test locales to the load path when we need them.

--- test/helpers/i18n_test_helper.rb
+++ test/helpers/i18n_test_helper.rb
@@ -1,4 +1,11 @@
 module I18nTestHelper
+  def use_all_fixture_flow_translation_files
+    @i18n_load_path_stack ||= []
+    @i18n_load_path_stack.push I18n.config.load_path.dup
+    I18n.load_path += Dir[Rails.root.join(*%w{test fixtures smart_answer_flows locales * *.{rb,yml}})]
+    I18n.reload!
+  end
+
   def using_additional_translation_file(filename, &block)
     use_additional_translation_file(filename)
     yield
--- test/integration/engine/engine_test_helper.rb
+++ test/integration/engine/engine_test_helper.rb
@@ -1,8 +1,10 @@
 require_relative '../../integration_test_helper'
+require_relative '../../helpers/i18n_test_helper'
 require 'gds_api/test_helpers/content_api'
 
 class EngineIntegrationTest < ActionDispatch::IntegrationTest
   include GdsApi::TestHelpers::ContentApi
+  include I18nTestHelper
 
   setup do
     fixture_flows_path = Rails.root.join(*%w{test fixtures smart_answer_flows})
@@ -11,11 +13,15 @@ class EngineIntegrationTest < ActionDispatch::IntegrationTest
     FLOW_REGISTRY_OPTIONS[:smart_answer_load_path] = fixture_flows_path
     SmartAnswer::FlowRegistry.reset_instance
     stub_content_api_default_artefact
+
+    use_all_fixture_flow_translation_files
   end
 
   teardown do
     FLOW_REGISTRY_OPTIONS.delete(:smart_answer_load_path)
     FLOW_REGISTRY_OPTIONS[:preload_flows] = @preload_flows
     SmartAnswer::FlowRegistry.reset_instance
+
+    reset_translation_files!
   end
 end
