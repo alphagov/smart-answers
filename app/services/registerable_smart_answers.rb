319fa94ddec73deb8c97120d7b0a6b9b5ec52701
Chris Roos <chris@seagul.co.uk>
2015-11-03 14:50:22 +0000

    Inline RegisterableSmartAnswers#smart_answer_registrables

--- app/services/registerable_smart_answers.rb
+++ app/services/registerable_smart_answers.rb
@@ -1,11 +1,5 @@
 class RegisterableSmartAnswers
   def flow_presenters
-    smart_answer_registrables
-  end
-
-private
-
-  def smart_answer_registrables
     flow_registry = SmartAnswer::FlowRegistry.new(FLOW_REGISTRY_OPTIONS)
     flow_registry.flows.map do |flow|
       FlowRegistrationPresenter.new(flow)
