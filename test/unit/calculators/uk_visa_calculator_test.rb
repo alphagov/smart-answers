01431257ac3dd3f651098ed5791a2eaacd5ad7a4
Chris Roos <chris@seagul.co.uk>
2015-11-13 11:56:27 +0000

    Add UkVisaCalculator#passport_country_in_ukot_list?

--- test/unit/calculators/uk_visa_calculator_test.rb
+++ test/unit/calculators/uk_visa_calculator_test.rb
@@ -30,6 +30,20 @@ module SmartAnswer
           refute calculator.passport_country_in_non_visa_national_list?
         end
       end
+
+      context '#passport_country_in_ukot_list?' do
+        should 'return true if passport_country is in list of uk overseas territories' do
+          calculator = UkVisaCalculator.new
+          calculator.passport_country = 'anguilla'
+          assert calculator.passport_country_in_ukot_list?
+        end
+
+        should 'return false if passport_country is not in list of uk overseas territories' do
+          calculator = UkVisaCalculator.new
+          calculator.passport_country = 'made-up-country'
+          refute calculator.passport_country_in_ukot_list?
+        end
+      end
     end
   end
 end
