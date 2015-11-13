ebdffec2210b4a0c9cb43011a7a30c1331228b19
James Mead <james@floehopper.org>
2015-11-13 16:06:27 +0000

    Convert custom-errors-sample to use question ERB templates
    I ran the following command:
    
        $ rails r script/convert-question-i18-yaml-to-erb-templates.rb test/fixtures/smart_answer_flows/custom-errors-sample

--- test/fixtures/smart_answer_flows/custom-errors-sample.rb
+++ test/fixtures/smart_answer_flows/custom-errors-sample.rb
@@ -4,6 +4,8 @@ module SmartAnswer
       name 'custom-errors-sample'
       status :draft
 
+      use_erb_templates_for_questions
+
       value_question :how_many_things_do_you_own? do
         next_node(permitted: [:done]) do |response|
           raise SmartAnswer::InvalidResponse, :custom_error unless response.to_i > 0
