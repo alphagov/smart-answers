a454c6c51bd305a54e97f4e26329303e24c87894
Chris Roos <chris@seagul.co.uk>
2015-11-13 10:50:14 +0000

    Add CheckUkVisa calculator
    I'm planning to move logic from this flow to the new calculator.
    
    I've started with the list of excluded countries. I imagine there's a
    better name for this list but I'll stick with excluded for now.

---
+++ lib/smart_answer/calculators/uk_visa_calculator.rb
@@ -0,0 +1,5 @@
+module SmartAnswer::Calculators
+  class UkVisaCalculator
+    EXCLUDE_COUNTRIES = %w(american-samoa british-antarctic-territory british-indian-ocean-territory french-guiana french-polynesia gibraltar guadeloupe holy-see martinique mayotte new-caledonia reunion st-pierre-and-miquelon the-occupied-palestinian-territories wallis-and-futuna western-sahara)
+  end
+end
