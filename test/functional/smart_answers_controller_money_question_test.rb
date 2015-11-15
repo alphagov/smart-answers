732161859da26ff0b31af3ce7bcbb8f681b283f0
James Mead <james@floehopper.org>
2015-11-15 23:48:16 +0000

    Extract SmartAnswersControllerSampleWithMoneyQuestionFlow
    Define an explicit flow class just for this test case to avoid sharing setup
    with other tests i.e. make this test less brittle at the expense of more
    verbosity.
    
    Note that although it wasn't strictly necessary for this commit (because of the
    `SmartAnswer::FlowRegistry.find`), I've changed the `id` parameter for all
    requests to match the name of the new flow. In order to do this, I also had to
    override `SmartAnswersControllerTestHelper#submit_response` and
    `#submit_json_response`.

--- test/functional/smart_answers_controller_money_question_test.rb
+++ test/functional/smart_answers_controller_money_question_test.rb
@@ -1,6 +1,6 @@
 require_relative '../test_helper'
 require_relative '../helpers/i18n_test_helper'
-require_relative '../fixtures/smart_answer_flows/smart-answers-controller-sample'
+require_relative '../fixtures/smart_answer_flows/smart-answers-controller-sample-with-money-question'
 require_relative 'smart_answers_controller_test_helper'
 require 'gds_api/test_helpers/content_api'
 
@@ -14,10 +14,10 @@ class SmartAnswersControllerMoneyQuestionTest < ActionController::TestCase
   def setup
     stub_content_api_default_artefact
 
-    @flow = SmartAnswer::SmartAnswersControllerSampleFlow.build
+    @flow = SmartAnswer::SmartAnswersControllerSampleWithMoneyQuestionFlow.build
     load_path = fixture_file('smart_answer_flows')
     SmartAnswer::FlowRegistry.stubs(:instance).returns(stub("Flow registry", find: @flow, load_path: load_path))
-    use_additional_translation_file(fixture_file('smart_answer_flows/locales/en/smart-answers-controller-sample.yml'))
+    use_additional_translation_file(fixture_file('smart_answer_flows/locales/en/smart-answers-controller-sample-with-money-question.yml'))
   end
 
   def teardown
@@ -26,19 +26,8 @@ class SmartAnswersControllerMoneyQuestionTest < ActionController::TestCase
 
   context "GET /<slug>" do
     context "money question" do
-      setup do
-        @flow = SmartAnswer::Flow.new do
-          name "smart-answers-controller-sample"
-          money_question :how_much? do
-            next_node :done
-          end
-          outcome :done
-        end
-        @controller.stubs(:flow_registry).returns(stub("Flow registry", find: @flow))
-      end
-
       should "display question" do
-        get :show, id: 'smart-answers-controller-sample', started: 'y'
+        get :show, id: 'smart-answers-controller-sample-with-money-question', started: 'y'
         assert_select ".step.current h2", /How much\?/
         assert_select "input[type=text][name=response]"
       end
@@ -51,16 +40,22 @@ class SmartAnswersControllerMoneyQuestionTest < ActionController::TestCase
 
       context "suffix_label in translation file" do
         setup do
-          using_additional_translation_file(fixture_file('smart_answer_flows/locales/en/smart-answers-controller-sample-with-suffix-label.yml')) do
-            get :show, id: 'smart-answers-controller-sample', started: 'y'
-          end
+          get :show, id: 'smart-answers-controller-sample-with-money-question', started: 'y', responses: '1.23'
         end
 
         should "show the label after the question input" do
           assert_select "label > input[type=text][name=response]"
-          assert_match /input.*?name="response".*?millions\./, response.body
+          assert_match /input.*?name="response".*?money-question-suffix-label/, response.body
         end
       end
     end
+  end
+
+  def submit_response(response = nil, other_params = {})
+    super(response, other_params.merge(id: 'smart-answers-controller-sample-with-money-question'))
+  end
+
+  def submit_json_response(response = nil, other_params = {})
+    super(response, other_params.merge(id: 'smart-answers-controller-sample-with-money-question'))
   end
 end
