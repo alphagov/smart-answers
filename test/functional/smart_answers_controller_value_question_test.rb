0642a0ffdde4ab567607d849d867813d6d4dddae
James Mead <james@floehopper.org>
2015-11-15 23:29:19 +0000

    Extract SmartAnswersControllerSampleWithValueQuestionFlow
    Define an explicit flow class just for this test case to avoid sharing setup
    with other tests i.e. make this test less brittle at the expense of more
    verbosity.
    
    Note that although it wasn't strictly necessary for this commit (because of the
    `SmartAnswer::FlowRegistry.find`), I've changed the `id` parameter for all
    requests to match the name of the new flow. In order to do this, I also had to
    override `SmartAnswersControllerTestHelper#submit_response` and
    `#submit_json_response`.

--- test/functional/smart_answers_controller_value_question_test.rb
+++ test/functional/smart_answers_controller_value_question_test.rb
@@ -1,6 +1,6 @@
 require_relative '../test_helper'
 require_relative '../helpers/i18n_test_helper'
-require_relative '../fixtures/smart_answer_flows/smart-answers-controller-sample'
+require_relative '../fixtures/smart_answer_flows/smart-answers-controller-sample-with-value-question'
 require_relative 'smart_answers_controller_test_helper'
 require 'gds_api/test_helpers/content_api'
 
@@ -14,10 +14,10 @@ class SmartAnswersControllerValueQuestionTest < ActionController::TestCase
   def setup
     stub_content_api_default_artefact
 
-    @flow = SmartAnswer::SmartAnswersControllerSampleFlow.build
+    @flow = SmartAnswer::SmartAnswersControllerSampleWithValueQuestionFlow.build
     load_path = fixture_file('smart_answer_flows')
     SmartAnswer::FlowRegistry.stubs(:instance).returns(stub("Flow registry", find: @flow, load_path: load_path))
-    use_additional_translation_file(fixture_file('smart_answer_flows/locales/en/smart-answers-controller-sample.yml'))
+    use_additional_translation_file(fixture_file('smart_answer_flows/locales/en/smart-answers-controller-sample-with-value-question.yml'))
   end
 
   def teardown
@@ -26,58 +26,50 @@ class SmartAnswersControllerValueQuestionTest < ActionController::TestCase
 
   context "GET /<slug>" do
     context "value question" do
-      setup do
-        @flow = SmartAnswer::Flow.new do
-          name "smart-answers-controller-sample"
-          value_question :how_many_green_bottles? do
-            next_node :done
-          end
-
-          outcome :done
-        end
-        @controller.stubs(:flow_registry).returns(stub("Flow registry", find: @flow))
-      end
-
       should "display question" do
-        get :show, id: 'smart-answers-controller-sample', started: 'y'
+        get :show, id: 'smart-answers-controller-sample-with-value-question', started: 'y'
         assert_select ".step.current h2", /How many green bottles\?/
         assert_select "input[type=text][name=response]"
       end
 
       should "accept question input and redirect to canonical url" do
         submit_response "10"
-        assert_redirected_to '/smart-answers-controller-sample/y/10'
+        assert_redirected_to '/smart-answers-controller-sample-with-value-question/y/10'
       end
 
       should "display collapsed question, and format number" do
-        get :show, id: 'smart-answers-controller-sample', started: 'y', responses: "12345"
+        get :show, id: 'smart-answers-controller-sample-with-value-question', started: 'y', responses: "12345"
         assert_select ".done-questions", /How many green bottles\?\s+12,345/
       end
 
       context "label in translation file" do
         setup do
-          using_additional_translation_file(fixture_file('smart_answer_flows/locales/en/smart-answers-controller-sample-with-label.yml')) do
-            get :show, id: 'smart-answers-controller-sample', started: 'y'
-          end
+          get :show, id: 'smart-answers-controller-sample-with-value-question', started: 'y', responses: "12345"
         end
         should "show the label text before the question input" do
-          assert_match /Enter a number.*?input.*?name="response".*?/, response.body
+          assert_match /value-question-label.*?input.*?name="response".*?/, response.body
           assert_select "label > input[type=text][name=response]"
         end
       end
 
       context "suffix_label in translation file" do
         setup do
-          using_additional_translation_file(fixture_file('smart_answer_flows/locales/en/smart-answers-controller-sample-with-suffix-label.yml')) do
-            get :show, id: 'smart-answers-controller-sample', started: 'y'
-          end
+          get :show, id: 'smart-answers-controller-sample-with-value-question', started: 'y', responses: "123/456"
         end
 
         should "show the label text after the question input" do
-          assert_match /input.*?name="response".*?bottles\./, response.body
+          assert_match /input.*?name="response".*?value-question-suffix-label/, response.body
           assert_select "label > input[type=text][name=response]"
         end
       end
     end
+  end
+
+  def submit_response(response = nil, other_params = {})
+    super(response, other_params.merge(id: 'smart-answers-controller-sample-with-value-question'))
+  end
+
+  def submit_json_response(response = nil, other_params = {})
+    super(response, other_params.merge(id: 'smart-answers-controller-sample-with-value-question'))
   end
 end
