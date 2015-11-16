c58868dee22d63198a1f57703678219bb908ac1d
James Mead <james@floehopper.org>
2015-11-16 00:35:06 +0000

    Extract FixtureFlowsHelper

--- test/integration/engine/engine_test_helper.rb
+++ test/integration/engine/engine_test_helper.rb
@@ -1,26 +1,22 @@
 require_relative '../../integration_test_helper'
+require_relative '../../helpers/fixture_flows_helper'
 require_relative '../../helpers/i18n_test_helper'
 require 'gds_api/test_helpers/content_api'
 
 class EngineIntegrationTest < ActionDispatch::IntegrationTest
   include GdsApi::TestHelpers::ContentApi
+  include FixtureFlowsHelper
   include I18nTestHelper
 
   setup do
-    fixture_flows_path = Rails.root.join(*%w{test fixtures smart_answer_flows})
-    @preload_flows = FLOW_REGISTRY_OPTIONS[:preload_flows]
-    FLOW_REGISTRY_OPTIONS[:preload_flows] = false
-    FLOW_REGISTRY_OPTIONS[:smart_answer_load_path] = fixture_flows_path
-    SmartAnswer::FlowRegistry.reset_instance
+    setup_fixture_flows
     stub_content_api_default_artefact
 
     use_all_fixture_flow_translation_files
   end
 
   teardown do
-    FLOW_REGISTRY_OPTIONS.delete(:smart_answer_load_path)
-    FLOW_REGISTRY_OPTIONS[:preload_flows] = @preload_flows
-    SmartAnswer::FlowRegistry.reset_instance
+    teardown_fixture_flows
 
     reset_translation_files
   end
