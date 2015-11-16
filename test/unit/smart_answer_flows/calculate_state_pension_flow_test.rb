alphagov/smart-answers

commit f89112454eb899b8570c474f01db91cbd96862b2

Use next_node block in :dob_age?
I've replaced `define_predicate` with `next_node_calculation` in an attempt to
keep the code as similar as possible. I've had to update the names used by
`next_node_calculation` as the question marks cause problems when the state
object is converted to a hash in preparation for rendering the ERB templates.

I've had to remove the `|response|` parameter from the `next_node_calculation`
blocks to satisfy Rubocop.

lib/smart_answer_flows/calculate-state-pension.rb
@@ -91,19 +91,30 @@ module SmartAnswer
           Calculators::StatePensionAmountCalculator.new(gender: gender, dob: response)
         end
 
-        define_predicate(:near_pension_date?) do |response|
+        next_node_calculation(:near_pension_date) do
           calc.before_state_pension_date? and calc.within_four_months_one_day_from_state_pension?
         end
 
-        define_predicate(:under_20_years_old?) do |response|
+        next_node_calculation(:under_20_years_old) do
           calc.under_20_years_old?
         end
 
         validate { |response| response <= Date.today }
 
-        next_node_if(:too_young, under_20_years_old?)
-        next_node_if(:near_state_pension_age, near_pension_date?)
-        next_node(:age_result)
+        permitted_next_nodes = [
+          :too_young,
+          :near_state_pension_age,
+          :age_result
+        ]
+        next_node(permitted: permitted_next_nodes) do
+          if under_20_years_old
+            :too_young
+          elsif near_pension_date
+            :near_state_pension_age
+          else
+            :age_result
+          end
+        end
       end
 
       date_question :dob_bus_pass? do

commit b456771e45e8c605b7580bb12dbcefff6dc04ec5

Fix bug when dob_age is more than 2 days in future
Partly fixes issue #2079.

This problem was introduced in f89112454eb899b8570c474f01db91cbd96862b2
(part of PR #2061) when we switched to using `next_node` with a block in
the `:dob_age?` question.

The implementation before f89112454eb899b8570c474f01db91cbd96862b2
relied on the `validate` block preventing the
`StatePensionAmountCalculator` being instantiated with a date of birth
that it couldn't handle (notably a date of birth more than two days in
the future). The switch to instantiating the calculator in the
`next_node_calculation` block meant that it was created before the
`validate` block is executed, which in turn meant that it could be
created with an invalid date of birth. The fix in this commit is to
instantiate the calculator in the `next_node` block so that we once
again rely on the `validate` block to reject invalid dates.

The test I've added is for the unchanged `validate` behaviour but it
gave me a failing test that I was able to use to drive the fix.

The problem ultimately stems from `StatePensionDateQuery#find_date`.
When the date is more than two days in the future, no data can be found
in either the static or dynamic pension data.

Note. The last entry in the
`StatePensionDateQuery#pension_dates_dynamic` array has an end date of
`Date.today + 1`. This explains why the error only occurs when the date
is more than two days in the future.

Note. I've had to change the `and` to `&&` when assigning a value to the
`near_pension_date` variable. Without this change, some of the
integration and regression tests for calculate-state-pension failed.

lib/smart_answer_flows/calculate-state-pension.rb
@@ -87,18 +87,6 @@ module SmartAnswer
           calculator.ni_years_to_date_from_dob
         end
 
-        next_node_calculation :calc do |response|
-          Calculators::StatePensionAmountCalculator.new(gender: gender, dob: response)
-        end
-
-        next_node_calculation(:near_pension_date) do
-          calc.before_state_pension_date? and calc.within_four_months_one_day_from_state_pension?
-        end
-
-        next_node_calculation(:under_20_years_old) do
-          calc.under_20_years_old?
-        end
-
         validate { |response| response <= Date.today }
 
         permitted_next_nodes = [
@@ -106,7 +94,11 @@ module SmartAnswer
           :near_state_pension_age,
           :age_result
         ]
-        next_node(permitted: permitted_next_nodes) do
+        next_node(permitted: permitted_next_nodes) do |response|
+          calc = Calculators::StatePensionAmountCalculator.new(gender: gender, dob: response)
+          near_pension_date = calc.before_state_pension_date? && calc.within_four_months_one_day_from_state_pension?
+          under_20_years_old = calc.under_20_years_old?
+
           if under_20_years_old
             :too_young
           elsif near_pension_date

test/unit/smart_answer_flows/calculate_state_pension_flow_test.rb
@@ -1,0 +1,29 @@
+require_relative '../../test_helper'
+
+require 'smart_answer_flows/calculate-state-pension'
+
+module SmartAnswer
+  class CalculateStatePensionFlowTest < ActiveSupport::TestCase
+    setup do
+      @flow = CalculateStatePensionFlow.build
+    end
+
+    context 'validation' do
+      context "for :dob_age?" do
+        setup do
+          @question = @flow.node(:dob_age?)
+          @state = SmartAnswer::State.new(@question)
+          @state.gender = 'male'
+        end
+
+        should "raise if the date of birth is later than today's date" do
+          invalid_date_of_birth = 1.week.from_now.to_date
+          @state.response = invalid_date_of_birth
+          assert_raise(SmartAnswer::InvalidResponse) do
+            @question.transition(@state, invalid_date_of_birth)
+          end
+        end
+      end
+    end
+  end
+end