3d1c71d6687dd1416f1e221db66e7dac3e3ffdd0
James Mead <james@floehopper.org>
2015-11-16 17:48:13 +0000

    Convert smart-answers-controller-sample-with-money-question to use question ERB templates
    I ran the following command:
    
        $ rails r script/convert-question-i18-yaml-to-erb-templates.rb test/fixtures/smart-answers-controller-sample-with-money-question
    
    I've also removed all the i18n-related code from the test.

--- test/functional/smart_answers_controller_money_question_test.rb
+++ test/functional/smart_answers_controller_money_question_test.rb
@@ -1,5 +1,4 @@
 require_relative '../test_helper'
-require_relative '../helpers/i18n_test_helper'
 require_relative '../helpers/fixture_flows_helper'
 require_relative '../fixtures/smart_answer_flows/smart-answers-controller-sample-with-money-question'
 require_relative 'smart_answers_controller_test_helper'
@@ -8,21 +7,17 @@ require 'gds_api/test_helpers/content_api'
 class SmartAnswersControllerMoneyQuestionTest < ActionController::TestCase
   tests SmartAnswersController
 
-  include I18nTestHelper
   include FixtureFlowsHelper
   include SmartAnswersControllerTestHelper
   include GdsApi::TestHelpers::ContentApi
 
   def setup
     stub_content_api_default_artefact
-
     setup_fixture_flows
-    use_additional_translation_file(fixture_file('smart_answer_flows/locales/en/smart-answers-controller-sample-with-money-question.yml'))
   end
 
   def teardown
     teardown_fixture_flows
-    reset_translation_files
   end
 
   context "GET /<slug>" do
@@ -39,7 +34,7 @@ class SmartAnswersControllerMoneyQuestionTest < ActionController::TestCase
         assert_select "body", /Please answer this question/
       end
 
-      context "suffix_label in translation file" do
+      context "suffix_label in erb template" do
         setup do
           get :show, id: 'smart-answers-controller-sample-with-money-question', started: 'y', responses: '1.23'
         end
