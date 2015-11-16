e4b0edac163315e9bb6970370d32ea278873af49
James Mead <james@floehopper.org>
2015-11-16 16:55:23 +0000

    Convert smart-answers-controller-sample-with-date-question to use question ERB templates
    I ran the following command:
    
        $ rails r script/convert-question-i18-yaml-to-erb-templates.rb test/fixtures/smart-answers-controller-sample-with-date-question
    
    I've also removed all the i18n-related code from the test.

--- test/fixtures/smart_answer_flows/smart-answers-controller-sample-with-date-question.rb
+++ test/fixtures/smart_answer_flows/smart-answers-controller-sample-with-date-question.rb
@@ -2,6 +2,7 @@ module SmartAnswer
   class SmartAnswersControllerSampleWithDateQuestionFlow < Flow
     def define
       name "smart-answers-controller-sample-with-date-question"
+      use_erb_templates_for_questions
       date_question :when? do
         next_node :done
       end
