9170be1908dce4d51df56566728e9cf5b19a1786
Chris Roos <chris@seagul.co.uk>
2015-11-17 12:35:00 +0000

    Add UkVisaCalculator#diplomatic_visit?

--- test/unit/calculators/uk_visa_calculator_test.rb
+++ test/unit/calculators/uk_visa_calculator_test.rb
@@ -184,6 +184,20 @@ module SmartAnswer
           refute calculator.medical_visit?
         end
       end
+
+      context '#diplomatic_visit?' do
+        should 'return true if purpose_of_visit_answer is "diplomatic"' do
+          calculator = UkVisaCalculator.new
+          calculator.purpose_of_visit_answer = 'diplomatic'
+          assert calculator.diplomatic_visit?
+        end
+
+        should 'return false if purpose_of_visit_answer is not "diplomatic"' do
+          calculator = UkVisaCalculator.new
+          calculator.purpose_of_visit_answer = 'not-diplomatic'
+          refute calculator.diplomatic_visit?
+        end
+      end
     end
   end
 end
