1d7f0db893a8dff0da0e86228490321d76044a44
Chris Roos <chris@seagul.co.uk>
2015-11-26 15:50:16 +0000

    Remove unused i18n helper
    None of the i18n helper methods are being used now that we've completely
    switched to ERB templates for questions.

--- test/integration/engine/engine_test_helper.rb
+++ test/integration/engine/engine_test_helper.rb
@@ -1,23 +1,17 @@
 require_relative '../../integration_test_helper'
 require_relative '../../helpers/fixture_flows_helper'
-require_relative '../../helpers/i18n_test_helper'
 require 'gds_api/test_helpers/content_api'
 
 class EngineIntegrationTest < ActionDispatch::IntegrationTest
   include GdsApi::TestHelpers::ContentApi
   include FixtureFlowsHelper
-  include I18nTestHelper
 
   setup do
     setup_fixture_flows
     stub_content_api_default_artefact
-
-    use_all_fixture_flow_translation_files
   end
 
   teardown do
     teardown_fixture_flows
-
-    reset_translation_files
   end
 end
