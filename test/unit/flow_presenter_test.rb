76485745abeaa7ab0f9b97acab6118d669b8e9c9
James Mead <james@floehopper.org>
2015-12-02 17:24:28 +0000

    Add unit tests for FlowPresenter#presenter_for
    It's pretty surprising that such a critical class (`FlowPresenter`) had no unit
    tests prior to this.
    
    I want to make changes to the `#presenter_for` method, so I've only added tests
    for this method for now in order to give myself confidence that my upcoming
    changes do not break any existing behaviour.
    
    Note that these tests do not endorse the existing behaviour.

---
+++ test/unit/flow_presenter_test.rb
@@ -0,0 +1,69 @@
+require_relative "../test_helper"
+
+class FlowPresenterTest < ActiveSupport::TestCase
+  setup do
+    @flow = SmartAnswer::Flow.new { value_question :first_question_key }
+    @request = ActionDispatch::TestRequest.new
+    @flow_presenter = FlowPresenter.new(@request, @flow)
+  end
+
+  test '#presenter_for returns presenter for Date question' do
+    question = @flow.date_question(:question_key).last
+    node_presenter = @flow_presenter.presenter_for(question)
+    assert_instance_of DateQuestionPresenter, node_presenter
+  end
+
+  test '#presenter_for returns presenter for CountrySelect question' do
+    question = @flow.country_select(:question_key).last
+    node_presenter = @flow_presenter.presenter_for(question)
+    assert_instance_of CountrySelectQuestionPresenter, node_presenter
+  end
+
+  test '#presenter_for returns presenter for MultipleChoice question' do
+    question = @flow.multiple_choice(:question_key).last
+    node_presenter = @flow_presenter.presenter_for(question)
+    assert_instance_of MultipleChoiceQuestionPresenter, node_presenter
+  end
+
+  test '#presenter_for returns presenter for Checkbox question' do
+    question = @flow.checkbox_question(:question_key).last
+    node_presenter = @flow_presenter.presenter_for(question)
+    assert_instance_of CheckboxQuestionPresenter, node_presenter
+  end
+
+  test '#presenter_for returns presenter for Value question' do
+    question = @flow.value_question(:question_key).last
+    node_presenter = @flow_presenter.presenter_for(question)
+    assert_instance_of ValueQuestionPresenter, node_presenter
+  end
+
+  test '#presenter_for returns presenter for Money question' do
+    question = @flow.money_question(:question_key).last
+    node_presenter = @flow_presenter.presenter_for(question)
+    assert_instance_of MoneyQuestionPresenter, node_presenter
+  end
+
+  test '#presenter_for returns presenter for Salary question' do
+    question = @flow.salary_question(:question_key).last
+    node_presenter = @flow_presenter.presenter_for(question)
+    assert_instance_of SalaryQuestionPresenter, node_presenter
+  end
+
+  test '#presenter_for returns presenter for other question types' do
+    question = @flow.send(:add_question, SmartAnswer::Question::Base, :question_key).last
+    node_presenter = @flow_presenter.presenter_for(question)
+    assert_instance_of QuestionPresenter, node_presenter
+  end
+
+  test '#presenter_for returns presenter for outcome' do
+    outcome = @flow.outcome(:outcome_key).last
+    node_presenter = @flow_presenter.presenter_for(outcome)
+    assert_instance_of OutcomePresenter, node_presenter
+  end
+
+  test '#presenter_for returns presenter for other node types' do
+    node = @flow.send(:add_question, SmartAnswer::Node, :node_key).last
+    node_presenter = @flow_presenter.presenter_for(node)
+    assert_instance_of NodePresenter, node_presenter
+  end
+end
