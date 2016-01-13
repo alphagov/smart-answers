5c114b30f46ffee769c9176635dffcfdac8b97af
Erik Eide <erik.eide@gmail.com>
2016-01-13 14:44:03 +0000

    Add data file for rates for benefit cap calculator
    These were hard-coded into the calculator, and with the PR aiming to allow devolution of rates based on geographic area, this file will be used to store rates per area

--- lib/smart_answer/calculators/benefit_calculator.rb
+++ lib/smart_answer/calculators/benefit_calculator.rb
@@ -1,9 +1,11 @@
 module SmartAnswer::Calculators
   class BenefitCalculator
     attr_accessor :single_couple_lone_parent
+    attr_reader :data
 
     def initialize
       @benefits = Hash.new(0)
+      @data = self.class.benefit_cap_rates
     end
 
     def claim(benefit, amount)
@@ -19,11 +21,15 @@ module SmartAnswer::Calculators
     end
 
     def benefit_cap
-      single_couple_lone_parent == 'single' ? 350 : 500
+      single_couple_lone_parent == 'single' ? data[:single] : data[:couple]
     end
 
     def total_over_cap
       total_benefits - benefit_cap
+    end
+
+    def self.benefit_cap_rates
+      @rates ||= YAML::load_file(Rails.root.join("lib", "data", "benefit_caps.yml"))
     end
   end
 end
