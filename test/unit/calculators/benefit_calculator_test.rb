dd2f617a88d2577f599d1ce6ab770e81875a04fa
Erik Eide <erik.eide@gmail.com>
2016-01-14 08:47:27 +0000

    Update calculator for benefit cap calculator
    After adding new rates, the test was still passing as it was only using assert, and not assert_equal for two values.

--- test/unit/calculators/benefit_calculator_test.rb
+++ test/unit/calculators/benefit_calculator_test.rb
@@ -8,59 +8,59 @@ module SmartAnswer::Calculators
       end
 
       context '#benefit_cap' do
-        should 'default to 500' do
-          assert @calculator.benefit_cap, 500
+        should 'default value' do
+          assert_equal @calculator.benefit_cap, 384
         end
 
         should 'return 350 when single' do
-          @calculator.single_couple_lone_parent == 'single'
-          assert @calculator.benefit_cap, 350
+          @calculator.single_couple_lone_parent = 'single'
+          assert_equal @calculator.benefit_cap, 257
         end
       end
 
       context '#amount' do
         should 'default to 0' do
-          assert @calculator.amount(:anything), 0
+          assert_equal @calculator.amount(:anything), 0
         end
 
         should 'return the claimed amount' do
           @calculator.claim(:maternity, 200)
 
-          assert @calculator.amount(:maternity), 200
+          assert_equal @calculator.amount(:maternity), 200
         end
       end
 
       context '#total_benefits' do
         should 'default to 0' do
-          assert @calculator.total_benefits, 0
+          assert_equal @calculator.total_benefits, 0
         end
 
         should 'sum claimed benefits' do
           @calculator.claim(:maternity, 200)
-          @calculator.claim(:paternity, 200)
+          @calculator.claim(:paternity, 100)
 
-          assert @calculator.total_benefits, 400
+          assert_equal @calculator.total_benefits, 300
         end
       end
 
       context '#total_over_cap' do
         should 'return total_benefits minus benefit_cap' do
-          assert @calculator.total_benefits, 0
-          assert @calculator.benefit_cap, 500
-          assert @calculator.total_over_cap, -500
+          assert_equal @calculator.total_benefits, 0
+          assert_equal @calculator.benefit_cap, 384
+          assert_equal @calculator.total_over_cap, -384
 
           @calculator.claim(:maternity, 200)
-          @calculator.claim(:paternity, 200)
+          @calculator.claim(:paternity, 100)
 
-          assert @calculator.total_benefits, 400
-          assert @calculator.benefit_cap, 500
-          assert @calculator.total_over_cap, -100
+          assert_equal @calculator.total_benefits, 300
+          assert_equal @calculator.benefit_cap, 384
+          assert_equal @calculator.total_over_cap, -84
 
           @calculator.single_couple_lone_parent = 'single'
 
-          assert @calculator.total_benefits, 400
-          assert @calculator.benefit_cap, 350
-          assert @calculator.total_over_cap, 50
+          assert_equal @calculator.total_benefits, 300
+          assert_equal @calculator.benefit_cap, 257
+          assert_equal @calculator.total_over_cap, 43
         end
       end
     end
