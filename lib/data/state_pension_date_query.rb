d2ab7290b5a839288c0d0850cd8b8e41ad6f8f5e
Erik Eide <erik.eide@gmail.com>
2016-01-21 12:23:05 +0000

    Extract StatePensionDate into its own file
    And add test coverage.

--- lib/data/state_pension_date_query.rb
+++ lib/data/state_pension_date_query.rb
@@ -1,17 +1,3 @@
-StatePensionDate = Struct.new(:gender, :start_date, :end_date, :pension_date) do
-  def match?(dob, sex)
-    same_gender?(sex) && born_in_range?(dob)
-  end
-
-  def same_gender?(sex)
-    gender == sex || :both == gender
-  end
-
-  def born_in_range?(dob)
-    dob >= start_date && dob <= end_date
-  end
-end
-
 StatePensionDateQuery = Struct.new(:dob, :gender) do
   def self.find(dob, gender)
     state_pension_query = new(dob, gender)
