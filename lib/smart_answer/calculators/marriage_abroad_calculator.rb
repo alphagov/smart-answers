2aa7fb492634e2b16300df8ad2e7dd1bb86d8fb0
Chris Roos <chris@seagul.co.uk>
2016-02-08 12:59:56 +1300

    Add MarriageAbroadCalculator#partner_british?
    Remove some duplication by encapsulating the check for the answer to
    `:what_is_your_partners_nationality?` being equal to ‘partner_british’.
    
    I haven’t updated `MarriageAbroadDataQuery#ss_alt_fees_table_country?`
    because it doesn’t yet have access to the MarriageAbroadCalculator.

--- lib/smart_answer/calculators/marriage_abroad_calculator.rb
+++ lib/smart_answer/calculators/marriage_abroad_calculator.rb
@@ -1,5 +1,9 @@
 module SmartAnswer::Calculators
   class MarriageAbroadCalculator
     attr_accessor :partner_nationality
+
+    def partner_british?
+      partner_nationality == 'partner_british'
+    end
   end
 end
--- test/unit/calculators/marriage_abroad_calculator_test.rb
+++ test/unit/calculators/marriage_abroad_calculator_test.rb
@@ -3,6 +3,21 @@ require_relative "../../test_helper"
 module SmartAnswer
   module Calculators
     class MarriageAbroadCalculatorTest < ActiveSupport::TestCase
+      context '#partner_british?' do
+        setup do
+          @calculator = MarriageAbroadCalculator.new
+        end
+
+        should 'be true if partner_nationality == "partner_british"' do
+          @calculator.partner_nationality = 'partner_british'
+          assert @calculator.partner_british?
+        end
+
+        should 'be false if partner_nationality != "partner_british"' do
+          @calculator.partner_nationality = 'not-partner_british'
+          refute @calculator.partner_british?
+        end
+      end
     end
   end
 end
