bc5ad8f87cd17f814e571a7402d2e6a446881102
Chris Roos <chris@seagul.co.uk>
2016-02-08 13:32:15 +1300

    Make MarriageAbroadCalculator#partner_nationality write-only
    To make it clear that we should be using the partner-nationality related
    methods on `MarriageAbroadCalculator`.
    
    Note that I've used `instance_variable_get` in the test to ensure it
    keeps passing. I'm not particularly keen on this but wasn't sure what
    else to do.

--- lib/smart_answer/calculators/marriage_abroad_calculator.rb
+++ lib/smart_answer/calculators/marriage_abroad_calculator.rb
@@ -1,9 +1,9 @@
 module SmartAnswer::Calculators
   class MarriageAbroadCalculator
-    attr_accessor :partner_nationality
+    attr_writer :partner_nationality
 
     def partner_british?
-      partner_nationality == 'partner_british'
+      @partner_nationality == 'partner_british'
     end
 
     def partner_not_british?
@@ -11,7 +11,7 @@ module SmartAnswer::Calculators
     end
 
     def partner_is_national_of_ceremony_country?
-      partner_nationality == 'partner_local'
+      @partner_nationality == 'partner_local'
     end
 
     def partner_is_not_national_of_ceremony_country?
@@ -19,7 +19,7 @@ module SmartAnswer::Calculators
     end
 
     def partner_is_neither_british_nor_a_national_of_ceremony_country?
-      partner_nationality == 'partner_other'
+      @partner_nationality == 'partner_other'
     end
   end
 end
