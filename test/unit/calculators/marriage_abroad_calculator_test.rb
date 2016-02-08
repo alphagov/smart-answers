54f3eae25424a25683f2eddf7ff0930752b4ba85
Chris Roos <chris@seagul.co.uk>
2016-02-08 13:21:16 +1300

    Add MarriageAbroadCalculator#partner_is_not_national_of_ceremony_country?
    Remove some duplication by encapsulating the check for the answer to
    `:what_is_your_partners_nationality?` _not_ being equal to
    ‘partner_local’.
    
    I haven’t updated `MarriageAbroadDataQuery#ss_alt_fees_table_country?`
    because it doesn’t yet have access to an instance of
    `MarriageAbroadCalculator`.

--- test/unit/calculators/marriage_abroad_calculator_test.rb
+++ test/unit/calculators/marriage_abroad_calculator_test.rb
@@ -50,6 +50,22 @@ module SmartAnswer
           refute @calculator.partner_is_national_of_ceremony_country?
         end
       end
+
+      context '#partner_is_not_national_of_ceremony_country?' do
+        setup do
+          @calculator = MarriageAbroadCalculator.new
+        end
+
+        should 'be true if partner is not a national of the ceremony country' do
+          @calculator.partner_nationality = 'not-partner_local'
+          assert @calculator.partner_is_not_national_of_ceremony_country?
+        end
+
+        should 'be false if partner is a national of the ceremony country' do
+          @calculator.partner_nationality = 'partner_local'
+          refute @calculator.partner_is_not_national_of_ceremony_country?
+        end
+      end
     end
   end
 end
