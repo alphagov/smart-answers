eaf105f6adcd546c715d3b175f1a458b58c343fe
Chris Roos <chris@seagul.co.uk>
2016-02-08 13:25:35 +1300

    Add MarriageAbroadCalculator#partner_is_not_british_nor_a_national_of_ceremony_country?
    Remove some duplication by encapsulating the check for the answer to
    `:what_is_your_partners_nationality?` being equal to ‘partner_other’.

--- test/unit/calculators/marriage_abroad_calculator_test.rb
+++ test/unit/calculators/marriage_abroad_calculator_test.rb
@@ -66,6 +66,22 @@ module SmartAnswer
           refute @calculator.partner_is_not_national_of_ceremony_country?
         end
       end
+
+      context '#partner_is_not_british_nor_a_national_of_ceremony_country?' do
+        setup do
+          @calculator = MarriageAbroadCalculator.new
+        end
+
+        should 'be true if partner_nationality == "partner_other"' do
+          @calculator.partner_nationality = 'partner_other'
+          assert @calculator.partner_is_not_british_nor_a_national_of_ceremony_country?
+        end
+
+        should 'be false unless partner_nationality == "partner_other"' do
+          @calculator.partner_nationality = 'not-partner_other'
+          refute @calculator.partner_is_not_british_nor_a_national_of_ceremony_country?
+        end
+      end
     end
   end
 end
