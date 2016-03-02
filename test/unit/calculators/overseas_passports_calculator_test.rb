7d7e4fe14845b4a956d7dad8f293fbb4c5b37c7b
Erik Eide <erik.eide@gmail.com>
2016-03-02 20:21:31 +0000

    Remove data from outcome in overseas passports
    Remove the data declared in partial `make_your_application` for
    `uk_visa_application_with_colour_pictures` and replace with a method in
    `OverseasPassportCalculator` class that is now passed to the partial.

--- test/unit/calculators/overseas_passports_calculator_test.rb
+++ test/unit/calculators/overseas_passports_calculator_test.rb
@@ -49,6 +49,25 @@ module SmartAnswer
           refute @calculator.uk_visa_application_centre?
         end
       end
+
+
+      context '#uk_visa_application_with_colour_pictures?' do
+        setup do
+          @calculator = OverseasPassportsCalculator.new
+        end
+
+        should 'be true if current_location is in UK_VISA_APPLICATION_WITH_COLOUR_PICTURES_COUNTRIES' do
+          OverseasPassportsCalculator::UK_VISA_APPLICATION_WITH_COLOUR_PICTURES_COUNTRIES.each do |country|
+            @calculator.current_location = country
+            assert @calculator.uk_visa_application_with_colour_pictures?
+          end
+        end
+
+        should 'be false if current_location is not in UK_VISA_APPLICATION_WITH_COLOUR_PICTURES_COUNTRIES' do
+          @calculator.current_location = 'antarctica'
+          refute @calculator.uk_visa_application_with_colour_pictures?
+        end
+      end
     end
   end
 end
