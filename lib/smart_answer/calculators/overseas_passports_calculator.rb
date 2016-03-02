cec8068a82f3fbdd5cf696576e27755bf504012f
Erik Eide <erik.eide@gmail.com>
2016-03-02 20:11:33 +0000

    Remove data in outcome for overseas passports
    Remove the data declared in partial `make_your_application` for
    `uk_visa_application_centre_countries` and replace with a method in
    `OverseasPassportCalculator` class that is now passed to the partial.

--- lib/smart_answer/calculators/overseas_passports_calculator.rb
+++ lib/smart_answer/calculators/overseas_passports_calculator.rb
@@ -4,10 +4,16 @@ module SmartAnswer::Calculators
       kyrgyzstan tajikistan turkmenistan uzbekistan
     )
 
+    UK_VISA_APPLICATION_CENTRE_COUNTRIES = %w(afghanistan algeria azerbaijan bangladesh belarus burundi burma cambodia china gaza georgia india indonesia kazakhstan kyrgyzstan laos lebanon mauritania morocco nepal pakistan russia tajikistan thailand turkmenistan ukraine uzbekistan western-sahara vietnam venezuela)
+
     attr_accessor :current_location
 
     def book_appointment_online?
       BOOK_APPOINTMENT_ONLINE_COUNTRIES.include?(current_location)
+    end
+
+    def uk_visa_application_centre?
+      UK_VISA_APPLICATION_CENTRE_COUNTRIES.include?(current_location)
     end
   end
 end
