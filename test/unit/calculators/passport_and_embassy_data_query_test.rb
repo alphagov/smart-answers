d7c7137f4506e44945e4a4fe9ade55a663a2e57b
Erik Eide <erik.eide@gmail.com>
2016-03-04 16:23:14 +0000

    Remove calculate blocks from flow definition
    Passport costs no longer appear to be used with overseas passports
    so removing from the flow, data query class and unit test.

--- test/unit/calculators/passport_and_embassy_data_query_test.rb
+++ test/unit/calculators/passport_and_embassy_data_query_test.rb
@@ -20,17 +20,6 @@ module SmartAnswer::Calculators
           assert_equal '14 weeks', @query.find_passport_data('afghanistan')['replacing']
         end
       end
-
-      context "passport_costs" do
-        should "format passport costs" do
-          %w(south_african_rand_adult_32 south_african_rand_adult_48 south_african_rand_child
-            euros_adult_32 euros_adult_48 euros_child).each do |k|
-            assert @query.passport_costs.has_key?(k), "passport_costs should have key #{k}"
-          end
-          assert_match /^\d,\d\d\d South African Rand/, @query.passport_costs["south_african_rand_adult_48"]
-        end
-      end
-
     end
   end
 end
