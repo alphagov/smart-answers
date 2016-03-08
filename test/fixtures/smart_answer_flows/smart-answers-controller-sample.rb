fb0d3e19bcd64a644f31de119b8e9b7c98add83e
James Mead <james@floehopper.org>
2016-03-08 16:09:56 +0100

    Use `permitted: :auto` in smart-answers-controller-sample fixture

--- test/fixtures/smart_answer_flows/smart-answers-controller-sample.rb
+++ test/fixtures/smart_answer_flows/smart-answers-controller-sample.rb
@@ -8,16 +8,12 @@ module SmartAnswer
         option :yes
         option :no
 
-        permitted_next_nodes = [
-          :you_have_a_sweet_tooth,
-          :do_you_like_jam?
-        ]
-        next_node(permitted: permitted_next_nodes) do |response|
+        next_node(permitted: :auto) do |response|
           case response
           when 'yes'
-            :you_have_a_sweet_tooth
+            outcome :you_have_a_sweet_tooth
           when 'no'
-            :do_you_like_jam?
+            question :do_you_like_jam?
           end
         end
       end
@@ -26,16 +22,12 @@ module SmartAnswer
         option :yes
         option :no
 
-        permitted_next_nodes = [
-          :you_have_a_sweet_tooth,
-          :you_have_a_savoury_tooth
-        ]
-        next_node(permitted: permitted_next_nodes) do |response|
+        next_node(permitted: :auto) do |response|
           case response
           when 'yes'
-            :you_have_a_sweet_tooth
+            outcome :you_have_a_sweet_tooth
           when 'no'
-            :you_have_a_savoury_tooth
+            outcome :you_have_a_savoury_tooth
           end
         end
       end
