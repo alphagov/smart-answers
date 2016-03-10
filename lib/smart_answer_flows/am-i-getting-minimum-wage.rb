72dc33f638b0c88bd58b673df341d5d803ab4566
James Mead <james@floehopper.org>
2016-03-10 16:18:56 +0100

    Automatic next_node detection in am-i-getting-minimum-wage

--- lib/smart_answer_flows/am-i-getting-minimum-wage.rb
+++ lib/smart_answer_flows/am-i-getting-minimum-wage.rb
@@ -20,20 +20,14 @@ module SmartAnswer
           nil
         end
 
-        permitted_next_nodes = [
-          :are_you_an_apprentice?,
-          :will_you_be_a_first_year_apprentice?,
-          :past_payment_date?
-        ]
-
-        next_node(permitted: permitted_next_nodes) do |response|
+        next_node(permitted: :auto) do |response|
           case response
           when 'current_payment'
-            :are_you_an_apprentice?
+            question :are_you_an_apprentice?
           when 'current_payment_april_2016'
-            :will_you_be_a_first_year_apprentice?
+            question :will_you_be_a_first_year_apprentice?
           when 'past_payment'
-            :past_payment_date?
+            question :past_payment_date?
           end
         end
       end
@@ -43,18 +37,13 @@ module SmartAnswer
         option :yes
         option :no
 
-        permitted_next_nodes = [
-          :does_not_apply_to_first_year_apprentices,
-          :how_old_are_you?
-        ]
-
-        next_node(permitted: permitted_next_nodes) do |response|
+        next_node(permitted: :auto) do |response|
           case response
           when 'yes'
             calculator.is_apprentice = true
-            :does_not_apply_to_first_year_apprentices
+            outcome :does_not_apply_to_first_year_apprentices
           when 'no'
-            :how_old_are_you? #Q3
+            question :how_old_are_you? #Q3
           end
         end
       end
@@ -81,17 +70,12 @@ module SmartAnswer
           end
         end
 
-        permitted_next_nodes = [
-          :under_school_leaving_age,
-          :how_often_do_you_get_paid?
-        ]
-
-        next_node(permitted: permitted_next_nodes) do |response|
+        next_node(permitted: :auto) do |response|
           calculator.age = response
           if calculator.under_school_leaving_age?
-            :under_school_leaving_age
+            outcome :under_school_leaving_age
           else
-            :how_often_do_you_get_paid?
+            question :how_often_do_you_get_paid?
           end
         end
       end
