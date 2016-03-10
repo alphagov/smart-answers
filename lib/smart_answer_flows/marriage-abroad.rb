4b63ed03df55e36bc40854165ffc12af37cc03a9
Ikenna Okpala <ikenna.okpala@digital.cabinet-office.gov.uk>
2016-03-10 10:57:29 +0000

    Apply outcome for Dominican Republic
    This commit contains changes to the marriage abroad in Dominican Republic,
    it adds the outcome_dominican_republic and redirects the flow for all marriage
    outcomes where the country of ceremony is the Dominican Republic

--- lib/smart_answer_flows/marriage-abroad.rb
+++ lib/smart_answer_flows/marriage-abroad.rb
@@ -102,7 +102,7 @@ module SmartAnswer
           elsif calculator.ceremony_country == "spain"
             outcome :outcome_spain
           elsif calculator.ceremony_country == 'dominican-republic'
-            outcome :outcome_ss_marriage
+            outcome :outcome_dominican_republic
           elsif calculator.partner_is_opposite_sex?
             if calculator.ceremony_country == 'hong-kong'
               outcome :outcome_os_hong_kong
@@ -287,6 +287,8 @@ module SmartAnswer
       outcome :outcome_ss_affirmation
 
       outcome :outcome_os_marriage_impossible_no_laos_locals
+
+      outcome :outcome_dominican_republic
     end
   end
 end
