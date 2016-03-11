146af04d49098eea199afaa726f8f6091d9c8a0e
Erik Eide <erik.eide@gmail.com>
2016-03-11 20:11:54 +0000

    Add book_appointments_online? to calculator
    Move list of countries from outcome partial to calculator, and add 
    method for outcomes to determine if the current_location allows it.

--- test/unit/calculators/overseas_passports_calculator_test.rb
+++ test/unit/calculators/overseas_passports_calculator_test.rb
@@ -13,6 +13,24 @@ module SmartAnswer
           assert_equal @calculator.current_location, 'springfield'
         end
       end
+
+      context '#book_appointment_online?' do
+        setup do
+          @calculator = OverseasPassportsCalculator.new
+        end
+
+        should 'be true if current_location is in BOOK_APPOINTMENT_ONLINE_COUNTRIES countries' do
+          OverseasPassportsCalculator::BOOK_APPOINTMENT_ONLINE_COUNTRIES.each do |country|
+            @calculator.current_location = country
+            assert @calculator.book_appointment_online?
+          end
+        end
+
+        should 'be false if current_location is not in BOOK_APPOINTMENT_ONLINE_COUNTRIES' do
+          @calculator.current_location = 'antarctica'
+          refute @calculator.book_appointment_online?
+        end
+      end
     end
   end
 end
