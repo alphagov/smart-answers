e42f9d28eaca0fc902069693817c53ad26584fc6
James Mead <james@floehopper.org>
2016-03-14 18:08:04 +0100

    Split CalculateRedundancyPayTest into two tests
    This integration test was testing two flows, albeit ones with a significant
    amount of shared logic. It seems less surprising to have one test per flow
    as we do elsewhere.
    
    There are a bunch of indentation changes in this commit, so it's better to
    view it using the following command:
    
        $ git show -p --ignore-space-change <sha>
    
    I plan to fix the filename of the original test in a separate commit to keep
    the diff as simple as possible.

--- test/integration/smart_answer_flows/calculate_your_redundancy_pay_test.rb
+++ test/integration/smart_answer_flows/calculate_your_redundancy_pay_test.rb
@@ -1,16 +1,16 @@
 require_relative '../../test_helper'
 require_relative 'flow_test_helper'
 
-require "smart_answer_flows/calculate-employee-redundancy-pay"
+require "smart_answer_flows/calculate-your-redundancy-pay"
 
-class CalculateEmployeeRedundancyPayTest < ActiveSupport::TestCase
+class CalculateYourRedundancyPayTest < ActiveSupport::TestCase
   include FlowTestHelper
 
   setup do
-    setup_for_testing_flow SmartAnswer::CalculateEmployeeRedundancyPayFlow
+    setup_for_testing_flow SmartAnswer::CalculateYourRedundancyPayFlow
   end
 
-  should "ask when the employee was made redundant" do
+  should "ask when you were made redundant" do
     assert_current_node :date_of_redundancy?
   end
 
@@ -20,15 +20,13 @@ class CalculateEmployeeRedundancyPayTest < ActiveSupport::TestCase
       add_response '2013-01-31'
     end
 
-    should "ask the age of the employee" do
+    should "be in employee flow for age" do
       assert_current_node :age_of_employee?
       assert_state_variable :rate, 430
       assert_state_variable :ni_rate, 430
-      assert_state_variable :max_amount, "12,900"
-      assert_state_variable :ni_max_amount, "12,900"
     end
 
-    context "aged 42" do
+    context "42 years old" do
       setup do
         add_response "42"
       end
@@ -37,9 +35,9 @@ class CalculateEmployeeRedundancyPayTest < ActiveSupport::TestCase
         assert_current_node :years_employed?
       end
 
-      context "1 year of employment" do
+      context "under 2 years" do
         setup do
-          add_response "1"
+          add_response "1.8"
         end
 
         should "bypass the salary question" do
@@ -47,9 +45,9 @@ class CalculateEmployeeRedundancyPayTest < ActiveSupport::TestCase
         end
       end
 
-      context "4 years of employment" do
+      context "over 2 years" do
         setup do
-          add_response "4"
+          add_response "4.5"
         end
 
         should "ask for salary" do
@@ -68,6 +66,10 @@ class CalculateEmployeeRedundancyPayTest < ActiveSupport::TestCase
           should "give me a figure no higher than 430 per week" do
             assert_state_variable :statutory_redundancy_pay, "1,935"
             assert_state_variable :statutory_redundancy_pay_ni, "1,935"
+          end
+
+          should "give me 2 weeks total entitlement" do
+            assert_state_variable :number_of_weeks_entitlement, 4.5
           end
         end
       end
@@ -114,11 +116,6 @@ class CalculateEmployeeRedundancyPayTest < ActiveSupport::TestCase
             assert_state_variable :statutory_redundancy_pay, "860"
             assert_state_variable :statutory_redundancy_pay_ni, "860"
           end
-
-          should "give me the number of weeks entitlement" do
-            assert_state_variable :number_of_weeks_entitlement, 2.0
-          end
-
         end
 
         context "weekly salary of under 430 before tax" do
@@ -130,17 +127,37 @@ class CalculateEmployeeRedundancyPayTest < ActiveSupport::TestCase
             assert_state_variable :statutory_redundancy_pay, "600"
             assert_state_variable :statutory_redundancy_pay_ni, "600"
           end
-
-          should "give me 2 weeks total entitlement" do
-            assert_state_variable :number_of_weeks_entitlement, 2.0
-          end
         end
       end
     end
 
-    context "under 22 years of age" do
+    context "catches years_employed greater than age_of_employee" do
+      context "be 18 years old and worked 20" do
+        setup do
+          add_response 18
+        end
+        should "fail on 4 years" do
+          add_response 4
+          assert_current_node_is_error
+        end
+        should "fail on 20 years" do
+          add_response 20
+          assert_current_node_is_error
+        end
+        should "succeed on 3" do
+          add_response 3
+          assert_current_node :weekly_pay_before_tax?
+        end
+        should "succeed on 2" do
+          add_response 2
+          assert_current_node :weekly_pay_before_tax?
+        end
+      end
+    end
+
+    context "21 years of age" do
       setup do
-        add_response "19"
+        add_response "21"
       end
 
       should "ask how long the employee has been employed" do
@@ -159,7 +176,7 @@ class CalculateEmployeeRedundancyPayTest < ActiveSupport::TestCase
 
       context "over 2 years" do
         setup do
-          add_response "4"
+          add_response "6"
         end
 
         should "ask for salary" do
@@ -176,11 +193,8 @@ class CalculateEmployeeRedundancyPayTest < ActiveSupport::TestCase
           end
 
           should "give me a figure no higher than 430 per week" do
-            assert_state_variable :statutory_redundancy_pay, "860"
-            assert_state_variable :statutory_redundancy_pay_ni, "860"
-          end
-          should "give me 2 weeks total entitlement" do
-            assert_state_variable :number_of_weeks_entitlement, 2.0
+            assert_state_variable :statutory_redundancy_pay, "1,290"
+            assert_state_variable :statutory_redundancy_pay_ni, "1,290"
           end
         end
 
@@ -190,49 +204,47 @@ class CalculateEmployeeRedundancyPayTest < ActiveSupport::TestCase
           end
 
           should "give me a figure below 430" do
-            assert_state_variable :statutory_redundancy_pay, "600"
-            assert_state_variable :statutory_redundancy_pay_ni, "600"
-          end
-
-          should "give me 2 weeks total entitlement" do
-            assert_state_variable :number_of_weeks_entitlement, 2.0
+            assert_state_variable :statutory_redundancy_pay, "900"
+            assert_state_variable :statutory_redundancy_pay_ni, "900"
           end
         end
       end
     end
-  end # Before 1 Feb 2013
-  context "answer after 1 Feb 2013" do
+  end # Before Feb 2013
+  context "answer 1 Feb 2013, 42 y/o, worked for 4.5 years" do
     should "give the answer using the new rate" do
       add_response '2013-02-01'
-      add_response '19'
-      add_response '2'
-      add_response '500'
+      add_response '42'
+      add_response '4.5'
+      add_response '700'
       assert_current_node :done
       assert_state_variable :rate, 450
       assert_state_variable :ni_rate, 450
-      assert_state_variable :max_amount, "13,500"
-      assert_state_variable :ni_max_amount, "13,500"
     end
   end # After Feb 2013
 
-  context "answer tomorrow" do
-    setup do
-      add_response (Date.today + 1.day).to_s
-    end
-    should "ask employee age" do
-      assert_current_node :age_of_employee?
+  context "2015/2016" do
+    should "Use the correct rates" do
+      add_response '2015-05-01'
+      add_response '22'
+      add_response '7'
+      add_response '700'
+      assert_current_node :done
+      assert_state_variable :rate, 475
+      assert_state_variable :ni_rate, 490
+      assert_state_variable :max_amount, "14,250"
+      assert_state_variable :ni_max_amount, "14,700"
+      assert_state_variable :statutory_redundancy_pay, "1,662.50"
+      assert_state_variable :statutory_redundancy_pay_ni, "1,715"
     end
   end
 
-  context "dates out of range" do
-    should "not allow dates before 2012" do
-      add_response Date.parse("2011-12-31")
-      assert_current_node_is_error
+  context "answer 05 April 2014" do
+    setup do
+      add_response Date.parse("2014-04-05")
     end
-
-    should "not allow dates next year" do
-      add_response (Date.today.end_of_year + 1.day).to_s
-      assert_current_node_is_error
+    should "ask employee age" do
+      assert_current_node :age_of_employee?
     end
   end
 end
