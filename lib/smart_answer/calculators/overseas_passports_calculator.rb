47daa7d81c5f50a0d2bdc79724d9447c91fbdd7c
Erik Eide <erik.eide@gmail.com>
2016-03-15 08:48:11 +0000

    Re-format long array over multiple lines

--- lib/smart_answer/calculators/overseas_passports_calculator.rb
+++ lib/smart_answer/calculators/overseas_passports_calculator.rb
@@ -4,7 +4,12 @@ module SmartAnswer::Calculators
       kyrgyzstan tajikistan turkmenistan uzbekistan
     )
 
-    UK_VISA_APPLICATION_CENTRE_COUNTRIES = %w(afghanistan algeria azerbaijan bangladesh belarus burundi burma cambodia china gaza georgia india indonesia kazakhstan kyrgyzstan laos lebanon mauritania morocco nepal pakistan russia tajikistan thailand turkmenistan ukraine uzbekistan western-sahara vietnam venezuela)
+    UK_VISA_APPLICATION_CENTRE_COUNTRIES = %w(
+      afghanistan algeria azerbaijan bangladesh belarus burundi burma cambodia
+      china gaza georgia india indonesia kazakhstan kyrgyzstan laos lebanon
+      mauritania morocco nepal pakistan russia tajikistan thailand turkmenistan
+      ukraine uzbekistan western-sahara vietnam venezuela
+    )
 
     attr_accessor :current_location
 
