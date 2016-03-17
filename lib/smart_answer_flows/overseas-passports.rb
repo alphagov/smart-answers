b0ca41d3a3aecb96c0bb9ba166188447adfce64e
erikse <erikse@users.noreply.github.com>
2016-03-17 15:52:48 +0000

    Revert "Refactor overseas passports flow updates"

--- lib/smart_answer_flows/overseas-passports.rb
+++ lib/smart_answer_flows/overseas-passports.rb
@@ -6,28 +6,47 @@ module SmartAnswer
       status :published
       satisfies_need "100131"
 
+      data_query = Calculators::PassportAndEmbassyDataQuery.new
+
+      exclude_countries = %w(holy-see british-antarctic-territory)
+
       # Q1
-      country_select :which_country_are_you_in?, exclude_countries: Calculators::OverseasPassportsCalculator::EXCLUDE_COUNTRIES do
-        next_node_calculation :calculator do
-          Calculators::OverseasPassportsCalculator.new
+      country_select :which_country_are_you_in?, exclude_countries: exclude_countries do
+        save_input_as :current_location
+
+        calculate :location do
+          loc = WorldLocation.find(current_location)
+          if Calculators::PassportAndEmbassyDataQuery::ALT_EMBASSIES.has_key?(current_location)
+            loc = WorldLocation.find(Calculators::PassportAndEmbassyDataQuery::ALT_EMBASSIES[current_location])
+          end
+          raise InvalidResponse unless loc
+          loc
         end
 
-        validate do |response|
-          calculator.world_location(response)
+        calculate :birth_location do
+          nil
+        end
+        calculate :embassy_address do
+          nil
+        end
+        calculate :send_colour_photocopy_bulletpoint do
+          nil
         end
 
-        calculate :overseas_passports_embassies do |response|
-          calculator.overseas_passports_embassies(response)
+        next_node_calculation :ineligible_country do |response|
+          %w{iran libya syria yemen}.include?(response)
+        end
+
+        next_node_calculation :apply_in_neighbouring_countries do |response|
+          %w(british-indian-ocean-territory north-korea south-georgia-and-south-sandwich-islands).include?(response)
         end
 
         next_node(permitted: :auto) do |response|
-          calculator.current_location = response
-
-          if calculator.ineligible_country?
+          if ineligible_country
             outcome :cannot_apply
           elsif response == 'the-occupied-palestinian-territories'
             question :which_opt?
-          elsif calculator.apply_in_neighbouring_countries?
+          elsif apply_in_neighbouring_countries
             outcome :apply_in_neighbouring_country
           else
             question :renewing_replacing_applying?
@@ -40,14 +59,8 @@ module SmartAnswer
         option :gaza
         option :"jerusalem-or-westbank"
 
-        permitted_next_nodes = [
-          :renewing_replacing_applying?
-        ]
-        next_node(permitted: permitted_next_nodes) do |response|
-          calculator.current_location = response
-
-          :renewing_replacing_applying?
-        end
+        save_input_as :current_location
+        next_node :renewing_replacing_applying?
       end
 
       # Q2
@@ -57,14 +70,72 @@ module SmartAnswer
         option :applying
         option :replacing
 
-        permitted_next_nodes = [
-          :child_or_adult_passport?
-        ]
-        next_node(permitted: permitted_next_nodes) do |response|
-          calculator.application_action = response
+        save_input_as :application_action
 
-          :child_or_adult_passport?
+        precalculate :organisation do
+          location.fco_organisation
         end
+
+        calculate :overseas_passports_embassies do
+          if organisation
+            organisation.offices_with_service 'Overseas Passports Service'
+          else
+            []
+          end
+        end
+
+        calculate :general_action do |response|
+          response =~ /^renewing_/ ? 'renewing' : response
+        end
+
+        calculate :passport_data do
+          data_query.find_passport_data(current_location)
+        end
+        calculate :application_type do
+          passport_data['type']
+        end
+        calculate :is_ips_application do
+          %w{ips_application_1 ips_application_2 ips_application_3}.include?(application_type)
+        end
+        calculate :ips_number do
+          application_type.split("_")[2] if is_ips_application
+        end
+
+        calculate :application_form do
+          passport_data['app_form']
+        end
+
+        calculate :supporting_documents do
+          passport_data['group']
+        end
+
+        calculate :application_address do
+          passport_data['address']
+        end
+
+        calculate :ips_docs_number do
+          supporting_documents.split("_")[3] if is_ips_application
+        end
+
+        calculate :ips_result_type do
+          passport_data['online_application'] ? :ips_application_result_online : :ips_application_result
+        end
+
+        data_query.passport_costs.each do |k, v|
+          calculate "costs_#{k}".to_sym do
+            v
+          end
+        end
+
+        calculate :waiting_time do
+          passport_data[application_action]
+        end
+
+        calculate :optimistic_processing_time do
+          passport_data['optimistic_processing_time?']
+        end
+
+        next_node :child_or_adult_passport?
       end
 
       # Q3
@@ -72,13 +143,13 @@ module SmartAnswer
         option :adult
         option :child
 
-        next_node(permitted: :auto) do
-          calculator.child_or_adult = response
+        save_input_as :child_or_adult
 
-          if calculator.ips_application?
-            if calculator.applying? || calculator.renewing_old?
+        next_node(permitted: :auto) do
+          if is_ips_application
+            if %w(applying renewing_old).include?(application_action)
               question :country_of_birth?
-            elsif calculator.ips_online_application?
+            elsif ips_result_type == :ips_application_result_online
               outcome :ips_application_result_online
             else
               outcome :ips_application_result
@@ -88,12 +159,24 @@ module SmartAnswer
       end
 
       # Q4
-      country_select :country_of_birth?, include_uk: true, exclude_countries: Calculators::OverseasPassportsCalculator::EXCLUDE_COUNTRIES do
-        next_node(permitted: :auto) do
-          calculator.birth_location = response
+      country_select :country_of_birth?, include_uk: true, exclude_countries: exclude_countries do
+        save_input_as :birth_location
 
-          if calculator.ips_application?
-            if calculator.ips_online_application?
+        calculate :application_group do |response|
+          data_query.find_passport_data(response)['group']
+        end
+
+        calculate :supporting_documents do |response|
+          response == 'united-kingdom' ? supporting_documents : application_group
+        end
+
+        calculate :ips_docs_number do
+          supporting_documents.split("_")[3]
+        end
+
+        next_node(permitted: :auto) do
+          if is_ips_application
+            if ips_result_type == :ips_application_result_online
               outcome :ips_application_result_online
             else
               outcome :ips_application_result
@@ -103,15 +186,31 @@ module SmartAnswer
       end
 
       ## Online IPS Application Result
-      outcome :ips_application_result_online
+      outcome :ips_application_result_online do
+        precalculate :birth_location do
+          birth_location
+        end
+      end
 
       ## IPS Application Result
-      outcome :ips_application_result
+      outcome :ips_application_result do
+        precalculate :data_query do
+          data_query
+        end
+
+        precalculate :birth_location do
+          birth_location
+        end
+      end
 
       ## No-op outcome.
       outcome :cannot_apply
 
-      outcome :apply_in_neighbouring_country
+      outcome :apply_in_neighbouring_country do
+        precalculate :title_output do
+          location.name
+        end
+      end
     end
   end
 end
