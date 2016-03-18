286d671681eaac86209679ce6e1dc04fc7cee94b
James Mead <james@floehopper.org>
2016-03-18 16:42:07 +0000

    Always use next_node with block in smart-answers-controller-sample-with-money-question

--- test/fixtures/smart_answer_flows/smart-answers-controller-sample-with-money-question.rb
+++ test/fixtures/smart_answer_flows/smart-answers-controller-sample-with-money-question.rb
@@ -3,10 +3,14 @@ module SmartAnswer
     def define
       name "smart-answers-controller-sample-with-money-question"
       money_question :how_much? do
-        next_node :money_question_with_suffix_label?
+        next_node do
+          question :money_question_with_suffix_label?
+        end
       end
       money_question :money_question_with_suffix_label? do
-        next_node :done
+        next_node do
+          outcome :done
+        end
       end
       outcome :done
     end
