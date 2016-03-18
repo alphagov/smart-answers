b902402367354868efa30ded644a8226f75f2eed
Pablo Manrubia <pmanrubia@gmail.com>
2016-03-18 15:47:55 +0000

    Add Q1: What do you want to know?

--- lib/smart_answer_flows/national-minimum-wage.rb
+++ lib/smart_answer_flows/national-minimum-wage.rb
@@ -6,6 +6,16 @@ module SmartAnswer
 
       status :draft
       satisfies_need '100145'
+
+      multiple_choice :what_do_you_want_to_know? do
+        option 'current_payment'
+
+        next_node do
+          outcome :earning_more_than_living_wage
+        end
+      end
+
+      outcome :earning_more_than_living_wage
     end
   end
 end
