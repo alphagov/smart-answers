017c20e9da7a3c2c93ce3ac7eca5c351e18fd88b
James Mead <james@floehopper.org>
2016-03-19 12:48:54 +0000

    Remove unused state attribute from Flow
    As far as I can tell this is not used and I'm pretty sure it's *never* been
    used. The state for a request seems to be handled entirely locally within
    the `Flow#process` method.

--- lib/smart_answer/flow.rb
+++ lib/smart_answer/flow.rb
@@ -3,7 +3,7 @@ require 'ostruct'
 module SmartAnswer
   class Flow
     attr_reader :nodes, :outcomes
-    attr_accessor :state, :status, :need_id
+    attr_accessor :status, :need_id
 
     def self.build
       new.tap do |flow|
