dfce5a818be2cf7369350ca3ba07f4f5ceaba9fb
Chris Roos <chris@seagul.co.uk>
2016-03-20 21:35:52 +1100

    Rename outcome_os_marriage_impossible_no_laos_locals
    I used a combination of the
    marriage-abroad-responses-and-expected-results.yml file and outcome
    template to confirm that this outcome is used for for opposite sex
    marriages in Laos when the applicant isn't marrying a Lao national.

--- lib/smart_answer_flows/marriage-abroad.rb
+++ lib/smart_answer_flows/marriage-abroad.rb
@@ -138,7 +138,7 @@ module SmartAnswer
             elsif calculator.ceremony_country == "indonesia"
               outcome :outcome_opposite_sex_marriage_in_indonesia
             elsif calculator.ceremony_country == "laos" && calculator.partner_is_not_national_of_ceremony_country?
-              outcome :outcome_os_marriage_impossible_no_laos_locals
+              outcome :outcome_opposite_sex_marriage_in_laos_without_lao_national
             elsif calculator.ceremony_country == "laos"
               outcome :outcome_opposite_sex_marriage_in_laos_with_lao_national
             elsif calculator.ceremony_country == 'poland'
@@ -286,7 +286,7 @@ module SmartAnswer
 
       outcome :outcome_same_sex_civil_partnership_in_affirmation_countries
 
-      outcome :outcome_os_marriage_impossible_no_laos_locals
+      outcome :outcome_opposite_sex_marriage_in_laos_without_lao_national
 
       outcome :outcome_dominican_republic
     end
