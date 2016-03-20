4214fdac7455a0583067c3b9b56cfdbc386283f3
Chris Roos <chris@seagul.co.uk>
2016-03-20 12:22:36 +1100

    Rename outcome_os_indonesia
    I used a combination of the
    marriage-abroad-responses-and-expected-results.yml file and outcome
    template to confirm that this outcome is used for for all opposite sex
    marriages in Indonesia.

--- lib/smart_answer_flows/marriage-abroad.rb
+++ lib/smart_answer_flows/marriage-abroad.rb
@@ -136,7 +136,7 @@ module SmartAnswer
             elsif calculator.ceremony_country == "kosovo"
               outcome :outcome_os_kosovo
             elsif calculator.ceremony_country == "indonesia"
-              outcome :outcome_os_indonesia
+              outcome :outcome_opposite_sex_marriage_in_indonesia
             elsif calculator.ceremony_country == "laos" && calculator.partner_is_not_national_of_ceremony_country?
               outcome :outcome_os_marriage_impossible_no_laos_locals
             elsif calculator.ceremony_country == "laos"
@@ -219,7 +219,7 @@ module SmartAnswer
 
       outcome :outcome_opposite_sex_marriage_in_kuwait
 
-      outcome :outcome_os_indonesia
+      outcome :outcome_opposite_sex_marriage_in_indonesia
 
       outcome :outcome_os_laos
 
