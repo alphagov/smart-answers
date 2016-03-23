7da6525689d255efbd0889b7c43a77fdc91bafa1
Chris Roos <chris@seagul.co.uk>
2016-03-24 10:50:39 +1100

    Remove same_sex_marriage_and_civil_partnership_in_other_countries
    Now that it's no longer being used.

--- lib/smart_answer_flows/marriage-abroad.rb
+++ lib/smart_answer_flows/marriage-abroad.rb
@@ -246,7 +246,6 @@ module SmartAnswer
       outcome :outcome_same_sex_civil_partnership_in_no_cni_countries
       outcome :outcome_same_sex_marriage_and_civil_partnership
       outcome :outcome_same_sex_marriage_and_civil_partnership_in_malta
-      outcome :outcome_same_sex_marriage_and_civil_partnership_in_other_countries
       outcome :outcome_same_sex_marriage_and_civil_partnership_not_possible
     end
   end
