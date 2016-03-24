22b5e00f276e54915cffd31540e1645dacb4df4d
Chris Roos <chris@seagul.co.uk>
2016-03-24 11:28:01 +1100

    Add outcome_opposite_sex_marriage_in_north_korea
    The new outcome is a duplicate of
    outcome_opposite_sex_marriage_in_other_countries:
    
        $ cp lib/smart_answer_flows/marriage-abroad/outcomes/outcome_opposite_sex_marriage_in_{other_countries,north_korea}.govspeak.erb
    
    The outcome_opposite_sex_marriage_in_other_countries outcome used a case
    statement to display 5 different outcomes. It feels more natural that
    these should be split into separate outcomes.
    
    The next steps will be to remove the North Korea specific code from
    outcome_opposite_sex_marriage_in_other_countries and to remove all but
    the North Korea specific code from
    outcome_opposite_sex_marriage_in_north_korea.
    
    I've also updated the marriage_abroad_test and responses-and-expected-results
    regression test data as part of this commit.

--- test/integration/smart_answer_flows/marriage_abroad_test.rb
+++ test/integration/smart_answer_flows/marriage_abroad_test.rb
@@ -1159,8 +1159,8 @@ class MarriageAbroadTest < ActiveSupport::TestCase
       add_response 'partner_local'
       add_response 'opposite_sex'
     end
-    should "go to consular cni os outcome" do
-      assert_current_node :outcome_opposite_sex_marriage_in_other_countries
+    should "go to outcome_opposite_sex_marriage_in_north_korea" do
+      assert_current_node :outcome_opposite_sex_marriage_in_north_korea
     end
   end
 
