aec00d175d0ab83e1ea565e8e284870a8bc337f7
Ikenna Okpala <ikenna.okpala@digital.cabinet-office.gov.uk>
2016-04-15 16:09:46 +0100

    Content changes for UK or South Africa Resident
    These changes affect All opposite and same sex outcomes where user lives in the
    United Kingdom or in South Africa.
    
    These changes were requested by the content team.
    
    Affecting only these URLs/pages:
    
    UK citizen living in the UK
    
    /marriage-abroad/y/south-africa/uk/partner_british/opposite_sex
    /marriage-abroad/y/south-africa/uk/partner_british/same_sex
    /marriage-abroad/y/south-africa/uk/partner_local/opposite_sex
    /marriage-abroad/y/south-africa/uk/partner_local/same_sex
    /marriage-abroad/y/south-africa/uk/partner_other/opposite_sex
    /marriage-abroad/y/south-africa/uk/partner_other/same_sex
    
    UK citizen in ceremony  country
    
    /marriage-abroad/y/south-africa/ceremony_country/partner_british/opposite_sex
    /marriage-abroad/y/south-africa/ceremony_country/partner_british/same_sex
    /marriage-abroad/y/south-africa/ceremony_country/partner_local/opposite_sex
    /marriage-abroad/y/south-africa/ceremony_country/partner_local/same_sex
    /marriage-abroad/y/south-africa/ceremony_country/partner_other/opposite_sex
    /marriage-abroad/y/south-africa/ceremony_country/partner_other/same_sex
    
    UK citizen in third country
    
    /marriage-abroad/y/south-africa/third_country/partner_british/opposite_sex
    /marriage-abroad/y/south-africa/third_country/partner_british/same_sex
    /marriage-abroad/y/south-africa/third_country/partner_local/opposite_sex
    /marriage-abroad/y/south-africa/third_country/partner_local/same_sex
    /marriage-abroad/y/south-africa/third_country/partner_other/opposite_sex
    /marriage-abroad/y/south-africa/third_country/partner_other/same_sex
    
    Update marriage abroad test expectations.
    
    Add tests for marriage outcomes South Africa

--- test/integration/smart_answer_flows/marriage_abroad_test.rb
+++ test/integration/smart_answer_flows/marriage_abroad_test.rb
@@ -251,16 +251,154 @@ class MarriageAbroadTest < ActiveSupport::TestCase
     end
   end
 
-  context "uk resident ceremony in south-africa" do
+  context "ceremony in south africa," do
     setup do
       worldwide_api_has_organisations_for_location('south-africa', read_fixture_file('worldwide/south-africa_organisations.json'))
       add_response 'south-africa'
-      add_response 'uk'
-      add_response 'partner_local'
-      add_response 'opposite_sex'
     end
-    should "go to commonwealth os outcome" do
-      assert_current_node :outcome_opposite_sex_marriage_in_commonwealth_countries
+    context "resident in uk," do
+      setup { add_response 'uk' }
+      context "partner is british," do
+        setup { add_response 'partner_british' }
+        context "opposite sex" do
+          setup { add_response 'opposite_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+        context "same sex" do
+          setup { add_response 'same_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+      end
+      context "partner is local," do
+        setup { add_response 'partner_local' }
+        context "opposite sex" do
+          setup { add_response 'opposite_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+        context "same sex" do
+          setup { add_response 'same_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+      end
+      context "partner is other," do
+        setup { add_response 'partner_other' }
+        context "opposite sex" do
+          setup { add_response 'opposite_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+        context "same sex" do
+          setup { add_response 'same_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+      end
+    end
+    context "resident in ceremony country" do
+      setup { add_response 'ceremony_country' }
+      context "partner is british," do
+        setup { add_response 'partner_british' }
+        context "opposite sex" do
+          setup { add_response 'opposite_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+        context "same sex" do
+          setup { add_response 'same_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+      end
+      context "partner is local," do
+        setup { add_response 'partner_local' }
+        context "opposite sex" do
+          setup { add_response 'opposite_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+        context "same sex" do
+          setup { add_response 'same_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+      end
+      context "partner is other," do
+        setup { add_response 'partner_other' }
+        context "opposite sex" do
+          setup { add_response 'opposite_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+        context "same sex" do
+          setup { add_response 'same_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+      end
+    end
+    context "resident in elsewhere," do
+      setup { add_response 'third_country' }
+      context "partner is british," do
+        setup { add_response 'partner_british' }
+        context "opposite sex" do
+          setup { add_response 'opposite_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+        context "same sex" do
+          setup { add_response 'same_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+      end
+      context "partner is local," do
+        setup { add_response 'partner_local' }
+        context "opposite sex" do
+          setup { add_response 'opposite_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+        context "same sex" do
+          setup { add_response 'same_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+      end
+      context "partner is other," do
+        setup { add_response 'partner_other' }
+        context "opposite sex" do
+          setup { add_response 'opposite_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+        context "same sex" do
+          setup { add_response 'same_sex' }
+          should "go to south african or equivalent commonwealth marriage outcome" do
+            assert_current_node :outcome_opposite_same_sex_marriage_residing_in_uk_or_south_africa
+          end
+        end
+      end
     end
   end
 
