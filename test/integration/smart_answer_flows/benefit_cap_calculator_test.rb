a9249db57182d39a79d561f59929386a5186e9ad
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2016-04-18 18:20:59 +0100

    Add a new first question to benefit-cap-calculator
    TODO: Consider adding only single response to the first question for now.
    
    To ask the user whether they'd like to check whether their benefits will be
    capped based on the current rules, or the new rules which are going to come
    into effect in Autumn 2016.
    
    The question and template have been cherry-picked from
    60303ff972ef46c468dd4c57f0ee57a1ee0a2fb0. That commit contained a breaking
    change to the flow which hasn't been included here.
    
    This commit has no real effect at the moment, as we'll give the same advice
    irrespective of the answer the user has given.
    
    I've updated the benefit-cap-calculator integration test:
    
    * I've added a test for the new question
    
    * I've nested all the existing tests under a new "when using pre-Autumn 2016
      rates and rules" context. Use the `--ignore-all-space` option to better see
      this change.
    
    * I've duplicated all the existing tests under a new "when using post-Autumn
      2016 rates and rules". I think these are going to come in useful as the
      behaviour between the two paths starts to diverge.
    
    I've manually added options for the new question to
    benefit-cap-calculator-questions-and-responses.yml and then regenerated
    benefit-cap-calculator-responses-and-expected-results.yml.
    
    I've updated the regression test artefacts in this commit. The changes include:
    
    * Renaming existing artefacts from /benefit-cap-calculator/ to
      /benefit-cap-calculator/default.
    
    * Added new artefacts for /benefit-cap-calculator/future.
    
    * You can see that the _outcome_ artefacts are currently identical for /default
      and /future by running `diff -r
      test/artefacts/benefit-cap-calculator/{default,future}`.
    
    * You can see that the question page artefacts are all in the /default
      namespace by running `diff -r
      test/artefacts/benefit-cap-calculator/{default,future}`.
    
    Given that I'm happy that the regression test artefact changes are all valid,
    I've also updated the checksum data as part of this commit.

--- test/integration/smart_answer_flows/benefit_cap_calculator_test.rb
+++ test/integration/smart_answer_flows/benefit_cap_calculator_test.rb
@@ -11,6 +11,192 @@ class BenefitCapCalculatorTest < ActiveSupport::TestCase
   end
 
   context "Benefit cap calculator" do
+    should "ask whether you want to check your situation using pre or post Autumn 2016 changes" do
+      assert_current_node :choose_cap_to_calculate?
+    end
+
+    context "when using pre-Autumn 2016 rates and rules" do
+      setup { add_response :default }
+
+      #Q1 Receiving Housing Benefit
+      should "ask do you receive housing benefit" do
+        assert_current_node :receive_housing_benefit?
+      end
+
+      context "answer yes" do
+        setup { add_response :yes }
+
+        #Q2 Qualify for working tax credit
+        should "ask if qualify for working tax credit" do
+          assert_current_node :working_tax_credit?
+        end
+
+        context "answer yes" do
+          setup { add_response :yes }
+
+          should "go to Outcome 1" do
+            assert_current_node :outcome_not_affected_exemptions
+          end
+        end # Q2 Qualify for working tax credit at Outcome 1
+
+        ## Not qualify for working tax credit from Q3
+        context "answer no" do
+          setup { add_response :no }
+
+          #Q3
+          should "Ask if household receiving exemption benefits" do
+            assert_current_node :receiving_exemption_benefits?
+          end
+
+          context "answer yes" do
+            setup { add_response :yes }
+
+            should "go to outcome 1" do
+              assert_current_node :outcome_not_affected_exemptions
+            end
+          end # Q3 receiving benefits end at Outcome 1
+
+          # not receiving exemption benefits from Q3
+          context "answer no" do
+            setup { add_response :no }
+
+            #Q4
+            should "ask if household receiving other benefits" do
+              assert_current_node :receiving_non_exemption_benefits?
+            end
+
+            context "answer receiving additional benefits" do
+              setup { add_response 'guardian,sda' }
+
+              #Q5f
+              should "ask how much for guardian allowance benefit" do
+                assert_state_variable :benefit_types, [:sda]
+                assert_current_node :guardian_amount?
+                assert_state_variable :total_benefits, 0
+              end
+
+              context "answer guardian allowance amount" do
+                setup { add_response "300" }
+
+                #Q5k
+                should "ask how much for severe disability allowance" do
+                  assert_state_variable :benefit_types, []
+                  assert_current_node :sda_amount?
+                  assert_state_variable :total_benefits, 300
+                end
+
+                context "answer sda amount" do
+                  setup { add_response "300" }
+
+                  #Q5p
+                  should "ask how much for housing benefit" do
+                    assert_state_variable :benefit_types, []
+                    assert_state_variable :total_benefits, 600
+                    assert_current_node :housing_benefit_amount?
+                  end
+
+                  context "answer housing benefit amount" do
+                    setup { add_response "300" }
+
+                    #Q6
+                    should "ask whether single, living with couple or lone parent" do
+                      assert_current_node :single_couple_lone_parent?
+                    end
+
+                    context "answer single above cap" do
+                      setup { add_response "single" }
+
+                      should "go to outcome 3" do
+                        assert_current_node :outcome_affected_greater_than_cap
+                      end
+                    end #Q6 single greater than cap, at Outcome 3
+                  end #Q5p how much for housing benefit
+                end #Q5k how much for severe disablity allowance
+              end #Q5f how much for guardian allowance benefit
+            end #Q4 receiving additional benefits, above cap
+
+            context "answer receiving additional benefits" do
+              setup { add_response 'esa,maternity' }
+
+              should "ask ask how much for esa benefit" do
+                assert_state_variable :benefit_types, [:maternity]
+                assert_current_node :esa_amount?
+              end
+
+              context "answer esa amount" do
+                setup { add_response "10" }
+
+                should "ask how much for maternity benefits" do
+                  assert_state_variable :benefit_types, []
+                  assert_current_node :maternity_amount?
+                end
+
+                context "answer maternity amount" do
+                  setup { add_response "10" }
+
+                  should "ask how much for housing benefits" do
+                    assert_state_variable :benefit_types, []
+                    assert_current_node :housing_benefit_amount?
+                  end
+
+                  context "answer housing benefit amount" do
+                    setup { add_response "10" }
+
+                    should "ask if single, couple or lone parent" do
+                      assert_current_node :single_couple_lone_parent?
+                    end
+
+                    context "answer lone parent" do
+                      setup { add_response 'parent' }
+
+                      should "go to outcome" do
+                        assert_current_node :outcome_not_affected_less_than_cap
+                      end
+                    end #Q6 lone parent, under cap, at Outcome 4
+                  end #Q5p how much for housing, under cap
+                end #Q5j how much for maternity, under cap
+              end #Q5e how much for esa, under cap
+            end #Q4 receiving additional benefits, under cap
+
+            # not receiving additional benefits from Q4
+            context "no additional benefits selected" do
+              setup { add_response 'none' }
+
+              should "go to outcome" do
+                assert_current_node :outcome_not_affected
+              end
+            end #Q4 no additional benefits at Outcome 5
+          end #Q3 not receiving benefits
+        end # Q2 not qualify for working tax credit
+      end # Q1 Receiving housing benefit
+
+      # Not receiving housing benefit
+      context "answer no" do
+        setup { add_response :no }
+
+        should "go to Outcome 1" do
+          assert_current_node :outcome_not_affected_no_housing_benefit
+        end
+      end # Q1 not receving housing benefit at Outcome 2
+
+      context "housing benefit less than 0.5" do
+        should "show :outcome_affected_greater_than_cap outcome" do
+          add_response :yes
+          add_response :no
+          add_response :no
+          add_response :child_benefit
+          add_response "100"
+          add_response "400"
+          add_response :single
+          assert_current_node :outcome_affected_greater_than_cap
+        end
+      end
+    end
+  end # Benefit cap calculator
+
+  context "when using post-Autumn 2016 rates and rules" do
+    setup { add_response :future }
+
     #Q1 Receiving Housing Benefit
     should "ask do you receive housing benefit" do
       assert_current_node :receive_housing_benefit?
@@ -184,5 +370,5 @@ class BenefitCapCalculatorTest < ActiveSupport::TestCase
         assert_current_node :outcome_affected_greater_than_cap
       end
     end
-  end # Benefit cap calculator
+  end
 end # BenefitCapCalculatorTest
