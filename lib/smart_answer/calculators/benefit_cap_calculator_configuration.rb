a9e9c5f2bd20806319716abab2b394a37ea3a2eb
James Mead <james@floehopper.org>
2016-04-19 17:50:56 +0100

    Allow user to choose between 2 versions of benefit-cap-calculator
    This is a really hack-y spike to demonstrate what I meant by option 1 in my
    email earlier today [1].
    
    There's lots not to like about this approach. In particular, the duplication of
    Q4 and Q6 and their templates. Although this could almost certainly be
    improved, because I did a very naive copy and paste of these two questions.
    
    Note that I had to duplicate these two questions, because they were the only
    two places which needed the value of `chosen_cap` at flow-definition time as
    opposed to at request-time. In my email I hadn't appreciated that Q6 needed it
    too, which is why I only suggested duplicating Q4.
    
    Note that I've removed the memoisation in `BenefitCapCalculatorConfiguration`,
    because I suspected it would be problematic once I added a `version` argument to
    the `data` method. I'm not convinced the memoisation is really necessary anyway.
    
    For demo purposes I moved the existing YAML data under a new `default` key and
    copied the same data under a `future` key. I then removed a bunch of the
    benefits and changed some of the other values.
    
    Note that I have not updated any of the tests and I have only tested the
    solution very briefly - I may well have overlooked something.
    
    [1]: https://groups.google.com/a/digital.cabinet-office.gov.uk/forum/#!topic/content-team-developers/2cCz7vvMXp8

--- lib/smart_answer/calculators/benefit_cap_calculator_configuration.rb
+++ lib/smart_answer/calculators/benefit_cap_calculator_configuration.rb
@@ -1,49 +1,51 @@
 module SmartAnswer::Calculators
   class BenefitCapCalculatorConfiguration
-    def weekly_benefit_caps
-      @weekly_benefit_caps ||= data.fetch("weekly_benefit_caps").with_indifferent_access
+    def weekly_benefit_caps(version)
+      data(version).fetch("weekly_benefit_caps").with_indifferent_access
     end
 
-    def weekly_benefit_cap_descriptions
-      @weekly_benefit_cap_descriptions ||=
-        weekly_benefit_caps.inject(HashWithIndifferentAccess.new) do |weekly_benefit_cap_description, (key, value)|
-          weekly_benefit_cap_description[key] = value.fetch("description")
-          weekly_benefit_cap_description
-        end
+    def weekly_benefit_cap_descriptions(version)
+      weekly_benefit_caps(version).inject(HashWithIndifferentAccess.new) do |weekly_benefit_cap_description, (key, value)|
+        weekly_benefit_cap_description[key] = value.fetch("description")
+        weekly_benefit_cap_description
+      end
     end
 
-    def weekly_benefit_cap_amount(family_type)
-      weekly_benefit_caps.fetch(family_type)["amount"]
+    def weekly_benefit_cap_amount(version, family_type)
+      weekly_benefit_caps(version).fetch(family_type)["amount"]
     end
 
-    def benefits
-      @benefits ||= data.fetch("benefits").with_indifferent_access
+    def benefits(version)
+      data(version).fetch("benefits").with_indifferent_access
     end
 
-    def exempt_benefits
-      @exempt_benefits ||= data.fetch("exempt_benefits")
+    def exempt_benefits(version)
+      data(version).fetch("exempt_benefits")
     end
 
-    def questions
-      @questions ||=
-        benefits.inject(HashWithIndifferentAccess.new) do |benefits_and_questions, (key, value)|
-          benefits_and_questions[key] = value.fetch("question")
-          benefits_and_questions
-        end
+    def questions(version)
+      benefits(version).inject(HashWithIndifferentAccess.new) do |benefits_and_questions, (key, value)|
+        benefits_and_questions[key] = value.fetch("question")
+        benefits_and_questions
+      end
     end
 
-    def descriptions
-      @descriptions ||=
-        benefits.inject(HashWithIndifferentAccess.new) do |benefits_and_descriptions, (key, value)|
-          benefits_and_descriptions[key] = value.fetch("description")
-          benefits_and_descriptions
-        end
+    def all_questions
+      questions('default').merge(questions('future'))
+    end
+
+    def descriptions(version)
+      benefits(version).inject(HashWithIndifferentAccess.new) do |benefits_and_descriptions, (key, value)|
+        benefits_and_descriptions[key] = value.fetch("description")
+        benefits_and_descriptions
+      end
     end
 
   private
 
-    def data
-      @data ||= YAML.load_file(Rails.root.join('lib', 'data', 'benefit_cap_data.yml'))
+    def data(version)
+      hash = YAML.load_file(Rails.root.join('lib', 'data', 'benefit_cap_data.yml'))
+      hash.fetch(version)
     end
   end
 end
