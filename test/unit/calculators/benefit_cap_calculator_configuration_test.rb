3f8186f4cb9ef2a4a8e717a4906b378d2cee1a1a
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2016-04-20 10:28:57 +0100

    Load entire contents of YAML. Pass in a version to get the subset

--- test/unit/calculators/benefit_cap_calculator_configuration_test.rb
+++ test/unit/calculators/benefit_cap_calculator_configuration_test.rb
@@ -10,32 +10,32 @@ module SmartAnswer::Calculators
 
       context "weekly_benefit_caps" do
         should "be £350 for a single person" do
-          assert_equal 350, @config.weekly_benefit_cap_amount(:single)
+          assert_equal 350, @config.weekly_benefit_cap_amount(:default, :single)
         end
         should "be £500 for a couple with or without children" do
-          assert_equal 500, @config.weekly_benefit_cap_amount(:couple)
+          assert_equal 500, @config.weekly_benefit_cap_amount(:default, :couple)
         end
         should "be £500 for a lone parent" do
-          assert_equal 500, @config.weekly_benefit_cap_amount(:parent)
+          assert_equal 500, @config.weekly_benefit_cap_amount(:default, :parent)
         end
       end
 
       context "benefits data" do
         should "have a question and description for each benefit" do
-          assert_equal :jsa_amount?, @config.benefits.fetch("jsa")[:question]
-          assert_equal "Jobseeker’s Allowance", @config.benefits.fetch(:jsa)[:description]
+          assert_equal :jsa_amount?, @config.benefits(:default).fetch("jsa")[:question]
+          assert_equal "Jobseeker’s Allowance", @config.benefits(:default).fetch(:jsa)[:description]
         end
         should "be able get a hash of just benefits and questions" do
-          assert_kind_of Hash, @config.questions
+          assert_kind_of Hash, @config.questions(:default)
         end
         should "be able get a hash of just benefits and descriptions" do
-          assert_kind_of Hash, @config.descriptions
+          assert_kind_of Hash, @config.descriptions(:default)
         end
       end
 
       context "exempt_benefits" do
         should "contain an entry for Disability Living Allowance" do
-          assert_includes @config.exempt_benefits, "Disability Living Allowance"
+          assert_includes @config.exempt_benefits(:default), "Disability Living Allowance"
         end
       end
 
@@ -91,31 +91,25 @@ module SmartAnswer::Calculators
           )
         end
         context "default configuration" do
-          setup do
-            @config = BenefitCapCalculatorConfiguration.new
-          end
           should "get weekly_benefit_caps data for the default configuration" do
-            assert_equal 100, @config.weekly_benefit_cap_amount(:first)
+            assert_equal 100, @config.weekly_benefit_cap_amount(:default, :first)
           end
           should "get exempt benefits for the default configuration" do
-            assert_includes @config.exempt_benefits, "first exempt benefit"
+            assert_includes @config.exempt_benefits(:default), "first exempt benefit"
           end
           should "get benefits data for the default configuration" do
-            assert_equal "first_question", @config.benefits.fetch(:first_benefit)[:question]
+            assert_equal "first_question", @config.benefits(:default).fetch(:first_benefit)[:question]
           end
         end
         context "other configuration" do
-          setup do
-            @config = BenefitCapCalculatorConfiguration.new(:other)
-          end
           should "get weekly_benefit_caps data for the other configuration" do
-            assert_equal 300, @config.weekly_benefit_cap_amount(:first)
+            assert_equal 300, @config.weekly_benefit_cap_amount(:other, :first)
           end
           should "get exempt benefits for the other configuration" do
-            assert_includes @config.exempt_benefits, "first other exempt benefit"
+            assert_includes @config.exempt_benefits(:other), "first other exempt benefit"
           end
           should "get benefits data for the other configuration" do
-            assert_equal "other_first_question", @config.benefits.fetch("first_benefit")["question"]
+            assert_equal "other_first_question", @config.benefits(:other).fetch("first_benefit")["question"]
           end
         end
       end
