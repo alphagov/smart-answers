c06bc658f8dd3b6fd006be0c45019ee8fb498b35
James Mead <james@floehopper.org>
2016-04-26 13:09:07 +0100

    Fix YearRange#ends_on for year beginning on leap day (29 Feb)
    In this Trello card [1], @erikse pointed out that a year range beginning on 29th
    February should end on 28th February the following year and not 27th February as
    in the previous implementation.
    
    Also a year range should always include either 365 or 366 days depending on
    whether it includes a leap day.
    
    This commit adds more test coverage to highlight the problem as well as fixing
    the implementation.
    
    [1]: https://trello.com/c/M17mqlSN

--- lib/smart_answer/year_range.rb
+++ lib/smart_answer/year_range.rb
@@ -1,7 +1,7 @@
 module SmartAnswer
   class YearRange < DateRange
     def initialize(begins_on:)
-      super(begins_on: begins_on, ends_on: begins_on.to_date + 1.year - 1)
+      super(begins_on: begins_on, ends_on: begins_on - 1 + 1.year)
     end
   end
 end
--- test/unit/year_range_test.rb
+++ test/unit/year_range_test.rb
@@ -2,16 +2,50 @@ require_relative '../test_helper'
 
 module SmartAnswer
   class YearRangeTest < ActiveSupport::TestCase
-    setup do
-      @year_range = YearRange.new(begins_on: Date.parse('2000-02-01'))
+    context 'beginning on 7th June, 2001' do
+      setup do
+        @year_range = YearRange.new(begins_on: Date.parse('2001-06-07'))
+      end
+
+      should 'begin on begins_on date' do
+        assert_equal Date.parse('2001-06-07'), @year_range.begins_on
+      end
+
+      should 'end a day before a year after the begins_on date' do
+        assert_equal Date.parse('2002-06-06'), @year_range.ends_on
+      end
+
+      should 'be 365 days long, because it does not include 29th Feb' do
+        assert_equal 365, @year_range.number_of_days
+      end
     end
 
-    should 'begin on begins_on date' do
-      assert_equal Date.parse('2000-02-01'), @year_range.begins_on
+    context 'beginning on 1st February, 2000' do
+      setup do
+        @year_range = YearRange.new(begins_on: Date.parse('2000-02-01'))
+      end
+
+      should 'end a day before a year after the begins_on date' do
+        assert_equal Date.parse('2001-01-31'), @year_range.ends_on
+      end
+
+      should 'be 366 days long, because it does include 29th Feb' do
+        assert_equal 366, @year_range.number_of_days
+      end
     end
 
-    should 'end a day before a year after the begins_on date' do
-      assert_equal Date.parse('2001-01-31'), @year_range.ends_on
+    context 'beginning on 29th February, 2012' do
+      setup do
+        @year_range = YearRange.new(begins_on: Date.parse('2012-02-29'))
+      end
+
+      should 'end a day before a year after the begins_on date' do
+        assert_equal Date.parse('2013-02-28'), @year_range.ends_on
+      end
+
+      should 'be 366 days long, because it does include 29th Feb' do
+        assert_equal 366, @year_range.number_of_days
+      end
     end
   end
 end
