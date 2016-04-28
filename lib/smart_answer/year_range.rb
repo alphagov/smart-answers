5e2217f6abf695a6d21bbe2229ca4163fed9172e
James Mead <james@floehopper.org>
2016-04-28 10:44:59 +0100

    fixup! Fix YearRange#ends_on for year beginning on leap day (29 Feb)

--- lib/smart_answer/year_range.rb
+++ lib/smart_answer/year_range.rb
@@ -1,7 +1,8 @@
 module SmartAnswer
   class YearRange < DateRange
     def initialize(begins_on:)
-      super(begins_on: begins_on, ends_on: begins_on - 1 + 1.year)
+      ends_on = begins_on - 1 + 1.year
+      super(begins_on: begins_on, ends_on: ends_on)
     end
   end
 end
