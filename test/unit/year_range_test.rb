9e042d618d50b9aa6f7d0d50c2046b9b3d6d49f8
James Mead <james@floehopper.org>
2016-04-28 10:42:41 +0100

    fixup! Fix YearRange#ends_on for year beginning on leap day (29 Feb)

--- test/unit/year_range_test.rb
+++ test/unit/year_range_test.rb
@@ -2,17 +2,17 @@ require_relative '../test_helper'
 
 module SmartAnswer
   class YearRangeTest < ActiveSupport::TestCase
-    context 'beginning on 7th June, 2001' do
+    context 'not including the 29th Feb of a leap year' do
       setup do
-        @year_range = YearRange.new(begins_on: Date.parse('2001-06-07'))
+        @year_range = YearRange.new(begins_on: Date.parse('2000-03-01'))
       end
 
       should 'begin on begins_on date' do
-        assert_equal Date.parse('2001-06-07'), @year_range.begins_on
+        assert_equal Date.parse('2000-03-01'), @year_range.begins_on
       end
 
       should 'end a day before a year after the begins_on date' do
-        assert_equal Date.parse('2002-06-06'), @year_range.ends_on
+        assert_equal Date.parse('2001-02-28'), @year_range.ends_on
       end
 
       should 'be 365 days long, because it does not include 29th Feb' do
@@ -20,7 +20,7 @@ module SmartAnswer
       end
     end
 
-    context 'beginning on 1st February, 2000' do
+    context 'including the 29th Feb of a leap year' do
       setup do
         @year_range = YearRange.new(begins_on: Date.parse('2000-02-01'))
       end
@@ -34,13 +34,13 @@ module SmartAnswer
       end
     end
 
-    context 'beginning on 29th February, 2012' do
+    context 'beginning on 29th February of a leap year' do
       setup do
-        @year_range = YearRange.new(begins_on: Date.parse('2012-02-29'))
+        @year_range = YearRange.new(begins_on: Date.parse('2000-02-29'))
       end
 
       should 'end a day before a year after the begins_on date' do
-        assert_equal Date.parse('2013-02-28'), @year_range.ends_on
+        assert_equal Date.parse('2001-02-28'), @year_range.ends_on
       end
 
       should 'be 366 days long, because it does include 29th Feb' do
