f5ef49ffe093d7a1024e48472ccde35ef57e1195
James Mead <james@floehopper.org>
2016-04-28 10:45:40 +0100

    Fix YearRange#ends_on for year ending on leap day (29 Feb)
    In implementing the fix for a year beginning on a leap day [1], I realised that
    there was a similar issue for a year ending on a leap day. In both of these
    edge cases it seems somewhat subjective whether or not the leap day should be
    included. It's my sense that the most intuitive behaviour is to always include
    the leap day. However, it's possible that specific policies/legislation might
    have particular rules about this. We should bear this in mind when we make use
    of the `YearRange` class and potentially make this behaviour configurable if
    necessary.
    
    This commit adds more test coverage to highlight the problem as well as fixing
    the implementation.
    
    [1]: https://trello.com/c/M17mqlSN

--- lib/smart_answer/year_range.rb
+++ lib/smart_answer/year_range.rb
@@ -2,6 +2,9 @@ module SmartAnswer
   class YearRange < DateRange
     def initialize(begins_on:)
       ends_on = begins_on - 1 + 1.year
+      if ends_on.month == 2 && ends_on.day == 28 && ends_on.leap?
+        ends_on += 1
+      end
       super(begins_on: begins_on, ends_on: ends_on)
     end
   end
--- test/unit/year_range_test.rb
+++ test/unit/year_range_test.rb
@@ -47,5 +47,19 @@ module SmartAnswer
         assert_equal 366, @year_range.number_of_days
       end
     end
+
+    context 'ending on 29th February of a leap year' do
+      setup do
+        @year_range = YearRange.new(begins_on: Date.parse('1999-03-01'))
+      end
+
+      should 'end a day before a year after the begins_on date' do
+        assert_equal Date.parse('2000-02-29'), @year_range.ends_on
+      end
+
+      should 'be 366 days long, because it does include 29th Feb' do
+        assert_equal 366, @year_range.number_of_days
+      end
+    end
   end
 end
