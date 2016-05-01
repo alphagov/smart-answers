51701e1c7f01fc5e891ae555ae3f56929df5a0ba
Chris Roos <chris@seagul.co.uk>
2016-05-01 12:02:56 +1200

    Extract payment info from _fees.govspeak.erb
    The `outcome_opposite_sex_marriage_in_affirmation_countries` outcome is the
    only outcome that renders the `fees` partial. I used the following command to
    determine the countries that lead to this outcome:
    
        $ rails r script/marriage-abroad-countries-leading-to-outcome.rb \
        outcome_opposite_sex_marriage_in_affirmation_countries
    
    I created the add-payment-partials-to-marriage-abroad-services-data.rb to
    update the data in marriage_abroad_services.yml. I plan to update this script
    as I continue to extract the payment info from the templates.
    
    Running the script in this commit results in no output which means that the
    payment information for each of the countries listed has been successfully
    added to marriage_abroad_services.yml.

---
+++ script/add-payment-partials-to-marriage-abroad-services-data.rb
@@ -0,0 +1,52 @@
+countries = %w(
+  belgium
+  china
+  ecuador
+  egypt
+  finland
+  lebanon
+  macao
+  mongolia
+  morocco
+  norway
+  peru
+  philippines
+  qatar
+  south-korea
+  thailand
+  turkey
+  united-arab-emirates
+  vietnam
+)
+
+payment_method_partial = {
+  'default'     => 'pay_by_cash_or_credit_card_no_cheque',
+  'finland'     => 'pay_in_euros_or_visa_electron',
+  'norway'      => 'pay_by_visas_or_mastercard',
+  'philippines' => 'pay_by_cash_or_bank_cheque',
+  'south-korea' => 'pay_by_cash_in_local_currency_or_credit_card_not_cheque'
+}
+
+marriage_abroad_services_file = Rails.root.join('lib', 'data', 'marriage_abroad_services.yml')
+marriage_abroad_services = YAML.load_file(marriage_abroad_services_file)
+
+countries.each do |country|
+  payment_partial_name = payment_method_partial[country] ||
+    payment_method_partial['default']
+
+  if marriage_abroad_services[country].present?
+    current_payment_partial_name = marriage_abroad_services[country]['payment_partial_name']
+
+    if current_payment_partial_name.blank? || current_payment_partial_name == payment_partial_name
+      marriage_abroad_services[country]['payment_partial_name'] = payment_partial_name
+    else
+      warn "Payment method partial already set for #{country}. Existing value: #{current_payment_partial_name}. New value: #{payment_partial_name}."
+    end
+  else
+    warn "Entry not found for #{country}."
+  end
+end
+
+File.open(marriage_abroad_services_file, 'w') do |file|
+  file.puts marriage_abroad_services.to_yaml
+end
