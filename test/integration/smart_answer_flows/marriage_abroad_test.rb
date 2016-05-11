d7635348aa8853e6475eea0227590a1a08ca1984
Pablo Manrubia <pmanrubia@gmail.com>
2016-05-11 15:52:03 +0100

    Update flow to use Denmark outcome: living UK, same sex
    The flow is updated to display the outcome: `outcome_ceremonies_in_denmark_when_residing_in_uk_or_denmark`
    for same sex ceremonies in Denmark only when the user is UK resident.

--- test/integration/smart_answer_flows/marriage_abroad_test.rb
+++ test/integration/smart_answer_flows/marriage_abroad_test.rb
@@ -1181,8 +1181,8 @@ class MarriageAbroadTest < ActiveSupport::TestCase
       add_response 'partner_other'
       add_response 'same_sex'
     end
-    should "go to cp or equivalent outcome" do
-      assert_current_node :outcome_same_sex_civil_partnership
+    should "go to outcome_ceremonies_in_denmark_when_residing_in_uk_or_denmark" do
+      assert_current_node :outcome_ceremonies_in_denmark_when_residing_in_uk_or_denmark
     end
   end
 
