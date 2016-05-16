27a1da31d2c69d2767bb16fc0e6dc9d47be6acf3
James Mead <james@floehopper.org>
2016-05-16 12:39:35 +0100

    Add tests for personal allowance in post-2013-2014 & fix implementation
    The age-related personal allowance was only based on age up until the end of the
    2012-2013 tax year. Since then it should have been based on the person's date of
    birth [1]. This commit introduces tests to check this and fixes the
    implementation of `PersonalAllowanceCalculator#age_related_allowance`.
    
    [1]: https://www.gov.uk/government/publications/rates-and-allowances-income-tax/income-tax-rates-and-allowances-current-and-past#personal-allowances-for-people-born-before-6-april-1948

--- test/unit/calculators/personal_allowance_calculator_test.rb
+++ test/unit/calculators/personal_allowance_calculator_test.rb
@@ -48,5 +48,39 @@ module SmartAnswer::Calculators
         assert_equal(@higher_allowance_2, result)
       end
     end
+
+    context 'in or after 2013 to 2014 tax year' do
+      setup do
+        Timecop.freeze(Date.parse('2014-04-06'))
+      end
+
+      teardown do
+        Timecop.return
+      end
+
+      should "return the basic allowance for someone born on 6th April 1948" do
+        date_of_birth = Date.parse('1948-04-06')
+        result = @calculator.age_related_allowance(date_of_birth)
+        assert_equal(@personal_allowance, result)
+      end
+
+      should "return the 1st higher allowance for someone born on 5th April 1948" do
+        date_of_birth = Date.parse('1948-04-05')
+        result = @calculator.age_related_allowance(date_of_birth)
+        assert_equal(@higher_allowance_1, result)
+      end
+
+      should "return the 1st higher allowance for someone born on 6th April 1938" do
+        date_of_birth = Date.parse('1938-04-06')
+        result = @calculator.age_related_allowance(date_of_birth)
+        assert_equal(@higher_allowance_1, result)
+      end
+
+      should "return the 2nd higher allowance for someone born on 6th April 1938" do
+        date_of_birth = Date.parse('1938-04-05')
+        result = @calculator.age_related_allowance(date_of_birth)
+        assert_equal(@higher_allowance_2, result)
+      end
+    end
   end
 end
