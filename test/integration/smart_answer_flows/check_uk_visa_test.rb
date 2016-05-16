d5b6e03244abf02c6654dbc499783663155ae45d
Chris Roos <chris@seagul.co.uk>
2016-05-16 19:04:35 +1200

    Use stub_worldwide_locations in check_uk_visa_test

--- test/integration/smart_answer_flows/check_uk_visa_test.rb
+++ test/integration/smart_answer_flows/check_uk_visa_test.rb
@@ -1,16 +1,14 @@
 require_relative '../../test_helper'
 require_relative 'flow_test_helper'
-require 'gds_api/test_helpers/worldwide'
 
 require "smart_answer_flows/check-uk-visa"
 
 class CheckUkVisaTest < ActiveSupport::TestCase
   include FlowTestHelper
-  include GdsApi::TestHelpers::Worldwide
 
   setup do
     @location_slugs = %w(andorra anguilla armenia bolivia canada china colombia croatia mexico south-africa stateless-or-refugee syria turkey yemen oman united-arab-emirates qatar taiwan venezuela)
-    worldwide_api_has_locations(@location_slugs)
+    stub_worldwide_locations(@location_slugs)
     setup_for_testing_flow SmartAnswer::CheckUkVisaFlow
   end
 
