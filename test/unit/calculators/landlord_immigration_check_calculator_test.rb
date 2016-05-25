a4c55f1dacf86841f3b3b43272d784446b86087f
James Mead <james@floehopper.org>
2016-05-25 13:43:13 +0100

    Use contexts in LandlordImmigrationCheckCalculatorTest
    I think this makes the test easier to read.

--- test/unit/calculators/landlord_immigration_check_calculator_test.rb
+++ test/unit/calculators/landlord_immigration_check_calculator_test.rb
@@ -7,33 +7,44 @@ module SmartAnswer::Calculators
 
     setup do
       @calculator = LandlordImmigrationCheckCalculator.new
-
-      # Excluded countries
-      imminence_has_areas_for_postcode("PA3%202SW",   [{ slug: 'renfrewshire-council', country_name: 'Scotland' }])
-
-      # Included country
-      imminence_has_areas_for_postcode("RH6%200NP",   [{ slug: 'crawley-borough-council', country_name: 'England' }])
     end
 
-    test "with an unknown postcode" do
-      stub_request(:get, %r{\A#{Plek.new.find('imminence')}/areas/E15\.json}).
-        to_return(body: { _response_info: { status: 404 }, total: 0, results: [] }.to_json)
+    context 'when postcode is unknown' do
+      setup do
+        stub_request(:get, %r{\A#{Plek.new.find('imminence')}/areas/E15\.json}).
+          to_return(body: { _response_info: { status: 404 }, total: 0, results: [] }.to_json)
+        @calculator.postcode = "E15"
+      end
 
-      @calculator.postcode = "E15"
+      should 'return no areas for postcode' do
+        assert_equal [], @calculator.areas_for_postcode
+      end
 
-      assert_equal [], @calculator.areas_for_postcode
+      should 'determine that the rules do not apply' do
+        refute @calculator.rules_apply?
+      end
     end
 
-    test "with a postcode outside England" do
-      @calculator.postcode = "PA3 2SW"
+    context 'when postcode is outside England' do
+      setup do
+        imminence_has_areas_for_postcode("PA3%202SW", [{ slug: 'renfrewshire-council', country_name: 'Scotland' }])
+        @calculator.postcode = "PA3 2SW"
+      end
 
-      refute @calculator.rules_apply?
+      should 'determine that the rules do not apply' do
+        refute @calculator.rules_apply?
+      end
     end
 
-    test "with a postcode in England" do
-      @calculator.postcode = "RH6 0NP"
+    context 'when postcode is in England' do
+      setup do
+        imminence_has_areas_for_postcode("RH6%200NP", [{ slug: 'crawley-borough-council', country_name: 'England' }])
+        @calculator.postcode = "RH6 0NP"
+      end
 
-      assert @calculator.rules_apply?
+      should 'determine that the rules do not apply' do
+        assert @calculator.rules_apply?
+      end
     end
   end
 end
