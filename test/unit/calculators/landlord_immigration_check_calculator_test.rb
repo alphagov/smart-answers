348b6a441d3bac6f20ed91821e3423e5cd12f8cd
James Mead <james@floehopper.org>
2016-05-25 13:35:45 +0100

    Remove unnecessary tests from LandlordImmigrationCheckCalculatorTest
    Although at some point, the rules may apply in countries outside England, I'm
    pretty sure this isn't imminent and so I'd prefer ignore that possibility for
    now and keep the code as simple as possible for the current requirements.

--- test/unit/calculators/landlord_immigration_check_calculator_test.rb
+++ test/unit/calculators/landlord_immigration_check_calculator_test.rb
@@ -10,8 +10,6 @@ module SmartAnswer::Calculators
 
       # Excluded countries
       imminence_has_areas_for_postcode("PA3%202SW",   [{ slug: 'renfrewshire-council', country_name: 'Scotland' }])
-      imminence_has_areas_for_postcode("SA2%207JU",   [{ slug: 'swansea-council', country_name: 'Wales' }])
-      imminence_has_areas_for_postcode("BT29%204AB",  [{ slug: 'antrim-south-east', country_name: 'Northern Ireland' }])
 
       # Included country
       imminence_has_areas_for_postcode("RH6%200NP",   [{ slug: 'crawley-borough-council', country_name: 'England' }])
@@ -26,20 +24,8 @@ module SmartAnswer::Calculators
       assert_equal [], @calculator.areas_for_postcode
     end
 
-    test "with a postcode in Scotland" do
+    test "with a postcode outside England" do
       @calculator.postcode = "PA3 2SW"
-
-      refute @calculator.rules_apply?
-    end
-
-    test "with a postcode in Wales" do
-      @calculator.postcode = "SA2 7JU"
-
-      refute @calculator.rules_apply?
-    end
-
-    test "with a postcode in Northern Ireland" do
-      @calculator.postcode = "BT29 4AB"
 
       refute @calculator.rules_apply?
     end
