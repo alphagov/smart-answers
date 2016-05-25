b365b396cf9b0e5a0253e8b54ead28314d7eccb8
James Mead <james@floehopper.org>
2016-05-25 10:11:12 +0100

    Convert add_response to assert_current_node in RegisterADeathTest
    This test was incorrectly trying to add an inappropriate response,
    `no_embassy_result`, and it had no assertion. The test was passing previously,
    because `add_response` doesn't cause any processing of the responses.
    
    It looks to me as if the call to `add_response` was intended to be a call to
    `assert_current_node`.
    
    This was highlighted by the enabling of the Lint/UselessAssignment cop.

--- test/integration/smart_answer_flows/register_a_death_test.rb
+++ test/integration/smart_answer_flows/register_a_death_test.rb
@@ -309,7 +309,7 @@ class RegisterADeathTest < ActiveSupport::TestCase
         add_response 'iran'
       end
       should "give the no embassy result" do
-        add_response :no_embassy_result
+        assert_current_node :no_embassy_result
       end
     end # Iran
 
