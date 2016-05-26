3018e8a8a801c7e33b63af2cc213721033ac3cfa
James Mead <james@floehopper.org>
2016-05-26 12:45:13 +0100

    Use stub_worldwide_location in CountryNameFormatterTest
    It seems better & simpler to stub at the `WorldLocation` level rather than by
    using `GdsApi::TestHelpers::Worldwide#worldwide_api_has_location` which stubs at
    a much lower level using `WebMock`.
    
    Note that I changed `antigua-and-barbuda` to `argentina`, because it only
    matters that the country is not in the
    `CountryNameFormatter::COUNTRIES_WITH_DEFINITIVE_ARTICLES` list and our stubbed
    versions of `WorldLocation` have a primitively generated `name` which doesn't
    work very well for countries with multi-word slugs/names.

--- test/unit/calculators/country_name_formatter_test.rb
+++ test/unit/calculators/country_name_formatter_test.rb
@@ -1,28 +1,25 @@
 require_relative '../../test_helper'
-require 'gds_api/test_helpers/worldwide'
 
 module SmartAnswer::Calculators
   class CountryNameFormatterTest < ActiveSupport::TestCase
-    include GdsApi::TestHelpers::Worldwide
-
     context '#definitive_article' do
       setup do
         @formatter = CountryNameFormatter.new
       end
 
       should 'return the country name prepended by "the"' do
-        worldwide_api_has_location('bahamas')
+        stub_worldwide_location('bahamas')
         assert_equal 'the Bahamas', @formatter.definitive_article('bahamas')
       end
 
       should 'return the country name prepended by "The"' do
-        worldwide_api_has_location('bahamas')
+        stub_worldwide_location('bahamas')
         assert_equal 'The Bahamas', @formatter.definitive_article('bahamas', true)
       end
 
       should 'return the country name when definite article is not required' do
-        worldwide_api_has_location('antigua-and-barbuda')
-        assert_equal 'Antigua And Barbuda', @formatter.definitive_article('antigua-and-barbuda')
+        stub_worldwide_location('argentina')
+        assert_equal 'Argentina', @formatter.definitive_article('argentina')
       end
     end
 
