2f12ecc4d1d3708ab65470360e8ecb46a084c6a0
James Mead <james@floehopper.org>
2016-05-26 15:43:14 +0100

    Generate more realistic names for WorldLocation stubs
    I want to use these stubbing methods in the regression tests, but previously
    the generated names did not match those in the regression test artefacts. This
    change means that the generated name will be exactly the same as if we'd been
    using the `GdsApi::TestHelpers::Worldwide` helper methods.
    
    Note that the generated names are still not that great for slugs including the
    word "and", because the "and" gets capitalized along with the other words e.g.
    "st-pierre-and-miquelon" -> "St Pierre And Miquelon".
    
    However, since this is how the regression test artefacts have been generated,
    it seems the most sensible option for now.

--- test/support/world_location_stubbing_methods.rb
+++ test/support/world_location_stubbing_methods.rb
@@ -1,8 +1,13 @@
+require 'gds_api/test_helpers/common_responses'
+
 module WorldLocationStubbingMethods
+  include GdsApi::TestHelpers::CommonResponses
+
   def stub_world_location(location_slug)
     location = stub.quacks_like(WorldLocation.new({}))
     location.stubs(:slug).returns(location_slug)
-    location.stubs(:name).returns(location_slug.humanize)
+    name = titleize_slug(location_slug, title_case: true)
+    location.stubs(:name).returns(name)
     location.stubs(:fco_organisation).returns(nil)
     WorldLocation.stubs(:find).with(location_slug).returns(location)
     location
