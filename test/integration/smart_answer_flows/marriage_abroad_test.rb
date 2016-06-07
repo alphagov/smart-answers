0c6b3d9b34a32c2056d5114475cf0253aac3dfe3
Pablo Manrubia <pmanrubia@gmail.com>
2016-06-07 14:53:48 +0100

    Update flow to use Denmark outcome: living in Denmark, oppos sex
    The flow is updated to display the outcome:
    `outcome_ceremonies_in_denmark_when_residing_in_uk_or_denmark` for opposite sex
    ceremonies in Denmark when the user is resident in Denmark

--- test/integration/smart_answer_flows/marriage_abroad_test.rb
+++ test/integration/smart_answer_flows/marriage_abroad_test.rb
@@ -635,7 +635,7 @@ class MarriageAbroadTest < ActiveSupport::TestCase
       add_response 'opposite_sex'
     end
     should "go to consular cni os outcome" do
-      assert_current_node :outcome_opposite_sex_marriage_in_consular_cni_countries_when_residing_in_uk_or_ceremony_country
+      assert_current_node :outcome_ceremonies_in_denmark_when_residing_in_uk_or_denmark
     end
   end
 
