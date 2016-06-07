414bfbae681bfb5823c1b2178f57f67393ecafe4
Pablo Manrubia <pmanrubia@gmail.com>
2016-06-07 14:54:21 +0100

    Update flow to use Denmark outcome: living Denmark, same sex
    The flow is updated to display the outcome: `outcome_ceremonies_in_denmark`
    for opposite sex ceremonies in Denmark when the user is resident in Denmark

--- lib/smart_answer_flows/marriage-abroad.rb
+++ lib/smart_answer_flows/marriage-abroad.rb
@@ -282,7 +282,7 @@ module SmartAnswer
                 ) ||
                 calculator.same_sex_marriage_and_civil_partnership?
               outcome :outcome_same_sex_marriage_and_civil_partnership
-            elsif calculator.ceremony_country == 'denmark' && calculator.resident_of_uk?
+            elsif calculator.ceremony_country == 'denmark' && (calculator.resident_of_uk? || calculator.resident_of_ceremony_country?)
               outcome :outcome_ceremonies_in_denmark_when_residing_in_uk_or_denmark
             elsif calculator.civil_partnership_equivalent_country?
               outcome :outcome_same_sex_civil_partnership
