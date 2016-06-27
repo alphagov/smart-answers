96c61da9ffd216e61681ddc627fbd3af10e10b7e
Chris Roos <chris@seagul.co.uk>
2016-06-27 18:10:13 +1000

    Prepare for splitting marriage-abroad outcome for Brazil
    The current outcome for ceremonies in Brazil
    (outcome_marriage_in_brazil_when_residing_in_brazil_or_third_country) contains
    two logical outcomes:
    
    * One for residents of Brazil
    * One for residents of a third country (i.e. not resident in the UK or Brazill)
    
    The outcome body contains a single `if/else` statement that decides which
    logical outcome to display. Splitting these logical outcomes and moving the
    "routing logic" to the flow is more consistent with the majority of this Smart
    Answer.
    
    This change will make it easier to split the outcome in a subsequent commit.

--- lib/smart_answer_flows/marriage-abroad.rb
+++ lib/smart_answer_flows/marriage-abroad.rb
@@ -111,7 +111,9 @@ module SmartAnswer
         end
 
         next_node do
-          if calculator.ceremony_country == 'brazil' && calculator.resident_outside_of_uk?
+          if calculator.ceremony_country == 'brazil' && calculator.resident_of_ceremony_country?
+            outcome :outcome_marriage_in_brazil_when_residing_in_brazil_or_third_country
+          elsif calculator.ceremony_country == 'brazil' && calculator.resident_of_third_country?
             outcome :outcome_marriage_in_brazil_when_residing_in_brazil_or_third_country
           elsif calculator.ceremony_country == "netherlands"
             outcome :outcome_ceremonies_in_netherlands_or_marriage_via_local_authority_countries
