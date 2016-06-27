b155a9ddad6f5199cd0bcd20dc54bfe67d42a5ed
Chris Roos <chris@seagul.co.uk>
2016-06-27 18:11:39 +1000

    Add Brazil outcome for third country residents
    The new template is a duplicate of
    outcome_marriage_in_brazil_when_residing_in_brazil_or_third_country.
    
    I'll remove the resident-of-Brazil logic and content from this new template in
    a subsequent commit.

--- lib/smart_answer_flows/marriage-abroad.rb
+++ lib/smart_answer_flows/marriage-abroad.rb
@@ -114,7 +114,7 @@ module SmartAnswer
           if calculator.ceremony_country == 'brazil' && calculator.resident_of_ceremony_country?
             outcome :outcome_marriage_in_brazil_when_residing_in_brazil_or_third_country
           elsif calculator.ceremony_country == 'brazil' && calculator.resident_of_third_country?
-            outcome :outcome_marriage_in_brazil_when_residing_in_brazil_or_third_country
+            outcome :outcome_marriage_in_brazil_when_residing_in_third_country
           elsif calculator.ceremony_country == "netherlands"
             outcome :outcome_ceremonies_in_netherlands_or_marriage_via_local_authority_countries
           elsif calculator.ceremony_country == "portugal"
@@ -260,6 +260,7 @@ module SmartAnswer
       outcome :outcome_civil_partnership_in_france_or_french_overseas_territory
       outcome :outcome_civil_partnership_in_monaco
       outcome :outcome_marriage_in_brazil_when_residing_in_brazil_or_third_country
+      outcome :outcome_marriage_in_brazil_when_residing_in_third_country
       outcome :outcome_marriage_in_france_or_french_overseas_territory
       outcome :outcome_marriage_in_monaco
       outcome :outcome_opposite_sex_in_no_cni_countries_when_residing_in_ceremony_or_third_country
