78c774c93678cce7bac2676cbd0327b8a24e49fb
James Mead <james@floehopper.org>
2016-06-27 15:02:40 +0100

    Add unit tests for current_rules_national_insurance_no_state_pension? calculator method
    I want to refactor this method and so I've elected to add fairly exhaustive
    unit test coverage.
    
    Note that these tests just characterise the existing behaviour, they don't
    validate that it's correct in any way.

--- test/unit/calculators/state_pension_through_partner_calculator_test.rb
+++ test/unit/calculators/state_pension_through_partner_calculator_test.rb
@@ -209,5 +209,121 @@ module SmartAnswer::Calculators
         end
       end
     end
+
+    context 'current_rules_national_insurance_no_state_pension?' do
+      context 'when married' do
+        setup do
+          @calculator.marital_status = 'married'
+        end
+
+        context 'and reached pension age before specific date' do
+          setup do
+            @calculator.when_will_you_reach_pension_age = 'your_pension_age_before_specific_date'
+          end
+
+          should 'be false when partner reached pension age before specific date' do
+            @calculator.when_will_your_partner_reach_pension_age = 'partner_pension_age_before_specific_date'
+            refute @calculator.current_rules_national_insurance_no_state_pension?
+          end
+
+          should 'be true when partner reached pension age after specific date' do
+            @calculator.when_will_your_partner_reach_pension_age = 'partner_pension_age_after_specific_date'
+            assert @calculator.current_rules_national_insurance_no_state_pension?
+          end
+        end
+
+        context 'and reached pension age after specific date' do
+          setup do
+            @calculator.when_will_you_reach_pension_age = 'your_pension_age_after_specific_date'
+          end
+
+          should 'be false when partner reached pension age before specific date' do
+            @calculator.when_will_your_partner_reach_pension_age = 'partner_pension_age_before_specific_date'
+            refute @calculator.current_rules_national_insurance_no_state_pension?
+          end
+
+          should 'be false when partner reached pension age after specific date' do
+            @calculator.when_will_your_partner_reach_pension_age = 'partner_pension_age_after_specific_date'
+            refute @calculator.current_rules_national_insurance_no_state_pension?
+          end
+        end
+      end
+
+      context 'when widowed' do
+        setup do
+          @calculator.marital_status = 'widowed'
+        end
+
+        context 'and reached pension age before specific date' do
+          setup do
+            @calculator.when_will_you_reach_pension_age = 'your_pension_age_before_specific_date'
+          end
+
+          should 'be false when partner reached pension age before specific date' do
+            @calculator.when_will_your_partner_reach_pension_age = 'partner_pension_age_before_specific_date'
+            refute @calculator.current_rules_national_insurance_no_state_pension?
+          end
+
+          should 'be false when partner reached pension age after specific date' do
+            @calculator.when_will_your_partner_reach_pension_age = 'partner_pension_age_after_specific_date'
+            refute @calculator.current_rules_national_insurance_no_state_pension?
+          end
+        end
+
+        context 'and reached pension age after specific date' do
+          setup do
+            @calculator.when_will_you_reach_pension_age = 'your_pension_age_after_specific_date'
+          end
+
+          should 'be false when partner reached pension age before specific date' do
+            @calculator.when_will_your_partner_reach_pension_age = 'partner_pension_age_before_specific_date'
+            refute @calculator.current_rules_national_insurance_no_state_pension?
+          end
+
+          should 'be false when partner reached pension age after specific date' do
+            @calculator.when_will_your_partner_reach_pension_age = 'partner_pension_age_after_specific_date'
+            refute @calculator.current_rules_national_insurance_no_state_pension?
+          end
+        end
+      end
+
+      context 'when divorced' do
+        setup do
+          @calculator.marital_status = 'divorced'
+        end
+
+        context 'and reached pension age before specific date' do
+          setup do
+            @calculator.when_will_you_reach_pension_age = 'your_pension_age_before_specific_date'
+          end
+
+          should 'be false when partner reached pension age before specific date' do
+            @calculator.when_will_your_partner_reach_pension_age = 'partner_pension_age_before_specific_date'
+            refute @calculator.current_rules_national_insurance_no_state_pension?
+          end
+
+          should 'be false when partner reached pension age after specific date' do
+            @calculator.when_will_your_partner_reach_pension_age = 'partner_pension_age_after_specific_date'
+            refute @calculator.current_rules_national_insurance_no_state_pension?
+          end
+        end
+
+        context 'and reached pension age after specific date' do
+          setup do
+            @calculator.when_will_you_reach_pension_age = 'your_pension_age_after_specific_date'
+          end
+
+          should 'be false when partner reached pension age before specific date' do
+            @calculator.when_will_your_partner_reach_pension_age = 'partner_pension_age_before_specific_date'
+            refute @calculator.current_rules_national_insurance_no_state_pension?
+          end
+
+          should 'be false when partner reached pension age after specific date' do
+            @calculator.when_will_your_partner_reach_pension_age = 'partner_pension_age_after_specific_date'
+            refute @calculator.current_rules_national_insurance_no_state_pension?
+          end
+        end
+      end
+    end
   end
 end
