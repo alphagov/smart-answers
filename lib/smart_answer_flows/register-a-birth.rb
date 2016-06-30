5d8e03f2c52cc7ac9fb9a920b08b661bf5681ce4
James Mead <james@floehopper.org>
2016-06-30 13:54:45 +0100

    Extract british_national_father? calculator method

--- lib/smart_answer_flows/register-a-birth.rb
+++ lib/smart_answer_flows/register-a-birth.rb
@@ -55,7 +55,7 @@ module SmartAnswer
         end
 
         next_node do
-          if calculator.paternity_declaration? && calculator.british_national_parent == 'father'
+          if calculator.paternity_declaration? && calculator.british_national_father?
             question :childs_date_of_birth?
           else
             question :where_are_you_now?
