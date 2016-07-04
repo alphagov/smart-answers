d577e204a1b2200c5a117c26f807b79c8910aa78
Pablo Manrubia <pmanrubia@gmail.com>
2016-07-04 14:57:39 +0100

    Add support to retrieve fees from a YML file
    Extend the `passport_and_embassy_data_query.rb` to retrieve passport fees
    from a YML file.
    
    The YML file have this format:
    
    ```
    ---
    adult:
      standard:
        fee: 1
        total: 2
      jumbo:
        fee: 3
        total: 4
    ```
    
    And the data can be retrieved by:
    ```
    @query.find_passport_fees('adult', 'standard', 'total') # => 2
    ```
    
    This format for the YML file will be evolve once exception to the default
    values are introduced. But the first step is to have all the tool using
    this YML file for the default values.
    
    This is done in order to decouple the fees values from the view, as they
    are currently hardcoded and are not easy to maintain.

--- test/unit/calculators/passport_and_embassy_data_query_test.rb
+++ test/unit/calculators/passport_and_embassy_data_query_test.rb
@@ -18,6 +18,21 @@ module SmartAnswer::Calculators
           assert_equal '14 weeks', @query.find_passport_data('afghanistan')['replacing']
         end
       end
+
+      context '#find_passport_fee' do
+        setup do
+          @fees = YAML.load_file(Rails.root.join('test', 'fixtures', 'fees', 'passport_fees.yml'))
+          @query = PassportAndEmbassyDataQuery.new(passport_fees: @fees)
+        end
+
+        should 'return the default fees for an adult navigating through the YML file' do
+          assert_equal 2, @query.find_passport_fee('adult', 'standard', 'total')
+        end
+
+        should 'raise an exception if the fee is not found' do
+          assert_raise(KeyError) { @query.find_passport_fee('adult', 'i_dont_exist') }
+        end
+      end
     end
   end
 end
