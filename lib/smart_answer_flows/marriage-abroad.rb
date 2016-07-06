d5dbb1648a22f967ab6298e639971486822e7a2a
Chris Roos <chris@seagul.co.uk>
2016-07-06 11:35:08 +0100

    Add British Indian Ocean Territories outcome
    The new template is a duplicate of
    outcome_opposite_sex_marriage_in_british_overseas_territory.
    
    I'll remove the non British Indian Ocean Territory logic and content from this
    new template in a subsequent commit.

--- lib/smart_answer_flows/marriage-abroad.rb
+++ lib/smart_answer_flows/marriage-abroad.rb
@@ -194,7 +194,7 @@ module SmartAnswer
                 calculator.ceremony_country == 'zimbabwe'
               outcome :outcome_opposite_sex_marriage_in_commonwealth_countries
             elsif calculator.ceremony_country == 'british-indian-ocean-territory'
-              outcome :outcome_opposite_sex_marriage_in_british_overseas_territory
+              outcome :outcome_opposite_sex_marriage_in_british_indian_ocean_territory
             elsif calculator.ceremony_country == 'british-virgin-islands'
               outcome :outcome_opposite_sex_marriage_in_british_overseas_territory
             elsif calculator.ceremony_country_is_british_overseas_territory?
@@ -270,6 +270,7 @@ module SmartAnswer
       outcome :outcome_opposite_sex_in_no_cni_countries_when_residing_in_ceremony_or_third_country
       outcome :outcome_opposite_sex_marriage_in_affirmation_countries
       outcome :outcome_opposite_sex_marriage_in_belarus
+      outcome :outcome_opposite_sex_marriage_in_british_indian_ocean_territory
       outcome :outcome_opposite_sex_marriage_in_british_overseas_territory
       outcome :outcome_opposite_sex_marriage_in_burma
       outcome :outcome_opposite_sex_marriage_in_cambodia
