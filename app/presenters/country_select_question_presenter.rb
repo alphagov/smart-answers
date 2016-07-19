54f2b7c02833517a5f7a09455bb814ab6339e187
James Mead <james@floehopper.org>
2016-07-19 17:56:30 +0100

    Bring country_select options into line with other question types
    The options for checkbox & multiple-choice questions have `value` & `label`
    attributes. This brings country select questions into line.
    
    This will make it simpler to render question options in a text version of a
    question page.

--- app/presenters/country_select_question_presenter.rb
+++ app/presenters/country_select_question_presenter.rb
@@ -1,9 +1,11 @@
 class CountrySelectQuestionPresenter < QuestionPresenter
   def response_label(value)
-    options.find { |option| option.slug == value }.name
+    options.find { |option| option.value == value }.label
   end
 
   def options
-    @node.options
+    @node.options.map do |option|
+      OpenStruct.new(label: option.name, value: option.slug)
+    end
   end
 end
