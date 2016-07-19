d278db2b916d6616b963fc7ec5b3fa574b1b9127
Anika Henke <anika.henke@digital.cabinet-office.gov.uk>
2016-07-19 19:24:39 +0100

    Add draft Smart Answer with all question types
    This is not intended to ever be live, it's specifically here
    to make it easier to work on all the front-end elements.
    Therefore no tests are necessary.

---
+++ lib/smart_answer_flows/all-smart-answer-questions.rb
@@ -0,0 +1,123 @@
+# not a real smart answer, this is here to make testing and changing all front-end elements easier
+module SmartAnswer
+  class AllSmartAnswerQuestionsFlow < Flow
+    def define
+      name 'all-smart-answer-questions'
+      status :draft
+
+      additional_countries = [OpenStruct.new(slug: "mordor", name: "Mordor")]
+
+      checkbox_question :which_checkboxes? do
+        option :radagast
+        option :mithrandir
+        option :olorin
+        option :tharkun
+        option :elrohir
+
+        next_node do
+          question :which_country?
+        end
+      end
+
+      country_select :which_country?, additional_countries: additional_countries do
+        next_node do
+          question :which_date?
+        end
+      end
+
+      date_question :which_date? do
+        next_node do
+          question :which_date_of_birth?
+        end
+      end
+
+      date_question :which_date_of_birth? do
+        date_of_birth_defaults
+
+        next_node do
+          question :which_date_within_range?
+        end
+      end
+
+      date_question :which_date_within_range? do
+        from { Date.today }
+        to { 4.years.since(Date.today) }
+
+        validate_in_range
+
+        next_node do
+          question :which_date_this_year?
+        end
+      end
+
+      date_question :which_date_this_year? do
+        from { 1.years.ago.beginning_of_year.to_date }
+        to { ::Date.today.end_of_year }
+
+        default_year { 0 }
+
+        next_node do
+          question :how_much_money?
+        end
+      end
+
+      money_question :how_much_money? do
+        next_node do
+          question :which_choice?
+        end
+      end
+
+      multiple_choice :which_choice? do
+        option :one
+        option :two
+        option :three
+        option :four
+
+        next_node do
+          question :which_boolean_choice?
+        end
+      end
+
+      multiple_choice :which_boolean_choice? do
+        option :yes
+        option :no
+
+        next_node do
+          question :which_postcode?
+        end
+      end
+
+      postcode_question :which_postcode? do
+        next_node do
+          question :how_much_salary?
+        end
+      end
+
+      salary_question :how_much_salary? do
+        next_node do
+          question :which_value?
+        end
+      end
+
+      value_question :which_value? do
+        next_node do
+          outcome :which_integer?
+        end
+      end
+
+      value_question :which_integer?, parse: Integer do
+        next_node do
+          outcome :which_float?
+        end
+      end
+
+      value_question :which_float?, parse: Float do
+        next_node do
+          outcome :outcome
+        end
+      end
+
+      outcome :outcome
+    end
+  end
+end
