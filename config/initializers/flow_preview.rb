4f595fd61b8d102d57c32bb9ba5ecb257afc68a4
James Mead <james@floehopper.org>
2016-07-26 17:26:05 +0100

    Fix commented out code in flow_preview initializer
    This commented out code is intended to allow a developer to use the app to view
    fixture flows. Since the code is commented out, inevitably it has become
    out-of-date. I'm not totally convinced it's worth keeping this hanging around,
    but in the meantime I thought it was worth fixing it.

--- config/initializers/flow_preview.rb
+++ config/initializers/flow_preview.rb
@@ -8,4 +8,4 @@ end
 
 # Uncomment the following to run smartanswers with the test flows instead of the real ones
 #
-#FLOW_REGISTRY_OPTIONS[:load_path] = Rails.root.join('test', 'fixtures', 'flows')
+#FLOW_REGISTRY_OPTIONS[:smart_answer_load_path] = Rails.root.join('test', 'fixtures', 'smart_answer_flows')
