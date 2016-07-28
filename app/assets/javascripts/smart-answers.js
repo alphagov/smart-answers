524fe5a86b4b1b6b6f204dbca5e7a94a8bf0c1c0
James Mead <james@floehopper.org>
2016-07-28 13:23:52 +0100

    Add function to annotate content with link to template on Github
    This JavaScript relies on the `debug-template-path` data attributes made
    available in #2667. This idea is to call the new function from a bookmarklet or
    the GOV.UK Chrome extension.
    
    I wanted to add the new function in a separate file, but I couldn't get this to
    work. So for the moment I've added it in the existing `smart-answers.js` file.
    
    Ideally I would've added some unit tests around the function. However, we don't
    have any such tests at the moment, so it would be a fair bit of work.
    
    Today is my last day working on Smart Answers, I wanted to get this code live so
    the Content Designers could start using it. Since it's not critical
    functionality I think not having tests is OK for the moment.

--- app/assets/javascripts/smart-answers.js
+++ app/assets/javascripts/smart-answers.js
@@ -171,3 +171,20 @@ $(document).ready(function() {
   contentPosition.init();
 
 });
+
+function linkToTemplatesOnGithub() {
+  $('*[data-debug-template-path]').each(function() {
+    var element = $(this);
+    var path = element.data('debug-template-path');
+    var filename = path.split('/').pop();
+    var host = 'https://github.com';
+    var organisation = 'alphagov';
+    var repository = 'smart-answers'
+    var branch = 'deployed-to-production';
+    var url = [host, organisation, repository, 'blob', branch, path].join('/');
+    var anchor = $('<a>Template on GitHub</a>').attr('href', url).attr('style', 'color: deeppink;').attr('title', filename);
+    element.prepend(anchor);
+    element.attr('style', 'border: 3px solid deeppink; padding: 10px; margin: 3px');
+    element.removeAttr('data-debug-template-path');
+  });
+};
