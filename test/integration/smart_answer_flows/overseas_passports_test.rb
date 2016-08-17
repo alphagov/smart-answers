8faca0fe25bcbfb85ba716f7163d62298a09d974
Pabberc <pablo.cabello@digital.cabinet-office.gov.uk>
2016-08-17 17:30:18 +0100

    Amendment to Durham Passport Office address
    The address of the Durham Passport Office has changed, so I've amended the _sending_your_application.govspeak.erb for <% when 'durham' %>
    
    Needs to go live on 06/09/2016.

--- test/integration/smart_answer_flows/overseas_passports_test.rb
+++ test/integration/smart_answer_flows/overseas_passports_test.rb
@@ -100,7 +100,7 @@ class OverseasPassportsTest < ActiveSupport::TestCase
           end
           should "give the result and be done" do
             assert_current_node :ips_application_result
-            assert_match(/Millburngate House/, outcome_body)
+            assert_match(/Freeman’s Reach/, outcome_body)
           end
         end
       end
@@ -395,7 +395,7 @@ class OverseasPassportsTest < ActiveSupport::TestCase
       add_response 'adult'
       add_response 'united-kingdom'
       assert_current_node :ips_application_result
-      assert_match(/Millburngate House/, outcome_body)
+      assert_match(/Freeman’s Reach/, outcome_body)
     end
   end # Jordan (IPS1 with custom phrases)
 
@@ -473,7 +473,7 @@ class OverseasPassportsTest < ActiveSupport::TestCase
       add_response 'renewing_new'
       add_response 'adult'
       assert_current_node :ips_application_result
-      assert_match(/Millburngate House/, outcome_body)
+      assert_match(/Freeman’s Reach/, outcome_body)
     end
   end # Cameroon (custom phrases)
 
@@ -484,7 +484,7 @@ class OverseasPassportsTest < ActiveSupport::TestCase
       add_response 'adult'
       add_response 'united-kingdom'
       assert_current_node :ips_application_result
-      assert_match(/Millburngate House/, outcome_body)
+      assert_match(/Freeman’s Reach/, outcome_body)
     end
   end # Kenya (custom phrases)
 
@@ -495,7 +495,7 @@ class OverseasPassportsTest < ActiveSupport::TestCase
       add_response 'adult'
       add_response 'united-kingdom'
       assert_current_node :ips_application_result
-      assert_match(/Millburngate House/, outcome_body)
+      assert_match(/Freeman’s Reach/, outcome_body)
     end
   end # Kenya (custom phrases)
 
@@ -562,7 +562,7 @@ class OverseasPassportsTest < ActiveSupport::TestCase
       add_response 'replacing'
       add_response 'adult'
       assert_current_node :ips_application_result
-      assert_match(/Millburngate House/, outcome_body)
+      assert_match(/Freeman’s Reach/, outcome_body)
     end
   end # Jamaica
 
