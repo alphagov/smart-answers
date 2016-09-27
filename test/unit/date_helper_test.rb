88ddbe46eaa101666e77e1531d3317e5e99e5bdd
Pablo Manrubia <pmanrubia@gmail.com>
2016-09-27 17:00:41 +0100

    Add function to retrieve the reference date for calculations
    Add a function that returns today’s date or the date from an environment 
    variable.
    
    We have created this function for minimum-wage-calculator to support the
    use of the RATES_QUERY_DATE environment variable. This variable was used
    in RatesQuery, but we also need to access to this value from the 
    calculator itself, so we have decided to expose this functionality in 
    its own helper function.

--- test/unit/date_helper_test.rb
+++ test/unit/date_helper_test.rb
@@ -10,5 +10,24 @@ module SmartAnswer
         assert_equal Date.parse("2012 Mar 03"), next_saturday(Date.parse("2012 March 01"))
       end
     end
+
+    context "current day" do
+      should "return today, if rates query date is not set" do
+        Timecop.freeze("2016-09-27") do
+          assert_equal Date.parse("2016-09-27"), SmartAnswer::DateHelper.current_day
+        end
+      end
+
+      context "rates query date is set" do
+        should "return the rates query date" do
+          ENV['RATES_QUERY_DATE'] = '2016-12-31'
+          assert_equal Date.parse('2016-12-31'), SmartAnswer::DateHelper.current_day
+        end
+
+        teardown do
+          ENV.delete 'RATES_QUERY_DATE'
+        end
+      end
+    end
   end
 end
