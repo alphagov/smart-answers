535257366f291e2d9ac1706d299e64a834957ddb
Ikenna Okpala <me@ikennaokpala.com>
2016-10-17 12:26:29 +0100

    Preparatory: Remove 2015-16 legacy strategy
    This commit shows the preparatory steps taken to remove 2015-16
    from the regression test configuration files and the student finance
    calculator tests, legacy strategy and implementation.
    
    In the integration tests 2015-2016 has been replaced with 2016-2017
    option. Unit tests that tested the Legacy strategy have also been
    removed.
    
    These have been come necessary has the Legacy strategy is no longer
    required.
    
    The regression tests show that all the 2015-2016 artefacts have been
    deleted.
    
    This triggers the removal of 2015-16 the regression files, making way for
    the intended addition of 2017-2018 options.
    
    Already existing unit, regression and integration tests passed, giving
    assurance that the aforementioned commit/changes have had no
    (interfering) effect to other smart answers.

--- lib/smart_answer/calculators/student_finance_calculator.rb
+++ lib/smart_answer/calculators/student_finance_calculator.rb
@@ -2,11 +2,6 @@ module SmartAnswer
   module Calculators
     class StudentFinanceCalculator
       LOAN_MAXIMUMS = {
-        "2015-2016" => {
-          "at-home" => 4_565,
-          "away-outside-london" => 5_740,
-          "away-in-london" => 8_009
-        },
         "2016-2017" => {
           "at-home" => 6_904,
           "away-outside-london" => 8_200,
@@ -34,19 +29,12 @@ module SmartAnswer
 
     private
 
-      def legacy_scheme?
-        %w(2015-2016).include?(@course_start)
-      end
-
       def strategy
-        @strategy ||= begin
-          klass = legacy_scheme? ? LegacyStrategy : Strategy
-          klass.new(
-            course_start: @course_start,
-            household_income: @household_income,
-            residence: @residence
-          )
-        end
+        @strategy ||= Strategy.new(
+          course_start: @course_start,
+          household_income: @household_income,
+          residence: @residence
+        )
       end
 
       class Strategy
@@ -95,47 +83,6 @@ module SmartAnswer
 
           ratio = INCOME_PENALTY_RATIO[@course_start][@residence]
           ((@household_income - 25_000) / ratio).floor
-        end
-      end
-
-      class LegacyStrategy
-        def initialize(course_start:, household_income:, residence:)
-          @course_start = course_start
-          @household_income = household_income
-          @residence = residence
-        end
-
-        def maintenance_grant_amount
-          return Money.new('3387') if @household_income <= 25_000
-          return Money.new('0') if @household_income > 42_620
-          Money.new(3387 - grant_reduction_based_on_income)
-        end
-
-        def maintenance_loan_amount
-          if @household_income <= 42_875
-            Money.new(max_loan_amount - (maintenance_grant_amount.value * 0.5).floor)
-          else
-            reduced_loan_amount = max_loan_amount - loan_reduction_based_on_income
-            Money.new([reduced_loan_amount, min_loan_amount].max)
-          end
-        end
-
-      private
-
-        def loan_reduction_based_on_income
-          ((@household_income - 42_875) / 9.59).floor
-        end
-
-        def grant_reduction_based_on_income
-          ((@household_income - 25_000) / 5.28).floor
-        end
-
-        def min_loan_amount
-          (0.65 * max_loan_amount.value).floor
-        end
-
-        def max_loan_amount
-          Money.new(LOAN_MAXIMUMS[@course_start][@residence])
         end
       end
     end
