af9cf2775c13987e6b9f62565545d9a34b2ae694
Tijmen Brommet <tijmen@gmail.com>
2017-02-22 15:26:33 +0000

    Bump slimmer to use Rails' cache
    This bumps slimmer to make it use the rails cache and send a user agent:
    
    - https://github.com/alphagov/slimmer/pull/184
    - https://github.com/alphagov/slimmer/pull/183

--- config/initializers/slimmer.rb
+++ config/initializers/slimmer.rb
@@ -1,10 +1,6 @@
 SmartAnswers::Application.configure do
   config.slimmer.logger = Rails.logger
 
-  if Rails.env.production?
-    config.slimmer.use_cache = true
-  end
-
   if Rails.env.development?
     config.slimmer.asset_host = ENV["STATIC_DEV"] || Plek.new.find("static")
   end
