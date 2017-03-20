aec696780603f6d9c271988e7082db694f6921f3
Alec Gibson <aleckgibson@googlemail.com>
2017-03-20 17:33:44 +0000

    Set up Benchmarking We will be benchmarking user behaviour using mouseflow and the existing A/B testing framework. This adds temporary code for responding to the A/B test, setting up the appropriate response and injecting the required mouseflow JavaScript onto the page.

---
+++ app/assets/javascripts/mouseflow.js
@@ -0,0 +1,9 @@
+var _mfq = _mfq || [];
+
+(function() {
+  var mf = document.createElement("script");
+  mf.type = "text/javascript";
+  mf.async = true;
+  mf.src = "//cdn.mouseflow.com/projects/807ab5b2-3fe4-4b3b-b4d7-e83b8f6e71fe.js";
+  document.getElementsByTagName("head")[0].appendChild(mf);
+})();
