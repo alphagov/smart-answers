9fa74c8cde84c8ffa180e7924eae648ac722e0dc
Tatiana Stantonian <tatiana.stantonian@digital.cabinet-office.gov.uk>
2017-06-20 17:58:15 +0100

    Add StartPageLessFlowContentItem
    This will allow to publish flow content items with a different content-id to
    their start pages. This also provides a new content-id for the
    "part-year-profit-tax-credits" smart answer, which is the first one we'll be
    moving the start page (aka landing page) to Publisher for.
    
    Omitting the "public_updated_at" in the payload so Publishing API will use the
    publish time of the edition instead, and not update its value at each deploy.
    More information in [this
    commit](https://github.com/alphagov/calculators/pull/185/commits/e4d06b5e7619e625f34a9bb515e335d5b7ada9e6).

--- app/services/content_item_publisher.rb
+++ app/services/content_item_publisher.rb
@@ -1,7 +1,6 @@
 class ContentItemPublisher
   def publish(flow_presenters)
     flow_presenters.each do |smart_answer|
-      content_item = FlowContentItem.new(smart_answer)
       Services.publishing_api.put_content(content_item.content_id, content_item.payload)
       Services.publishing_api.publish(content_item.content_id, 'minor')
     end
