43286505d7e0628c2be04862caabfdffa1ea430d
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2017-06-26 12:17:49 +0100

    Add --live flag to allow easy local development

--- startup.sh
+++ startup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 bundle install
-bundle exec rails s -p 3010
+
+if [[ $1 == "--live" ]] ; then
+  GOVUK_APP_DOMAIN=www.gov.uk \
+  GOVUK_WEBSITE_ROOT=https://www.gov.uk \
+  PLEK_SERVICE_WHITEHALL_ADMIN_URI=${PLEK_SERVICE_WHITEHALL_ADMIN_URI-https://www.gov.uk} \
+  PLEK_SERVICE_CONTENT_STORE_URI=${PLEK_SERVICE_CONTENT_STORE_URI-https://www.gov.uk/api} \
+  PLEK_SERVICE_STATIC_URI=${PLEK_SERVICE_STATIC_URI-assets.publishing.service.gov.uk} \
+  bundle exec rails s -p 3010
+else
+  bundle exec rails s -p 3010
+fi
