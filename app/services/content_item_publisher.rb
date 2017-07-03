a892cd3f73c3d9c11da21b345f8bb6d079c5e4d9
Chris Roos <chris@seagul.co.uk>
2017-07-03 12:47:52 +0100

    Rename var to make way for start page presenter

--- app/services/content_item_publisher.rb
+++ app/services/content_item_publisher.rb
@@ -1,9 +1,9 @@
 class ContentItemPublisher
   def publish(flow_presenters)
     flow_presenters.each do |smart_answer|
-      content_item = FlowContentItem.new(smart_answer)
-      Services.publishing_api.put_content(content_item.content_id, content_item.payload)
-      Services.publishing_api.publish(content_item.content_id, 'minor')
+      flow_content_item = FlowContentItem.new(smart_answer)
+      Services.publishing_api.put_content(flow_content_item.content_id, flow_content_item.payload)
+      Services.publishing_api.publish(flow_content_item.content_id, 'minor')
     end
   end
 
