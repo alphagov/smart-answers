3e35fcb0c603c5d6a5b5b59cee47f2ba0682a0f8
Chris Roos <chris@seagul.co.uk>
2017-07-04 19:45:07 +0100

    Improve tests for FlowContentItem
    Using a stub instead of an instead of `FlowRegistrationPresenter` should
    make it easier to add tests for the currently untested behaviour of
    FlowContentItem. I want to add tests as I'm planning to duplicate this
    class as a new StartPageContentItem that will represent the mapping
    between a Smart Answer start page and the Publishing API.

--- test/unit/flow_content_item_test.rb
+++ test/unit/flow_content_item_test.rb
@@ -10,30 +10,38 @@ module SmartAnswer
     end
 
     test '#payload returns a valid content-item' do
-      presenter = FlowRegistrationPresenter.new(stub('flow', name: 'bridge-of-death', content_id: '3e6f33b8-0723-4dd5-94a2-cab06f23a685', external_related_links: nil))
+      presenter = stub('flow-registration-presenter',
+        slug: 'flow-slug',
+        title: 'flow-title',
+        external_related_links: []
+      )
       content_item = FlowContentItem.new(presenter)
 
-      payload = content_item.payload
-
-      assert_valid_against_schema(payload, 'generic_with_external_related_links')
+      assert_valid_against_schema(content_item.payload, 'generic_with_external_related_links')
     end
 
     test '#payload returns a valid content-item with external related links' do
-      presenter = FlowRegistrationPresenter.new(stub('flow', name: 'bridge-of-death', content_id: '3e6f33b8-0723-4dd5-94a2-cab06f23a685', external_related_links: [{ title: "hmrc", url: "https://hmrc.gov.uk" }]))
+      presenter = stub('flow-registration-presenter',
+        slug: 'flow-slug',
+        title: 'flow-title',
+        external_related_links: [
+          { title: "hmrc", url: "https://hmrc.gov.uk" }
+        ]
+      )
       content_item = FlowContentItem.new(presenter)
 
-      payload = content_item.payload
-
-      assert_valid_against_schema(payload, 'generic_with_external_related_links')
+      assert_valid_against_schema(content_item.payload, 'generic_with_external_related_links')
     end
 
-    test '#base_path is the name of the flow' do
-      presenter = FlowRegistrationPresenter.new(stub('flow', name: 'bridge-of-death', content_id: '25b98dfb-fabe-4b16-b587-072c8233f6bc', external_related_links: nil))
+    test '#base_path is the slug of the presenter with a prepended slash' do
+      presenter = stub('flow-registration-presenter',
+        slug: 'flow-slug',
+        title: 'flow-title',
+        external_related_links: []
+      )
       content_item = FlowContentItem.new(presenter)
 
-      base_path = content_item.payload[:base_path]
-
-      assert_equal "/bridge-of-death", base_path
+      assert_equal "/flow-slug", content_item.payload[:base_path]
     end
   end
 end
