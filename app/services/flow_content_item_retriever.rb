ac4fee656dca864753efec7b7c7b814494ce0d7f
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-06-19 08:45:25 +0100

    Rename ContentItemRetriever to FlowContentItemRetriever
    Since this better represents what it does.

--- app/services/flow_content_item_retriever.rb
+++ app/services/flow_content_item_retriever.rb
@@ -1,8 +1,7 @@
-class ContentItemRetriever
+class FlowContentItemRetriever
   def self.fetch(slug)
     Services.content_store.content_item("/#{slug}/y")
       .to_hash.with_indifferent_access
-
   rescue GdsApi::HTTPNotFound, GdsApi::HTTPGone
     {}
   end
