1072db038fcf9c22ab392169d7f01ac307f7ab3c
Jonathan Kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2018-12-21 10:54:34 +0000

    Fixes tests and makes variables more dynamic
    This commit changes the way variables are passed through each context block. We’ve done this because they were previously quite brittle and had a number of hard-coded values that would have to be changed every leap year. Now the number of days in a year is dynamically calculated by the number_of_days method of the YearRange class.
    
    Co-authored-by: Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>

--- test/integration/calculators/part_year_profit_tax_credits_calculator_test.rb
+++ test/integration/calculators/part_year_profit_tax_credits_calculator_test.rb
@@ -6,42 +6,43 @@ module SmartAnswer
       setup do
         @calculator = Calculators::PartYearProfitTaxCreditsCalculator.new
 
-        @calculator.tax_credits_award_ends_on  = Date.parse('2015-08-01')
+        @calculator.tax_credits_award_ends_on  = Date.parse('2018-08-01')
         @calculator.accounts_end_month_and_day = Date.parse('0000-04-05')
         @calculator.taxable_profit             = SmartAnswer::Money.new(10_000)
+
+        @expected_accounting_year = YearRange.new(
+          begins_on: Date.parse('2018-04-06')
+          )
+        @expected_profit_per_day = (
+          SmartAnswer::Money.new(10_000) / @expected_accounting_year.number_of_days
+          ).floor(2)
+        @expected_award_period = DateRange.new(
+          begins_on: Date.parse('2018-04-06'),
+          ends_on:   Date.parse('2018-08-01')
+          )
       end
 
       should "use the 2015/16 tax year" do
-        expected_tax_year = YearRange.tax_year.starting_in(2015)
+        expected_tax_year = YearRange.tax_year.starting_in(2018)
         assert_equal expected_tax_year, @calculator.tax_year
       end
 
-      should "use the accounting period from 6th Apr 2015 to 5th Apr 2016" do
-        expected_accounting_year = YearRange.new(
-          begins_on: Date.parse('2015-04-06')
-        )
-        assert_equal expected_accounting_year, @calculator.accounting_year
-        assert_equal 366, @calculator.accounting_year.number_of_days
+      should "use the accounting period from 6th Apr 2018 to 5th Apr 2019" do
+        assert_equal @expected_accounting_year, @calculator.accounting_year
+        assert_equal @expected_accounting_year.number_of_days, @calculator.accounting_year.number_of_days
       end
 
       should "have an award period from the start of the tax year to the date the tax credits award end" do
-        expected_award_period = DateRange.new(
-          begins_on: Date.parse('2015-04-06'),
-          ends_on:   Date.parse('2015-08-01')
-        )
-        assert_equal expected_award_period, @calculator.award_period
-        assert_equal 118, @calculator.award_period.number_of_days
+        assert_equal @expected_award_period, @calculator.award_period
+        assert_equal @expected_award_period.number_of_days, @calculator.award_period.number_of_days
       end
 
       should "calculate the profit per day" do
-        expected_profit_per_day = (SmartAnswer::Money.new(10_000) / 366).floor(2)
-        assert_equal 27.32, expected_profit_per_day
-        assert_equal expected_profit_per_day, @calculator.profit_per_day
+        assert_equal @expected_profit_per_day, @calculator.profit_per_day
       end
 
       should "calculate taxable profit for the award period" do
-        expected_taxable_profit = (27.32 * 118).floor
-        assert_equal 3223, expected_taxable_profit
+        expected_taxable_profit = (@expected_profit_per_day * @expected_award_period.number_of_days).floor
         assert_equal expected_taxable_profit, @calculator.award_period_taxable_profit
       end
     end
@@ -53,6 +54,18 @@ module SmartAnswer
         @calculator.tax_credits_award_ends_on  = Date.parse('2015-08-01')
         @calculator.accounts_end_month_and_day = Date.parse('0000-12-31')
         @calculator.taxable_profit             = SmartAnswer::Money.new(10_000)
+
+        @expected_accounting_year = YearRange.new(
+          begins_on: Date.parse('2015-01-01')
+        )
+        @expected_award_period = DateRange.new(
+          begins_on: Date.parse('2015-04-06'),
+          ends_on:   Date.parse('2015-08-01')
+        )
+
+        @expected_profit_per_day = (
+          SmartAnswer::Money.new(10_000) / @expected_accounting_year.number_of_days
+        ).floor(2)
       end
 
       should "use the 2015/16 tax year" do
@@ -61,31 +74,21 @@ module SmartAnswer
       end
 
       should "use the accounting period from 1st Jan to 31st Dec 2015" do
-        expected_accounting_year = YearRange.new(
-          begins_on: Date.parse('2015-01-01')
-        )
-        assert_equal expected_accounting_year, @calculator.accounting_year
-        assert_equal 365, @calculator.accounting_year.number_of_days
+        assert_equal @expected_accounting_year, @calculator.accounting_year
+        assert_equal @expected_accounting_year.number_of_days, @calculator.accounting_year.number_of_days
       end
 
       should "have an award period from the start of the tax year to the date the tax credits award end" do
-        expected_award_period = DateRange.new(
-          begins_on: Date.parse('2015-04-06'),
-          ends_on:   Date.parse('2015-08-01')
-        )
-        assert_equal expected_award_period, @calculator.award_period
-        assert_equal 118, @calculator.award_period.number_of_days
+        assert_equal @expected_award_period, @calculator.award_period
+        assert_equal @expected_award_period.number_of_days, @calculator.award_period.number_of_days
       end
 
       should "calculate the profit per day" do
-        expected_profit_per_day = (SmartAnswer::Money.new(10_000) / 365).floor(2)
-        assert_equal 27.39, expected_profit_per_day
-        assert_equal expected_profit_per_day, @calculator.profit_per_day
+        assert_equal @expected_profit_per_day, @calculator.profit_per_day
       end
 
       should "calculate taxable profit for the award period" do
-        expected_taxable_profit = (27.39 * 118).floor
-        assert_equal 3232, expected_taxable_profit
+        expected_taxable_profit = (@expected_profit_per_day * @expected_award_period.number_of_days).floor
         assert_equal expected_taxable_profit, @calculator.award_period_taxable_profit
       end
     end
@@ -98,6 +101,14 @@ module SmartAnswer
         @calculator.stopped_trading_on         = Date.parse('2015-07-01')
         @calculator.accounts_end_month_and_day = Date.parse('0000-04-05')
         @calculator.taxable_profit             = SmartAnswer::Money.new(10_000)
+        @expected_basis_period                 = DateRange.new(
+          begins_on: Date.parse('2015-04-06'),
+          ends_on:   Date.parse('2015-07-01')
+        )
+        @expected_award_period = DateRange.new(
+          begins_on: Date.parse('2015-04-06'),
+          ends_on:   Date.parse('2015-07-01')
+        )
       end
 
       should "use the 2015/16 tax year" do
@@ -106,21 +117,13 @@ module SmartAnswer
       end
 
       should "use the basis period from start of tax year to stopped trading date" do
-        expected_basis_period = DateRange.new(
-          begins_on: Date.parse('2015-04-06'),
-          ends_on:   Date.parse('2015-07-01')
-        )
-        assert_equal expected_basis_period, @calculator.basis_period
-        assert_equal 87, @calculator.basis_period.number_of_days
+        assert_equal @expected_basis_period, @calculator.basis_period
+        assert_equal @expected_basis_period.number_of_days, @calculator.basis_period.number_of_days
       end
 
       should "have an award period from the start of the tax year to the stopped trading date" do
-        expected_award_period = DateRange.new(
-          begins_on: Date.parse('2015-04-06'),
-          ends_on:   Date.parse('2015-07-01')
-        )
-        assert_equal expected_award_period, @calculator.award_period
-        assert_equal 87, @calculator.award_period.number_of_days
+        assert_equal @expected_award_period, @calculator.award_period
+        assert_equal @expected_award_period.number_of_days, @calculator.award_period.number_of_days
       end
 
       should "return the taxable profit figure entered" do
@@ -137,6 +140,18 @@ module SmartAnswer
         @calculator.stopped_trading_on         = Date.parse('2015-07-01')
         @calculator.accounts_end_month_and_day = Date.parse('0000-05-31')
         @calculator.taxable_profit             = SmartAnswer::Money.new(10_000)
+
+        @expected_basis_period = DateRange.new(
+          begins_on: Date.parse('2014-06-01'),
+          ends_on:   Date.parse('2015-07-01')
+        )
+
+        @expected_award_period = DateRange.new(
+          begins_on: Date.parse('2015-04-06'),
+          ends_on:   Date.parse('2015-07-01')
+        )
+
+        @expected_profit_per_day = (SmartAnswer::Money.new(10_000) / 396).floor(2)
       end
 
       should "use the 2015/16 tax year" do
@@ -152,32 +167,21 @@ module SmartAnswer
       end
 
       should "use a combination of the current accounting period and the next accounting period truncated by stopped trading date as the basis period" do
-        expected_basis_period = DateRange.new(
-          begins_on: Date.parse('2014-06-01'),
-          ends_on:   Date.parse('2015-07-01')
-        )
-        assert_equal expected_basis_period, @calculator.basis_period
-        assert_equal 396, @calculator.basis_period.number_of_days
+        assert_equal @expected_basis_period, @calculator.basis_period
+        assert_equal @expected_basis_period.number_of_days, @calculator.basis_period.number_of_days
       end
 
       should "have an award period from the start of the tax year to the stopped trading date" do
-        expected_award_period = DateRange.new(
-          begins_on: Date.parse('2015-04-06'),
-          ends_on:   Date.parse('2015-07-01')
-        )
-        assert_equal expected_award_period, @calculator.award_period
-        assert_equal 87, @calculator.award_period.number_of_days
+        assert_equal @expected_award_period, @calculator.award_period
+        assert_equal @expected_award_period.number_of_days, @calculator.award_period.number_of_days
       end
 
       should "calculate the profit per day" do
-        expected_profit_per_day = (SmartAnswer::Money.new(10_000) / 396).floor(2)
-        assert_equal 25.25, expected_profit_per_day
-        assert_equal expected_profit_per_day, @calculator.profit_per_day
+        assert_equal @expected_profit_per_day, @calculator.profit_per_day
       end
 
       should "calculate taxable profit for the award period" do
-        expected_taxable_profit = (25.25 * 87).floor
-        assert_equal 2196, expected_taxable_profit
+        expected_taxable_profit = (@expected_profit_per_day * @expected_award_period.number_of_days).floor
         assert_equal expected_taxable_profit, @calculator.award_period_taxable_profit
       end
     end
@@ -190,6 +194,20 @@ module SmartAnswer
         @calculator.stopped_trading_on         = Date.parse('2015-07-01')
         @calculator.accounts_end_month_and_day = Date.parse('0000-09-30')
         @calculator.taxable_profit             = SmartAnswer::Money.new(10_000)
+
+        @expected_accounting_year = YearRange.new(
+          begins_on: Date.parse('2014-10-01')
+        )
+        @expected_basis_period = DateRange.new(
+          begins_on: Date.parse('2014-10-01'),
+          ends_on:   Date.parse('2015-07-01')
+        )
+        @expected_award_period = DateRange.new(
+          begins_on: Date.parse('2015-04-06'),
+          ends_on:   Date.parse('2015-07-01')
+        )
+        @expected_profit_per_day = (
+          SmartAnswer::Money.new(10_000) / @expected_basis_period.number_of_days).floor(2)
       end
 
       should "use the 2015/16 tax year" do
@@ -198,39 +216,25 @@ module SmartAnswer
       end
 
       should "use the accounting period that ends in the 2015/16 tax year" do
-        expected_accounting_year = YearRange.new(
-          begins_on: Date.parse('2014-10-01')
-        )
-        assert_equal expected_accounting_year, @calculator.accounting_year
+        assert_equal @expected_accounting_year, @calculator.accounting_year
       end
 
       should "use the accounting period to the stopped trading date as the basis period" do
-        expected_basis_period = DateRange.new(
-          begins_on: Date.parse('2014-10-01'),
-          ends_on:   Date.parse('2015-07-01')
-        )
-        assert_equal expected_basis_period, @calculator.basis_period
-        assert_equal 274, @calculator.basis_period.number_of_days
+        assert_equal @expected_basis_period, @calculator.basis_period
+        assert_equal @expected_basis_period.number_of_days, @calculator.basis_period.number_of_days
       end
 
       should "have an award period from the start of the tax year to the stopped trading date" do
-        expected_award_period = DateRange.new(
-          begins_on: Date.parse('2015-04-06'),
-          ends_on:   Date.parse('2015-07-01')
-        )
-        assert_equal expected_award_period, @calculator.award_period
-        assert_equal 87, @calculator.award_period.number_of_days
+        assert_equal @expected_award_period, @calculator.award_period
+        assert_equal @expected_award_period.number_of_days, @calculator.award_period.number_of_days
       end
 
       should "calculate the profit per day" do
-        expected_profit_per_day = (SmartAnswer::Money.new(10_000) / 274).floor(2)
-        assert_equal 36.49, expected_profit_per_day
-        assert_equal expected_profit_per_day, @calculator.profit_per_day
+        assert_equal @expected_profit_per_day, @calculator.profit_per_day
       end
 
       should "calculate taxable profit for the award period" do
-        expected_taxable_profit = (36.49 * 87).floor
-        assert_equal 3174, expected_taxable_profit
+        expected_taxable_profit = (@expected_profit_per_day * @expected_award_period.number_of_days).floor
         assert_equal expected_taxable_profit, @calculator.award_period_taxable_profit
       end
     end
