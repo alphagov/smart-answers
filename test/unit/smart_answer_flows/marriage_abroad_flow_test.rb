c769170ded8e4e3d98d4be055eacb90732270c6d
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2019-04-01 10:51:21 +0100

    Set the calculator country for a marriage abroad test
    Not having the ceremony country set will now prevent this path from completing any further since a "next step" is not set after the `partner_opposite_or_same_sex?` question unless `calculator.has_outcome_per_path?` is true (which requires a set country).

--- test/unit/smart_answer_flows/marriage_abroad_flow_test.rb
+++ test/unit/smart_answer_flows/marriage_abroad_flow_test.rb
@@ -80,6 +80,7 @@ module SmartAnswer
     context 'when answering partner_opposite_or_same_sex? question' do
       setup do
         Calculators::MarriageAbroadCalculator.stubs(:new).returns(@calculator)
+        @calculator.ceremony_country = "france"
         setup_states_for_question(:partner_opposite_or_same_sex?,
           responding_with: 'same_sex', initial_state: {
             calculator: @calculator })
