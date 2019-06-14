73b8872621268d3d6f67877f580df546d22a483f
John-Paul Dickie <9640287+johnpauldickie@users.noreply.github.com>
2019-06-14 09:45:03 +0100

    Re-adding 2012 option

--- lib/smart_answer_flows/shared/minimum_wage_flow.rb
+++ lib/smart_answer_flows/shared/minimum_wage_flow.rb
@@ -11,6 +11,8 @@ module SmartAnswer
           option "2015-10-01"
           option "2014-10-01"
           option "2013-10-01"
+          option "2013-10-01"
+          option "2012-10-01"
 
           next_node do |response|
             calculator.date = Date.parse(response)
