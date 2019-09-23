8dfc5b9e1c93abc578ff5ebe44e91c37b17f5691
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2019-09-23 09:13:18 +0100

    Use select component for country select

--- test/integration/engine/country_and_date_questions_test.rb
+++ test/integration/engine/country_and_date_questions_test.rb
@@ -19,10 +19,8 @@ class CountryAndDateQuestionsTest < EngineIntegrationTest
     should "handle country and date questions" do
       visit "/country-and-date-sample/y"
 
-      within '.current-question' do
-        within '.govuk-fieldset__legend' do
-          assert_page_has_content "Which country do you live in?"
-        end
+      within ".current-question" do
+        assert_page_has_content "Which country do you live in?"
       end
       within ".question-body" do
         assert page.has_select?("response")
@@ -46,10 +44,8 @@ class CountryAndDateQuestionsTest < EngineIntegrationTest
         end
       end
 
-      within '.current-question' do
-        within '.govuk-fieldset__legend' do
-          assert_page_has_content "What date did you move there?"
-        end
+      within ".current-question" do
+        assert_page_has_content "What date did you move there?"
       end
 
       within ".question-body" do
@@ -86,10 +82,8 @@ class CountryAndDateQuestionsTest < EngineIntegrationTest
         end
       end
 
-      within '.current-question' do
-        within '.govuk-fieldset__legend' do
-          assert_page_has_content "Which country were you born in?"
-        end
+      within ".current-question" do
+        assert_page_has_content "Which country were you born in?"
       end
       within ".question-body" do
         assert page.has_select?("response")
