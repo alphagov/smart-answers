a2665f66b801c796156d9cb0794a3de35a4674d1
Issy Long <me@issyl0.co.uk>
2019-10-08 16:57:56 +0100

    Add ? to the end of methods that only return booleans
    - This way, we know at a glance what they return and it is [idiomatic
    Ruby](http://ruby-for-beginners.rubymonstas.org/objects/predicates.html).

--- lib/smart_answer/calculators/holiday_entitlement.rb
+++ lib/smart_answer/calculators/holiday_entitlement.rb
@@ -24,7 +24,7 @@ module SmartAnswer::Calculators
 
     def full_time_part_time_days
       days = STATUTORY_HOLIDAY_ENTITLEMENT_IN_WEEKS * days_per_week
-      actual_days = if left_before_year_end || (days_per_week < STANDARD_DAYS_PER_WEEK)
+      actual_days = if left_before_year_end? || (days_per_week < STANDARD_DAYS_PER_WEEK)
                       days
                     else
                       [MAXIMUM_STATUTORY_HOLIDAY_ENTITLEMENT_IN_DAYS, days].min
@@ -33,7 +33,7 @@ module SmartAnswer::Calculators
     end
 
     def rounded_full_time_part_time_days
-      if started_after_year_began || worked_full_year
+      if started_after_year_began? || worked_full_year?
         (full_time_part_time_days * 2).ceil / 2.00
       else
         full_time_part_time_days
@@ -51,7 +51,7 @@ module SmartAnswer::Calculators
     end
 
     def rounded_full_time_part_time_hours
-      if started_after_year_began
+      if started_after_year_began?
         (rounded_full_time_part_time_days * hours_per_week) / days_per_week
       else
         full_time_part_time_hours
@@ -65,11 +65,11 @@ module SmartAnswer::Calculators
     def fraction_of_year
       days_in_year = leave_year_range.leap? ? DAYS_PER_LEAP_YEAR : DAYS_PER_YEAR
 
-      if started_after_year_began
+      if started_after_year_began?
         months_worked / MONTHS_PER_YEAR
-      elsif worked_partial_year
+      elsif worked_partial_year?
         BigDecimal(leaving_date - start_date + 1.0, 10) / days_in_year
-      elsif left_before_year_end
+      elsif left_before_year_end?
         BigDecimal(leaving_date - leave_year_range.begins_on + 1, 10) / days_in_year
       else
         MONTHS_PER_YEAR / MONTHS_PER_YEAR
@@ -82,19 +82,19 @@ module SmartAnswer::Calculators
       leaving_date ? leaving_date.beginning_of_year : Date.today.beginning_of_year
     end
 
-    def worked_full_year
+    def worked_full_year?
       start_date.nil? && leaving_date.nil?
     end
 
-    def started_after_year_began
+    def started_after_year_began?
       start_date.present? && leaving_date.nil?
     end
 
-    def left_before_year_end
+    def left_before_year_end?
       start_date.nil? && leaving_date.present?
     end
 
-    def worked_partial_year
+    def worked_partial_year?
       start_date.present? && leaving_date.present?
     end
 
@@ -109,7 +109,7 @@ module SmartAnswer::Calculators
     end
 
     def date_calc
-      return leave_year_start_date if worked_full_year
+      return leave_year_start_date if worked_full_year?
 
       return leaving_date if leaving_date
 
