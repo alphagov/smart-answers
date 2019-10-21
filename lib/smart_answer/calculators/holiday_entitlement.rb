6f65ab3d4239809bb7414cbb07ca4aa1053e3645
Alan Gabbianelli <alan.gabbianelli@digital.cabinet-office.gov.uk>
2019-10-21 10:56:28 +0100

    Update logic for shifts, irregular-hours and annualised-hours calculations
    Add logic and tests for shifts work calculations
    
    -Added hours_per_shift, shifts_per_shift_pattern and days_per_shift_pattern to the constructor,
    this will be stored from user input from the flow.
    -Added shift_entitlement function to work out holiday entitlement for shift workers
    -Added shifts_per_week calculation as it is required by shift_entitlement
    
    Refactor entitlement for shifts calculator
    
    - Rename `STANDARD_DAYS_PER_WEEK` to `STANDARD_WORKING_DAYS_PER_WEEK`
    - Rename `days_per_week` to `working_days_per_week`
    - Remove `hours_per_shift` as it's not used anywhere
    - Use only one trailing decimal zero instead of two (2.0 vs 2.00)
    - Refactor `shift_entitlement` introducing `rounded_shift_entitlement`
      and removing `calculate_holiday`

--- lib/smart_answer/calculators/holiday_entitlement.rb
+++ lib/smart_answer/calculators/holiday_entitlement.rb
@@ -9,22 +9,32 @@ module SmartAnswer::Calculators
     MAXIMUM_STATUTORY_HOLIDAY_ENTITLEMENT_IN_DAYS = 28.to_d
     MONTHS_PER_YEAR = 12.to_d
     DAYS_PER_YEAR = 365.to_d
+    DAYS_PER_WEEK = 7.to_d
     DAYS_PER_LEAP_YEAR = 366.to_d
-    STANDARD_DAYS_PER_WEEK = 5.to_d
+    STANDARD_WORKING_DAYS_PER_WEEK = 5.to_d
 
-    attr_reader :days_per_week, :hours_per_week, :start_date, :leaving_date, :leave_year_start_date
+    attr_reader :working_days_per_week, :hours_per_week, :start_date, :leaving_date, :leave_year_start_date,
+                :shifts_per_shift_pattern, :days_per_shift_pattern
 
-    def initialize(days_per_week: 0, hours_per_week: 0, start_date: nil, leaving_date: nil, leave_year_start_date: nil)
-      @days_per_week = BigDecimal(days_per_week, 10)
+    def initialize(working_days_per_week: 0,
+                   hours_per_week: 0,
+                   start_date: nil,
+                   leaving_date: nil,
+                   leave_year_start_date: nil,
+                   shifts_per_shift_pattern: 0,
+                   days_per_shift_pattern: 0)
+      @working_days_per_week = BigDecimal(working_days_per_week, 10)
       @hours_per_week = BigDecimal(hours_per_week, 10)
       @start_date = start_date
       @leaving_date = leaving_date
       @leave_year_start_date = leave_year_start_date || calculate_leave_year_start_date
+      @shifts_per_shift_pattern = BigDecimal(shifts_per_shift_pattern, 10)
+      @days_per_shift_pattern = BigDecimal(days_per_shift_pattern, 10)
     end
 
     def full_time_part_time_days
-      days = STATUTORY_HOLIDAY_ENTITLEMENT_IN_WEEKS * days_per_week
-      actual_days = if left_before_year_end? || (days_per_week < STANDARD_DAYS_PER_WEEK)
+      days = STATUTORY_HOLIDAY_ENTITLEMENT_IN_WEEKS * working_days_per_week
+      actual_days = if left_before_year_end? || (working_days_per_week < STANDARD_WORKING_DAYS_PER_WEEK)
                       days
                     else
                       [MAXIMUM_STATUTORY_HOLIDAY_ENTITLEMENT_IN_DAYS, days].min
@@ -34,7 +44,7 @@ module SmartAnswer::Calculators
 
     def rounded_full_time_part_time_days
       if started_after_year_began? || worked_full_year?
-        (full_time_part_time_days * 2).ceil / 2.00
+        (full_time_part_time_days * 2).ceil / 2.0
       else
         full_time_part_time_days
       end
@@ -45,14 +55,14 @@ module SmartAnswer::Calculators
     end
 
     def full_time_part_time_hours
-      minimum_days_per_week = [days_per_week, MAXIMUM_STATUTORY_DAYS_PER_WEEK].min
+      minimum_days_per_week = [working_days_per_week, MAXIMUM_STATUTORY_DAYS_PER_WEEK].min
 
-      STATUTORY_HOLIDAY_ENTITLEMENT_IN_WEEKS * minimum_days_per_week * (hours_per_week / days_per_week)
+      STATUTORY_HOLIDAY_ENTITLEMENT_IN_WEEKS * minimum_days_per_week * (hours_per_week / working_days_per_week)
     end
 
     def rounded_full_time_part_time_hours
       if started_after_year_began?
-        (rounded_full_time_part_time_days * hours_per_week) / days_per_week
+        (rounded_full_time_part_time_days * hours_per_week) / working_days_per_week
       else
         full_time_part_time_hours
       end
@@ -78,6 +88,22 @@ module SmartAnswer::Calculators
       fraction_of_year * rounded_full_time_part_time_hours
     end
 
+    def full_time_part_time_weeks
+      if worked_full_year?
+        STATUTORY_HOLIDAY_ENTITLEMENT_IN_WEEKS
+      else
+        STATUTORY_HOLIDAY_ENTITLEMENT_IN_WEEKS * fraction_of_year
+      end
+    end
+
+    def formatted_full_time_part_time_weeks
+      if worked_full_year?
+        format_number(full_time_part_time_weeks)
+      else
+        format_number(full_time_part_time_weeks, 2)
+      end
+    end
+
     def fraction_of_year
       days_in_year = leave_year_range.leap? ? DAYS_PER_LEAP_YEAR : DAYS_PER_YEAR
 
@@ -92,7 +118,28 @@ module SmartAnswer::Calculators
       end
     end
 
+    def shift_entitlement
+      if left_before_year_end? || worked_partial_year?
+        format_number(rounded_shift_entitlement, 2)
+      elsif started_after_year_began?
+        format_number(rounded_shift_entitlement)
+      else
+        format_number(STATUTORY_HOLIDAY_ENTITLEMENT_IN_WEEKS * shifts_per_week)
+      end
+    end
+
+    def rounded_shift_entitlement
+      minimum_days_per_week = [shifts_per_week, MAXIMUM_STATUTORY_DAYS_PER_WEEK].min
+      pro_rated_shifts = STATUTORY_HOLIDAY_ENTITLEMENT_IN_WEEKS * fraction_of_year * minimum_days_per_week
+
+      started_after_year_began? ? (pro_rated_shifts * 2).ceil / 2.0 : pro_rated_shifts
+    end
+
   private
+
+    def shifts_per_week
+      (shifts_per_shift_pattern / days_per_shift_pattern * DAYS_PER_WEEK).round(10)
+    end
 
     def calculate_leave_year_start_date
       leaving_date ? leaving_date.beginning_of_year : Date.today.beginning_of_year
