4b2abfc3fb1f898e7507bc7697d4eb3b8c805eb8
Alan Gabbianelli <alan.gabbianelli@digital.cabinet-office.gov.uk>
2019-10-25 18:46:14 +0100

    Update days-worked-per-week "full-year" flow
    - Update wording in the outcome.
    - Show extra message in case of more than 5 working days per week
      regardless of the resulting days.
    - Edit flow to pass unchanged user input to the calculator. It's
      perfectly fine to pass a number of working days higher than 5 as the
      calculator itself will take care of the calculation, avoiding to
      return more than 28 statutory days.
    - Migrate tests to rspec and add some extra ones. The migration to rspec
      is just partial at this point because we still reference minitest
      helpers.
    
    Trello card: https://trello.com/c/Me3aeYlL/1363-8-update-the-days-worked-per-week-flow-for-the-holiday-entitlement-calculator

---
+++ spec/integration/smart_answer_flows/calculate_your_holiday_entitlement_spec.rb
@@ -0,0 +1,63 @@
+require_relative "../../../lib/smart_answer_flows/calculate-your-holiday-entitlement.rb"
+require_relative "../../../test/integration/smart_answer_flows/flow_test_helper.rb"
+
+RSpec.describe(SmartAnswer::CalculateYourHolidayEntitlementFlow) do
+  include(FlowTestHelper)
+
+  before do
+    setup_for_testing_flow(SmartAnswer::CalculateYourHolidayEntitlementFlow)
+  end
+
+  let(:calculator_class) { SmartAnswer::Calculators::HolidayEntitlement }
+  let(:calculator_instance) { calculator_class.new }
+  let(:some_value) { "some value" }
+
+  context "for days worked per week" do
+    before do
+      assert_current_node(:basis_of_calculation?)
+      add_response("days-worked-per-week")
+      assert_current_node(:calculation_period?)
+    end
+
+    valid_working_days_per_week = [1, 2, 3, 4, 5, 6, 7]
+    invalid_working_days_per_week = [0, 8]
+
+    context "when full leave year" do
+      before do
+        add_response("full-year")
+      end
+
+      context "with valid number of days worked per week" do
+        valid_working_days_per_week.each do |working_days_per_week|
+          it "calculates the outcome" do
+            assert_current_node(:how_many_days_per_week?)
+            add_response(working_days_per_week)
+            expect(calculator_class).to receive(:new)
+              .with(
+                working_days_per_week: working_days_per_week,
+                start_date: nil,
+                leaving_date: nil,
+                leave_year_start_date: nil
+              )
+              .and_return(calculator_instance)
+            expect(calculator_instance).to receive(:formatted_full_time_part_time_days)
+              .and_return(some_value)
+            assert_current_node(:days_per_week_done)
+            assert_state_variable(:holiday_entitlement_days, some_value)
+          end
+        end
+      end
+
+      context "with invalid number of days worked per week" do
+        invalid_working_days_per_week.each do |working_days_per_week|
+          it "raises an invalid response error" do
+            assert_current_node(:how_many_days_per_week?)
+            add_response(working_days_per_week)
+            expect(calculator_class).to_not receive(:new)
+            assert_current_node(:how_many_days_per_week?, error: SmartAnswer::InvalidResponse)
+          end
+        end
+      end
+    end
+  end
+end
