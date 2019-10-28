c9ffa3d6f9245c4b6934dafd2b543714e3952480
Alan Gabbianelli <alan.gabbianelli@digital.cabinet-office.gov.uk>
2019-10-28 17:34:26 +0000

    Update days-worked-per-week "starting-and-leaving" flow
    - Migrate tests to rspec and add some extra ones. The migration to rspec
      is just partial at this point because we still reference minitest
      helpers.
    
    Trello card: https://trello.com/c/Me3aeYlL/1363-8-update-the-days-worked-per-week-flow-for-the-holiday-entitlement-calculator

--- spec/integration/smart_answer_flows/calculate_your_holiday_entitlement_spec.rb
+++ spec/integration/smart_answer_flows/calculate_your_holiday_entitlement_spec.rb
@@ -157,5 +157,54 @@ RSpec.describe(SmartAnswer::CalculateYourHolidayEntitlementFlow) do
         end
       end
     end
+
+    context "when starting and leaving part-way through a leave year" do
+      before do
+        add_response("starting-and-leaving")
+      end
+
+      let(:start_date) { "2019-07-14" }
+      let(:leaving_date) { "2019-10-14" }
+
+      context "with valid number of days worked per week" do
+        valid_working_days_per_week.each do |working_days_per_week|
+          it "calculates the outcome" do
+            assert_current_node(:what_is_your_starting_date?)
+            add_response(start_date)
+            assert_current_node(:what_is_your_leaving_date?)
+            add_response(leaving_date)
+            assert_current_node(:how_many_days_per_week?)
+            add_response(working_days_per_week)
+            expect(calculator_class).to receive(:new)
+              .with(
+                working_days_per_week: working_days_per_week,
+                start_date: Date.parse(start_date),
+                leaving_date: Date.parse(leaving_date),
+                leave_year_start_date: nil
+              )
+              .and_return(calculator_instance)
+            expect(calculator_instance).to receive(:formatted_full_time_part_time_days)
+              .and_return(some_value)
+            assert_current_node(:days_per_week_done)
+            assert_state_variable(:holiday_entitlement_days, some_value)
+          end
+        end
+      end
+
+      context "with invalid number of days worked per week" do
+        invalid_working_days_per_week.each do |working_days_per_week|
+          it "calculates the outcome" do
+            assert_current_node(:what_is_your_starting_date?)
+            add_response(start_date)
+            assert_current_node(:what_is_your_leaving_date?)
+            add_response(leaving_date)
+            assert_current_node(:how_many_days_per_week?)
+            add_response(working_days_per_week)
+            expect(calculator_class).to_not receive(:new)
+            assert_current_node(:how_many_days_per_week?, error: SmartAnswer::InvalidResponse)
+          end
+        end
+      end
+    end
   end
 end
