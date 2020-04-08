4a32d62d37c8e3b3d6cc7d277abc82dd87e3eb8c
Kevin Dew <kevindew@me.com>
2020-04-08 16:26:22 +0100

    Remove unused statsd service
    No application code is calling this and statsd is provided by
    govuk-app-config.

--- lib/services.rb
+++ lib/services.rb
@@ -21,12 +21,4 @@ module Services
   def self.content_store=(new_content_store)
     @content_store = new_content_store
   end
-
-  def self.statsd
-    @statsd ||= begin
-      statsd_client = Statsd.new("localhost")
-      statsd_client.namespace = ENV["GOVUK_STATSD_PREFIX"].to_s
-      statsd_client
-    end
-  end
 end
