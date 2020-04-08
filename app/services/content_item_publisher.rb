73fcf378d5262c0b0a3d3984d757ef3e70258acf
Kevin Dew <kevindew@me.com>
2020-04-08 18:15:24 +0100

    Remove superfluous tasks to replace content
    These two tasks allow creating content items that are publishable by
    Mainstream Publisher. Rather than using these tasks a publisher should
    instead create new items in Mainstream Publisher.
    
    In this commit I've removed the documentation for these tasks with the
    intention of reworking some of this documentation in a later commit.

--- app/services/content_item_publisher.rb
+++ app/services/content_item_publisher.rb
@@ -56,106 +56,10 @@ class ContentItemPublisher
     )
   end
 
-  def publish_transaction(base_path, publishing_app:, title:, content:, link:)
-    raise "The base path isn't supplied" if base_path.blank?
-    raise "The publishing_app isn't supplied" if publishing_app.blank?
-    raise "The title isn't supplied" if title.blank?
-    raise "The content isn't supplied" if content.blank?
-    raise "The link isn't supplied" if link.blank?
-
-    publish_transaction_via_publishing_api(
-      base_path,
-      publishing_app: publishing_app,
-      title: title,
-      content: content,
-      link: link,
-    )
-  end
-
-  def publish_answer(base_path, publishing_app:, title:, content:)
-    raise "The base path isn't supplied" if base_path.blank?
-    raise "The publishing_app isn't supplied" if publishing_app.blank?
-    raise "The title isn't supplied" if title.blank?
-    raise "The content isn't supplied" if content.blank?
-
-    publish_answer_via_publishing_api(
-      base_path,
-      publishing_app: publishing_app,
-      title: title,
-      content: content,
-    )
-  end
-
 private
 
   def reserve_path_url(base_path)
     "#{Plek.new.find('publishing-api')}/paths/#{base_path}"
   end
 
-  def publish_answer_via_publishing_api(base_path, publishing_app:, title:, content:)
-    payload = {
-      base_path: base_path,
-      title: title,
-      document_type: :answer,
-      schema_name: :answer,
-      publishing_app: publishing_app,
-      rendering_app: :frontend,
-      update_type: :major,
-      locale: :en,
-      details: {
-        body: [
-          {
-            content: content,
-            content_type: "text/govspeak",
-          },
-        ],
-      },
-      routes: [
-        {
-          type: :exact,
-          path: base_path,
-        },
-      ],
-    }
-
-    create_and_publish_via_publishing_api(payload)
-  end
-
-  def publish_transaction_via_publishing_api(base_path, publishing_app:, title:, content:, link:)
-    payload = {
-      base_path: base_path,
-      title: title,
-      update_type: :major,
-      document_type: :transaction,
-      publishing_app: publishing_app,
-      rendering_app: :frontend,
-      locale: :en,
-      details: {
-        introductory_paragraph: [
-          {
-            content: content,
-            content_type: "text/govspeak",
-          },
-        ],
-        transaction_start_link: link,
-      },
-      routes: [
-        {
-          type: :exact,
-          path: base_path,
-        },
-      ],
-      schema_name: :transaction,
-    }
-
-    create_and_publish_via_publishing_api(payload)
-  end
-
-  def create_and_publish_via_publishing_api(payload)
-    content_id = SecureRandom.uuid
-    response = Services.publishing_api.put_content(content_id, payload)
-    raise "This content item has not been created" unless response.code == 200
-
-    Services.publishing_api.publish(content_id)
-  end
 end
