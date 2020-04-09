a3b8c4117ffd012e5f45f5d641ed02b2b2b247e4
Kelvin Gan <kelvin.gan@digital.cabinet-office.gov.uk>
2020-04-09 13:59:36 +0100

    Add calculator integration test
    For CoronavirusBusinessSupportCalculator.

--- test/integration/smart_answer_flows/coronavirus_business_support_test.rb
+++ test/integration/smart_answer_flows/coronavirus_business_support_test.rb
@@ -1,13 +1,13 @@
 require_relative "../../test_helper"
 require_relative "flow_test_helper"
 
-require "smart_answer_flows/coronavirus-business-support.rb"
+require "smart_answer_flows/business-coronavirus-support-finder.rb"
 
-class CoronaVirusBusinessSupportFlowTest < ActiveSupport::TestCase
+class BusinessCoronavirusSupportFinderFlowTest < ActiveSupport::TestCase
   include FlowTestHelper
 
   setup do
-    setup_for_testing_flow SmartAnswer::CoronavirusBusinessSupportFlow
+    setup_for_testing_flow SmartAnswer::BusinessCoronavirusSupportFinderFlow
   end
 
   context "business based in England" do
