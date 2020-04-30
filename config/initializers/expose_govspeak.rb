a564e8bb770ff8afe7b101cb659db8a3f953553d
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2020-04-30 10:03:13 +0100

    Remove ability to view un-rendered govspeak
    This feature was used for debugging by allowing a user to append .txt
    to a url to show the govspeak unrendered. This adds complexity to the
    view rendering logic with little value as a user can reference the
    source code instead.

--- config/initializers/expose_govspeak.rb
+++
@@ -1,5 +0,0 @@
-SmartAnswers::Application.config.expose_govspeak = if Rails.env.production?
-                                                     ENV["EXPOSE_GOVSPEAK"].present?
-                                                   else
-                                                     true
-                                                   end
