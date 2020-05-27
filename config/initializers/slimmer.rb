83848ddcac0e5ac99d2184861597b11ba114cfe1
Kevin Dew <kevindew@me.com>
2020-05-27 22:20:55 +0100

    Remove redundant slimmer configuration
    It's unlikely anyone uses the STATIC_DEV env var and the fallback is
    just the default option.

--- config/initializers/slimmer.rb
+++ config/initializers/slimmer.rb
@@ -1,7 +1,3 @@
 SmartAnswers::Application.configure do
   config.slimmer.logger = Rails.logger
-
-  if Rails.env.development?
-    config.slimmer.asset_host = ENV["STATIC_DEV"] || Plek.new.find("static")
-  end
 end
