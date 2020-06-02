f138b1a15a0d9f3c21331ae4a89dbe584671f19b
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2020-06-02 11:37:52 +0100

    updates

--- lib/smart_answer_flows/coronavirus-employee-risk-assessment.rb
+++ lib/smart_answer_flows/coronavirus-employee-risk-assessment.rb
@@ -6,22 +6,47 @@ module SmartAnswer
       flow_content_id "e1a58c2f-c609-4644-a631-d21dc57b8cd6"
       status :draft
 
-      multiple_choice :work_from_home? do
+      # Questions
+      multiple_choice :can_work_from_home? do
         option :yes
         option :maybe
         option :no
 
         on_response do |response|
-          self.calculator = Calculators::CoronavirusEmployeeRiskAssessmentCalculator.new
-          calculator.work_from_home = response
+          # We don't need to do any calculations right now
+          #self.calculator = Calculators::CoronavirusEmployeeRiskAssessmentCalculator.new
+          #calculator.can_work_from_home = response
         end
 
-        next_node do
-          outcome :work_from_home
+        next_node do |response|
+          case response
+            when "yes"
+              outcome :work_from_home
+            when "maybe"
+              outcome :maybe_work_from_home
+            when "no"
+              question :where_do_you_work?
+          end
         end
       end
 
-      outcome :results
+      multiple_choice :where_do_you_work? do
+        option :food_and_drink
+        option :salon_parlour
+        option :retail
+        option :car_showroom
+        option :outdoor_market
+        option :auction_house
+        option :other
+
+        next_node do |response|
+          # to be set 
+        end
+      end
+
+      # Outcomes
+      outcome :work_from_home
+      outcome :maybe_work_from_home
     end
   end
 end
