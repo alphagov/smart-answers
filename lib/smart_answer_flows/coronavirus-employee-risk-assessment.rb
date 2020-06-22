b4ca4df0802c9dc7b69e59d4a91f1c55c0e2643b
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2020-06-22 17:16:15 +0100

    Publish coronavirus employee risk assessment smart answer

--- lib/smart_answer_flows/coronavirus-employee-risk-assessment.rb
+++ lib/smart_answer_flows/coronavirus-employee-risk-assessment.rb
@@ -4,7 +4,7 @@ module SmartAnswer
       name "coronavirus-employee-risk-assessment"
       start_page_content_id "450fb30a-2e70-4f78-b3c0-8ed2fa276033"
       flow_content_id "e1a58c2f-c609-4644-a631-d21dc57b8cd6"
-      status :draft
+      status :published
 
       # Questions
       multiple_choice :can_work_from_home? do
