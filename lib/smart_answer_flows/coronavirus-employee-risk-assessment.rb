1a9f511d5fceeba864a824293bd3112e56938add
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2020-06-24 23:17:53 +0100

    change the flow to add new is your employer asking you to work question

--- lib/smart_answer_flows/coronavirus-employee-risk-assessment.rb
+++ lib/smart_answer_flows/coronavirus-employee-risk-assessment.rb
@@ -7,23 +7,6 @@ module SmartAnswer
       status :published
 
       # Questions
-      multiple_choice :can_work_from_home? do
-        option :yes
-        option :maybe
-        option :no
-
-        next_node do |response|
-          case response
-          when "yes"
-            outcome :work_from_home
-          when "maybe"
-            outcome :work_from_home_help
-          when "no"
-            question :where_do_you_work?
-          end
-        end
-      end
-
       multiple_choice :where_do_you_work? do
         option :food_and_drink
         option :salon_parlour
@@ -66,33 +49,10 @@ module SmartAnswer
             question :is_your_workplace_an_exception?
           when "auction_house"
             question :is_your_workplace_an_auction_house?
-          when
-            "salon_parlour",
-            "nightclubs_or_gambling",
-            "indoor_recreation",
-            "funfair",
-            "indoor_visitor_centres"
-            outcome :workplace_should_be_closed
+          when "other"
+            question :can_work_from_home?
           else
-            question :are_you_shielding?
-          end
-        end
-      end
-
-      multiple_choice :is_your_workplace_an_exception? do
-        option :yes
-        option :no
-
-        next_node do |response|
-          work_in_retail = calculator.where_do_you_work == "retail"
-          if response == "yes" && work_in_retail
-            outcome :workplace_should_be_closed
-          elsif response == "yes"
-            question :are_you_shielding?
-          elsif work_in_retail
-            outcome :go_back_to_work
-          else
-            outcome :workplace_should_be_closed
+            question :is_your_employer_asking_you_to_work?
           end
         end
       end
@@ -103,9 +63,56 @@ module SmartAnswer
 
         next_node do |response|
           if response == "yes"
-            question :are_you_shielding?
+            question :can_work_from_home?
           else
-            outcome :workplace_should_be_closed
+            question :is_your_employer_asking_you_to_work?
+          end
+        end
+      end
+
+      multiple_choice :is_your_workplace_an_exception? do
+        option :yes
+        option :no
+
+        next_node do |response|
+          work_in_retail = calculator.where_do_you_work == "retail"
+
+          if response == "yes" && work_in_retail
+            question :is_your_employer_asking_you_to_work?
+          elsif response == "yes" || (response == "no" && work_in_retail)
+            question :can_work_from_home?
+          else
+            question :is_your_employer_asking_you_to_work?
+          end
+        end
+      end
+
+      multiple_choice :is_your_employer_asking_you_to_work? do
+        option :yes
+        option :no
+
+        next_node do |response|
+          if response == "yes"
+            question :can_work_from_home?
+          else
+            outcome :you_should_not_be_going_to_work
+          end
+        end
+      end
+
+      multiple_choice :can_work_from_home? do
+        option :yes
+        option :maybe
+        option :no
+
+        next_node do |response|
+          case response
+          when "yes"
+            outcome :work_from_home
+          when "maybe"
+            outcome :work_from_home_help
+          when "no"
+            question :are_you_shielding?
           end
         end
       end
@@ -127,12 +134,12 @@ module SmartAnswer
         option :yes
         option :no
 
-        next_node do |response|
-          if response == "yes"
-            outcome :vulnerable_work_arrangements
-          else
-            question :do_you_live_with_someone_vulnerable?
-          end
+        on_response do |response|
+          calculator.are_you_vulnerable = response
+        end
+
+        next_node do
+          question :do_you_live_with_someone_vulnerable?
         end
       end
 
@@ -140,12 +147,12 @@ module SmartAnswer
         option :yes
         option :no
 
-        next_node do |response|
-          if response == "yes"
-            outcome :keep_your_household_safe
-          else
-            question :have_childcare_responsibility?
-          end
+        on_response do |response|
+          calculator.do_you_live_with_someone_vulnerable = response
+        end
+
+        next_node do
+          question :have_childcare_responsibility?
         end
       end
 
@@ -153,16 +160,17 @@ module SmartAnswer
         option :yes
         option :no
 
-        next_node do |response|
-          if response == "yes"
-            outcome :help_with_childcare
-          else
-            outcome :go_back_to_work
-          end
+        on_response do |response|
+          calculator.have_childcare_responsibility = response
+        end
+
+        next_node do
+          outcome :go_back_to_work
         end
       end
 
       # Outcomes
+      outcome :you_should_not_be_going_to_work
       outcome :work_from_home
       outcome :work_from_home_help
       outcome :workplace_should_be_closed
