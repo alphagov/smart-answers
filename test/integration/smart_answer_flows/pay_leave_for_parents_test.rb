c27a04f8bbd68eb537db6857878abac1d7f9aaf7
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2020-07-10 14:43:35 +0100

    Remove logic from pay leave for parents flow for old due dates
    The flow did handle due dates prior to 2015-04-05, I guess because the
    policy changed at this time. I believe this smart answer only needs to
    work going forward, so this complexity can be removed.
    
    As well as changing the flow, I've removed all outcomes that don't
    look to be reachable, and all tests that mention removed outcomes.

--- test/integration/smart_answer_flows/pay_leave_for_parents_test.rb
+++ test/integration/smart_answer_flows/pay_leave_for_parents_test.rb
@@ -186,96 +186,6 @@ class PayLeaveForParentsFlowTest < ActiveSupport::TestCase
       assert_current_node :outcome_mat_allowance_mat_leave
     end
 
-    should "Mother employee, passes continuity test, does not pass lower earnings test, passes earnings and employment test;Partner employee, passes continuity test, passes lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "employee" # employment_status_of_mother
-      add_response "employee" # employment_status_of_partner
-      add_response "yes" # mother_started_working_before_continuity_start_date
-      add_response "yes" # mother_still_working_on_continuity_end_date
-      add_response "no" # mother_earned_more_than_lower_earnings_limit
-      add_response "yes" # mother_worked_at_least_26_weeks
-      add_response "yes" # mother_earned_at_least_390
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "yes" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_allowance_mat_leave_pat_leave_pat_pay_additional_pat_leave_additional_pat_pay
-    end
-
-    should "Mother employee, passes continuity test, does not pass lower earnings test, passes earnings and employment test;Partner worker, passes continuity test, passes lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "employee" # employment_status_of_mother
-      add_response "worker" # employment_status_of_partner
-      add_response "yes" # mother_started_working_before_continuity_start_date
-      add_response "yes" # mother_still_working_on_continuity_end_date
-      add_response "no" # mother_earned_more_than_lower_earnings_limit
-      add_response "yes" # mother_worked_at_least_26_weeks
-      add_response "yes" # mother_earned_at_least_390
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "yes" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_allowance_mat_leave_pat_pay_additional_pat_pay
-    end
-
-    should "Mother self-employed, passes earnings and employment test;Partner employee, passes continuity test, passes lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "self-employed" # employment_status_of_mother
-      add_response "employee" # employment_status_of_partner
-      add_response "yes" # mother_worked_at_least_26_weeks
-      add_response "yes" # mother_earned_at_least_390
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "yes" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_allowance_pat_leave_pat_pay_additional_pat_leave_additional_pat_pay
-    end
-
-    should "Mother worker, does not pass continuity test, does not pass lower earnings test, passes earnings and employment test;Partner employee, passes continuity test, passes lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "worker" # employment_status_of_mother
-      add_response "employee" # employment_status_of_partner
-      add_response "no" # mother_started_working_before_continuity_start_date
-      add_response "no" # mother_still_working_on_continuity_end_date
-      add_response "no" # mother_earned_more_than_lower_earnings_limit
-      add_response "yes" # mother_worked_at_least_26_weeks
-      add_response "yes" # mother_earned_at_least_390
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "yes" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_allowance_pat_leave_pat_pay_additional_pat_leave_additional_pat_pay
-    end
-
-    should "Mother self-employed, passes earnings and employment test;Partner worker, passes continuity test, passes lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "self-employed" # employment_status_of_mother
-      add_response "worker" # employment_status_of_partner
-      add_response "yes" # mother_worked_at_least_26_weeks
-      add_response "yes" # mother_earned_at_least_390
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "yes" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_allowance_pat_pay_additional_pat_pay
-    end
-
-    should "Mother worker, does not pass continuity test, does not pass lower earnings test, passes earnings and employment test;Partner worker, passes continuity test, passes lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "worker" # employment_status_of_mother
-      add_response "worker" # employment_status_of_partner
-      add_response "no" # mother_started_working_before_continuity_start_date
-      add_response "no" # mother_still_working_on_continuity_end_date
-      add_response "no" # mother_earned_more_than_lower_earnings_limit
-      add_response "yes" # mother_worked_at_least_26_weeks
-      add_response "yes" # mother_earned_at_least_390
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "yes" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_allowance_pat_pay_additional_pat_pay
-    end
-
     should "Mother employee, passes continuity test, does not pass lower earnings test, does not pass earnings and employment test;Partner self-employed" do
       add_response "yes" # two_carers
       add_response "2014-1-1" # due_date
@@ -328,50 +238,6 @@ class PayLeaveForParentsFlowTest < ActiveSupport::TestCase
       add_response "yes" # partner_still_working_on_continuity_end_date
       add_response "no" # partner_earned_more_than_lower_earnings_limit
       assert_current_node :outcome_mat_leave_mat_pay_pat_leave_additional_pat_leave
-    end
-
-    should "Mother employee, passes continuity test, passes lower earnings test;Partner employee, passes continuity test, passes lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "employee" # employment_status_of_mother
-      add_response "employee" # employment_status_of_partner
-      add_response "yes" # mother_started_working_before_continuity_start_date
-      add_response "yes" # mother_still_working_on_continuity_end_date
-      add_response "yes" # mother_earned_more_than_lower_earnings_limit
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "yes" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_leave_mat_pay_pat_leave_pat_pay_additional_pat_leave_additional_pat_pay
-    end
-
-    should "Mother employee, passes continuity test, passes lower earnings test;Partner worker, passes continuity test, passes lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "employee" # employment_status_of_mother
-      add_response "worker" # employment_status_of_partner
-      add_response "yes" # mother_started_working_before_continuity_start_date
-      add_response "yes" # mother_still_working_on_continuity_end_date
-      add_response "yes" # mother_earned_more_than_lower_earnings_limit
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "yes" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_leave_mat_pay_pat_pay_additional_pat_pay
-    end
-
-    should "Mother employee, passes continuity test, does not pass lower earnings test, does not pass earnings and employment test;Partner employee, passes continuity test, passes lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "employee" # employment_status_of_mother
-      add_response "employee" # employment_status_of_partner
-      add_response "yes" # mother_started_working_before_continuity_start_date
-      add_response "yes" # mother_still_working_on_continuity_end_date
-      add_response "no" # mother_earned_more_than_lower_earnings_limit
-      add_response "no" # mother_worked_at_least_26_weeks
-      add_response "no" # mother_earned_at_least_390
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "yes" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_leave_pat_leave_pat_pay_additional_pat_leave
     end
 
     should "Mother employee, passes continuity test, does not pass lower earnings test, does not pass earnings and employment test;Partner worker, passes continuity test, passes lower earnings test" do
@@ -431,48 +297,6 @@ class PayLeaveForParentsFlowTest < ActiveSupport::TestCase
       assert_current_node :outcome_mat_pay
     end
 
-    should "Mother worker, passes continuity test, passes lower earnings test;Partner employee, passes continuity test, does not pass lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "worker" # employment_status_of_mother
-      add_response "employee" # employment_status_of_partner
-      add_response "yes" # mother_started_working_before_continuity_start_date
-      add_response "yes" # mother_still_working_on_continuity_end_date
-      add_response "yes" # mother_earned_more_than_lower_earnings_limit
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "no" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_pay_pat_leave_additional_pat_leave
-    end
-
-    should "Mother worker, passes continuity test, passes lower earnings test;Partner employee, passes continuity test, passes lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "worker" # employment_status_of_mother
-      add_response "employee" # employment_status_of_partner
-      add_response "yes" # mother_started_working_before_continuity_start_date
-      add_response "yes" # mother_still_working_on_continuity_end_date
-      add_response "yes" # mother_earned_more_than_lower_earnings_limit
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "yes" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_pay_pat_leave_pat_pay_additional_pat_leave_additional_pat_pay
-    end
-
-    should "Mother worker, passes continuity test, passes lower earnings test;Partner worker, passes continuity test, passes lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "worker" # employment_status_of_mother
-      add_response "worker" # employment_status_of_partner
-      add_response "yes" # mother_started_working_before_continuity_start_date
-      add_response "yes" # mother_still_working_on_continuity_end_date
-      add_response "yes" # mother_earned_more_than_lower_earnings_limit
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "yes" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_pay_pat_pay_additional_pat_pay
-    end
-
     should "Mother self-employed, does not pass earnings and employment test;Partner employee, passes continuity test, passes lower earnings test" do
       add_response "yes" # two_carers
       add_response "2014-1-1" # due_date
@@ -529,38 +353,6 @@ class PayLeaveForParentsFlowTest < ActiveSupport::TestCase
       add_response "yes" # partner_still_working_on_continuity_end_date
       add_response "yes" # partner_earned_more_than_lower_earnings_limit
       assert_current_node :outcome_pat_pay
-    end
-
-    should "Mother employee, does not pass continuity test, passes lower earnings test, still working;Partner employee, passes continuity test, passes lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "employee" # employment_status_of_mother
-      add_response "employee" # employment_status_of_partner
-      add_response "no" # mother_started_working_before_continuity_start_date
-      add_response "yes" # mother_still_working_on_continuity_end_date
-      add_response "yes" # mother_earned_more_than_lower_earnings_limit
-      add_response "yes" # mother_worked_at_least_26_weeks
-      add_response "yes" # mother_earned_at_least_390
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "yes" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_allowance_mat_leave_pat_leave_pat_pay_additional_pat_leave_additional_pat_pay
-    end
-
-    should "Mother employee, does not pass continuity test, does not pass lower earnings test, does not pass earnings and employment test, still working;Partner employee, passes continuity test, passes lower earnings test" do
-      add_response "yes" # two_carers
-      add_response "2014-1-1" # due_date
-      add_response "employee" # employment_status_of_mother
-      add_response "employee" # employment_status_of_partner
-      add_response "no" # mother_started_working_before_continuity_start_date
-      add_response "yes" # mother_still_working_on_continuity_end_date
-      add_response "no" # mother_earned_more_than_lower_earnings_limit
-      add_response "no" # mother_worked_at_least_26_weeks
-      add_response "no" # mother_earned_at_least_390
-      add_response "yes" # partner_started_working_before_continuity_start_date
-      add_response "yes" # partner_still_working_on_continuity_end_date
-      add_response "yes" # partner_earned_more_than_lower_earnings_limit
-      assert_current_node :outcome_mat_leave_pat_leave_pat_pay_additional_pat_leave
     end
   end
 
