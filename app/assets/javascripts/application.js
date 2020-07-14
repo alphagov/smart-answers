27d5d048c7106e1780b620fb553656640f62b9db
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2020-07-14 09:05:38 +0100

    Use individual component CSS/JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,11 @@
+//= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/button
+//= require govuk_publishing_components/components/checkboxes
+//= require govuk_publishing_components/components/feedback
+//= require govuk_publishing_components/components/govspeak
+//= require govuk_publishing_components/components/radio
+//= require govuk_publishing_components/components/step-by-step-nav
 //= require helpers
-//= require govuk_publishing_components/all_components
 
 $(document).ready(function() {
   $('#current-error').focus();
