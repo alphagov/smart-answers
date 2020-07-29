749c8a549d57cd823303298c76942a14af574381
Celia Collins <celiadcollins@gmail.com>
2020-07-29 09:47:00 +0100

    Fix asset pipeline with libsass
    Using a sass css compressor causes a scss file to be processed twice
    (once to build, once to compress) which breaks the usage of "unquote"
    to use CSS that has same function names as SCSS such as max.

--- config/application.rb
+++ config/application.rb
@@ -46,6 +46,12 @@ module SmartAnswers
     # Path within public/ where assets are compiled to
     config.assets.prefix = "/assets/smartanswers"
 
+    # Using a sass css compressor causes a scss file to be processed twice
+    # (once to build, once to compress) which breaks the usage of "unquote"
+    # to use CSS that has same function names as SCSS such as max.
+    # https://github.com/alphagov/govuk-frontend/issues/1350
+    config.assets.css_compressor = nil
+
     # allow overriding the asset host with an enironment variable, useful for
     # when router is proxying to this app but asset proxying isn't set up.
     config.asset_host = ENV["ASSET_HOST"]
