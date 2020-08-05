76e326ff79632e0fbed5add92e6507947f74e262
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2020-08-05 16:56:53 +0100

    Add functional test for checkbox type questions
    This was previously missing.

---
+++ test/fixtures/smart_answer_flows/smart-answers-controller-sample-with-checkbox-question.rb
@@ -0,0 +1,14 @@
+module SmartAnswer
+  class SmartAnswersControllerSampleWithCheckboxQuestionFlow < Flow
+    def define
+      name "smart-answers-controller-sample-with-checkbox-question"
+      checkbox_question :what? do
+        option :cheese
+        next_node do
+          outcome :done
+        end
+      end
+      outcome :done
+    end
+  end
+end
