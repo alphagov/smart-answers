1c35e64fa9ffc4fca3545b64d71b37a2b39e9500
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2020-08-07 13:44:18 +0100

    Remove _mat_shared_leave

--- lib/smart_answer_flows/pay-leave-for-parents.rb
+++ lib/smart_answer_flows/pay-leave-for-parents.rb
@@ -254,7 +254,7 @@ module SmartAnswer
                   if calculator.mother_earnings_employment?
                     outcome :outcome_mat_allowance_mat_leave_pat_leave_pat_pay_both_shared_leave
                   elsif !calculator.mother_earnings_employment?
-                    outcome :outcome_mat_leave_pat_leave_pat_pay_mat_shared_leave
+                    outcome :outcome_mat_leave_pat_leave_pat_pay
                   end
                 elsif calculator.mother_still_working_on_continuity_end_date == "yes"
                   if calculator.mother_earnings_employment?
@@ -363,12 +363,12 @@ module SmartAnswer
             if calculator.partner_continuity? && calculator.partner_lower_earnings?
               if calculator.employment_status_of_mother == "employee"
                 if calculator.mother_continuity? && calculator.mother_lower_earnings?
-                  outcome :outcome_mat_leave_mat_pay_pat_pay_mat_shared_leave
+                  outcome :outcome_mat_leave_mat_pay_pat_pay
                 elsif calculator.mother_continuity?
                   if calculator.mother_earnings_employment?
-                    outcome :outcome_mat_allowance_mat_leave_pat_pay_mat_shared_leave
+                    outcome :outcome_mat_allowance_mat_leave_pat_pay
                   elsif !calculator.mother_earnings_employment?
-                    outcome :outcome_mat_leave_pat_pay_mat_shared_leave
+                    outcome :outcome_mat_leave_pat_pay
                   end
                 elsif calculator.mother_still_working_on_continuity_end_date == "yes"
                   if !calculator.mother_earnings_employment?
@@ -497,7 +497,6 @@ module SmartAnswer
       outcome :outcome_mat_allowance_mat_leave_pat_leave_pat_pay_both_shared_leave
       outcome :outcome_mat_allowance_mat_leave_pat_leave_pat_pay_pat_shared_leave
       outcome :outcome_mat_allowance_mat_leave_pat_leave_pat_shared_leave
-      outcome :outcome_mat_allowance_mat_leave_pat_pay_mat_shared_leave
       outcome :outcome_mat_allowance_mat_leave_pat_pay
       outcome :outcome_mat_allowance_pat_leave_pat_pay_pat_shared_leave
       outcome :outcome_mat_allowance_pat_leave_pat_shared_leave
@@ -506,12 +505,10 @@ module SmartAnswer
       outcome :outcome_mat_leave_mat_pay
       outcome :outcome_mat_leave_mat_pay_pat_leave
       outcome :outcome_mat_leave_mat_pay_pat_leave_pat_pay_both_shared_leave
-      outcome :outcome_mat_leave_mat_pay_pat_pay_mat_shared_leave
+      outcome :outcome_mat_leave_mat_pay_pat_pay
       outcome :outcome_mat_leave_pat_leave
       outcome :outcome_mat_leave_pat_leave_pat_pay
-      outcome :outcome_mat_leave_pat_leave_pat_pay_mat_shared_leave
       outcome :outcome_mat_leave_pat_pay
-      outcome :outcome_mat_leave_pat_pay_mat_shared_leave
       outcome :outcome_mat_pay
       outcome :outcome_mat_pay_pat_leave
       outcome :outcome_mat_pay_pat_leave_pat_pay_pat_shared_leave
