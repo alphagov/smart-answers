e9c64f18f515d3f76185cd1aaa37367a5d1fbae3
Rob Nichols <rob.nichols@digital.cabinet-office.gov.uk>
2020-08-27 15:52:55 +0100

    Add session answers controller with controller tests
    - Add routes to session answers controller
    - update constraints to smart answers controller to avoid conflicts.
      Smart answer routes start /y/
    - Use separate actions for showing form and handling input

---
+++ test/controllers/session_answers_controller_test.rb
@@ -0,0 +1,53 @@
+require "test_helper"
+
+class SessionAnswersControllerTest < ActionDispatch::IntegrationTest
+  def flow_name
+    :coronavirus_find_support
+  end
+
+  def nodes
+    %i[need_help_with afford_food]
+  end
+
+  context "GET /:id/:node_name" do
+    setup do
+      get session_flow_path(id: flow_name, node_name: nodes[0])
+    end
+
+    should "be successful" do
+      assert_response :success
+    end
+
+    should "render correct page" do
+      assert_match(/What do you need help with because of coronavirus?/, response.body)
+    end
+  end
+
+  def params
+    { "response" => %w[getting_food], "next" => "1" }
+  end
+
+  context "GET /:id/:node_name/next" do
+    setup do
+      get update_session_flow_path(id: flow_name, node_name: nodes[0]), params: params
+    end
+
+    should "redirect to next node" do
+      assert_redirected_to(session_flow_path(id: flow_name, node_name: nodes[1]))
+    end
+  end
+
+  context "GET /:id/:node_name/next with error submission" do
+    setup do
+      get update_session_flow_path(id: flow_name, node_name: nodes[0]), params: { "response" => [], "next" => "1" }
+    end
+
+    should "render error successfully" do
+      assert_response :success
+    end
+
+    should "display error" do
+      assert_match(/govuk-error-message/, response.body)
+    end
+  end
+end
