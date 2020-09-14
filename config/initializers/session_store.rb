2e16673852ea5e553aae616b4f918062ce38d62e
Rob Nichols <rob.nichols@digital.cabinet-office.gov.uk>
2020-09-14 11:09:16 +0100

    Set cookie key used by session as 'smart_answer_session'
    Reinstated default secret token comment as session is now used in app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, expire_after: 4.hours
+Rails.application.config.session_store :cookie_store, expire_after: 4.hours, key: :smart_answer_session
