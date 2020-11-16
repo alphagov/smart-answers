be51b0ec5dbf864ff3660ae218fe1e34143dad6b
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2020-11-16 13:38:55 +0000

    Refactor BenefitCapCalculatorConfiguration into module
    This changes the BenefitCapCalculatorConfiguration from singleton class
    to a module as none of the methods were defined at an instance level.

--- lib/smart_answer/calculators/benefit_cap_calculator_configuration.rb
+++ lib/smart_answer/calculators/benefit_cap_calculator_configuration.rb
@@ -1,10 +1,31 @@
 module SmartAnswer::Calculators
-  class BenefitCapCalculatorConfiguration
-    def self.new_housing_benefit_amount(housing_benefit_amount, total_over_cap)
+  module BenefitCapCalculatorConfiguration
+    def self.data
+      @data ||= begin
+        filepath = Rails.root.join("config/smart_answers/benefit_cap_data.yml")
+        YAML.load_file(filepath).with_indifferent_access
+      end
+    end
+
+  module_function
+
+    def benefits
+      data.fetch(:benefits).with_indifferent_access
+    end
+
+    def exempt_benefits
+      data.fetch(:exempt_benefits)
+    end
+
+    def weekly_benefit_caps(region = :national)
+      data.fetch(:weekly_benefit_caps)[region].with_indifferent_access
+    end
+
+    def new_housing_benefit_amount(housing_benefit_amount, total_over_cap)
       housing_benefit_amount.to_f - total_over_cap.to_f
     end
 
-    def self.new_housing_benefit(amount)
+    def new_housing_benefit(amount)
       amount = sprintf("%.2f", amount)
       if amount < "0.5"
         amount = sprintf("%.2f", 0.5)
@@ -12,61 +33,45 @@ module SmartAnswer::Calculators
       amount
     end
 
-    def self.weekly_benefit_caps(region = :national)
-      data.fetch(:weekly_benefit_caps)[region].with_indifferent_access
-    end
-
-    def self.weekly_benefit_cap_descriptions(region = :national)
+    def weekly_benefit_cap_descriptions(region = :national)
       weekly_benefit_caps(region).each_with_object(HashWithIndifferentAccess.new) do |(key, value), weekly_benefit_cap_description|
         weekly_benefit_cap_description[key] = value.fetch(:description)
       end
     end
 
-    def self.weekly_benefit_cap_amount(family_type, region = :national)
+    def weekly_benefit_cap_amount(family_type, region = :national)
       weekly_benefit_caps(region).fetch(family_type)[:amount]
     end
 
-    def self.benefits
-      data.fetch(:benefits).with_indifferent_access
-    end
-
-    def self.exempt_benefits
-      data.fetch(:exempt_benefits)
-    end
-
-    def self.exempted_benefits?(exempted_benefits)
+    def exempted_benefits?(exempted_benefits)
       ListValidator.new(exempt_benefits.keys).all_valid?(exempted_benefits)
     end
 
-    def self.questions
+    def questions
       benefits.each_with_object(HashWithIndifferentAccess.new) do |(key, value), benefits_and_questions|
         benefits_and_questions[key] = value.fetch(:question)
       end
     end
 
-    def self.descriptions
+    def descriptions
       benefits.each_with_object(HashWithIndifferentAccess.new) do |(key, value), benefits_and_descriptions|
         benefits_and_descriptions[key] = value.fetch(:description)
       end
     end
 
-    def self.region(postcode)
+    def region(postcode)
       london?(postcode) ? :london : :national
     end
 
-    def self.london?(postcode)
+    def london?(postcode)
       area(postcode).any? do |result|
         result["type"] == "EUR" && result["name"] == "London"
       end
     end
 
-    def self.area(postcode)
+    def area(postcode)
       response = GdsApi.imminence.areas_for_postcode(postcode)&.to_hash
       OpenStruct.new(response).results || []
-    end
-
-    def self.data
-      @data ||= YAML.load_file(Rails.root.join("config/smart_answers/benefit_cap_data.yml")).with_indifferent_access
     end
   end
 end
