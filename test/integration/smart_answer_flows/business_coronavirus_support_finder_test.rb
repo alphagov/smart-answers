b3b836506d68bc1c36bd2ee7b431203f76964d05
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2020-11-18 16:40:14 +0000

    Add two questions to business support flow
    This adds questions about whether the business was effected by
    coronavirus restriction, which are need to evaluate whether the user is
    eligible for the 5 new grants made available.

--- test/integration/smart_answer_flows/business_coronavirus_support_finder_test.rb
+++ test/integration/smart_answer_flows/business_coronavirus_support_finder_test.rb
@@ -28,6 +28,34 @@ class BusinessCoronavirusSupportFinderFlowTest < ActiveSupport::TestCase
       add_response "retail_hospitality_or_leisure"
       assert_current_node :rate_relief_march_2020?
       add_response "yes"
+      assert_current_node :restricted_sector?
+      add_response "no"
+      assert_current_node :closed_by_restrictions?
+      add_response "no"
+      assert_current_node :results
+    end
+  end
+
+  context "flow without closed by restrictions" do
+    should "reach results node" do
+      assert_current_node :business_based?
+      add_response "england"
+      assert_current_node :business_size?
+      add_response "0_to_249"
+      assert_current_node :annual_turnover?
+      add_response "500m_and_over"
+      assert_current_node :paye_scheme?
+      add_response "yes"
+      assert_current_node :self_employed?
+      add_response "yes"
+      assert_current_node :non_domestic_property?
+      add_response "51k_and_over"
+      assert_current_node :sectors?
+      add_response "retail_hospitality_or_leisure"
+      assert_current_node :rate_relief_march_2020?
+      add_response "yes"
+      assert_current_node :restricted_sector?
+      add_response "yes"
       assert_current_node :results
     end
   end
@@ -46,6 +74,30 @@ class BusinessCoronavirusSupportFinderFlowTest < ActiveSupport::TestCase
       add_response "no"
       assert_current_node :non_domestic_property?
       add_response "none"
+      assert_current_node :restricted_sector?
+      add_response "no"
+      assert_current_node :closed_by_restrictions?
+      add_response "no"
+      assert_current_node :results
+    end
+  end
+
+  context "flow without other sectors and closed by restrictions" do
+    should "reach results node" do
+      assert_current_node :business_based?
+      add_response "scotland"
+      assert_current_node :business_size?
+      add_response "over_249"
+      assert_current_node :annual_turnover?
+      add_response "45m_to_500m"
+      assert_current_node :paye_scheme?
+      add_response "no"
+      assert_current_node :self_employed?
+      add_response "no"
+      assert_current_node :non_domestic_property?
+      add_response "none"
+      assert_current_node :restricted_sector?
+      add_response "yes"
       assert_current_node :results
     end
   end
