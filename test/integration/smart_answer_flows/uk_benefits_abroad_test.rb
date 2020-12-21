b2c96d2c42f55b368ca0cb95dbd8d386b3c993f9
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2020-12-21 11:53:02 +0000

    WFP /going_abroad other country test changes 20/21
    They should go straight to the not eligible outcome.

--- test/integration/smart_answer_flows/uk_benefits_abroad_test.rb
+++ test/integration/smart_answer_flows/uk_benefits_abroad_test.rb
@@ -485,14 +485,15 @@ class UKBenefitsAbroadTest < ActiveSupport::TestCase
           assert_current_node :wfp_not_eligible_outcome
         end
       end
-      # context "answer other country" do
-      #   setup do
-      #     add_response "albania"
-      #   end
-      #   should "take you to not eligible outcome" do
-      #     assert_current_node :wfp_not_eligible_outcome
-      #   end
-      # end
+
+      context "answer other country" do
+        setup do
+          add_response "albania"
+        end
+        should "take you to not eligible outcome" do
+          assert_current_node :wfp_not_eligible_outcome
+        end
+      end
     end
 
     # Maternity benefits
