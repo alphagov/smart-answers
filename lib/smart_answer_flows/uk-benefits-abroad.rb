8f4e52cac73c9b3223448e9c08be77a343c980ca
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2020-12-22 17:45:09 +0000

    IIDB /going_abroad EEA/Ireland/SS/other 20/21 changes
    - EEA, Ireland and other countries go to the same outcome
    - SS countries goes to a different same outcome

--- lib/smart_answer_flows/uk-benefits-abroad.rb
+++ lib/smart_answer_flows/uk-benefits-abroad.rb
@@ -128,12 +128,10 @@ module SmartAnswer
             end
           when "iidb"
             if calculator.going_abroad
-              if calculator.eea_country?
+              if calculator.eea_country? || !calculator.social_security_countries_iidb?
                 outcome :iidb_going_abroad_eea_outcome # A32 going_abroad
-              elsif calculator.social_security_countries_iidb?
-                outcome :iidb_going_abroad_ss_outcome # A33 going_abroad
               else
-                outcome :iidb_going_abroad_other_outcome # A34 going_abroad
+                outcome :iidb_going_abroad_ss_outcome # A34 going_abroad
               end
             elsif calculator.already_abroad
               if calculator.eea_country?
--- test/integration/smart_answer_flows/uk_benefits_abroad_test.rb
+++ test/integration/smart_answer_flows/uk_benefits_abroad_test.rb
@@ -1207,7 +1207,7 @@ class UKBenefitsAbroadTest < ActiveSupport::TestCase
       end
     end
 
-    # IIDB
+    # Industrial Injuries Disablement Benefit IIDB
     context "answer IIDB" do
       setup do
         add_response "iidb"
@@ -1230,6 +1230,14 @@ class UKBenefitsAbroadTest < ActiveSupport::TestCase
         end
         should "take you to country question" do
           assert_current_node :which_country?
+        end
+        context "answer Ireland" do
+          setup do
+            add_response :ireland
+          end
+          should "take you to EEA outcome" do
+            assert_current_node :iidb_going_abroad_eea_outcome
+          end
         end
         context "answer Guernsey" do
           setup do
@@ -1260,7 +1268,7 @@ class UKBenefitsAbroadTest < ActiveSupport::TestCase
             add_response "albania"
           end
           should "take you to other country outcome" do
-            assert_current_node :iidb_going_abroad_other_outcome
+            assert_current_node :iidb_going_abroad_eea_outcome
           end
         end
       end
@@ -2441,10 +2449,18 @@ class UKBenefitsAbroadTest < ActiveSupport::TestCase
     context "answer not claiming IIDB" do
       setup do
         add_response "iidb"
-        add_response "no"
       end
-      should "take you to maybe outcome" do
-        assert_current_node :iidb_maybe_outcome
+      should "ask are you already claiming IIDB" do
+        assert_current_node :iidb_already_claiming?
+      end
+
+      context "answer no" do
+        setup do
+          add_response "no"
+        end
+        should "take you to maybe outcome" do
+          assert_current_node :iidb_maybe_outcome
+        end
       end
     end
     context "answer already claiming, Guernsey" do
