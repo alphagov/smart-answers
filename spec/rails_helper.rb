237a3f87f0d456d9b32cde42e4d7c783955f5a45
Graham Lewis <gclssvglx@gmail.com>
2021-01-21 15:57:42 +0000

    Add grouping to SimpleCov report
    What
    
    Add a report grouping (tab) specifically for flows to the SimpleCov report output for RSpec tests.
    
    Why
    
    Makes finding the flow tests easier to find.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,10 @@ require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 
 require "simplecov"
-SimpleCov.start "rails"
+
+SimpleCov.start "rails" do
+  add_group "Smart Answer Flows", "lib/smart_answer_flows"
+end
 
 require File.expand_path("../config/environment", __dir__)
 # Prevent database truncation if the environment is production
