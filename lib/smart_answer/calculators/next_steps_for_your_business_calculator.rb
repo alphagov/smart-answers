a6f4b191685af04734a6c039e77202766c41c410
Alex <alex-ju@users.noreply.github.com>
2021-03-29 16:37:51 +0100

    Add standard results

--- lib/smart_answer/calculators/next_steps_for_your_business_calculator.rb
+++ lib/smart_answer/calculators/next_steps_for_your_business_calculator.rb
@@ -5,11 +5,17 @@
 
 module SmartAnswer::Calculators
   class NextStepsForYourBusinessCalculator
+    RESULT_DATA = YAML.load_file(Rails.root.join("config/smart_answers/next_steps_for_your_business.yml")).freeze
+
     attr_accessor :crn,
                   :annual_turnover,
                   :employ_someone,
                   :business_intent,
                   :business_support,
                   :business_premises
+
+    def grouped_results
+      RESULT_DATA.group_by { |result| result["section_name"] }
+    end
   end
 end
