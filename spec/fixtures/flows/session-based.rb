f4ac685d81b4e50837a9e20b5a8d8d9d21963f79
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2021-03-30 13:15:57 +0100

    Refactor test to be specific to session based flows
    This changes the existing fixtures and test to be specific for session
    based flows. This to allow adding future test that are specific to query
    parameters based flows.

--- spec/fixtures/flows/session-based.rb
+++ spec/fixtures/flows/session-based.rb
@@ -1,7 +1,7 @@
 module SmartAnswer
-  class TestFlow < Flow
+  class SessionBasedFlow < Flow
     def define
-      name "test"
+      name "session-based"
       satisfies_need "dccab509-bd3b-4f92-9af6-30f88485ac41"
       start_page_content_id "f26e566e-2557-4921-b944-9373c32255f1"
       response_store :session
