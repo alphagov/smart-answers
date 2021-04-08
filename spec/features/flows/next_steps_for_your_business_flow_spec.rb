b241ee3a61bbb72f50d444d7c2841dd5dce3e5f2
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2021-04-08 15:26:01 +0100

    Fetch company name from Companies House API
    For the "Next steps for your business" flow we use the company name in
    the title of the results page. This  derived from the supplied company
    registration number to query the Companies House Public Data API.
    Displaying the company name isn't critical, so there is a fallback
    heading so user's continue to see results if the request fails.

--- spec/features/flows/next_steps_for_your_business_flow_spec.rb
+++ spec/features/flows/next_steps_for_your_business_flow_spec.rb
@@ -9,13 +9,13 @@ RSpec.feature "SmartAnswer::NextStepsForYourBusinessFlow" do
       business_intent: "Does your business do any of the following?",
       business_support: "Are you looking for financial support for:",
       business_premises: "Where are you running your business?",
-      results: "Next steps for your business",
+      results: "Next steps for BUSINESS NAME LTD",
     }
   end
 
   let(:answers) do
     {
-      crn: "08161564",
+      crn: "123456789",
       annual_turnover: "Maybe in the future",
       employ_someone: "Maybe in the future",
       business_intent: "Sell goods online",
@@ -27,6 +27,14 @@ RSpec.feature "SmartAnswer::NextStepsForYourBusinessFlow" do
   before do
     stub_content_store_has_item("/next-steps-for-your-business")
     start(the_flow: headings[:flow_title], at: "next-steps-for-your-business")
+    stub_request(:get, "https://api.companieshouse.gov.uk/company/123456789")
+      .to_return(status: 200, body: { "company_name" => "BUSINESS NAME LTD" }.to_json)
+  end
+
+  around do |example|
+    ENV["COMPANIES_HOUSE_API_KEY"] = "api-key"
+    example.run
+    ENV["COMPANIES_HOUSE_API_KEY"] = nil
   end
 
   scenario "Answers all questions" do
