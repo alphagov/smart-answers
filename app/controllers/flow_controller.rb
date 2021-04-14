fd71aa3e8ef72cd2160ada09b3cdb7e2f5083267
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2021-04-14 11:38:38 +0100

    Add FlowHelper to abstract logic from controller
    This moves convience methods from the controller into a seperate helper
    module to allow them to be accessible in views and more easily unit
    tested.

--- app/controllers/flow_controller.rb
+++ app/controllers/flow_controller.rb
@@ -1,4 +1,6 @@
 class FlowController < ApplicationController
+  include FlowHelper
+
   before_action :set_cache_headers
   before_action :redirect_path_based_flows
 
@@ -45,41 +47,5 @@ private
     elsif Rails.configuration.set_http_cache_control_expiry_time
       expires_in(30.minutes, public: true)
     end
-  end
-
-  def forwarding_responses
-    flow.response_store == :session ? {} : response_store.all
-  end
-  helper_method :forwarding_responses
-
-  def presenter
-    @presenter ||= begin
-      params.merge!(responses: response_store.all, node_name: node_name)
-      FlowPresenter.new(params, flow)
-    end
-  end
-
-  def flow
-    @flow ||= SmartAnswer::FlowRegistry.instance.find(params[:id])
-  end
-
-  def response_store
-    @response_store ||= begin
-      if flow.response_store == :session
-        SessionResponseStore.new(flow_name: params[:id], session: session)
-      else
-        allowable_keys = flow.nodes.map(&:name)
-        query_parameters = request.query_parameters.slice(*allowable_keys)
-        ResponseStore.new(responses: query_parameters)
-      end
-    end
-  end
-
-  def node_name
-    @node_name ||= params[:node_slug].underscore if params[:node_slug].present?
-  end
-
-  def next_node_slug
-    presenter.current_state.current_node.to_s.dasherize
   end
 end
---
+++ spec/helpers/flow_helper_spec.rb
@@ -0,0 +1,90 @@
+RSpec.describe FlowHelper do
+  describe "#forwarding_responses" do
+    before do
+      response_store = ResponseStore.new(responses: { question1: "response1" })
+      allow(helper).to receive(:response_store).and_return(response_store)
+    end
+
+    it "returns an empty hash for session based flows" do
+      flow = SmartAnswer::Flow.new { response_store :session }
+      allow(helper).to receive(:flow).and_return(flow)
+
+      expect(helper.forwarding_responses).to eq({})
+    end
+
+    it "returns all the previous responses" do
+      flow = SmartAnswer::Flow.new { response_store :other }
+      allow(helper).to receive(:flow).and_return(flow)
+
+      expect(helper.forwarding_responses).to eq({ question1: "response1" })
+    end
+  end
+
+  describe "#presenter" do
+    it "returns the flow presenter" do
+      params[:node_slug] = "question-2"
+
+      flow = SmartAnswer::Flow.new { response_store :other }
+      allow(helper).to receive(:flow).and_return(flow)
+
+      response_store = ResponseStore.new(responses: { question1: "response1" })
+      allow(helper).to receive(:response_store).and_return(response_store)
+
+      expect(helper.presenter).to be_a(FlowPresenter)
+      expect(helper.presenter.flow).to be(flow)
+    end
+  end
+
+  describe "#flow" do
+    it "returns the flow for the current request" do
+      params[:id] = "flow-name"
+
+      flow = SmartAnswer::Flow.new { name "flow-name" }
+      flow_registry = instance_double("SmartAnswer::FlowRegistry")
+      allow(SmartAnswer::FlowRegistry).to receive(:instance).and_return(flow_registry)
+      allow(flow_registry).to receive(:find).with("flow-name").and_return(flow)
+
+      expect(helper.flow).to be(flow)
+    end
+  end
+
+  describe "#response_store" do
+    context "for session based flow" do
+      it "returns a session response store with flow name and session" do
+        params[:id] = "flow-name"
+
+        store = double("response-store")
+
+        flow = SmartAnswer::Flow.new { response_store :session }
+        allow(helper).to receive(:flow).and_return(flow)
+
+        expect(SessionResponseStore).to receive(:new).with(
+          flow_name: params[:id], session: session,
+        ).and_return(store)
+
+        expect(helper.response_store).to be(store)
+      end
+    end
+
+    context "for non-session based flow" do
+      it "returns a response store" do
+        store = double("response-store")
+        controller.request.query_parameters["question1"] = "response1"
+        controller.request.query_parameters["key"] = "value"
+
+        flow = SmartAnswer::Flow.new do
+          response_store :other
+          radio :question1
+        end
+
+        allow(helper).to receive(:flow).and_return(flow)
+
+        expect(ResponseStore).to receive(:new).with(
+          responses: { "question1" => "response1" },
+        ).and_return(store)
+
+        expect(helper.response_store).to be(store)
+      end
+    end
+  end
+end
