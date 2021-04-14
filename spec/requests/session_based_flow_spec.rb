d93c793a0ce0fff767f410eae831211cc23758d5
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2021-04-14 09:04:49 +0100

    Remove /flow/ prefix routes
    These are no longer needed as Smart Answer's is now represented by a
    single content item. Therefore the extra slug is not required to
    get around the constraint where the two content items had to have
    seperate base paths. Instead of renaming the the /s/ to /flow/ which was
    original intended, the can remove the slug altogether.

--- spec/requests/session_based_flow_spec.rb
+++ spec/requests/session_based_flow_spec.rb
@@ -1,110 +1,65 @@
 RSpec.describe "Session based flow navigation", flow_dir: :fixture do
   let(:no_cache_header) { "max-age=0, private, must-revalidate, no-store" }
 
-  context "urls have /s/ prefix" do
-    it "redirects to first node" do
-      get "/session-based/s"
-      expect(response).to redirect_to("/session-based/s/question1")
-      expect(response.headers["Cache-Control"]).to eq(no_cache_header)
-    end
-
-    it "renders the first question" do
-      get "/session-based/s/question1"
-      expect(response).to render_template("smart_answers/question")
-      expect(response.headers["Cache-Control"]).to eq(no_cache_header)
-    end
-
-    it "redirects to preceding unanswered question" do
-      get "/session-based/s/results"
-      expect(response).to redirect_to("/session-based/s/question1")
-      expect(response.headers["Cache-Control"]).to eq(no_cache_header)
-    end
-
-    it "redirects to next node when valid response provided" do
-      get "/session-based/s/question1/next", params: { response: "response1", next: "true" }
-      expect(response).to redirect_to("/session-based/s/question2")
-      expect(response.headers["Cache-Control"]).to eq(no_cache_header)
-    end
-
-    it "redirects to same node when invalid response provided" do
-      get "/session-based/s/question1/next", params: { response: "invalid", next: "true" }
-      expect(response).to redirect_to("/session-based/s/question1")
-      expect(response.headers["Cache-Control"]).to eq(no_cache_header)
-    end
-
-    it "clears the session and redirects to the start page" do
-      get "/session-based/s/destroy_session"
-      expect(response).to redirect_to("/session-based")
-      expect(response.headers["Cache-Control"]).to eq(no_cache_header)
-    end
-
-    it "clears the session and redirects to another page" do
-      get "/session-based/s/destroy_session", params: { ext_r: "true" }
-      expect(response).to redirect_to("https://www.bbc.co.uk/weather")
-    end
+  it "redirects to first node" do
+    get "/session-based/s"
+    expect(response).to redirect_to("/session-based/s/question1")
+    expect(response.headers["Cache-Control"]).to eq(no_cache_header)
   end
 
-  context "urls have /flow/ prefix" do
-    it "redirects to first node" do
-      get "/session-based/flow"
-      expect(response).to redirect_to("/session-based/s/question1")
-      expect(response.headers["Cache-Control"]).to eq(no_cache_header)
-    end
-
-    it "renders the first question" do
-      get "/session-based/flow/question1"
-      expect(response).to render_template("smart_answers/question")
-      expect(response.headers["Cache-Control"]).to eq(no_cache_header)
-    end
-
-    it "redirects to preceding unanswered question" do
-      get "/session-based/flow/results"
-      expect(response).to redirect_to("/session-based/s/question1")
-      expect(response.headers["Cache-Control"]).to eq(no_cache_header)
-    end
-
-    it "redirects to next node when valid response provided" do
-      get "/session-based/flow/question1/next", params: { response: "response1", next: "true" }
-      expect(response).to redirect_to("/session-based/s/question2")
-      expect(response.headers["Cache-Control"]).to eq(no_cache_header)
-    end
-
-    it "redirects to same node when invalid response provided" do
-      get "/session-based/flow/question1/next", params: { response: "invalid", next: "true" }
-      expect(response).to redirect_to("/session-based/s/question1")
-      expect(response.headers["Cache-Control"]).to eq(no_cache_header)
-    end
-
-    it "clears the session and redirects to the start page" do
-      get "/session-based/flow/destroy_session"
-      expect(response).to redirect_to("/session-based")
-      expect(response.headers["Cache-Control"]).to eq(no_cache_header)
-    end
-
-    it "clears the session and redirects to another page" do
-      get "/session-based/flow/destroy_session", params: { ext_r: "true" }
-      expect(response).to redirect_to("https://www.bbc.co.uk/weather")
-    end
+  it "renders the first question" do
+    get "/session-based/s/question1"
+    expect(response).to render_template("smart_answers/question")
+    expect(response.headers["Cache-Control"]).to eq(no_cache_header)
   end
 
-  context "urls have /flow/ prefix, but are path based flows" do
+  it "redirects to preceding unanswered question" do
+    get "/session-based/s/results"
+    expect(response).to redirect_to("/session-based/s/question1")
+    expect(response.headers["Cache-Control"]).to eq(no_cache_header)
+  end
+
+  it "redirects to next node when valid response provided" do
+    get "/session-based/s/question1/next", params: { response: "response1", next: "true" }
+    expect(response).to redirect_to("/session-based/s/question2")
+    expect(response.headers["Cache-Control"]).to eq(no_cache_header)
+  end
+
+  it "redirects to same node when invalid response provided" do
+    get "/session-based/s/question1/next", params: { response: "invalid", next: "true" }
+    expect(response).to redirect_to("/session-based/s/question1")
+    expect(response.headers["Cache-Control"]).to eq(no_cache_header)
+  end
+
+  it "clears the session and redirects to the start page" do
+    get "/session-based/s/destroy_session"
+    expect(response).to redirect_to("/session-based")
+    expect(response.headers["Cache-Control"]).to eq(no_cache_header)
+  end
+
+  it "clears the session and redirects to another page" do
+    get "/session-based/s/destroy_session", params: { ext_r: "true" }
+    expect(response).to redirect_to("https://www.bbc.co.uk/weather")
+  end
+
+  context "urls have /s/ prefix, but are path based flows" do
     it "redirects requests to old route" do
-      get "/path-based/flow"
+      get "/path-based/s"
       expect(response).to redirect_to("/path-based/y")
     end
 
     it "redirects requests for a specific node to old route" do
-      get "/path-based/flow/question1"
+      get "/path-based/s/question1"
       expect(response).to redirect_to("/path-based/y")
     end
 
     it "redirects requests for a next node to old route" do
-      get "/path-based/flow/question1/next", params: { response: "response1", next: "true" }
+      get "/path-based/s/question1/next", params: { response: "response1", next: "true" }
       expect(response).to redirect_to("/path-based/y")
     end
 
     it "redirects requests for destroying session to old route" do
-      get "/path-based/flow/destroy_session"
+      get "/path-based/s/destroy_session"
       expect(response).to redirect_to("/path-based/y")
     end
   end
