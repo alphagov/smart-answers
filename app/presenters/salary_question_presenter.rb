2d5ac1a19bb681cb55cf1b9d05a1fd8253221816
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-04-22 14:54:50 +0100

    Get salary amount from flow presenter

--- app/presenters/salary_question_presenter.rb
+++ app/presenters/salary_question_presenter.rb
@@ -4,4 +4,11 @@ class SalaryQuestionPresenter < QuestionPresenter
   def response_label(value)
     format_salary(SmartAnswer::Salary.new(value))
   end
+
+  def amount
+    return if response_for_current_question.blank?
+
+    response = to_response(response_for_current_question)
+    response ? response[:amount] : response_for_current_question[:amount]
+  end
 end
