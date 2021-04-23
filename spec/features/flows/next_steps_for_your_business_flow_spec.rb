ae1c6f10651141681c634f1f56e73de5feb10a6c
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2021-04-23 13:39:27 +0100

    Update start page for Next steps for you business flow
    This replaces the placeholder text with content for the start page.

--- spec/features/flows/next_steps_for_your_business_flow_spec.rb
+++ spec/features/flows/next_steps_for_your_business_flow_spec.rb
@@ -2,7 +2,7 @@ RSpec.feature "SmartAnswer::NextStepsForYourBusinessFlow" do
   let(:headings) do
     # <question name>: <text_for :title from erb>
     {
-      flow_title: "Next steps for your business",
+      flow_title: "Check the next steps for your limited company",
       annual_turnover_over_85k: "Do you think your business will take more than £85,000 in a 12 month period?",
       employ_someone: "Do you want to employ someone?",
       activities: "Does your business do any of the following?",
