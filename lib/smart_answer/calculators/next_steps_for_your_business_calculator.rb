7c40a359e532ea5f32fae0e22fed6e655983077b
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2021-04-26 09:37:55 +0100

    Add initial filtering rules for results
    This adds the initial filtering for the Next steps for you business
    results page.

--- lib/smart_answer/calculators/next_steps_for_your_business_calculator.rb
+++ lib/smart_answer/calculators/next_steps_for_your_business_calculator.rb
@@ -14,7 +14,43 @@ module SmartAnswer::Calculators
                   :business_premises
 
     def grouped_results
-      RESULT_DATA.group_by { |result| result["topic"] }
+      filtered_results.group_by { |result| result["topic"] }
     end
+
+    def filtered_results
+      RESULT_DATA.select do |result|
+        RULES[result["id"]].call(self)
+      end
+    end
+
+    RULES = {
+      r1: ->(_) { true },
+      r2: ->(_) { true },
+      r3: ->(_) { true },
+      r4: ->(_) { true },
+      r5: ->(_) { true },
+      r6: ->(_) { true },
+      r7: ->(_) { true },
+      r8: ->(_) { true },
+      r9: ->(_) { true },
+      r10: ->(_) { true },
+      r11: ->(_) { true },
+      r12: ->(_) { true },
+      r13: ->(calculator) { calculator.annual_turnover_over_85k != "no" },
+      r14: ->(calculator) { calculator.annual_turnover_over_85k != "no" },
+      r15: ->(calculator) { calculator.employer != "no" },
+      r16: ->(calculator) { calculator.needs_financial_support == "yes" },
+      r17: ->(calculator) { calculator.needs_financial_support == "yes" },
+      r18: ->(calculator) { calculator.business_premises.include?("home") },
+      r19: ->(calculator) { (calculator.business_premises & %w[rented owned]).empty? },
+      r20: ->(calculator) { calculator.employer != "no" },
+      r21: ->(calculator) { calculator.business_premises.include?("home") },
+      r22: ->(calculator) { calculator.business_premises.exclude?("none") },
+      r23: ->(calculator) { calculator.activities.include?("import_goods") },
+      r24: ->(calculator) { calculator.activities.include?("export_goods_or_services") },
+      r25: ->(calculator) { calculator.activities.include?("sell_online") },
+      r26: ->(_) { true },
+      r27: ->(calculator) { calculator.annual_turnover_over_85k != "yes" },
+    }.with_indifferent_access.freeze
   end
 end
