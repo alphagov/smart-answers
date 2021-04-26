314c7408807e53410f1b036ba705c220afe03fe0
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-04-26 14:54:40 +0100

    Get salary amount from FlowPresenter
    Currently pre-population of salary questions using the `prefill_*`
    methods in the helper only work for url-based flows, not session-based
    flows whereas `response_for_current_question` returns the current value
    for both.
    
    This uses the `response_for_current_question` method added in PR #5315
    to get the existing response for the amount in salary questions.
    
    To replace the `prefill_value_*?` we need to update the tests to
    stub the response from `response_for_current_question`. This is because
    `amount` method depends on `response_for_current_question` being
    available to call.
    
    `response_for_current_question` is delegated from `FlowPresenter`,
    however, for ease, in the tests, a `nil` FlowPresenter object is being
    passed to the presenter, so the presenter does not have access to any
    its methods. Rather than stubbing an entire FlowPresenter object, we can
    instead stub the response from the only method we need for the tests to
    pass.
    
    `response_for_current_question` calls the `to_response` methods for
    Salary if `previous_response` is in the query string. This `to_response`
    method returns `nil` if `previous_response` does not contain an amount,
    so we don't need to add any extra guards.

--- app/presenters/salary_question_presenter.rb
+++ app/presenters/salary_question_presenter.rb
@@ -4,4 +4,10 @@ class SalaryQuestionPresenter < QuestionPresenter
   def response_label(value)
     format_salary(SmartAnswer::Salary.new(value))
   end
+
+  def amount
+    return if response_for_current_question.blank?
+
+    response_for_current_question[:amount]
+  end
 end
---
+++ test/unit/salary_question_presenter_test.rb
@@ -0,0 +1,25 @@
+require_relative "../test_helper"
+
+module SmartAnswer
+  class SalaryQuestionPresenterTest < ActiveSupport::TestCase
+    setup do
+      @question = Question::Salary.new(nil, :question_name?)
+      @renderer = stub("renderer")
+
+      @presenter = SalaryQuestionPresenter.new(@question, nil, nil, renderer: @renderer)
+      @presenter.stubs(:response_for_current_question).returns(amount: "4000.0", period: "month")
+    end
+
+    context "#amount" do
+      should "return the amount" do
+        assert_equal "4000.0", @presenter.amount.to_s
+      end
+
+      should "return the user input if the amount is invalid" do
+        @presenter.stubs(:response_for_current_question).returns(amount: "-123", period: "week")
+        assert_equal "-123", @presenter.amount
+      end
+    end
+
+  end
+end
