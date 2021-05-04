71f5ef37b1fe9be5b4d09004e3707877481eeb5b
Sean Rankine <sean.rankine@me.com>
2021-05-04 14:11:51 +0100

    Refactor hint method for value questions
    To make the value question conistent with other question types,
    "hint_text" attribute has been renamed to "hint". This also removes
    prepending hint text with body text, as no flows use body text with
    value question types.

--- test/unit/value_question_presenter_test.rb
+++ test/unit/value_question_presenter_test.rb
@@ -10,21 +10,6 @@ module SmartAnswer
       @presenter = ValueQuestionPresenter.new(@question, nil, nil, renderer: @renderer)
     end
 
-    test "#hint_text returns single line of content rendered for hint block" do
-      @renderer.stubs(:content_for).with(:body).returns("")
-      @renderer.stubs(:content_for).with(:hint).returns("hint-text")
-      @renderer.stubs(:content_for).with(:suffix_label).returns("")
-
-      assert_equal "hint-text", @presenter.hint_text
-    end
-
-    test "#hint_text also returns body if present" do
-      @renderer.stubs(:content_for).with(:body).returns("body")
-      @renderer.stubs(:content_for).with(:hint).returns("hint-text")
-
-      assert_equal "body, hint-text", @presenter.hint_text
-    end
-
     test "#caption returns the given caption when a caption is given" do
       @renderer.stubs(:hide_caption).returns(false)
       @renderer.stubs(:content_for).with(:caption).returns("caption-text")
