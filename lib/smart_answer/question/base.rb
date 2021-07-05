317b6736fe2e3e41c641ef7af3d51348a9446c2b
Sean Rankine <sean.rankine@me.com>
2021-07-05 16:43:58 +0100

    Move validate from next node to transition

--- lib/smart_answer/question/base.rb
+++ lib/smart_answer/question/base.rb
@@ -35,7 +35,6 @@ module SmartAnswer
       end
 
       def next_node_for(current_state, input)
-        validate!(current_state, input)
         state = current_state.dup.extend(NextNodeBlock::InstanceMethods).freeze
         next_node = state.instance_exec(input, &next_node_block)
         if next_node.blank?
@@ -53,6 +52,7 @@ module SmartAnswer
 
       def transition(current_state, raw_input)
         input = parse_input(raw_input)
+        validate!(current_state, input)
         new_state = @on_response_blocks.inject(current_state.dup) do |state, block|
           block.evaluate(state, input)
         end
