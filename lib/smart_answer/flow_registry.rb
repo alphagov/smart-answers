ca4f43260e3a7d1de2fdadd07eeba02b70c244e7
Kevin Dew <kevindew@me.com>
2021-07-12 16:09:20 +0100

    Replace FlowRegistry needing to require/load flows
    Now that Flow classes are autoloadable the manual load/require in
    FlowRegistry behaviour is not needed. This behaviour was somewhat
    undesirable in dev as it relied on the overridding method names as a
    means to reload Flow classes when there was a dev change. This system
    wasn't smart enough to allow the auto-reload of any shared data in a
    SmartAnswer Flow.
    
    By removing this we still need the fixture flows to be available as
    these are not in the autoload path. To resolve this, these are manually
    required when the method to set-up fixture flows is called.

--- lib/smart_answer/flow_registry.rb
+++ lib/smart_answer/flow_registry.rb
@@ -53,11 +53,6 @@ module SmartAnswer
 
     def build_flow(name)
       class_prefix = name.tr("-", "_").camelize
-      if Rails.env.development?
-        load @load_path.join("#{name}.rb")
-      else
-        require @load_path.join(name)
-      end
       namespaced_class = "#{class_prefix}Flow".constantize
       namespaced_class.build
     end
