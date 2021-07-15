e717394c7efe65ae9c9e5b0468030a499de551a7
Kevin Dew <kevindew@me.com>
2021-07-16 00:32:43 +0100

    Rename country and date WIP

--- test/functional/smart_answers_controller_postcode_question_test.rb
+++ test/functional/smart_answers_controller_postcode_question_test.rb
@@ -1,5 +1,4 @@
 require_relative "../test_helper"
-require_relative "../fixtures/smart_answer_flows/smart-answers-controller-sample-with-postcode-question"
 require_relative "smart_answers_controller_test_helper"
 
 class SmartAnswersControllerPostcodeQuestionTest < ActionController::TestCase
@@ -7,31 +6,25 @@ class SmartAnswersControllerPostcodeQuestionTest < ActionController::TestCase
 
   include SmartAnswersControllerTestHelper
 
-  def setup
-    setup_fixture_flows
-    stub_content_store_has_item("/smart-answers-controller-sample-with-postcode-question")
-  end
-
-  def teardown
-    teardown_fixture_flows
-  end
+  setup { setup_fixture_flows }
+  teardown { teardown_fixture_flows }
 
   context "postcode question" do
     should "display question" do
-      get :show, params: { id: "smart-answers-controller-sample-with-postcode-question", started: "y" }
+      get :show, params: { id: "postcode-sample", started: "y" }
       assert_select ".govuk-caption-l", "Sample postcode question"
-      assert_select "h1.govuk-label-wrapper .govuk-label.govuk-label--l", /Postcode\?/
+      assert_select "h1.govuk-label-wrapper .govuk-label.govuk-label--l", /User input\?/
       assert_select "input[type=text][name=response]"
     end
 
     should "show a validation error if invalid input" do
       submit_response "invalid postcode"
-      assert_select "h1.govuk-label-wrapper .govuk-label.govuk-label--l", /Postcode\?/
+      assert_select "h1.govuk-label-wrapper .govuk-label.govuk-label--l", /User input\?/
       assert_select "body", /Please answer this question/
     end
   end
 
   def submit_response(response = nil, other_params = {})
-    super(response, other_params.merge(id: "smart-answers-controller-sample-with-postcode-question"))
+    super(response, other_params.merge(id: "postcode-sample"))
   end
 end
