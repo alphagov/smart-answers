2f3ab4cb719c2133e47afa7455b05a78deaad3fc
Kevin Dew <kevindew@me.com>
2021-07-16 00:17:08 +0100

    Checkbox sample WIP

--- test/functional/smart_answers_controller_checkbox_question_test.rb
+++ test/functional/smart_answers_controller_checkbox_question_test.rb
@@ -1,5 +1,4 @@
 require_relative "../test_helper"
-require_relative "../fixtures/smart_answer_flows/smart-answers-controller-sample-with-checkbox-question"
 require_relative "smart_answers_controller_test_helper"
 
 class SmartAnswersControllerCheckboxQuestionTest < ActionController::TestCase
@@ -7,25 +6,15 @@ class SmartAnswersControllerCheckboxQuestionTest < ActionController::TestCase
 
   include SmartAnswersControllerTestHelper
 
-  def setup
-    setup_fixture_flows
-    stub_content_store_has_item("/smart-answers-controller-sample-with-checkbox-question")
-  end
-
-  def teardown
-    teardown_fixture_flows
-  end
+  setup { setup_fixture_flows }
+  teardown { teardown_fixture_flows }
 
   context "checkbox question" do
     should "display question" do
-      get :show, params: { id: "smart-answers-controller-sample-with-checkbox-question", started: "y" }
+      get :show, params: { id: "checkbox-sample", started: "y" }
       assert_select ".govuk-fieldset__legend.govuk-fieldset__legend--l .govuk-caption-l", "Sample checkbox question"
-      assert_select ".govuk-fieldset__legend.govuk-fieldset__legend--l h1.govuk-fieldset__heading", /What\?/
+      assert_select ".govuk-fieldset__legend.govuk-fieldset__legend--l h1.govuk-fieldset__heading", /What do you want on your pizza\?/
       assert_select "input[type=checkbox][name=\"response[]\"]"
     end
-  end
-
-  def submit_response(response = nil, other_params = {})
-    super(response, other_params.merge(id: "smart-answers-controller-sample-with-checkbox-question"))
   end
 end
