a5bbbbae19d489d0b06b6b72058d3410a186f2d0
Kevin Dew <kevindew@me.com>
2021-07-15 09:18:55 +0100

    Remove unnecessary preload_flows option
    Avoiding preloading of flows in development has a near negligible impact
    on performance compared to more significant things (notably view
    rendering). However it's presence means that we need a quirky system
    where Flow classes have an unconventional file name pattern where that
    needs special autoload code and the extra logic in this flow registry
    class.
    
    This has been removed as a precursor to a step where the class names of
    Flows are changed from their unusual kebab-case style (e.g.
    benefits-cap-calculator.rb) to the conventional Ruby snake_case style
    (e.g. benefits_cap_calculator_flow.rb).
    
    Some benchmarks from rendering /benefits-cap-calculator
    
    main branch, initial request: Completed 200 OK in 2040ms (Views: 1934.1ms | Allocations: 1066068)
    main branch, subsequent request: Completed 200 OK in 725ms (Views: 675.3ms | Allocations: 169443)
    
    this branch, initial request: Completed 200 OK in 2100ms (Views: 1889.1ms | Allocations: 1136213)
    this branch, subsequent request: Completed 200 OK in 466ms (Views: 410.7ms | Allocations: 168485)
    
    The impact of this change is that there are more object allocations on
    an initial request, however given the view rendering takes ~2s and
    the time to load flows takes ~0.1s it's unlikely to be noticeable
    for any developers.

--- lib/smart_answer/flow_registry.rb
+++ lib/smart_answer/flow_registry.rb
@@ -3,6 +3,8 @@ module SmartAnswer
     class NotFound < StandardError; end
     FLOW_DIR = Rails.root.join("lib/smart_answer_flows")
 
+    attr_reader :flows, :load_path
+
     def self.instance
       @instance ||= new
     end
@@ -12,60 +14,22 @@ module SmartAnswer
     end
 
     def initialize(options = {})
-      @load_path = Pathname.new(options[:smart_answer_load_path] || FLOW_DIR)
-      preload_flows! if !Rails.env.development? || options[:preload_flows]
-    end
-    attr_reader :load_path
-
-    def find(name)
-      raise NotFound, "'#{name}' not found" unless available?(name)
-
-      find_by_name(name) || raise(NotFound)
-    end
-
-    def flows
-      available_flows.map { |s| find_by_name(s) }.compact
-    end
-
-    def available_flows
-      Dir[@load_path.join("*.rb")].map do |path|
-        File.basename(path, ".rb")
+      @load_path = Pathname.new(options.fetch(:smart_answer_load_path, FLOW_DIR))
+      @flows = Dir[@load_path.join("*.rb")].map do |path|
+        build_flow(File.basename(path, ".rb"))
       end
     end
 
-    def preloaded?
-      @preloaded.present?
+    def find(name)
+      @flows.find { |flow| flow.name == name } || raise(NotFound, "'#{name}' not found")
     end
 
   private
-
-    def find_by_name(name)
-      @preloaded ? preloaded(name) : build_flow(name)
-    end
-
-    def available?(name)
-      if @preloaded
-        @preloaded.key?(name)
-      else
-        available_flows.include?(name)
-      end
-    end
 
     def build_flow(name)
       class_prefix = name.tr("-", "_").camelize
       namespaced_class = "#{class_prefix}Flow".constantize
       namespaced_class.build
-    end
-
-    def preload_flows!
-      @preloaded = {}
-      available_flows.each do |flow_name|
-        @preloaded[flow_name] = build_flow(flow_name)
-      end
-    end
-
-    def preloaded(name)
-      @preloaded && @preloaded[name]
     end
   end
 end
