6553d8b8dc5ea6b6156d6be28d4af2bb7df95693
Kevin Dew <kevindew@me.com>
2021-07-21 21:09:26 +0100

    Configure Zeitwerk to ignore the flow template directories
    This fixes the ability to run `rake zeitwerk:check`

--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -1,4 +1,9 @@
 Rails.autoloaders.each do |autoloader|
   # Flows in the shared directory use the same namespace as other Flows.
   autoloader.collapse("app/flows/shared")
+
+  # These directories contain template files and may not have Zeitwerk naming
+  # conventions
+  autoloader.ignore("app/flows/**/outcomes/*")
+  autoloader.ignore("app/flows/**/questions/*")
 end
