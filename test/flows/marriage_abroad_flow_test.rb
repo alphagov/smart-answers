723609059662ebb1cc018d402e0d7af515f6fbc1
Kevin Dew <kevindew@me.com>
2021-08-04 19:50:08 +0100

    Use marriage abroad config in tests
    In the review for this flow rewrite it was pointed out that one of the
    constants was out of sync with the YAML config file (Congo was missing).
    Rather than just fix the constant it seemed better to use the YAML file
    as the means to power the tests, since it essentially just duplicates
    the data.
    
    To do this I've added some helper functions. I've also moved the helper
    functions to the top of the file as the class method (`countries_list`)
    needed to be defined before the tests and I wanted all the helper
    functions to be together.
    
    Whether methods are instance or class scoped is dependent on the test
    need. The only method used outside of a test is `countries_list`, thus
    this is the only method defined at a class level.
    
    I've added some modest protection in the helpers so they will raise an
    error if any of the lists accessed is empty. This has been done so we
    don't have a chunk of tests defined based on a YAML list which is empty,
    and thus never executes. In that scenario I'd expect the list and tests
    to be removed.

--- test/flows/marriage_abroad_flow_test.rb
+++ test/flows/marriage_abroad_flow_test.rb
@@ -3,17 +3,40 @@ require "support/flow_test_helper"
 
 class MarriageAbroadFlowTest < ActiveSupport::TestCase
   include FlowTestHelper
-
-  CEREMONY_LOCATION_COUNTRIES = %w[finland iceland].freeze
-  MARRIAGE_OR_PACS_COUNTRIES = %w[monaco new-caledonia wallis-and-futuna].freeze
-  ONE_QUESTION_COUNTRIES = %w[french-guiana french-polynesia guadeloupe martinique mayotte reunion st-pierre-and-miquelon].freeze
-  TWO_QUESTION_COUNTRIES = %w[australia croatia cyprus egypt france india ireland japan luxembourg philippines south-africa south-korea thailand turkey usa zimbabwe].freeze
-  THREE_QUESTION_COUNTRIES = %w[congo greece italy poland spain switzerland].freeze
-  FOUR_QUESTION_COUNTRIES = %w[afghanistan albania algeria american-samoa andorra angola anguilla antigua-and-barbuda argentina armenia aruba austria azerbaijan bahamas bahrain bangladesh barbados belarus belgium belize benin bermuda bhutan bolivia bonaire-st-eustatius-saba bosnia-and-herzegovina botswana brazil british-indian-ocean-territory british-virgin-islands brunei bulgaria burkina-faso burundi cambodia cameroon canada cape-verde cayman-islands central-african-republic chad chile china colombia comoros costa-rica cote-d-ivoire cuba curacao czech-republic democratic-republic-of-the-congo denmark djibouti dominica dominican-republic ecuador el-salvador equatorial-guinea eritrea estonia eswatini ethiopia falkland-islands fiji gabon georgia germany ghana gibraltar grenada guatemala guinea guinea-bissau guyana haiti honduras hong-kong hungary indonesia iran iraq israel jamaica jordan kazakhstan kenya kiribati kosovo kuwait kyrgyzstan laos latvia lebanon lesotho liberia libya liechtenstein lithuania macao madagascar malawi malaysia maldives malta marshall-islands mauritania mauritius mexico micronesia moldova mongolia montenegro montserrat morocco mozambique myanmar namibia nauru nepal netherlands new-zealand nicaragua niger nigeria north-korea north-macedonia norway oman pakistan palau panama papua-new-guinea paraguay peru pitcairn-island portugal qatar romania russia rwanda saint-barthelemy san-marino sao-tome-and-principe saudi-arabia senegal serbia seychelles sierra-leone singapore slovakia slovenia solomon-islands somalia south-georgia-and-the-south-sandwich-islands south-sudan sri-lanka st-helena-ascension-and-tristan-da-cunha st-kitts-and-nevis st-lucia st-maarten st-martin st-vincent-and-the-grenadines sudan suriname sweden syria taiwan tajikistan tanzania the-gambia timor-leste togo tonga trinidad-and-tobago tunisia turkmenistan turks-and-caicos-islands tuvalu uganda ukraine united-arab-emirates uruguay uzbekistan vanuatu venezuela vietnam western-sahara yemen zambia].freeze
-  ALL_COUNTRIES = CEREMONY_LOCATION_COUNTRIES + MARRIAGE_OR_PACS_COUNTRIES + ONE_QUESTION_COUNTRIES + TWO_QUESTION_COUNTRIES + THREE_QUESTION_COUNTRIES + FOUR_QUESTION_COUNTRIES
+  delegate :countries_list, to: :class
 
   # These countries use the worldwide organisation API and need an extra stub
   WORLDWIDE_ORGANISATION_API_COUNTRIES = %w[sweden].freeze
+
+  def self.countries_list(type)
+    @countries ||= YAML.load_file(Rails.root.join("config/smart_answers/marriage_abroad_data.yml"))
+    list = @countries.fetch(type.to_s)
+    raise "Expected some countries for #{type}" unless list.any?
+
+    list
+  end
+
+  def stub_worldwide_api(country_slugs)
+    stub_worldwide_api_has_locations(country_slugs)
+
+    WORLDWIDE_ORGANISATION_API_COUNTRIES.intersection(country_slugs).each do |slug|
+      stub_worldwide_api_has_organisations_for_location(slug, { results: [] })
+    end
+  end
+
+  def all_countries_list
+    all_types = %w[countries_with_ceremony_location_outcomes
+                   countries_with_2_outcomes_marriage_or_pacs
+                   countries_with_1_outcome
+                   countries_with_2_outcomes
+                   countries_with_6_outcomes
+                   countries_with_18_outcomes]
+    all_types.sum { |t| countries_list(t) }
+  end
+
+  def random_country(type = nil)
+    type ? countries_list(type).sample : all_countries_list.sample
+  end
 
   setup { testing_flow MarriageAbroadFlow }
 
@@ -24,7 +47,7 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
   context "question: country_of_ceremony?" do
     setup do
       testing_node :country_of_ceremony?
-      stub_worldwide_api(ALL_COUNTRIES)
+      stub_worldwide_api(all_countries_list)
     end
 
     should "render question" do
@@ -37,25 +60,25 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
       end
 
       should "be valid for a country that exists" do
-        assert_valid_response ALL_COUNTRIES.sample
+        assert_valid_response random_country
       end
     end
 
     context "next_node" do
-      should "have a next_node of partner_opposite_or_same_sex? for a two questions country" do
-        assert_next_node :partner_opposite_or_same_sex?, for_response: TWO_QUESTION_COUNTRIES.sample
+      should "have a next_node of partner_opposite_or_same_sex? for a two outcome country" do
+        assert_next_node :partner_opposite_or_same_sex?, for_response: random_country(:countries_with_2_outcomes)
       end
 
-      should "have a next_node of marriage_or_pacs? for a pacs country" do
-        assert_next_node :marriage_or_pacs?, for_response: MARRIAGE_OR_PACS_COUNTRIES.sample
+      should "have a next_node of marriage_or_pacs? for a marriage or pacs country" do
+        assert_next_node :marriage_or_pacs?, for_response: random_country(:countries_with_2_outcomes_marriage_or_pacs)
       end
 
-      should "have a next_node of outcome_marriage_abroad_in_country for a french overseas territory" do
-        assert_next_node :outcome_marriage_abroad_in_country, for_response: ONE_QUESTION_COUNTRIES.sample
+      should "have a next_node of outcome_marriage_abroad_in_country for a 1 outcome country" do
+        assert_next_node :outcome_marriage_abroad_in_country, for_response: random_country(:countries_with_1_outcome)
       end
 
       should "have a next_node of legal_residency? for other countries" do
-        assert_next_node :legal_residency?, for_response: THREE_QUESTION_COUNTRIES.sample
+        assert_next_node :legal_residency?, for_response: random_country(:countries_with_6_outcomes)
       end
     end
   end
@@ -63,8 +86,8 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
   context "question: legal_residency?" do
     setup do
       testing_node :legal_residency?
-      stub_worldwide_api(ALL_COUNTRIES)
-      add_responses country_of_ceremony?: FOUR_QUESTION_COUNTRIES.sample
+      stub_worldwide_api(all_countries_list)
+      add_responses country_of_ceremony?: random_country(:countries_with_18_outcomes)
     end
 
     should "render question" do
@@ -73,12 +96,12 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
 
     context "next_node" do
       should "have a next_node of outcome_marriage_abroad_in_country for a ceremony location country" do
-        add_responses country_of_ceremony?: CEREMONY_LOCATION_COUNTRIES.sample
+        add_responses country_of_ceremony?: random_country(:countries_with_ceremony_location_outcomes)
         assert_next_node :outcome_marriage_abroad_in_country, for_response: "uk"
       end
 
-      should "have a next_node of partner_opposite_or_same_sex? for a three questions country" do
-        add_responses country_of_ceremony?: THREE_QUESTION_COUNTRIES.sample
+      should "have a next_node of partner_opposite_or_same_sex? for a 6 outcome country" do
+        add_responses country_of_ceremony?: random_country(:countries_with_6_outcomes)
         assert_next_node :partner_opposite_or_same_sex?, for_response: "uk"
       end
 
@@ -91,8 +114,8 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
   context "question: marriage_or_pacs?" do
     setup do
       testing_node :marriage_or_pacs?
-      stub_worldwide_api(ALL_COUNTRIES)
-      add_responses country_of_ceremony?: MARRIAGE_OR_PACS_COUNTRIES.sample
+      stub_worldwide_api(all_countries_list)
+      add_responses country_of_ceremony?: random_country(:countries_with_2_outcomes_marriage_or_pacs)
     end
 
     should "render question" do
@@ -109,8 +132,8 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
   context "question: what_is_your_partners_nationality?" do
     setup do
       testing_node :what_is_your_partners_nationality?
-      stub_worldwide_api(ALL_COUNTRIES)
-      add_responses country_of_ceremony?: FOUR_QUESTION_COUNTRIES.sample,
+      stub_worldwide_api(all_countries_list)
+      add_responses country_of_ceremony?: random_country(:countries_with_18_outcomes),
                     legal_residency?: "uk"
     end
 
@@ -128,8 +151,8 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
   context "question: partner_opposite_or_same_sex?" do
     setup do
       testing_node :partner_opposite_or_same_sex?
-      stub_worldwide_api(ALL_COUNTRIES)
-      add_responses country_of_ceremony?: FOUR_QUESTION_COUNTRIES.sample,
+      stub_worldwide_api(all_countries_list)
+      add_responses country_of_ceremony?: random_country(:countries_with_18_outcomes),
                     legal_residency?: "uk",
                     what_is_your_partners_nationality?: "partner_british"
     end
@@ -157,7 +180,7 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
   context "outcome :outcome_marriage_abroad_in_country" do
     setup { testing_node :outcome_marriage_abroad_in_country }
 
-    MARRIAGE_OR_PACS_COUNTRIES.each do |country|
+    countries_list(:countries_with_2_outcomes_marriage_or_pacs).each do |country|
       context "marriage or pacs country: #{country}" do
         setup do
           stub_worldwide_api([country])
@@ -176,7 +199,7 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
       end
     end
 
-    CEREMONY_LOCATION_COUNTRIES.each do |country|
+    countries_list(:countries_with_ceremony_location_outcomes).each do |country|
       context "ceremony location country: #{country}" do
         setup do
           stub_worldwide_api([country])
@@ -200,8 +223,8 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
       end
     end
 
-    ONE_QUESTION_COUNTRIES.each do |country|
-      context "one question country" do
+    countries_list(:countries_with_1_outcome).each do |country|
+      context "1 outcome country #{country}" do
         setup do
           stub_worldwide_api([country])
           add_responses country_of_ceremony?: country
@@ -213,8 +236,8 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
       end
     end
 
-    TWO_QUESTION_COUNTRIES.each do |country|
-      context "two question country: #{country}" do
+    countries_list(:countries_with_2_outcomes).each do |country|
+      context "2 outcome country: #{country}" do
         setup do
           stub_worldwide_api([country])
           add_responses country_of_ceremony?: country
@@ -232,8 +255,8 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
       end
     end
 
-    THREE_QUESTION_COUNTRIES.each do |country|
-      context "three question country: #{country}" do
+    countries_list(:countries_with_6_outcomes).each do |country|
+      context "6 outcome country: #{country}" do
         setup do
           stub_worldwide_api([country])
           add_responses country_of_ceremony?: country
@@ -277,8 +300,8 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
       end
     end
 
-    FOUR_QUESTION_COUNTRIES.each do |country|
-      context "Four question country: #{country}" do
+    countries_list(:countries_with_18_outcomes).each do |country|
+      context "18 outcome country: #{country}" do
         setup do
           # stubbing a single country at a time makes this test > 60s faster
           stub_worldwide_api([country])
@@ -423,14 +446,6 @@ class MarriageAbroadFlowTest < ActiveSupport::TestCase
           assert_rendered_outcome
         end
       end
-    end
-  end
-
-  def stub_worldwide_api(country_slugs)
-    stub_worldwide_api_has_locations(country_slugs)
-
-    WORLDWIDE_ORGANISATION_API_COUNTRIES.intersection(country_slugs).each do |slug|
-      stub_worldwide_api_has_organisations_for_location(slug, { results: [] })
     end
   end
 end
