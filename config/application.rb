e682e1f38594b01301edefe37f99ea6af08608e2
Benjamin Eskola <benjamin.eskola@digital.cabinet-office.gov.uk>
2021-10-04 17:36:55 +0100

    Bump load_defaults

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(*Rails.groups)
 module SmartAnswers
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
     config.time_zone = "London"
 
     # Settings in config/environments/* take precedence over those specified here.
