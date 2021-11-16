fe0aa098a3213eac3d66b520bcba8b4705ba0a78
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-11-16 10:30:22 +0000

    Add "any other countries" question
    Remove first "number of countries question" and replace with "any other countries"
    
    Duplicates the "which_country?" question so that the calculator object can be created.

--- app/flows/covid_travel_abroad_flow.rb
+++ app/flows/covid_travel_abroad_flow.rb
@@ -4,31 +4,48 @@ class CovidTravelAbroadFlow < SmartAnswer::Flow
     content_id "b46df1e7-e770-43ab-8b4c-ce402736420c"
     status :draft
 
-    value_question :how_many_countries?, parse: Integer do
+    country_select "which_country?".to_sym, exclude_countries: [] do
       on_response do |response|
         self.calculator = SmartAnswer::Calculators::CovidTravelAbroadCalculator.new
-        calculator.country_count = response.to_i
+        calculator.country_count += 1
+        calculator.countries << response
       end
 
       next_node do
-        question :which_1_country?
+        question "any_other_countries_#{calculator.country_count}?".to_sym
       end
     end
 
     (1..SmartAnswer::Calculators::CovidTravelAbroadCalculator::MAX_COUNTRIES).each do |num|
+      radio "any_other_countries_#{num}?".to_sym do
+        template_name "any_other_countries"
+
+        option :yes
+        option :no
+
+        on_response do |response|
+          calculator.any_other_countries = response
+        end
+
+        next_node do
+          if calculator.any_other_countries == "no"
+            question :vaccine_status?
+          else
+            question "which_#{calculator.country_count}_country?".to_sym
+          end
+        end
+      end
+
       country_select "which_#{num}_country?".to_sym, exclude_countries: [] do
         template_name "which_country"
 
         on_response do |response|
           calculator.countries << response
+          calculator.country_count += 1
         end
 
         next_node do
-          if num < calculator.country_count
-            question "which_#{num + 1}_country?".to_sym
-          else
-            question :vaccine_status?
-          end
+          question "any_other_countries_#{calculator.country_count}?".to_sym
         end
       end
     end
