9639e37f079d3402c83ca503b1ab642ca737998c
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2022-02-11 16:26:13 +0000

    Move the coronavirus travel data file
    Moves the check travel during coronavirus data file to be in the same
    location as the new vaccination status config file. This should make it
    easier to find all config for the check travel during coronavirus
    smart-answer.

--- app/models/world_location.rb
+++ app/models/world_location.rb
@@ -63,7 +63,7 @@ class WorldLocation
   end
 
   def self.travel_rules
-    @travel_rules ||= YAML.load_file(Rails.root.join("config/smart_answers/check_travel_during_coronavirus_data.yml"))
+    @travel_rules ||= YAML.load_file(Rails.root.join("config/smart_answers/check_travel_during_coronavirus/country_data.yml"))
   end
 
   def initialize(location)
