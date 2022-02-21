6aaf6a6bdec9d065583fe0648e65bcd0b99ca9f9
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2022-02-21 09:53:12 +0000

    Add foreign travel advice API stubs
    Add helper for foreign travel advice API stubs for all countries
    that are tested up to the 'check_travel_during_coronavirus' flow
    outcome stage. Plus incomplete and error states.
    
    Update existing tests to use the new helper.

--- test/unit/services/travel_advice_header_check_retriever_test.rb
+++ test/unit/services/travel_advice_header_check_retriever_test.rb
@@ -1,60 +1,25 @@
 require "test_helper"
+require "support/foreign_travel_advice_helper"
 
 class TravelAdviceHeaderCheckRetrieverTest < ActiveSupport::TestCase
+  include ForeignTravelAdviceHelper
+
   setup do
     Rails.cache.clear
   end
 
   context ".fetch" do
     should "return true if the country has all the headers" do
-      response = {
-        details: {
-          parts: [
-            {
-              body: "<h2 id=\"all-travellers\">Header</h2><p>Content</p>" \
-                "<h2 id=\"if-youre-transiting-through-spain\">Header</h2><p>Content</p>" \
-                "<h2 id=\"if-youre-not-fully-vaccinated\">Header</h2><p>Content</p>" \
-                "<h2 id=\"if-youre-fully-vaccinated\">Header</h2><p>Content</p>" \
-                "<h2 id=\"children-and-young-people\">Header</h2><p>Content</p>" \
-                "<h2 id=\"exemptions\">Header</h2><p>Content</p>",
-              slug: "entry-requirements",
-            },
-          ],
-        },
-      }.to_json
-
-      travel_advice_header_check_request = stub_request(:get, "https://www.gov.uk/api/content/foreign-travel-advice/spain").with(
-        headers: { "Content-Type" => "application/json" },
-      ).to_return(status: 200, body: response)
-
+      stub_foreign_travel_advice
       status = TravelAdviceHeaderCheckRetriever.fetch("spain")
 
-      assert_requested travel_advice_header_check_request
       assert_equal true, status
     end
 
     should "return false if the country does not have all the headers" do
-      response = {
-        details: {
-          parts: [
-            {
-              body: "<h2 id=\"all-travellers\">Header</h2><p>Content</p>" \
-                "<h2 id=\"if-youre-transiting-through-spain\">Header</h2><p>Content</p>" \
-                "<h2 id=\"children-and-young-people\">Header</h2><p>Content</p>" \
-                "<h2 id=\"exemptions\">Header</h2><p>Content</p>",
-              slug: "entry-requirements",
-            },
-          ],
-        },
-      }.to_json
-
-      travel_advice_header_check_request = stub_request(:get, "https://www.gov.uk/api/content/foreign-travel-advice/spain").with(
-        headers: { "Content-Type" => "application/json" },
-      ).to_return(status: 200, body: response)
-
+      stub_incomplete_foreign_travel_advice
       status = TravelAdviceHeaderCheckRetriever.fetch("spain")
 
-      assert_requested travel_advice_header_check_request
       assert_equal false, status
     end
   end
--- test/unit/travel_advice_header_checker_test.rb
+++ test/unit/travel_advice_header_checker_test.rb
@@ -1,68 +1,34 @@
 require_relative "../test_helper"
+require "support/foreign_travel_advice_helper"
 
 class TravelAdviceHeaderCheckerTest < ActiveSupport::TestCase
+  include ForeignTravelAdviceHelper
+
   context "#has_all_content_headers?" do
     setup do
       @header_checker = TravelAdviceHeaderChecker.new("spain")
     end
 
     should "return true when country has all of the required headers" do
-      response = {
-        details: {
-          parts: [
-            {
-              body: "<h2 id=\"all-travellers\">Header</h2><p>Content</p>" \
-                "<h2 id=\"if-youre-transiting-through-spain\">Header</h2><p>Content</p>" \
-                "<h2 id=\"if-youre-not-fully-vaccinated\">Header</h2><p>Content</p>" \
-                "<h2 id=\"if-youre-fully-vaccinated\">Header</h2><p>Content</p>" \
-                "<h2 id=\"children-and-young-people\">Header</h2><p>Content</p>" \
-                "<h2 id=\"exemptions\">Header</h2><p>Content</p>",
-              slug: "entry-requirements",
-            },
-          ],
-        },
-      }.to_json
-
-      stub_request(:get, "https://www.gov.uk/api/content/foreign-travel-advice/spain").with(
-        headers: { "Content-Type" => "application/json" },
-      ).to_return(status: 200, body: response)
+      stub_foreign_travel_advice
 
       assert_equal true, @header_checker.has_content_headers?
     end
 
     should "return false when country does not have all of the required headers" do
-      response = {
-        details: {
-          parts: [
-            {
-              body: "<h2 id=\"all-travellers\">Header</h2><p>Content</p>" \
-                "<h2 id=\"if-youre-transiting-through-spain\">Header</h2><p>Content</p>" \
-                "<h2 id=\"children-and-young-people\">Header</h2><p>Content</p>",
-              slug: "entry-requirements",
-            },
-          ],
-        },
-      }.to_json
-
-      stub_request(:get, "https://www.gov.uk/api/content/foreign-travel-advice/spain").with(
-        headers: { "Content-Type" => "application/json" },
-      ).to_return(status: 200, body: response)
+      stub_incomplete_foreign_travel_advice
 
       assert_equal false, @header_checker.has_content_headers?
     end
 
     should "handle JSON::ParserError errors" do
-      stub_request(:get, "https://www.gov.uk/api/content/foreign-travel-advice/spain").with(
-        headers: { "Content-Type" => "application/json" },
-      ).to_return(status: 200, body: "")
+      stub_unparsable_json_foreign_travel_advice
 
       assert_equal false, @header_checker.has_content_headers?
     end
 
     should "handle connection timeouts" do
-      stub_request(:get, "https://www.gov.uk/api/content/foreign-travel-advice/spain").with(
-        headers: { "Content-Type" => "application/json" },
-      ).to_timeout
+      stub_server_timeout_foreign_travel_advice
 
       assert_equal false, @header_checker.has_content_headers?
     end
