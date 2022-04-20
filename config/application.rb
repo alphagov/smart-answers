715f71257e7ac2f23aca52c6e4d4c25b65a7a1ce
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2022-04-20 12:42:42 +0100

    Rotate SHA1 encrypted cookies to SHA256

--- config/application.rb
+++ config/application.rb
@@ -63,5 +63,22 @@ module SmartAnswers
 
     # Allow requests for all domains e.g. <app>.dev.gov.uk
     config.hosts.clear
+
+    # Rotate SHA1 cookies to SHA256 (the new Rails 7 default)
+    # TODO: Remove this after existing user sessions have been rotated
+    # https://guides.rubyonrails.org/v7.0/upgrading_ruby_on_rails.html#key-generator-digest-class-changing-to-use-sha256
+    Rails.application.config.action_dispatch.cookies_rotations.tap do |cookies|
+      salt = Rails.application.config.action_dispatch.authenticated_encrypted_cookie_salt
+      secret_key_base = Rails.application.secrets.secret_key_base
+      next if secret_key_base.blank?
+
+      key_generator = ActiveSupport::KeyGenerator.new(
+        secret_key_base, iterations: 1000, hash_digest_class: OpenSSL::Digest::SHA1
+      )
+      key_len = ActiveSupport::MessageEncryptor.key_len
+      secret = key_generator.generate_key(salt, key_len)
+
+      cookies.rotate :encrypted, secret
+    end
   end
 end
