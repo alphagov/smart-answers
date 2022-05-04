0776d8a3989c0e2f13aa6c96a2fbd8759d48aa61
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2022-05-04 14:43:20 +0100

    Move to jasmine-browser-runner

--- lib/tasks/js_test.rake
+++
@@ -1,4 +0,0 @@
-desc "Run Javascript tests"
-task js_test: [:environment] do
-  sh "rake jasmine:ci"
-end
--- spec/javascripts/support/jasmine_helper.rb
+++
@@ -1,7 +0,0 @@
-require "jasmine_selenium_runner/configure_jasmine"
-
-class HeadlessChromeJasmineConfigurer < JasmineSeleniumRunner::ConfigureJasmine
-  def selenium_options
-    { options: GovukTest.headless_chrome_selenium_options }
-  end
-end
