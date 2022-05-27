7e9de7b55d7265330fcfd92c80dd77933f7e890b
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2022-05-27 11:05:17 +0100

    Remove `passport_country_is_saudi_arabia_or_bahrain?`
    This method is no longer used.

--- lib/smart_answer/calculators/uk_visa_calculator.rb
+++ lib/smart_answer/calculators/uk_visa_calculator.rb
@@ -124,10 +124,6 @@ module SmartAnswer::Calculators
       @passport_country == "turkey"
     end
 
-    def passport_country_is_saudi_arabia_or_bahrain?
-      %w[saudi-arabia bahrain].include?(@passport_country)
-    end
-
     def applicant_is_stateless_or_a_refugee?
       @passport_country == "stateless-or-refugee"
     end
