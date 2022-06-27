c673447663cf48beaa5c0f2fabcb4231655533ba
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2022-06-27 16:23:33 +0100

    Add question building_over_11_metres

--- app/flows/property_fire_safety_payment_flow.rb
+++ app/flows/property_fire_safety_payment_flow.rb
@@ -3,4 +3,23 @@ class PropertyFireSafetyPaymentFlow < SmartAnswer::Flow
     name "property-fire-safety-payment"
     content_id "29355604-e9a1-499a-9b0c-18abd833f02e"
     status :draft
+
+    radio :building_over_11_metres? do
+      option :yes
+      option :no
+
+      next_node do |response|
+        if response == "yes"
+          question :own_freehold?
+        else
+          outcome :unlikely_to_need_fixing
+        end
+      end
+    end
+
+    radio :own_freehold? do
+    end
+
+    outcome :unlikely_to_need_fixing
+  end
 end
--- test/flows/property_fire_safety_payment_flow_test.rb
+++ test/flows/property_fire_safety_payment_flow_test.rb
@@ -11,4 +11,36 @@ class PropertyFireSafetyPaymentFlowTest < ActiveSupport::TestCase
   should "render a start page" do
     assert_rendered_start_page
   end
+
+  context "question: building_over_11_metres?" do
+    setup { testing_node :building_over_11_metres? }
+
+    should "render the question" do
+      assert_rendered_question
+    end
+
+    context "next_node" do
+      should "have a next node of own_freehold? if yes" do
+        assert_next_node :own_freehold?, for_response: "yes"
+      end
+
+      should "have an outcome of unlikely_to_need_fixing if no" do
+        assert_next_node :unlikely_to_need_fixing, for_response: "no"
+      end
+    end
+  end
+
+
+  context "outcomes" do
+    context "when building is under 11 metres" do
+      setup do
+        testing_node :unlikely_to_need_fixing
+        add_responses building_over_11_metres?: "no"
+      end
+
+      should "render outcome text" do
+        assert_rendered_outcome text: "Your building is unlikely to need fixing"
+      end
+    end
+  end
 end
