f827656a98ceec4526ed70c83d556c6ef602c8d0
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2022-06-27 15:02:12 +0100

    Create property_fire_safety_payment skeleton flow files

---
+++ app/flows/property_fire_safety_payment_flow.rb
@@ -0,0 +1,6 @@
+class PropertyFireSafetyPaymentFlow < SmartAnswer::Flow
+  def define
+    name "property-fire-safety-payment"
+    content_id "29355604-e9a1-499a-9b0c-18abd833f02e"
+    status :draft
+end
