70be124fb85151c0f75e838a96bf9e2aea7aba22
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2022-06-29 13:39:20 +0100

    Add uprated_value_of_property method to calculator
    This method reads from a YAML file listing years with associated
    uprating values. The value_of_property is multiplied
    by the rating for the year_of_purchase to give an uprated value,
    accounting for the current estimated value of the property.
    
    If the year_of_purchase is not present (currently, before 1970) a
    default uprating value is used.

--- lib/smart_answer/calculators/property_fire_safety_payment_calculator.rb
+++ lib/smart_answer/calculators/property_fire_safety_payment_calculator.rb
@@ -12,5 +12,19 @@ module SmartAnswer::Calculators
     def valid_year_of_purchase?
       @year_of_purchase.between?(FIRST_VALID_YEAR, LAST_VALID_YEAR)
     end
+
+    def property_uprating_values
+      @property_uprating_values ||= YAML.load_file(Rails.root.join("config/smart_answers/property_fire_safety_payment_data.yml")).freeze
+    end
+
+    def uprated_value_of_property
+      (property_uprating_values.fetch(@year_of_purchase, default_uprating_value) * @value_of_property.to_f).ceil
+    end
+
+  private
+
+    def default_uprating_value
+      property_uprating_values["default"]
+    end
   end
 end
--- test/unit/calculators/property_fire_safety_payment_calculator_test.rb
+++ test/unit/calculators/property_fire_safety_payment_calculator_test.rb
@@ -22,5 +22,28 @@ module SmartAnswer::Calculators
         assert_not @calculator.valid_year_of_purchase?
       end
     end
+
+    context "#uprated_value_of_property" do
+      setup do
+        stubbed_uprating_data = {
+          "default" => 10,
+          2000 => 5,
+        }
+
+        YAML.stubs(:load_file).returns(stubbed_uprating_data)
+      end
+
+      should "return the value_of_property multiplied by the uprating value for the year_of_purchase" do
+        @calculator.year_of_purchase = 2000
+        @calculator.value_of_property = "99999.99"
+        assert_equal @calculator.uprated_value_of_property, 500_000
+      end
+
+      should "return the value_of_property multiplied by the default uprating value if year_of_purchase is not found" do
+        @calculator.year_of_purchase = 2001
+        @calculator.value_of_property = "100000.99"
+        assert_equal @calculator.uprated_value_of_property, 1_000_010
+      end
+    end
   end
 end
