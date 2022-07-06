fbf008a4f1322c523f74ce0c65c272da185256fe
Ian James <ian.james@digital.cabinet-office.gov.uk>
2022-07-06 15:12:11 +0100

    Update component stylesheets and JavaScript imports
    * Added step by step navigation header to print stylesheet
    * Removed details, inset text, phase banner, and signup link component styles and
      JavaScript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/checkboxes
-//= require govuk_publishing_components/components/details
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/print-link
 //= require govuk_publishing_components/components/radio
