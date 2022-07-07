7bc75d804e91cad7065eb17b17c650ed0d895c1a
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2022-07-07 15:28:14 +0100

    Change slug property-fire-safety-payment to check-fire-safety-costs

--- app/flows/property_fire_safety_payment_flow.rb
+++ app/flows/property_fire_safety_payment_flow.rb
@@ -1,6 +1,6 @@
 class PropertyFireSafetyPaymentFlow < SmartAnswer::Flow
   def define
-    name "property-fire-safety-payment"
+    name "check-fire-safety-costs"
     content_id "29355604-e9a1-499a-9b0c-18abd833f02e"
     status :draft
 
