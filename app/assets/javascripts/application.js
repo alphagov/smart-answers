339c28394d988f450f56af634af591be3c8149aa
Martin Jones <martin.jones@digital.cabinet-office.gov.uk>
2022-07-12 09:49:12 +0100

    Remove track results module
    The track-results JavaScript module used for tracking Google Analytics events was replaced with the gem-track-click module from publishing components

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,5 +5,4 @@
 //= require govuk_publishing_components/components/radio
 //= require govuk_publishing_components/components/step-by-step-nav
 //= require modules/track-responses
-//= require modules/track-results
 //= require components/autocomplete
