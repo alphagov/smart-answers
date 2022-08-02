d9ac5cdce51df45dcd1e73498e459d8cc67da614
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2022-08-02 16:27:04 +0100

    Add Healthy Start outcome

--- lib/smart_answer/calculators/check_benefits_financial_support_calculator.rb
+++ lib/smart_answer/calculators/check_benefits_financial_support_calculator.rb
@@ -56,6 +56,13 @@ module SmartAnswer::Calculators
       general_child_benefit_eligibility?(skip_benefit_list, age_groups)
     end
 
+    def eligible_for_healthy_start?
+      skip_benefit_list = %w[housing_benefit]
+      age_groups = %w[pregnant 1_or_under 2 3_to_4]
+
+      general_child_benefit_eligibility?(skip_benefit_list, age_groups)
+    end
+
     def eligible_for_employment_and_support_allowance?
       @over_state_pension_age == "no" &&
         @are_you_working != "yes_over_16_hours_per_week" &&
