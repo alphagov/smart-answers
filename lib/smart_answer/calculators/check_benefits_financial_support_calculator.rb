e83f51e0d8c7c33caf9d1f33d9fd31cdbc8b00f4
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2022-08-02 16:59:00 +0100

    Add Free school meals [England] outcome

--- lib/smart_answer/calculators/check_benefits_financial_support_calculator.rb
+++ lib/smart_answer/calculators/check_benefits_financial_support_calculator.rb
@@ -70,6 +70,13 @@ module SmartAnswer::Calculators
       general_child_benefit_eligibility?(skip_benefit_list, age_groups)
     end
 
+    def eligible_for_free_school_meals?
+      skip_benefit_list = %w[housing_benefit]
+      age_groups = %w[3_to_4 5_to_7 8_to_11 12_to_15 16_to_17]
+
+      general_child_benefit_eligibility?(skip_benefit_list, age_groups)
+    end
+
     def eligible_for_employment_and_support_allowance?
       @over_state_pension_age == "no" &&
         @are_you_working != "yes_over_16_hours_per_week" &&
