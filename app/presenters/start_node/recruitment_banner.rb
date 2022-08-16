69961f7c1025c1ec4b4fa6ac4754e49fccc66ccb
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2022-08-16 12:05:51 +0100

    Remove Tree test survey recruitment banners

--- app/presenters/start_node/recruitment_banner.rb
+++ app/presenters/start_node/recruitment_banner.rb
@@ -1,7 +1,6 @@
 module StartNode
   module RecruitmentBanner
     SURVEY_URLS = {
-      "/maternity-paternity-pay-leave" => "https://GDSUserResearch.optimalworkshop.com/treejack/8f1yr1fj",
       "/check-uk-visa" => "https://surveys.publishing.service.gov.uk/s/0DZCPX/",
     }.freeze
 
--- test/integration/recruitment_banner_test.rb
+++ test/integration/recruitment_banner_test.rb
@@ -1,13 +1,6 @@
 require_relative "../integration_test_helper"
 
 class RecruitmentBannerTest < ActionDispatch::IntegrationTest
-  def tree_test_banner
-    {
-      URL: "https://GDSUserResearch.optimalworkshop.com/treejack/8f1yr1fj",
-      path: "/maternity-paternity-pay-leave",
-    }
-  end
-
   def visa_banner
     {
       URL: "https://surveys.publishing.service.gov.uk/s/0DZCPX/",
@@ -17,33 +10,6 @@ class RecruitmentBannerTest < ActionDispatch::IntegrationTest
 
   setup { setup_fixture_flows }
   teardown { teardown_fixture_flows }
-
-  context "Tree test recruitment banners" do
-    setup do
-      stub_content_store_has_item(tree_test_banner[:path])
-      stub_content_store_has_item("/bridge-of-death")
-    end
-
-    should "display Recruitment Banner on the landing page of the specific smart answer" do
-      visit tree_test_banner[:path]
-      assert page.has_css?(".gem-c-intervention")
-      assert page.has_link?("Take part in user research (opens in a new tab)", href: tree_test_banner[:URL])
-    end
-
-    should "not display Recruitment Banner on non-landing pages of the specific smart answer" do
-      visit "#{tree_test_banner[:path]}/y"
-
-      assert_not page.has_css?(".gem-c-intervention")
-      assert_not page.has_link?("Take part in user research (opens in a new tab)", href: tree_test_banner[:URL])
-    end
-
-    should "not display Recruitment Banner unless survey URL is specified for the base path" do
-      visit "/bridge-of-death"
-
-      assert_not page.has_css?(".gem-c-intervention")
-      assert_not page.has_link?("Take part in user research (opens in a new tab)", href: tree_test_banner[:URL])
-    end
-  end
 
   context "Visa service recruitment banners" do
     setup do
