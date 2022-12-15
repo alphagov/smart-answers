0fe2c61c99e9aa5f647b260f96295a6674345d2c
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2022-12-15 12:10:01 +0000

    Move banner above the breadcrumb

--- app/presenters/start_node/recruitment_banner.rb
+++ app/presenters/start_node/recruitment_banner.rb
@@ -5,14 +5,18 @@ module StartNode
       "/child-benefit-tax-calculator" => "https://GDSUserResearch.optimalworkshop.com/treejack/cbd7a696cbf57c683cbb2e95b4a36c8a",
     }.freeze
 
-    def recruitment_survey_url
-      SURVEY_URLS[base_path]
+    def recruitment_survey_url(path)
+      landing_page?(path) && SURVEY_URLS[base_path]
     end
 
   private
 
     def base_path
       "/#{@flow_presenter.name}"
+    end
+
+    def landing_page?(current_path)
+      base_path == current_path
     end
   end
 end
