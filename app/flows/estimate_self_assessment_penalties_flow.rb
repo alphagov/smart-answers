7e85f801645c8a20043006b24c06bf9a5a8b7ce9
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-01-11 15:53:25 +0000

    Remove 2014-2015 and add 2021-2022 to SA penalty flow.
    - Update tests to 2022 values (from 2015), so that we have some time before we have to do it again (hopefully in that time we can replace this section of the test suite with non-date-dependent tests)

--- app/flows/estimate_self_assessment_penalties_flow.rb
+++ app/flows/estimate_self_assessment_penalties_flow.rb
@@ -5,13 +5,13 @@ class EstimateSelfAssessmentPenaltiesFlow < SmartAnswer::Flow
     status :published
 
     radio :which_year? do
-      option :"2014-15"
       option :"2015-16"
       option :"2016-17"
       option :"2017-18"
       option :"2018-19"
       option :"2019-20"
       option :"2020-21"
+      option :"2021-22"
 
       on_response do |response|
         self.calculator = SmartAnswer::Calculators::SelfAssessmentPenalties.new
