649c9381599918e2ecfc46fa1c3e3244af70160d
Cynthia Anyaeriuba <cynthia.anyaeriuba@digital.cabinet-office.gov.uk>
2023-08-11 12:25:17 +0100

    Minor change to wording for ETA message

--- test/flows/check_uk_visa_flow_test.rb
+++ test/flows/check_uk_visa_flow_test.rb
@@ -20,7 +20,7 @@ class CheckUkVisaFlowTest < ActiveSupport::TestCase
     @epassport_gate_country = "australia"
     @youth_mobility_scheme_country = "canada"
 
-    @eta_text = "If you’re travelling after 15 November 2023, you’ll need to apply for an electronic travel authorisation (ETA) instead of an electronic visa waiver. You’ll be able to apply for an ETA from 25 October 2023."
+    @eta_text = "If you’re travelling on or after 15 November 2023, you’ll need to apply for an electronic travel authorisation (ETA) instead of an electronic visa waiver. You’ll be able to apply for an ETA from 25 October 2023."
 
     # stub only the countries used in this test for less of a performance impact
     stub_worldwide_api_has_locations(["china",
