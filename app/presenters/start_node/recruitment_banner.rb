a75a32f3affff2507474a92c9fe4fba665e89317
Catalina Ilie <catalina.ilie@digital.cabinet-office.gov.uk>
2023-10-06 14:32:35 +0100

    Add user research banner to benefits pages

--- app/presenters/start_node/recruitment_banner.rb
+++ app/presenters/start_node/recruitment_banner.rb
@@ -6,8 +6,19 @@ module StartNode
       "/calculate-statutory-sick-pay" => SURVERY_URL,
     }.freeze
 
+    BENEFITS_SURVEY_URL = "https://signup.take-part-in-research.service.gov.uk/home?utm_campaign=Content_History&utm_source=Hold_gov_to_account&utm_medium=gov.uk&t=GDS&id=16".freeze
+    BENEFITS_SURVEY_URLS = {
+      "/state-pension-age" => BENEFITS_SURVEY_URL,
+      "/check-benefits-financial-support" => BENEFITS_SURVEY_URL,
+      "/child-benefit-tax-calculator" => BENEFITS_SURVEY_URL,
+    }.freeze
+
     def survey_url(path)
       landing_page?(path) && SURVEY_URLS[base_path]
+    end
+
+    def benefits_survey_url(path)
+      landing_page?(path) && BENEFITS_SURVEY_URLS[base_path]
     end
 
   private
