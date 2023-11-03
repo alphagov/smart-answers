71335a9ab00fa00f9127e4132fbba58c37f1037d
Cynthia Anyaeriuba <cynthia.anyaeriuba@digital.cabinet-office.gov.uk>
2023-11-03 16:33:07 +0000

    Qatar ETA changes for outcome_work_waiver.erb
    - Removed any reference to EVWs for all ETA countries, which at the moment is just Qatar
    
    - Removed the call out box as the information in it is no longer needed for Qatar from the 15th Nov 2023.

--- test/flows/check_uk_visa_flow_test.rb
+++ test/flows/check_uk_visa_flow_test.rb
@@ -965,14 +965,9 @@ class CheckUkVisaFlowTest < ActiveSupport::TestCase
                     staying_for_how_long?: "six_months_or_less"
     end
 
-    should "render specific guidance for Electronic Travel Authorisation" do
+    should "not have any reference to electronic visa waivers (EVWs) for ETA countries" do
       add_responses what_passport_do_you_have?: @electronic_travel_authorisation_country
-      assert_rendered_outcome text: @eta_text
-    end
-
-    should "not render Electronic Travel Authorisation guidance for non-ETA countries" do
-      add_responses what_passport_do_you_have?: @electronic_visa_waiver_country
-      assert_no_rendered_outcome text: @eta_text
+      assert_no_rendered_outcome text: "electronic visa waiver"
     end
   end
 
