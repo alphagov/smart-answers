e568c1b4218b36fb68d6da96a85eaaf29fc39237
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2024-01-03 14:33:41 +0000

    Remove 'requires' from outcome names
    Slightly simplify/shorten the outcome names.

--- app/flows/check_uk_visa_flow.rb
+++ app/flows/check_uk_visa_flow.rb
@@ -316,9 +316,9 @@ class CheckUkVisaFlow < SmartAnswer::Flow
     outcome :outcome_transit_to_the_republic_of_ireland
     outcome :outcome_tourism_n
     outcome :outcome_tourism_visa_partner
-    outcome :outcome_medical_requires_electronic_travel_authorisation
-    outcome :outcome_tourism_requires_electronic_travel_authorisation
-    outcome :outcome_requires_electronic_visa_waiver
+    outcome :outcome_medical_electronic_travel_authorisation
+    outcome :outcome_tourism_electronic_travel_authorisation
+    outcome :outcome_electronic_visa_waiver
     outcome :outcome_visit_waiver_taiwan
     outcome :outcome_work_m
     outcome :outcome_work_n
@@ -354,9 +354,9 @@ class CheckUkVisaFlow < SmartAnswer::Flow
 
       if calculator.medical_visit?
         if calculator.passport_country_requires_electronic_travel_authorisation?
-          next outcome(:outcome_medical_requires_electronic_travel_authorisation)
+          next outcome(:outcome_medical_electronic_travel_authorisation)
         elsif calculator.passport_country_in_electronic_visa_waiver_list?
-          next outcome(:outcome_requires_electronic_visa_waiver)
+          next outcome(:outcome_electronic_visa_waiver)
         elsif calculator.passport_country_is_taiwan?
           next outcome(:outcome_visit_waiver_taiwan)
         elsif (calculator.passport_country_in_non_visa_national_list? ||
@@ -371,9 +371,9 @@ class CheckUkVisaFlow < SmartAnswer::Flow
 
       if calculator.tourism_visit?
         if calculator.passport_country_requires_electronic_travel_authorisation?
-          next outcome(:outcome_tourism_requires_electronic_travel_authorisation)
+          next outcome(:outcome_tourism_electronic_travel_authorisation)
         elsif calculator.passport_country_in_electronic_visa_waiver_list?
-          next outcome(:outcome_requires_electronic_visa_waiver)
+          next outcome(:outcome_electronic_visa_waiver)
         elsif calculator.passport_country_is_taiwan?
           next outcome(:outcome_visit_waiver_taiwan)
         elsif (calculator.passport_country_in_non_visa_national_list? ||
