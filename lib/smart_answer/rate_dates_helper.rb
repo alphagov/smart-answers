a0f099bc90ab15b95d1b78e99c11903fc1c2b8b1
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2024-03-14 09:25:21 +0000

    Get living wage min age from rates file for start
    Update the start page templates to dynamically fetch the minimum age to
     be eligible for the living wage from the rates file rather than being
     hard-coded in the template, similar to how is already done for the
     "last year end text".

--- lib/smart_answer/rate_dates_helper.rb
+++ lib/smart_answer/rate_dates_helper.rb
@@ -8,6 +8,15 @@ module SmartAnswer
       last_year(rates_file)[:end_date].strftime("%B %Y").to_s
     end
 
+    def this_year_living_wage_min_age(rates_file)
+      this_year(rates_file)[:living_wage_min_age]
+    end
+
+    def this_year(rates_file)
+      data = SmartAnswer::Calculators::RatesQuery.from_file(rates_file)
+      data.current_period
+    end
+
     def last_year(rates_file)
       data = SmartAnswer::Calculators::RatesQuery.from_file(rates_file)
       data.previous_period
