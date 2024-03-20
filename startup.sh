4460f66b32d45b72c1dc6f9d4243de18c38a1b45
Jon Kirwan <87758239+jon-kirwan@users.noreply.github.com>
2024-03-20 15:21:38 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ if [[ $1 == "--live" ]] ; then
   GOVUK_WEBSITE_ROOT=https://www.gov.uk \
   GOVUK_PROXY_STATIC_ENABLED=true \
   PLEK_SERVICE_CONTENT_STORE_URI=${PLEK_SERVICE_CONTENT_STORE_URI-https://www.gov.uk/api} \
-  PLEK_SERVICE_STATIC_URI=${PLEK_SERVICE_STATIC_URI-https://assets.publishing.service.gov.uk} \
+  PLEK_SERVICE_STATIC_URI=${PLEK_SERVICE_STATIC_URI-http://static.dev.gov.uk} \
   ./bin/dev
 else
   echo "ERROR: other startup modes are not supported"
