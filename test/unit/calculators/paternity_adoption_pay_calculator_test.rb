1eba0dc65383e91375c38d27f585d29cdb7b6969
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2024-04-30 15:37:42 +0100

    Update calculator for NI employees (paternity adop)
    Update the paternity adoption leave calculator for employers to handle
     employees who live in Northern Ireland—the rules for Northern
     Ireland-based employees remains the same, they did not change on the
     6th April 2024.

--- test/unit/calculators/paternity_adoption_pay_calculator_test.rb
+++ test/unit/calculators/paternity_adoption_pay_calculator_test.rb
@@ -9,12 +9,15 @@ module SmartAnswer::Calculators
             @placement_date = Date.parse("5 April 2024")
           end
 
-          should "set the paternity deadline to 55 days after the placement date" do
-            match_date = Date.parse("01 March 2024")
-            calculator = PaternityAdoptionPayCalculator.new(match_date)
-            calculator.adoption_placement_date = @placement_date
+          %w[england scotland wales northern_ireland].each do |location|
+            should "set the paternity deadline to 55 days after the placement date when employee lives in #{location}" do
+              match_date = Date.parse("01 March 2024")
+              calculator = PaternityAdoptionPayCalculator.new(match_date)
+              calculator.adoption_placement_date = @placement_date
+              calculator.where_does_the_employee_live = location
 
-            assert_equal Date.parse("30-05-2024"), calculator.paternity_deadline
+              assert_equal Date.parse("30-05-2024"), calculator.paternity_deadline
+            end
           end
         end
 
@@ -23,29 +26,63 @@ module SmartAnswer::Calculators
             @placement_date = Date.parse("6 April 2024")
           end
 
-          should "set the paternity deadline to 364 days after the placement date" do
+          %w[england scotland wales].each do |location|
+            should "set the paternity deadline to 364 days after the placement date when employee lives in #{location}" do
+              match_date = Date.parse("01 March 2024")
+              calculator = PaternityAdoptionPayCalculator.new(match_date)
+              calculator.adoption_placement_date = @placement_date
+              calculator.where_does_the_employee_live = location
+
+              assert_equal Date.parse("05-04-2025"), calculator.paternity_deadline
+            end
+          end
+
+          should "set the paternity deadline to 55 days after the placement date when employee lives in northern_ireland" do
             match_date = Date.parse("01 March 2024")
             calculator = PaternityAdoptionPayCalculator.new(match_date)
             calculator.adoption_placement_date = @placement_date
+            calculator.where_does_the_employee_live = "northern_ireland"
 
-            assert_equal Date.parse("05-04-2025"), calculator.paternity_deadline
+            assert_equal Date.parse("31-05-2024"), calculator.paternity_deadline
           end
         end
       end
 
       context "#leave_must_be_taken_consecutively?" do
-        should "be false when adoption placement date is 6 April 2024 (or after)" do
-          calculator = PaternityAdoptionPayCalculator.new(Date.parse("1 April 2024"))
-          calculator.adoption_placement_date = Date.parse("6 April 2024")
+        context "placement date is 6 April 2024 (or after)" do
+          setup do
+            @calculator = PaternityAdoptionPayCalculator.new(Date.parse("1 April 2024"))
+            @calculator.adoption_placement_date = Date.parse("6 April 2024")
+          end
 
-          assert_equal false, calculator.leave_must_be_taken_consecutively?
+          %w[england scotland wales].each do |location|
+            should "be false when employee lives in #{location}" do
+              @calculator.where_does_the_employee_live = location
+
+              assert_equal false, @calculator.leave_must_be_taken_consecutively?
+            end
+          end
+
+          should "be true when employee lives in northern_ireland" do
+            @calculator.where_does_the_employee_live = "northern_ireland"
+
+            assert_equal true, @calculator.leave_must_be_taken_consecutively?
+          end
         end
 
-        should "be true when adoption placement date is before 6 April 2024" do
-          calculator = PaternityAdoptionPayCalculator.new(Date.parse("1 April 2024"))
-          calculator.adoption_placement_date = Date.parse("5 April 2024")
+        context "placement date is before 6 April 2024" do
+          setup do
+            @calculator = PaternityAdoptionPayCalculator.new(Date.parse("1 April 2024"))
+            @calculator.adoption_placement_date = Date.parse("5 April 2024")
+          end
 
-          assert_equal true, calculator.leave_must_be_taken_consecutively?
+          %w[england scotland wales northern_ireland].each do |location|
+            should "be true when employee lives in #{location} " do
+              @calculator.where_does_the_employee_live = location
+
+              assert_equal true, @calculator.leave_must_be_taken_consecutively?
+            end
+          end
         end
       end
     end
