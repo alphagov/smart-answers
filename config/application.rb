d70041e2e400b81337f4b8f63d2140bc9dbe59f8
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2024-09-04 09:29:40 +0100

    Exclude generators directory for autoload

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,6 @@ module SmartAnswers
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets data tasks])
+    config.autoload_lib(ignore: %w[assets data generators tasks])
   end
 end
