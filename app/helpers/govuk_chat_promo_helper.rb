a44fb3d3b2022b7f840ebee3ec495c13f41cd1ee
David Gisbey <42515961+davidgisbey@users.noreply.github.com>
2024-11-29 09:51:05 +0000

    Revert "Render GOV.UK Chat promo on specific page"

--- app/helpers/govuk_chat_promo_helper.rb
+++
@@ -1,9 +0,0 @@
-module GovukChatPromoHelper
-  GOVUK_CHAT_PROMO_BASE_URLS = %w[
-    /am-i-getting-minimum-wage
-  ].freeze
-
-  def show_govuk_chat_promo?(base_url)
-    ENV["GOVUK_CHAT_PROMO_ENABLED"] == "true" && GOVUK_CHAT_PROMO_BASE_URLS.include?(base_url)
-  end
-end
