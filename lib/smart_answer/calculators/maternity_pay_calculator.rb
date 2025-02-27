34583c92284032a772c05cc636b78bca5e6b07a1
Barbara <barbara.slawinska@digital.cabinet-office.gov.uk>
2025-02-27 13:51:29 +0000

    Update Maternity, Paternity and Adoption calculators
    As part of 2025 uprating. All rates increase from £184.03 to £187.18 and LEL is changing from £123 to £125 from 6 April 2025. This commit also includes fix, changing incorrect date uprating_date(2023) to 2024 for the previous year.

--- lib/smart_answer/calculators/maternity_pay_calculator.rb
+++ lib/smart_answer/calculators/maternity_pay_calculator.rb
@@ -317,7 +317,8 @@ module SmartAnswer::Calculators
         { min: uprating_date(2021), max: uprating_date(2022), amount: 151.97 },
         { min: uprating_date(2022), max: uprating_date(2023), amount: 156.66 },
         { min: uprating_date(2023), max: uprating_date(2024), amount: 172.48 },
-        { min: uprating_date(2023), max: uprating_date(2200), amount: 184.03 },
+        { min: uprating_date(2024), max: uprating_date(2025), amount: 184.03 },
+        { min: uprating_date(2025), max: uprating_date(2200), amount: 187.18 },
         ### Change year in future
       ]
       rate = rates.find { |r| r[:min] <= date && date < r[:max] } || rates.last
