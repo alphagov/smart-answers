91cd6ab6c2df0182617514005c430dec7d14d52d
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2025-04-01 11:41:56 +0100

    Pin Chrome version used in integration tests
    There is an ongoing issue with recent versions of Chrome (see some
     discussion on this Capybara issue:
     https://github.com/teamcapybara/capybara/issues/2800) that are causing
     flakey integration tests. Pin to an older version of Chrome that does
     not exhibit this issue.
    
    There have been attempts to make the tests more reliable, which seem to
     have improved matters, but not 100% fixed the issues.
    
    See these previous PRs that were made to improve reliability:
    - https://github.com/alphagov/smart-answers/pull/7125
    - https://github.com/alphagov/smart-answers/pull/7131

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -6,6 +6,19 @@ Capybara.default_driver = :rack_test
 
 GovukTest.configure
 
+Capybara.register_driver :headless_chrome do |app|
+  chrome_options = GovukTest.headless_chrome_selenium_options
+  chrome_options.add_argument("--disable-web-security")
+  chrome_options.add_argument("--no-sandbox")
+  chrome_options.add_argument("--disable-dev-shm-usage")
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options: chrome_options,
+  )
+end
+
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
 
