4e8c3b015d80d4af0a6c855ef49ee2fb15c67e0b
Barbara <barbara.slawinska@digital.cabinet-office.gov.uk>
2025-06-02 17:11:22 +0100

    Add basic flow questions and outcome

--- lib/smart_answer/calculators/inheritance_tax_interest_calculator.rb
+++ lib/smart_answer/calculators/inheritance_tax_interest_calculator.rb
@@ -1,6 +1,6 @@
 module SmartAnswer::Calculators
   class InheritanceTaxInterestCalculator
-    attr_accessor :question
+    attr_accessor :start_date, :end_date, :inheritance_tax_owed
 
     def validate?
       return false if question.blank?
