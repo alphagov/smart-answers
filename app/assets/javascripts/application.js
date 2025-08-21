3183b61d548cb25aa8224eca6b12b422cbacf9f7
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-08-21 13:50:32 +0100

    feat: add basic elements stylesheet for app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,17 @@
+//= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/checkboxes
+//= require govuk_publishing_components/components/cookie-banner
 //= require govuk_publishing_components/components/error-summary
+//= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/global-banner
 //= require govuk_publishing_components/components/govspeak
+//= require govuk_publishing_components/components/layout-super-navigation-header
 //= require govuk_publishing_components/components/print-link
 //= require govuk_publishing_components/components/radio
+//= require govuk_publishing_components/components/search-with-autocomplete
+//= require govuk_publishing_components/components/skip-link
 //= require govuk_publishing_components/components/step-by-step-nav
 //= require govuk_publishing_components/components/table
 
