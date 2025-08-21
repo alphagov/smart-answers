418b92501d88cba422863992246957f56c5ac361
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2025-08-21 11:03:27 +0100

    feat: add support for the emergency and global banners.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,6 +20,8 @@ require "gds_api/test_helpers/publishing_api"
 require "gds_api/test_helpers/worldwide"
 require_relative "support/fixture_methods"
 
+Rails.application.config.emergency_banner_redis_client = instance_double(Redis, hgetall: {})
+
 class ActiveSupport::TestCase
   include FixtureMethods
   include GdsApi::TestHelpers::ContentStore
