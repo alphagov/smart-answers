f9f21b8e1034d317bd93164c973867ada8f9cdfd
pezholio <pezholio@gmail.com>
2025-10-13 10:56:31 +0100

    Use Content Blocks in StatePensionThroughPartner flow
    This replaces the use of content in a YAML file with content blocks
    rendered via their appropriate embed codes. As these return HTML, with
    the currencies already formatted, we don’t need to use the
    `format_money` helpers.
    
    If we wanted to use these values in calculations, we’d have to look at
    a way of “embedding” content blocks without HTML wrappers.

--- lib/smart_answer/calculators/state_pension_through_partner_calculator.rb
+++ lib/smart_answer/calculators/state_pension_through_partner_calculator.rb
@@ -7,12 +7,15 @@ module SmartAnswer::Calculators
                   :when_will_your_partner_reach_pension_age,
                   :gender
 
+    LOWER_BASIC_STATE_PENSION_RATE_EMBED_CODE = "{{embed:content_block_pension:basic-state-pension/rates/lower-basic-state-pension-rate/amount}}".freeze
+    HIGHER_BASIC_STATE_PENSION_RATE_EMBED_CODE = "{{embed:content_block_pension:basic-state-pension/rates/full-basic-state-pension-amount/amount}}".freeze
+
     def lower_basic_state_pension_rate
-      rates.lower_weekly_rate
+      @lower_basic_state_pension_rate ||= SmartAnswer::ContentBlock.new(LOWER_BASIC_STATE_PENSION_RATE_EMBED_CODE).render
     end
 
     def higher_basic_state_pension_rate
-      rates.weekly_rate
+      @higher_basic_state_pension_rate ||= SmartAnswer::ContentBlock.new(HIGHER_BASIC_STATE_PENSION_RATE_EMBED_CODE).render
     end
 
     def divorced?
@@ -65,12 +68,6 @@ module SmartAnswer::Calculators
 
     def current_rules_national_insurance_no_state_pension?
       married? && reached_pension_age_before_specific_date? && partner_reached_pension_age_after_specific_date?
-    end
-
-  private
-
-    def rates
-      @rates ||= RatesQuery.from_file("state_pension").rates
     end
   end
 end
