106e35c782bf4fc88365bccbc87a8b7be478d99d
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2025-11-03 11:15:57 +0000

    Fix component guide

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -6,6 +6,7 @@ APP_STYLESHEETS = {
   "visualise.scss" => "visualise.css",
 }.freeze
 
-Rails.application.config.dartsass.builds = APP_STYLESHEETS
+all_stylesheets = APP_STYLESHEETS.merge(GovukPublishingComponents::Config.component_guide_stylesheet)
+Rails.application.config.dartsass.builds = all_stylesheets
 Rails.application.config.dartsass.build_options << " --quiet-deps"
 Rails.application.config.dartsass.build_options << " --silence-deprecation=import"
