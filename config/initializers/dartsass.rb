4b93c2715ae21b228ac9b203bd830783eaffd1e4
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2026-03-05 14:08:20 +0000

    Stop compiling component CSS separately
    - component styles are now included in application.scss

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,8 +1,5 @@
 APP_STYLESHEETS = {
   "application.scss" => "application.css",
-  "components/_result-card.scss" => "components/_result-card.css",
-  "components/_result-item.scss" => "components/_result-item.css",
-  "components/_result-sections.scss" => "components/_result-sections.css",
   "visualise.scss" => "visualise.css",
 }.freeze
 
