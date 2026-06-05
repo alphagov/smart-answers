6a6879239646fd1e34ab9aedbcab667f2ff80272
Stacy Hayes <stacy.hayes@digital.cabinet-office.gov.uk>
2026-06-05 10:38:45 +0100

    Add test for question: is_your_course_eligible_nhs_bursary?

--- test/flows/student_finance_calculator_flow_test.rb
+++ test/flows/student_finance_calculator_flow_test.rb
@@ -1226,6 +1226,27 @@ class StudentFinanceCalculatorTest < ActiveSupport::TestCase
           end
         end
       end
+
+      context "question: is_your_course_eligible_nhs_bursary?" do
+        setup do
+          testing_node :is_your_course_eligible_nhs_bursary?
+          add_responses when_does_your_course_start?: "2027-2028",
+                        what_age_are_you_on_first_day_of_course?: "60-or-more",
+                        are_you_studying_one_of_these_courses?: "dental-medical-healthcare"
+        end
+
+        should "render the question" do
+          assert_rendered_question
+        end
+
+        context "next_node" do
+          %w[yes no].each do |response|
+            should "have a next node of how_are_you_planning_to_study for a #{response} response" do
+              assert_next_node :how_are_you_planning_to_study?, for_response: response
+            end
+          end
+        end
+      end
     end
   end
 end
