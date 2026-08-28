bf2cff1f5feb1769955f522a888ca5a57972ba23
Stacy Hayes <stacy.hayes@digital.cabinet-office.gov.uk>
2026-08-28 09:22:24 +0100

    Filter specific warnings from test output
    We currently get two warnings in our test outputs - that govspeak will
    eventually be affected by frozen_string_literal_true and that parser and
    govuk-rubocop are running slightly different versions. These are both warnings
    for applications we cannot impact, and they clog the test output, making it more
    difficult to see genuine warnings or errors we should be correcting, so this
    silences those two specific warnings.

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,21 @@
+module SelectiveWarningFilter
+  def warn(message, category: nil, **kwargs)
+    msg = message.to_s
+
+    if msg.include?("literal string will be frozen in the future") && msg.include?("govspeak.rb")
+      return
+    end
+
+    if msg.include?("parser/current is loading parser/") && msg.include?("recognizes")
+      return
+    end
+
+    super
+  end
+end
+
+Warning.extend(SelectiveWarningFilter)
+
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
